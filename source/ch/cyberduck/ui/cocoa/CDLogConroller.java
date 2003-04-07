774f6bdc30c45468b43583820e4e8009b24ebe90
David Kocher <dkocher@iterate.ch>
2003-04-07 20:30:39 +0000

    No log message.

---
+++ source/ch/cyberduck/ui/cocoa/CDLogConroller.java
@@ -0,0 +1,15 @@
+//
+//  CDLogConroller.java
+//  Cyberduck-Cocoa
+//
+//  Created by David Kocher on Mon Apr 07 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+import com.apple.cocoa.foundation.*;
+import com.apple.cocoa.application.*;
+
+
+public class CDLogConroller {
+
+}
