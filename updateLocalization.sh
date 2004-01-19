77981da58f71750158cc2aa7f2bb5000fd9a8c84
David Kocher <dkocher@iterate.ch>
2004-01-19 20:07:25 +0000

    Added Licence

--- updateLocalization.sh
+++ updateLocalization.sh
@@ -1,3 +1,19 @@
+#  Copyright (c) 2003 David Kocher. All rights reserved.
+#  http://cyberduck.ch/
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  Bug fixes, suggestions and comments should be sent to:
+#  dkocher@cyberduck.ch
+
 #!/bin/bash
         
 usage ( ) {
@@ -9,7 +25,9 @@ usage ( ) {
 if [ $# = 1 ] ; then
 {
     language=$1;
+    echo "**********************************"
     echo "Updating $language Localization..."
+    echo "**********************************"
     # First update the nib file with the modifications in the .strings file. 
     # The .strings file is always assumed to be newer - this means that all 
     # modifications should not be made in the .nib file directly but in 
@@ -23,15 +41,21 @@ else
     for lproj in `ls . | grep lproj`; do
         language=`basename $lproj .lproj`
         
-        echo "Updating $language Localization..."
-    
-        # First update the nib file with the modifications in the .strings file. 
-        # The .strings file is always assumed to be newer - this means that all 
-        # modifications should not be made in the .nib file directly but in 
-        # the .strings file instead.
-        ./updateNibFromStrings.sh $language
-        # Update the localized .strings file with new strings added to to the base (English) version.
-        ./updateStringsFromNib.sh $language
+        if [ $language != "English" ]; then
+        {
+            echo "**********************************"
+            echo "Updating $language Localization..."
+            echo "**********************************"
+        
+            # First update the nib file with the modifications in the .strings file. 
+            # The .strings file is always assumed to be newer - this means that all 
+            # modifications should not be made in the .nib file directly but in 
+            # the .strings file instead.
+            ./updateNibFromStrings.sh $language
+            # Update the localized .strings file with new strings added to to the base (English) version.
+            ./updateStringsFromNib.sh $language
+        }
+        fi;
     done;
 }
 fi;
