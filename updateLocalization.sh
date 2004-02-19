6e00a21757c6e2928b51e7d095650be02d1c7845
David Kocher <dkocher@iterate.ch>
2004-02-19 19:19:59 +0000

    *** empty log message ***

--- updateLocalization.sh
+++ updateLocalization.sh
@@ -16,44 +16,82 @@
 
 #!/bin/bash
         
-usage ( ) {
-	echo "Usage: udpateLocalization.sh <language>"
+usage() {
+	echo "Usage: udpateLocalization.sh -l <language> -n <nib>"
 	echo "       language must be Japanese, French, Spanish, ..."
-	echo "       as the base language, English is assumed"
+	echo "       nib must be Preferences.nib, Main.nib, ..."
+	echo "Call with no parameters to update all languages and all nib files"
 }
 
-if [ $# = 1 ] ; then
-{
-    language=$1;
-    echo "**********************************"
-    echo "Updating $language Localization..."
-    echo "**********************************"
-    # First update the nib file with the modifications in the .strings file. 
-    # The .strings file is always assumed to be newer - this means that all 
-    # modifications should not be made in the .nib file directly but in 
-    # the .strings file instead.
-    ./updateNibFromStrings.sh $language
+updateNibFromStrings() {
+    nib=`basename $nibfile .nib`
+
+    echo "Updating $nib in $language.lproj..."
+
+    rm -rf $language.lproj/$nibfile.bak
+    mv $language.lproj/$nibfile $language.lproj/$nibfile.bak
+
+    # force update
+    # nibtool --write $language.lproj/$nibfile --dictionary $language.lproj/$nib.strings English.lproj/$nibfile
+
+    # incremental update
+    nibtool --write $language.lproj/$nibfile \
+            --incremental $language.lproj/$nibfile.bak \
+            --dictionary $language.lproj/$nib.strings English.lproj/$nibfile
+
+    cp -R $language.lproj/$nibfile.bak/CVS $language.lproj/$nibfile/CVS
 }
-else 
-{
-    for lproj in `ls . | grep lproj`; do
-        language=`basename $lproj .lproj`
-        
-        if [ $language != "English" ]; then
-        {
-            echo "**********************************"
-            echo "Updating $language Localization..."
-            echo "**********************************"
-        
-            # Update the nib file with the modifications in the .strings file. 
-            # The .strings file is always assumed to be newer - this means that all 
-            # modifications should not be made in the .nib file directly but in 
-            # the .strings file instead.
-            ./updateNibFromStrings.sh $language
-        }
-        fi;
-    done;
-}
+
+#udpateStringsFromNib() {
+#    for nibfile in `ls $language.lproj | grep .nib | grep -v ~.nib | grep -v .bak`; do
+#        nib=`basename $nibfile .nib`
+#    
+#        echo "Updating $nib.strings in $language.lproj..."
+#        
+#        mv $language.lproj/$nib.strings $language.lproj/$nib.strings.bak
+#        nibtool --previous English.lproj/$nibfile \
+#                --incremental $language.lproj/$nibfile \
+#                --localizable-strings English.lproj/$nibfile > $language.lproj/$nib.strings
+#    done;
+#}
+
+
+if [ $# = 2 ] ; then
+    {
+        language=$1;
+        nibfile=$2;
+        echo "*** Updating $language Localization..."
+        # First update the nib file with the modifications in the .strings file. 
+        # The .strings file is always assumed to be newer - this means that all 
+        # modifications should not be made in the .nib file directly but in 
+        # the .strings file instead.
+        updateNibFromStrings;
+        exit 0;
+    }
 fi;
-exit 0
-
+if [ $# = 1 ] ; then
+    {
+       nibfile=$1;
+        for lproj in `ls . | grep lproj`; do
+            language=`basename $lproj .lproj`
+            
+            if [ $language != "English" ]; then
+            {
+                echo "*** Updating $language Localization..."
+    
+#                for nibfile in `ls $language.lproj | grep .nib | grep -v ~.nib | grep -v .bak`; do
+            
+                # Update the nib file with the modifications in the .strings file. 
+                # The .strings file is always assumed to be newer - this means that all 
+                # modifications should not be made in the .nib file directly but in 
+                # the .strings file instead.
+                updateNibFromStrings;
+ #               done;
+            }
+            fi;
+        done;
+        exit 0;
+    }
+fi;
+usage;
+exit 1;
