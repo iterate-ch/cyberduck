0dc65395296f6be5cee5467ac40cae775b50a5e3
David Kocher <dkocher@iterate.ch>
2005-05-21 15:00:42 +0000

    no message

--- source/ch/cyberduck/ui/cocoa/CDListView.m
+++ source/ch/cyberduck/ui/cocoa/CDListView.m
@@ -44,6 +44,33 @@
 	[super dealloc];
 }
 
+// Taken from http://www.cocoadev.com/index.pl?RightClickSelectInTableView
+- (NSMenu *) menuForEvent:(NSEvent *) event {
+	NSPoint where;
+	int row = -1, col = -1;
+	
+	where = [self convertPoint:[event locationInWindow] fromView:nil];
+	row = [self rowAtPoint:where];
+	col = [self columnAtPoint:where];
+	
+	if(row >= 0) {
+		NSTableColumn *column = nil;
+		if(col >= 0) {
+			column = [[self tableColumns] objectAtIndex:col];
+		}
+		if([[self delegate] respondsToSelector:@selector(tableView:shouldSelectRow:)]) {
+			if([[self delegate] tableView:self shouldSelectRow:row])
+				[self selectRow:row byExtendingSelection:NO];
+		} 
+		else {
+			[self selectRow:row byExtendingSelection:NO];
+		}
+		return [self menu];
+	}
+	[self deselectAll:nil];
+	return [self menu];
+}
+
 - (void)keyDown:(NSEvent *)event
 {
 	NSString *str = [event characters];
