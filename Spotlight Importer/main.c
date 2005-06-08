4fbb8eb82e1197965629a34df292acbe71981b8b
David Kocher <dkocher@iterate.ch>
2005-06-08 02:14:56 +0000

    *** empty log message ***

--- Spotlight Importer/main.c
+++ Spotlight Importer/main.c
@@ -1,28 +1,20 @@
-//
-//  main.c
-//  Cyberduck Spotlight Importer
-//
-//  Created by David Kocher on 07.6.2005.
-//  Copyright (c) 2005 __MyCompanyName__. All rights reserved.
-//
-
-
-
-
-
-//==============================================================================
-//
-//	DO NO MODIFY THE CONTENT OF THIS FILE
-//
-//	This file contains the generic CFPlug-in code necessary for your importer
-//	To complete your importer implement the function in GetMetadataForFile.c
-//
-//==============================================================================
-
-
-
-
-
+/*
+ *  Copyright (c) 2005 David Kocher. All rights reserved.
+ *  http://cyberduck.ch/
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Bug fixes, suggestions and comments should be sent to:
+ *  dkocher@cyberduck.ch
+ */
 
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreFoundation/CFPlugInCOM.h>
