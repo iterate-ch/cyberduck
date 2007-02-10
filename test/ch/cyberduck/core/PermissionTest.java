5a7676f6e16c15c0117ea03e83978f956b3c68e2
David Kocher <dkocher@iterate.ch>
2007-02-10 11:53:23 +0000

    Various

--- test/ch/cyberduck/core/PermissionTest.java
+++ test/ch/cyberduck/core/PermissionTest.java
@@ -34,20 +34,4 @@ public class PermissionTest extends TestCase {
         Permission p = new Permission("rwxrwxrwx");
         assertEquals(p, p.clone());
     }
-
-    public void testEmptyPermissions() {
-        Permission p = new Permission();
-        assertEquals(p.getMask(), "---------");
-        assertTrue(p.getDecimalCode() == 0);
-        assertFalse(p.getGroupPermissions()[0]);
-        assertFalse(p.getGroupPermissions()[1]);
-        assertFalse(p.getGroupPermissions()[2]);
-        assertFalse(p.getOwnerPermissions()[0]);
-        assertFalse(p.getOwnerPermissions()[1]);
-        assertFalse(p.getOwnerPermissions()[2]);
-        assertFalse(p.getOtherPermissions()[0]);
-        assertFalse(p.getOtherPermissions()[1]);
-        assertFalse(p.getOtherPermissions()[2]);
-        assertEquals(p.getOctalCode(), "000");
-    }
 }
