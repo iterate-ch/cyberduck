70594852899527f3d4548624605d94f1caf7ffd5
David Kocher <dkocher@iterate.ch>
2008-07-16 22:22:34 +0000

    Testcase for #1996.

---
+++ test/ch/cyberduck/core/ftp/parser/NetwareFTPEntryParserTest.java
@@ -0,0 +1,88 @@
+package ch.cyberduck.core.ftp.parser;
+
+/*
+ *  Copyright (c) 2007 David Kocher. All rights reserved.
+ *  http://cyberduck.ch/
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Bug fixes, suggestions and comments should be sent to:
+ *  dkocher@cyberduck.ch
+ */
+
+import ch.cyberduck.core.ftp.FTPParserFactory;
+
+import org.apache.commons.net.ftp.FTPFileEntryParser;
+import org.apache.commons.net.ftp.FTPFile;
+
+import java.util.Calendar;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+/**
+ * @version $Id:$
+ */
+public class NetwareFTPEntryParserTest extends TestCase {
+
+    public NetwareFTPEntryParserTest(String name) {
+        super(name);
+    }
+
+    private FTPFileEntryParser parser;
+
+
+    public void setUp() throws Exception {
+        this.parser = new FTPParserFactory().createFileEntryParser("NETWARE");
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * http://trac.cyberduck.ch/ticket/1996
+     * @throws Exception
+     */
+    public void testParse() throws Exception {
+        FTPFile parsed;
+
+        parsed = parser.parseFTPEntry(
+                "- [RWCEAFMS] wtubbs 24038 May 05 17:57 CIMSscheduler_log_May02_4.txt"
+        );
+        assertNotNull(parsed);
+        assertEquals(parsed.getName(), "CIMSscheduler_log_May02_4.txt");
+        assertTrue(parsed.getType() == FTPFile.FILE_TYPE);
+        assertEquals("wtubbs", parsed.getUser());
+        assertEquals(24038, parsed.getSize());
+        final int year = Calendar.getInstance().get(Calendar.YEAR);
+        assertTrue(parsed.getTimestamp().get(Calendar.YEAR) == year);
+        assertTrue(parsed.getTimestamp().get(Calendar.MONTH) == Calendar.MAY);
+        assertTrue(parsed.getTimestamp().get(Calendar.DAY_OF_MONTH) == 5);
+
+        parsed = parser.parseFTPEntry(
+                "- [RWCEAFMS] wtubbs 9965 May 01 18:15 CIMSscheduler_log_May01.txt"
+        );
+        assertNotNull(parsed);
+        assertEquals(parsed.getName(), "CIMSscheduler_log_May01.txt");
+        assertTrue(parsed.getType() == FTPFile.FILE_TYPE);
+        assertEquals("wtubbs", parsed.getUser());
+        assertEquals(9965, parsed.getSize());
+        assertTrue(parsed.getTimestamp().get(Calendar.YEAR) == year);
+        assertTrue(parsed.getTimestamp().get(Calendar.MONTH) == Calendar.MAY);
+        assertTrue(parsed.getTimestamp().get(Calendar.DAY_OF_MONTH) == 1);
+    }
+
+    public static Test suite() {
+        return new TestSuite(NetwareFTPEntryParserTest.class);
+    }
+}
