9ff2970e677f31838493a9fe6e6c38daa01bc958
David Kocher <dkocher@iterate.ch>
2009-04-21 15:24:59 +0000

    Testcase for #3119.

---
+++ test/ch/cyberduck/core/ftp/parser/FilezillaFTPEntryParserTest.java
@@ -0,0 +1,72 @@
+package ch.cyberduck.core.ftp.parser;
+
+/*
+ *  Copyright (c) 2007 David Kocher. All rights reserved.
+ *  http://cyberduck.ch/
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Bug fixes, suggestions and comments should be sent to:
+ *  dkocher@cyberduck.ch
+ */
+
+import ch.cyberduck.core.ftp.FTPParserFactory;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPFileEntryParser;
+
+import java.util.Calendar;
+
+/**
+ * @version $Id: TrellixFTPEntryParserTest.java 2042 2006-04-23 01:39:32Z dkocher $
+ */
+public class FilezillaFTPEntryParserTest extends TestCase {
+
+    public FilezillaFTPEntryParserTest(String name) {
+        super(name);
+    }
+
+    private FTPFileEntryParser parser;
+
+
+    public void setUp() throws Exception {
+        this.parser = new FTPParserFactory().createFileEntryParser("UNIX emulated by FileZilla");
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testParse() throws Exception {
+        FTPFile parsed = null;
+
+        // #3119
+        parsed = parser.parseFTPEntry(
+                "-rw-r--r-- 1 ftp ftp         100847 Sep 10  2004 octfront2.jpg"
+        );
+        assertNotNull(parsed);
+        assertEquals(parsed.getName(), "octfront2.jpg");
+        assertTrue(parsed.getType() == FTPFile.FILE_TYPE);
+        assertEquals(100847, parsed.getSize());
+        assertTrue(parsed.getTimestamp().get(Calendar.MONTH) == Calendar.SEPTEMBER);
+        assertTrue(parsed.getTimestamp().get(Calendar.DAY_OF_MONTH) == 10);
+        assertTrue(parsed.hasPermission(FTPFile.USER_ACCESS, FTPFile.READ_PERMISSION));
+        assertTrue(parsed.hasPermission(FTPFile.GROUP_ACCESS, FTPFile.READ_PERMISSION));
+        assertTrue(parsed.hasPermission(FTPFile.WORLD_ACCESS, FTPFile.READ_PERMISSION));
+        assertTrue(parsed.hasPermission(FTPFile.USER_ACCESS, FTPFile.WRITE_PERMISSION));
+    }
+
+    public static Test suite() {
+        return new TestSuite(FilezillaFTPEntryParserTest.class);
+    }
+}
