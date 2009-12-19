a8cfdf78d7edc80ecd94e451c66651f2e34f339f
David Kocher <dkocher@iterate.ch>
2009-12-19 23:30:40 +0000

    Fix #994.

---
+++ source/ch/cyberduck/core/NaturalOrderCollator.java
@@ -0,0 +1,178 @@
+package ch.cyberduck.core;
+
+/*
+ * @(#)OSXCollator.java  
+ *
+ * Copyright (c) 2004-2007 Werner Randelshofer
+ * Staldenmattweg 2, Immensee, CH-6405, Switzerland.
+ * All rights reserved.
+ *
+ * The copyright of this software is owned by Werner Randelshofer. 
+ * You may not use, copy or modify this software, except in  
+ * accordance with the license agreement you entered into with  
+ * Werner Randelshofer. For details see accompanying license terms. 
+ */
+
+import java.text.CollationKey;
+import java.text.Collator;
+import java.text.ParseException;
+import java.text.RuleBasedCollator;
+import java.util.Locale;
+
+/**
+ * The OSXCollator strives to match the collation rules used by the Mac OS X
+ * Finder and of Mac OS X file dialogs.
+ * <p/>
+ * If we wanted to match the OS X collation rules exactly, we would have to
+ * implement the rules for all languages supported by Mac OS X and Java.
+ * To reduce the amount of work needed for implementing these rules, the
+ * OSXCollator changes the collation rules returned by
+ * java.text.Collator.getInstance() to do the following:
+ * <ul>
+ * <li>Space characters are treated as primary collation differences</li>
+ * <li>Hyphen characters are treated as primary collation differences</li>
+ * <li>Sequence of digits (characters '0' through '9') is treated as a single
+ * collation object. The current implementation supports sequences of up to
+ * 999 characters length.</li>
+ * </ul>
+ * If java.text.Collator.getInstance() does not return an instance of
+ * java.text.RuleBasedCollator, then the returned collator is used, and only
+ * sequences of digits are changed to match the collation rules of Mac OS X.
+ *
+ * @author Werner Randelshofer
+ * @version $Id$
+ */
+public class NaturalOrderCollator extends Collator {
+    private Collator collator;
+
+    public NaturalOrderCollator() {
+        this(Locale.getDefault());
+    }
+
+    public NaturalOrderCollator(Locale locale) {
+        collator = Collator.getInstance(locale);
+
+        if(collator instanceof RuleBasedCollator) {
+            String rules = ((RuleBasedCollator) collator).getRules();
+
+            // If hyphen is ignored except for tertiary difference, make it
+            // a primary difference, and move in front of the first primary 
+            // difference found in the rules
+            int pos = rules.indexOf(",'-'");
+            int primaryRelationPos = rules.indexOf('<');
+            if(primaryRelationPos == rules.indexOf("'<'")) {
+                primaryRelationPos = rules.indexOf('<', primaryRelationPos + 2);
+            }
+            if(pos != -1 && pos < primaryRelationPos) {
+                rules = rules.substring(0, pos)
+                        + rules.substring(pos + 4, primaryRelationPos)
+                        + "<'-'"
+                        + rules.substring(primaryRelationPos);
+            }
+
+            // If space is ignored except for secondary and tertiary 
+            // difference, make it a primary difference, and move in front 
+            // of the first primary difference found in the rules
+            pos = rules.indexOf(";' '");
+            primaryRelationPos = rules.indexOf('<');
+            if(primaryRelationPos == rules.indexOf("'<'")) {
+                primaryRelationPos = rules.indexOf('<', primaryRelationPos + 2);
+            }
+            if(pos != -1 && pos < primaryRelationPos) {
+                rules = rules.substring(0, pos)
+                        + rules.substring(pos + 4, primaryRelationPos)
+                        + "<' '"
+                        + rules.substring(primaryRelationPos);
+            }
+
+            try {
+                collator = new RuleBasedCollator(rules);
+            }
+            catch(ParseException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public int compare(String source, String target) {
+        return collator.compare(expandNumbers(source), expandNumbers(target));
+    }
+
+    @Override
+    public CollationKey getCollationKey(String source) {
+        return collator.getCollationKey(expandNumbers(source));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof NaturalOrderCollator) {
+            NaturalOrderCollator that = (NaturalOrderCollator) o;
+            return this.collator.equals(that.collator);
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int hashCode() {
+        return collator.hashCode();
+    }
+
+    private String expandNumbers(String s) {
+        if(s == null) {
+            return null;
+        }
+
+        StringBuilder out = new StringBuilder();
+        StringBuilder digits = new StringBuilder();
+
+        for(int i = 0, n = s.length(); i < n; i++) {
+            char ch = s.charAt(i);
+            //if (Character.isDigit(ch)) {
+            if(ch >= '0' && ch <= '9') {
+                digits.append(ch);
+            }
+            else {
+                if(digits.length() != 0) {
+                    if(digits.length() < 10) {
+                        out.append("00");
+                        out.append(digits.length());
+                    }
+                    else if(digits.length() < 100) {
+                        out.append("0");
+                        out.append(digits.length());
+                    }
+                    else if(digits.length() < 1000) {
+                        out.append(digits.length());
+                    }
+                    else if(digits.length() > 999) {
+                        out.append("999");
+                    }
+                    out.append(digits.toString());
+                    digits.delete(0, digits.length());
+                }
+                out.append(ch);
+            }
+        }
+        if(digits.length() != 0) {
+            if(digits.length() < 10) {
+                out.append("00");
+                out.append(digits.length());
+            }
+            else if(digits.length() < 100) {
+                out.append("0");
+                out.append(digits.length());
+            }
+            else if(digits.length() < 1000) {
+                out.append(digits.length());
+            }
+            else if(digits.length() > 999) {
+                out.append("999");
+            }
+            out.append(digits);
+        }
+
+        return out.toString();
+    }
+}
--- source/ch/cyberduck/core/NaturalOrderComparator.java
+++ source/ch/cyberduck/core/NaturalOrderComparator.java
@@ -1,114 +1,35 @@
 package ch.cyberduck.core;
 
 /*
- * The Alphanum Algorithm is an improved sorting algorithm for strings
- * containing numbers.  Instead of sorting numbers in ASCII order like
- * a standard sort, this algorithm sorts numbers in numeric order.
+ * Copyright (c) 2002-2009 David Kocher. All rights reserved.
  *
- * The Alphanum Algorithm is discussed at http://www.DaveKoelle.com
+ * http://cyberduck.ch/
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * Bug fixes, suggestions and comments should be sent to:
+ * dkocher@cyberduck.ch
  */
 
 import java.text.Collator;
 import java.util.Comparator;
-import java.util.Locale;
 
 /**
- * This is an updated version with enhancements made by Daniel Migowski,
- * Andre Bogus, and David Koelle
- *
  * @version $Id$
  */
 public class NaturalOrderComparator implements Comparator<String> {
 
-    private Collator collator = Collator.getInstance(Locale.getDefault());
-
-    private boolean isDigit(char ch) {
-        return ch >= 48 && ch <= 57;
-    }
-
-    /**
-     * Length of string is passed in for improved efficiency (only need to calculate it once) *
-     */
-    private String getChunk(String s, int slength, int marker) {
-        final StringBuilder chunk = new StringBuilder();
-        char c = s.charAt(marker);
-        chunk.append(c);
-        marker++;
-        if(isDigit(c)) {
-            while(marker < slength) {
-                c = s.charAt(marker);
-                if(!isDigit(c))
-                    break;
-                chunk.append(c);
-                marker++;
-            }
-        }
-        else {
-            while(marker < slength) {
-                c = s.charAt(marker);
-                if(isDigit(c))
-                    break;
-                chunk.append(c);
-                marker++;
-            }
-        }
-        return chunk.toString();
-    }
+    private Collator collator = new NaturalOrderCollator();
 
     public int compare(String s1, String s2) {
-        int thisMarker = 0;
-        int thatMarker = 0;
-        int s1Length = s1.length();
-        int s2Length = s2.length();
-
-        while(thisMarker < s1Length && thatMarker < s2Length) {
-            String thisChunk = getChunk(s1, s1Length, thisMarker);
-            thisMarker += thisChunk.length();
-
-            String thatChunk = getChunk(s2, s2Length, thatMarker);
-            thatMarker += thatChunk.length();
-
-            // If both chunks contain numeric characters, sort them numerically
-            int result = 0;
-            if(isDigit(thisChunk.charAt(0)) && isDigit(thatChunk.charAt(0))) {
-                // Simple chunk comparison by length.
-                int thisChunkLength = thisChunk.length();
-                result = thisChunkLength - thatChunk.length();
-                // If equal, the first different number counts
-                if(result == 0) {
-                    for(int i = 0; i < thisChunkLength; i++) {
-                        result = thisChunk.charAt(i) - thatChunk.charAt(i);
-                        if(result != 0) {
-                            return result;
-                        }
-                    }
-                }
-            }
-            else {
-                result = collator.compare(thisChunk, thatChunk);
-//                result = thisChunk.compareTo(thatChunk);
-            }
-
-            if(result != 0)
-                return result;
-        }
-
-        return s1Length - s2Length;
+        return collator.compare(s1, s2);
     }
 }
