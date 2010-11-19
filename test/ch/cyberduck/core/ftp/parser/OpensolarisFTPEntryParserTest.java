11f4215f08b73447a77941140dfe68a02e246390
David Kocher <dkocher@iterate.ch>
2010-11-19 17:16:42 +0000

    Testcase for #3689.

---
+++ test/ch/cyberduck/core/ftp/parser/OpensolarisFTPEntryParserTest.java
@@ -0,0 +1,80 @@
+package ch.cyberduck.core.ftp.parser;
+
+/*
+ * Copyright (c) 2002-2010 David Kocher. All rights reserved.
+ *
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * dkocher@cyberduck.ch
+ */
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.ftp.FTPParserFactory;
+
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPFileEntryParser;
+
+import java.util.Calendar;
+
+/**
+ * @version $Id$
+ */
+public class OpensolarisFTPEntryParserTest extends AbstractTestCase {
+
+    public OpensolarisFTPEntryParserTest(String name) {
+        super(name);
+    }
+
+    private FTPFileEntryParser parser;
+
+
+    @Override
+    public void setUp() {
+        super.setUp();
+        this.parser = new FTPParserFactory().createFileEntryParser("opensolaris FTP server ready.");
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testParse() throws Exception {
+        FTPFile parsed = null;
+
+        // #3689
+        parsed = parser.parseFTPEntry(
+                "drwxr-xr-x+  5 niels    staff          7 Sep  6 13:46 data"
+        );
+        assertNotNull(parsed);
+        assertEquals(parsed.getName(), "data");
+        assertTrue(parsed.getType() == FTPFile.DIRECTORY_TYPE);
+        assertEquals(7, parsed.getSize());
+        assertTrue(parsed.getTimestamp().get(Calendar.MONTH) == Calendar.SEPTEMBER);
+        assertTrue(parsed.getTimestamp().get(Calendar.DAY_OF_MONTH) == 6);
+        assertTrue(parsed.hasPermission(FTPFile.USER_ACCESS, FTPFile.READ_PERMISSION));
+        assertTrue(parsed.hasPermission(FTPFile.USER_ACCESS, FTPFile.WRITE_PERMISSION));
+        assertTrue(parsed.hasPermission(FTPFile.USER_ACCESS, FTPFile.EXECUTE_PERMISSION));
+        assertTrue(parsed.hasPermission(FTPFile.GROUP_ACCESS, FTPFile.READ_PERMISSION));
+        assertTrue(parsed.hasPermission(FTPFile.GROUP_ACCESS, FTPFile.EXECUTE_PERMISSION));
+        assertTrue(parsed.hasPermission(FTPFile.WORLD_ACCESS, FTPFile.READ_PERMISSION));
+        assertTrue(parsed.hasPermission(FTPFile.WORLD_ACCESS, FTPFile.EXECUTE_PERMISSION));
+    }
+
+    public static Test suite() {
+        return new TestSuite(OpensolarisFTPEntryParserTest.class);
+    }
+}
