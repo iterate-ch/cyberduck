401d0d89050a8d2636c952aac10a32f3094fe25d
David Kocher <dkocher@iterate.ch>
2010-11-19 17:29:53 +0000

    Fix #3701.

---
+++ test/ch/cyberduck/core/ftp/parser/MicrosoftFTPEntryParserTest.java
@@ -0,0 +1,74 @@
+package ch.cyberduck.core.ftp.parser;
+
+/*
+ * Copyright (c) 2002-2010 David Kocher. All rights reserved.
+ *
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * dkocher@cyberduck.ch
+ */
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.ftp.FTPParserFactory;
+
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPFileEntryParser;
+
+import java.util.Calendar;
+
+/**
+ * @version $Id$
+ */
+public class MicrosoftFTPEntryParserTest extends AbstractTestCase {
+
+    public MicrosoftFTPEntryParserTest(String name) {
+        super(name);
+    }
+
+    private FTPFileEntryParser parser;
+
+
+    @Override
+    public void setUp() {
+        super.setUp();
+        this.parser = new FTPParserFactory().createFileEntryParser("Microsoft FTP Service");
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testParse() throws Exception {
+        FTPFile parsed = null;
+
+        // #3701
+        parsed = parser.parseFTPEntry(
+                "12-04-06  12:43PM                65335 fon1.kucuk.jpg"
+        );
+        assertNotNull(parsed);
+        assertEquals(parsed.getName(), "fon1.kucuk.jpg");
+        assertTrue(parsed.getType() == FTPFile.FILE_TYPE);
+        assertEquals(65335, parsed.getSize());
+        assertTrue(parsed.getTimestamp().get(Calendar.YEAR) == 2006);
+        assertTrue(parsed.getTimestamp().get(Calendar.MONTH) == Calendar.DECEMBER);
+        assertTrue(parsed.getTimestamp().get(Calendar.DAY_OF_MONTH) == 4);
+    }
+
+    public static Test suite() {
+        return new TestSuite(MicrosoftFTPEntryParserTest.class);
+    }
+}
