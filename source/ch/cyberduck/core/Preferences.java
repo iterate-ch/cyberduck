f2a0bc26ef51bb9b63020e3424a977ae58a7d525
David Kocher <dkocher@iterate.ch>
2010-11-22 22:10:51 +0000

    Enable OpenStack Swift protocol by default.

--- source/ch/cyberduck/core/Preferences.java
+++ source/ch/cyberduck/core/Preferences.java
@@ -197,7 +197,7 @@ public abstract class Preferences {
         defaults.put("protocol.s3.tls.enable", String.valueOf(true));
         defaults.put("protocol.s3.eucalyptus.enable", String.valueOf(true));
         defaults.put("protocol.cf.enable", String.valueOf(true));
-        defaults.put("protocol.cf.swift.enable", String.valueOf(false));
+        defaults.put("protocol.cf.swift.enable", String.valueOf(true));
         defaults.put("protocol.gdocs.enable", String.valueOf(true));
         defaults.put("protocol.gstorage.tls.enable", String.valueOf(true));
         defaults.put("protocol.azure.tls.enable", String.valueOf(true));
