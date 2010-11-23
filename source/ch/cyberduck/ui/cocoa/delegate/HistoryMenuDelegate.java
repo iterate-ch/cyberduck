5b620101dbccdc1dc3d5f1bdd98725843a7e644b
David Kocher <dkocher@iterate.ch>
2010-11-23 07:01:42 +0000

    Add timestamp to history menu items. #3805.

--- source/ch/cyberduck/ui/cocoa/delegate/HistoryMenuDelegate.java
+++ source/ch/cyberduck/ui/cocoa/delegate/HistoryMenuDelegate.java
@@ -18,14 +18,22 @@ package ch.cyberduck.ui.cocoa.delegate;
  *  dkocher@cyberduck.ch
  */
 
+import ch.cyberduck.core.AbstractHostCollection;
 import ch.cyberduck.core.HistoryCollection;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.i18n.Locale;
+import ch.cyberduck.ui.DateFormatterFactory;
 import ch.cyberduck.ui.cocoa.BrowserController;
 import ch.cyberduck.ui.cocoa.IconCache;
 import ch.cyberduck.ui.cocoa.MainController;
+import ch.cyberduck.ui.cocoa.TableCellAttributes;
+import ch.cyberduck.ui.cocoa.application.NSColor;
+import ch.cyberduck.ui.cocoa.application.NSFont;
 import ch.cyberduck.ui.cocoa.application.NSMenu;
 import ch.cyberduck.ui.cocoa.application.NSMenuItem;
+import ch.cyberduck.ui.cocoa.foundation.NSArray;
+import ch.cyberduck.ui.cocoa.foundation.NSAttributedString;
+import ch.cyberduck.ui.cocoa.foundation.NSDictionary;
 
 import org.rococoa.Foundation;
 import org.rococoa.Selector;
@@ -34,25 +42,35 @@ import org.rococoa.cocoa.foundation.NSInteger;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
+import java.util.Date;
+
 /**
  * @version $Id$
  */
 public abstract class HistoryMenuDelegate extends CollectionMenuDelegate<Host> {
     private static Logger log = Logger.getLogger(HistoryMenuDelegate.class);
 
+    protected static final NSDictionary TIMESTAMP_FONT_ATTRIBUTES = NSDictionary.dictionaryWithObjectsForKeys(
+            NSArray.arrayWithObjects(NSFont.userFontOfSize(NSFont.smallSystemFontSize()), NSColor.darkGrayColor(),
+                    TableCellAttributes.PARAGRAPH_STYLE_LEFT_ALIGNMENT_TRUNCATE_MIDDLE),
+            NSArray.arrayWithObjects(NSAttributedString.FontAttributeName, NSAttributedString.ForegroundColorAttributeName,
+                    NSAttributedString.ParagraphStyleAttributeName)
+    );
+
     public HistoryMenuDelegate() {
         super(HistoryCollection.defaultCollection());
     }
 
+    @Override
     public NSInteger numberOfItemsInMenu(NSMenu menu) {
         if(this.isPopulated()) {
             // If you return a negative value, the number of items is left unchanged
             // and menu:updateItem:atIndex:shouldCancel: is not called.
             return new NSInteger(-1);
         }
-        if(HistoryCollection.defaultCollection().size() > 0) {
+        if(this.collection().size() > 0) {
             // The number of history plus a delimiter and the 'Clear' menu
-            return new NSInteger(HistoryCollection.defaultCollection().size() + 2);
+            return new NSInteger(this.collection().size() * 2 + 2);
         }
         return new NSInteger(1);
     }
@@ -65,7 +83,7 @@ public abstract class HistoryMenuDelegate extends CollectionMenuDelegate<Host> {
         if(cancel) {
             return false;
         }
-        final int size = HistoryCollection.defaultCollection().size();
+        final int size = this.collection().size();
         if(size == 0) {
             item.setTitle(Locale.localizedString("No recently connected servers available"));
             item.setTarget(null);
@@ -75,16 +93,31 @@ public abstract class HistoryMenuDelegate extends CollectionMenuDelegate<Host> {
             // No more menu updates.
             return false;
         }
-        else if(index.intValue() < size) {
-            Host h = HistoryCollection.defaultCollection().get(index.intValue());
-            item.setTitle(h.getNickname());
-            item.setAction(this.getDefaultAction());
-            item.setRepresentedObject(h.getUuid());
-            item.setTarget(this.id());
-            item.setEnabled(true);
-            item.setImage(IconCache.iconNamed(h.getProtocol().icon(), 16));
+        else if(index.intValue() < size * 2) {
+            boolean label = index.intValue() % 2 == 0;
+            Host h = this.collection().get(index.intValue() / 2);
+            if(label) {
+                item.setTitle(h.getNickname());
+                item.setAction(this.getDefaultAction());
+                item.setRepresentedObject(h.getUuid());
+                item.setTarget(this.id());
+                item.setEnabled(true);
+                item.setImage(IconCache.iconNamed(h.getProtocol().icon(), 16));
+            }
+            else {
+                // Dummy menu item with timestamp
+                Date timestamp = h.getTimestamp();
+                if(null != timestamp) {
+                    item.setAttributedTitle(NSAttributedString.attributedStringWithAttributes(
+                            DateFormatterFactory.instance().getLongFormat(timestamp.getTime()), TIMESTAMP_FONT_ATTRIBUTES));
+                }
+                else {
+                    item.setAttributedTitle(NSAttributedString.attributedStringWithAttributes(
+                            Locale.localizedString("Unknown"), TIMESTAMP_FONT_ATTRIBUTES));
+                }
+            }
         }
-        else if(index.intValue() == size) {
+        else if(index.intValue() == size * 2) {
             // How to change an existing item to a separator item?
             item.setTitle(StringUtils.EMPTY);
             item.setTarget(null);
@@ -92,7 +125,7 @@ public abstract class HistoryMenuDelegate extends CollectionMenuDelegate<Host> {
             item.setImage(null);
             item.setEnabled(false);
         }
-        else if(index.intValue() == size + 1) {
+        else if(index.intValue() == size * 2 + 1) {
             item.setTitle(Locale.localizedString("Clear Menu"));
             item.setAction(Foundation.selector("clearMenuItemClicked:"));
             item.setTarget(this.id());
@@ -104,12 +137,12 @@ public abstract class HistoryMenuDelegate extends CollectionMenuDelegate<Host> {
     public void historyMenuItemClicked(NSMenuItem sender) {
         log.debug("historyMenuItemClicked:" + sender);
         BrowserController controller = MainController.newDocument();
-        controller.mount(HistoryCollection.defaultCollection().lookup(sender.representedObject()));
+        controller.mount(((AbstractHostCollection) this.collection()).lookup(sender.representedObject()));
     }
 
     public void clearMenuItemClicked(NSMenuItem sender) {
         // Delete all bookmark files
-        HistoryCollection.defaultCollection().clear();
+        this.collection().clear();
     }
 
     @Override
