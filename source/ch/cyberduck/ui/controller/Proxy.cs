7e15a9afe8e850c637ab6aca7f400afeadfe2350
Yves Langisch <yla@iterate.ch>
2011-01-25 16:00:41 +0000

    Use the framework classes to detect proxies.

--- source/ch/cyberduck/ui/controller/Proxy.cs
+++ source/ch/cyberduck/ui/controller/Proxy.cs
@@ -1,5 +1,5 @@
 ﻿// 
-// Copyright (c) 2010 Yves Langisch. All rights reserved.
+// Copyright (c) 2010-2011 Yves Langisch. All rights reserved.
 // http://cyberduck.ch/
 // 
 // This program is free software; you can redistribute it and/or modify
@@ -16,102 +16,20 @@
 // yves@cyberduck.ch
 // 
 using System;
-using System.Text.RegularExpressions;
+using System.Net;
 using ch.cyberduck.core;
-using Ch.Cyberduck.Core;
-using Microsoft.Win32;
 
 namespace Ch.Cyberduck.Ui.Controller
 {
     public class Proxy : AbstractProxy
     {
-        private static bool IsProxyEnabled()
-        {
-            RegistryKey registry =
-                Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", false);
-            if (registry != null)
-            {
-                return Convert.ToBoolean(registry.GetValue("ProxyEnable", 0));
-            }
-            return false;
-        }
+        private readonly IWebProxy _webProxy = WebRequest.DefaultWebProxy;
 
-        private static string GetProxy(string protocol, out int port)
+        private Uri GetProxy(string scheme, string hostname)
         {
-            string host = null;
-            port = 80;
-            RegistryKey registry =
-                Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", false);
-            if (registry != null)
-            {
-                string server = (string) registry.GetValue("ProxyServer");
-                if (Utils.IsNotBlank(server))
-                {
-                    string[] l = server.Split(';');
-                    foreach (string proxy in l)
-                    {
-                        Regex h = new Regex(String.Format("{0}=([\\w\\.]*)(:([\\d]+))?", protocol));
-                        if (h.IsMatch(proxy))
-                        {
-                            Match match = h.Match(proxy);
-                            host = match.Groups[1].Value;
-                            if (match.Groups.Count == 4 && Utils.IsNotBlank(match.Groups[3].Value))
-                            {
-                                port = Convert.ToInt32(match.Groups[3].Value);
-                            }
-                            break;
-                        }
-                    }
-                    if (l.Length == 1 && !"socks".Equals(protocol))
-                    {
-                        //Use the same proxy server for all protocols is checked
-                        //server has the format 'host:port' only
-                        string[] allServer = server.Split(':');
-                        host = allServer[0];
-                        if (allServer.Length == 2 && null != allServer[1])
-                        {
-                            port = Convert.ToInt32(allServer[1]);
-                        }
-                    }
-                }
-            }
-            return host;
-        }
-
-        public static Regex WildcardToRegex(string pattern)
-        {
-            return new Regex("^" +
-                             Regex.Escape(pattern).
-                                 Replace("\\*", ".*").
-                                 Replace("\\?", ".") +
-                             "$");
-        }
-
-        public override bool isSOCKSProxyEnabled(Host host)
-        {
-            if(this.isHostExcluded(host.getHostname())) {
-                return false;
-            }
-            if (IsProxyEnabled())
-            {
-                int port;
-                string proxy = GetProxy("socks", out port);
-                return (null != proxy);
-            }
-            return false;
-        }
-
-        public override string getSOCKSProxyHost()
-        {
-            int port;
-            return GetProxy("socks", out port);
-        }
-
-        public override int getSOCKSProxyPort()
-        {
-            int port;
-            GetProxy("socks", out port);
-            return port;
+            Uri uri = new Uri(scheme + hostname);
+            Uri proxy = _webProxy.GetProxy(uri);
+            return proxy;
         }
 
         public override bool usePassiveFTP()
@@ -119,80 +37,58 @@ namespace Ch.Cyberduck.Ui.Controller
             return true;
         }
 
+        public override bool isSOCKSProxyEnabled(Host host)
+        {
+            Uri proxy = GetProxy("socks://", host.getHostname());
+            return !proxy.Equals(new Uri("socks://" + host.getHostname()));
+        }
+
+        public override string getSOCKSProxyHost(Host host)
+        {
+            Uri proxy = GetProxy("socks://", host.getHostname());
+            return proxy.Host;
+        }
+
+        public override int getSOCKSProxyPort(Host host)
+        {
+            Uri proxy = GetProxy("socks://", host.getHostname());
+            return proxy.Port;
+        }
+
         public override bool isHTTPProxyEnabled(Host host)
         {
-            if(this.isHostExcluded(host.getHostname())) {
-                return false;
-            }
-            if (IsProxyEnabled())
-            {
-                int port;
-                string proxy = GetProxy("http", out port);
-                return (null != proxy);
-            }
-            return false;
+            Uri proxy = GetProxy("http://", host.getHostname());
+            return !proxy.Equals(new Uri("http://" + host.getHostname()));
         }
 
-        public override string getHTTPProxyHost()
+        public override string getHTTPProxyHost(Host host)
         {
-            int port;
-            return GetProxy("http", out port);
+            Uri proxy = GetProxy("http://", host.getHostname());
+            return proxy.Host;
         }
 
-        public override int getHTTPProxyPort()
+        public override int getHTTPProxyPort(Host host)
         {
-            int port;
-            GetProxy("http", out port);
-            return port;
+            Uri proxy = GetProxy("http://", host.getHostname());
+            return proxy.Port;
         }
 
         public override bool isHTTPSProxyEnabled(Host host)
         {
-            if(this.isHostExcluded(host.getHostname())) {
-                return false;
-            }
-            if (IsProxyEnabled())
-            {
-                int port;
-                string proxy = GetProxy("https", out port);
-                return (null != proxy);
-            }
-            return false;
+            Uri proxy = GetProxy("https://", host.getHostname());
+            return !proxy.Equals(new Uri("https://" + host.getHostname()));
         }
 
-        public override string getHTTPSProxyHost()
+        public override string getHTTPSProxyHost(Host host)
         {
-            int port;
-            return GetProxy("https", out port);
+            Uri proxy = GetProxy("https://", host.getHostname());
+            return proxy.Host;
         }
 
-        public override int getHTTPSProxyPort()
+        public override int getHTTPSProxyPort(Host host)
         {
-            int port;
-            GetProxy("https", out port);
-            return port;
-        }
-
-        private bool isHostExcluded(string hostname)
-        {
-            RegistryKey registry =
-                Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", false);
-            if (registry != null)
-            {
-                string excludes = (string) registry.GetValue("ProxyOverride");
-                if (Utils.IsNotBlank(excludes))
-                {
-                    string[] l = excludes.Split(';');
-                    foreach (string exclude in l)
-                    {
-                        if (WildcardToRegex(exclude).IsMatch(hostname))
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
+            Uri proxy = GetProxy("https://", host.getHostname());
+            return proxy.Port;
         }
 
         public static void Register()
