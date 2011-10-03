9f28be4402bd1b229ffef5dbc1e39ec37248e3e9
David Kocher <dkocher@iterate.ch>
2011-10-03 07:47:26 +0000

    Fix #6283 Dropbox protocol registration.

--- source/ch/cyberduck/core/Profile.java
+++ source/ch/cyberduck/core/Profile.java
@@ -281,4 +281,32 @@ public class Profile extends Protocol implements Serializable {
     public PathFactory getPathFactory() {
         return parent.getPathFactory();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) {
+            return true;
+        }
+        if(!(o instanceof Profile)) {
+            return false;
+        }
+        if(!super.equals(o)) {
+            return false;
+        }
+
+        Profile profile = (Profile) o;
+
+        if(parent != null ? !parent.equals(profile.parent) : profile.parent != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (parent != null ? parent.hashCode() : 0);
+        return result;
+    }
 }
