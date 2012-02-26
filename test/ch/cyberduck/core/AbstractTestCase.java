773e28c90333493b60ee4698e0ae5d353358f3c8
David Kocher <dkocher@iterate.ch>
2012-02-26 16:22:47 +0000

    Fix tests.
    Former-commit-id: f704bbae3df5a3ee6948716ccc1ed3dcd6c15a22

--- test/ch/cyberduck/core/AbstractTestCase.java
+++ test/ch/cyberduck/core/AbstractTestCase.java
@@ -19,7 +19,24 @@ package ch.cyberduck.core;
  * dkocher@cyberduck.ch
  */
 
-import ch.cyberduck.ui.cocoa.NativeBootstrapper;
+import ch.cyberduck.core.aquaticprime.Donation;
+import ch.cyberduck.core.threading.AutoreleaseActionOperationBatcher;
+import ch.cyberduck.ui.cocoa.AlertHostKeyController;
+import ch.cyberduck.ui.cocoa.PromptLoginController;
+import ch.cyberduck.ui.cocoa.UserDefaultsDateFormatter;
+import ch.cyberduck.ui.cocoa.UserDefaultsPreferences;
+import ch.cyberduck.ui.cocoa.i18n.BundleLocale;
+import ch.cyberduck.ui.cocoa.model.FinderLocal;
+import ch.cyberduck.ui.cocoa.model.OutlinePathReference;
+import ch.cyberduck.ui.cocoa.quicklook.DeprecatedQuickLook;
+import ch.cyberduck.ui.cocoa.quicklook.QuartzQuickLook;
+import ch.cyberduck.ui.cocoa.serializer.HostPlistReader;
+import ch.cyberduck.ui.cocoa.serializer.PlistDeserializer;
+import ch.cyberduck.ui.cocoa.serializer.PlistSerializer;
+import ch.cyberduck.ui.cocoa.serializer.PlistWriter;
+import ch.cyberduck.ui.cocoa.serializer.ProtocolPlistReader;
+import ch.cyberduck.ui.cocoa.serializer.TransferPlistReader;
+import ch.cyberduck.ui.growl.GrowlNative;
 
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Before;
@@ -34,8 +51,40 @@ public class AbstractTestCase {
     }
 
     @Before
-    public void setup() {
-        NativeBootstrapper.load();
+    public void factory() {
+        AutoreleaseActionOperationBatcher.register();
+        FinderLocal.register();
+        UserDefaultsPreferences.register();
+        BundleLocale.register();
+        GrowlNative.registerImpl();
+        Donation.register();
+
+        PlistDeserializer.register();
+        PlistSerializer.register();
+
+        HostPlistReader.register();
+        TransferPlistReader.register();
+        ProtocolPlistReader.register();
+        OutlinePathReference.register();
+
+        PlistWriter.register();
+
+        Keychain.register();
+        SystemConfigurationProxy.register();
+        SystemConfigurationReachability.register();
+        UserDefaultsDateFormatter.register();
+
+        DeprecatedQuickLook.register();
+        QuartzQuickLook.register();
+
+        PromptLoginController.register();
+        AlertHostKeyController.register();
+
+        if(Preferences.instance().getBoolean("rendezvous.enable")) {
+            RendezvousResponder.register();
+        }
+        ProtocolFactory.register();
+
         Preferences.instance().setProperty("growl.enable", false);
         Preferences.instance().setProperty("application.support.path", ".");
     }
