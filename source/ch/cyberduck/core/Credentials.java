b83887e936f5adb16d38b8e6c4bf719f278a6022
David Kocher <dkocher@iterate.ch>
2012-06-14 09:55:17 +0000

    Add equals implementation.
    Former-commit-id: fef22bf6f22b09d9437f3a407f6efb0b2940431b

--- source/ch/cyberduck/core/Credentials.java
+++ source/ch/cyberduck/core/Credentials.java
@@ -178,4 +178,32 @@ public abstract class Credentials {
     public abstract String getUsernamePlaceholder();
 
     public abstract String getPasswordPlaceholder();
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) {
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Credentials that = (Credentials) o;
+
+        if(password != null ? !password.equals(that.password) : that.password != null) {
+            return false;
+        }
+        if(user != null ? !user.equals(that.user) : that.user != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = user != null ? user.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
 }
