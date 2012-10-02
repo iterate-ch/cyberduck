c616fef4df72d3e2e846ca37090e6a4f5521398d
David Kocher <dkocher@iterate.ch>
2012-10-02 20:22:33 +0000

    Fix test.
    Former-commit-id: a62cb20e8147437296cdfe91230bb898f427287e

--- test/ch/cyberduck/core/analytics/QloudstatAnalyticsProviderTest.java
+++ test/ch/cyberduck/core/analytics/QloudstatAnalyticsProviderTest.java
@@ -8,14 +8,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class QloudstatAnalyticsProviderTest extends AbstractTestCase {
 
     @Test
     public void testGetSetup() throws Exception {
         QloudstatAnalyticsProvider q = new QloudstatAnalyticsProvider();
-        Assert.assertEquals("https://qloudstat.com/account/register?setup=cHJvdmlkZXI9czMuYW1hem9uYXdzLmNvbSxlbmRwb2ludD1jeWJlcmR1Y2stdGVzdGluZyxrZXk9cWxvdWRzdGF0LHNlY3JldD1zZWNyZXQ%3D", q.getSetup(Protocol.S3_SSL, "cyberduck-testing", new Credentials("qloudstat", "secret") {
+        Assert.assertEquals("https://qloudstat.com/configuration/add?setup=cHJvdmlkZXI9czMuYW1hem9uYXdzLmNvbSxlbmRwb2ludD1jeWJlcmR1Y2stdGVzdGluZyxrZXk9cWxvdWRzdGF0LHNlY3JldD1zZWNyZXQ%3D", q.getSetup(Protocol.S3_SSL, "cyberduck-testing", new Credentials("qloudstat", "secret") {
             @Override
             public String getUsernamePlaceholder() {
                 return Protocol.S3_SSL.getUsernamePlaceholder();
