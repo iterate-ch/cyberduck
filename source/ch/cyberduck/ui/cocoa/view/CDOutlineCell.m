189e97ce1370593946933e405f30d4e4c18bba2b
David Kocher <dkocher@iterate.ch>
2012-10-05 20:00:43 +0000

    Fix deprecation warning
    Former-commit-id: bde75228db27f9241ae48af7352db7f356f235c3

--- source/ch/cyberduck/ui/cocoa/view/CDOutlineCell.m
+++ source/ch/cyberduck/ui/cocoa/view/CDOutlineCell.m
@@ -56,12 +56,16 @@ NSString *CDOutlineCellIcon = @"ICON";
 - (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
 {
     if ([controlView isFlipped]) {
-        [icon compositeToPoint:NSMakePoint(cellFrame.origin.x + 3, cellFrame.origin.y + (cellFrame.size.height + [icon size].height) / 2)
-					 operation:NSCompositeSourceOver];
+        [icon drawAtPoint:NSMakePoint(cellFrame.origin.x + 3, cellFrame.origin.y + (cellFrame.size.height + [icon size].height) / 2)
+                     fromRect:cellFrame
+					 operation:NSCompositeSourceOver
+					 fraction:1.0];
     }
     else {
-        [icon compositeToPoint:NSMakePoint(cellFrame.origin.x + 3, cellFrame.origin.y + (cellFrame.size.height - [icon size].height) / 2)
-					 operation:NSCompositeSourceOver];
+        [icon drawAtPoint:NSMakePoint(cellFrame.origin.x + 3, cellFrame.origin.y + (cellFrame.size.height - [icon size].height) / 2)
+                     fromRect:cellFrame
+					 operation:NSCompositeSourceOver
+					 fraction:1.0];
     }
 	[super drawInteriorWithFrame:NSMakeRect(cellFrame.origin.x + 6 + [icon size].width, cellFrame.origin.y, cellFrame.size.width - 6 - [icon size].width, cellFrame.size.height)
 						  inView:controlView];
