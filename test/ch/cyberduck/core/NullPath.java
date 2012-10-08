2802b60285785c388b99732a954da056bf8aa75f
David Kocher <dkocher@iterate.ch>
2012-10-08 09:52:00 +0000

    Add test implementation.
    Former-commit-id: 44837192c6b76db3d5658133ba215585fe02eee4

---
+++ test/ch/cyberduck/core/NullPath.java
@@ -0,0 +1,62 @@
+package ch.cyberduck.core;
+
+import ch.cyberduck.core.io.BandwidthThrottle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * @version $Id:$
+ */
+public class NullPath extends Path {
+
+    public NullPath(final String path, final int type) {
+        super(path, type);
+    }
+
+    @Override
+    protected AttributedList<Path> list(final AttributedList<Path> children) {
+        return AttributedList.emptyList();
+    }
+
+    @Override
+    public Session getSession() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public InputStream read(final boolean check) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void download(final BandwidthThrottle throttle, final StreamListener listener, final boolean check, final boolean quarantine) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public OutputStream write(final boolean check) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void upload(final BandwidthThrottle throttle, final StreamListener listener, final boolean check) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void mkdir() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void delete() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void rename(final AbstractPath renamed) {
+        throw new UnsupportedOperationException();
+    }
+}
