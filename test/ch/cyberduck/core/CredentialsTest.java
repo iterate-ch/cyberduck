d24117e0da69725d3a68a7d3732b85c313e451b3
David Kocher <dkocher@iterate.ch>
2012-10-08 13:26:24 +0000

    Add test.
    Former-commit-id: f5df18f0f05c267bbb883735c8574d90a4975956

--- test/ch/cyberduck/core/CredentialsTest.java
+++ test/ch/cyberduck/core/CredentialsTest.java
@@ -5,58 +5,37 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class CredentialsTest {
 
     @Test
-    public void testSetIdentity() throws Exception {
-        Credentials c = new Credentials() {
-            @Override
-            public String getUsernamePlaceholder() {
-                throw new UnsupportedOperationException();
-            }
+    public void testEquals() {
+        assertEquals(new DefaultCredentials("a", "b"), new DefaultCredentials("a", "b"));
+        assertNotSame(new DefaultCredentials("a", "b"), new DefaultCredentials("a", "c"));
+    }
 
-            @Override
-            public String getPasswordPlaceholder() {
-                throw new UnsupportedOperationException();
-            }
-        };
+    @Test
+    public void testSetIdentity() throws Exception {
+        Credentials c = new DefaultCredentials();
         c.setIdentity(LocalFactory.createLocal("~/.ssh/unknown.rsa"));
         assertFalse(c.isPublicKeyAuthentication());
-        c.setIdentity(LocalFactory.createLocal("~/.ssh/id_rsa"));
+        final Local t = LocalFactory.createLocal(Preferences.instance().getProperty("tmp.dir"), "~/.ssh/id_rsa");
+        t.touch();
+        c.setIdentity(t);
         assertTrue(c.isPublicKeyAuthentication());
+        t.delete();
     }
 
     @Test
     public void testAnonymous() throws Exception {
-        Credentials c = new Credentials("anonymous", "") {
-            @Override
-            public String getUsernamePlaceholder() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public String getPasswordPlaceholder() {
-                throw new UnsupportedOperationException();
-            }
-        };
+        Credentials c = new DefaultCredentials("anonymous", "");
         assertEquals("cyberduck@example.net", c.getPassword());
     }
 
     @Test
     public void testDefault() throws Exception {
-        Credentials c = new Credentials() {
-            @Override
-            public String getUsernamePlaceholder() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public String getPasswordPlaceholder() {
-                throw new UnsupportedOperationException();
-            }
-        };
+        Credentials c = new DefaultCredentials();
         assertEquals(System.getProperty("user.name"), c.getUsername());
         assertEquals(null, c.getPassword());
     }
