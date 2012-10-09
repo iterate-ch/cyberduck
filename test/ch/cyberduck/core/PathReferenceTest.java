51ba5351b608367ce8500e77397250eb4180c9da
David Kocher <dkocher@iterate.ch>
2012-10-09 15:42:20 +0000

    Former-commit-id: b3f7ba5ff6f084a4711183af59644a4432ca46d9

--- test/ch/cyberduck/core/PathReferenceTest.java
+++ test/ch/cyberduck/core/PathReferenceTest.java
@@ -1,114 +1,18 @@
 package ch.cyberduck.core;
 
-import ch.cyberduck.core.io.BandwidthThrottle;
-
 import org.junit.Test;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 import static org.junit.Assert.assertEquals;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class PathReferenceTest {
 
     @Test
     public void testUnique() throws Exception {
-        Path one = new Path("a", Path.FILE_TYPE) {
-            @Override
-            protected AttributedList<Path> list(final AttributedList<Path> children) {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public Session getSession() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public InputStream read(final boolean check) throws IOException {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            protected void download(final BandwidthThrottle throttle, final StreamListener listener, final boolean check, final boolean quarantine) {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public OutputStream write(final boolean check) throws IOException {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            protected void upload(final BandwidthThrottle throttle, final StreamListener listener, final boolean check) {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public void mkdir() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public void delete() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public void rename(final AbstractPath renamed) {
-                throw new UnsupportedOperationException();
-            }
-        };
-        Path second = new Path("a", Path.FILE_TYPE) {
-            @Override
-            protected AttributedList<Path> list(final AttributedList<Path> children) {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public Session getSession() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public InputStream read(final boolean check) throws IOException {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            protected void download(final BandwidthThrottle throttle, final StreamListener listener, final boolean check, final boolean quarantine) {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public OutputStream write(final boolean check) throws IOException {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            protected void upload(final BandwidthThrottle throttle, final StreamListener listener, final boolean check) {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public void mkdir() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public void delete() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public void rename(final AbstractPath renamed) {
-                throw new UnsupportedOperationException();
-            }
-        };
+        Path one = new NullPath("a", Path.FILE_TYPE);
+        Path second = new NullPath("a", Path.FILE_TYPE);
         assertEquals(one.getReference(), second.getReference());
     }
 }
