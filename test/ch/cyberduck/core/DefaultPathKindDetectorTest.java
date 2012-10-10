149b7e980334926c84d7d073fb2538a981363912
David Kocher <dkocher@iterate.ch>
2012-10-10 16:44:09 +0000

    Fix test.
    Former-commit-id: 4d5e2e2b941a96f80265f9dde8aded068cf6b636

--- test/ch/cyberduck/core/DefaultPathKindDetectorTest.java
+++ test/ch/cyberduck/core/DefaultPathKindDetectorTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class DefaultPathKindDetectorTest {
 
     @Test
     public void testDetect() throws Exception {
         DefaultPathKindDetector d = new DefaultPathKindDetector();
-        assertEquals(Path.FILE_TYPE, d.detect(null));
+        assertEquals(Path.DIRECTORY_TYPE, d.detect(null));
         assertEquals(Path.DIRECTORY_TYPE, d.detect("/"));
         assertEquals(Path.DIRECTORY_TYPE, d.detect("/a"));
         assertEquals(Path.DIRECTORY_TYPE, d.detect("/a/"));
