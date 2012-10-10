3104d592647ea9aab725b1a2836b986216777400
David Kocher <dkocher@iterate.ch>
2012-10-10 19:24:42 +0000

    Add test.
    Former-commit-id: 9697011fb52755f25b3913ff86f44c3b0d70c457

---
+++ test/ch/cyberduck/core/importer/FetchBookmarkCollectionTest.java
@@ -0,0 +1,22 @@
+package ch.cyberduck.core.importer;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.LocalFactory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class FetchBookmarkCollectionTest extends AbstractTestCase {
+
+    @Test
+    public void testGetFile() throws Exception {
+        FetchBookmarkCollection c = new FetchBookmarkCollection();
+        assertEquals(0, c.size());
+        c.parse(LocalFactory.createLocal("test/ch/cyberduck/core/importer/com.fetchsoftworks.Fetch.Shortcuts.plist"));
+        assertEquals(2, c.size());
+    }
+}
