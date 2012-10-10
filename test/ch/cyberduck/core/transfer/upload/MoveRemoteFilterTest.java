d5896713efec001c29c7ba51bc82775fed8dcd73
David Kocher <dkocher@iterate.ch>
2012-10-10 15:52:21 +0000

    Add test.
    Former-commit-id: 928effb172726215473e766a76c5b470585b17e0

---
+++ test/ch/cyberduck/core/transfer/upload/MoveRemoteFilterTest.java
@@ -0,0 +1,80 @@
+package ch.cyberduck.core.transfer.upload;
+
+import ch.cyberduck.core.AbstractPath;
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.NullPath;
+import ch.cyberduck.core.NullProtocol;
+import ch.cyberduck.core.NullSession;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Session;
+import ch.cyberduck.core.transfer.NullSymlinkResolver;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @version $Id:$
+ */
+public class MoveRemoteFilterTest extends AbstractTestCase {
+
+    @Test
+    public void testAccept() throws Exception {
+        MoveRemoteFilter f = new MoveRemoteFilter(new NullSymlinkResolver());
+        assertTrue(f.accept(new NullPath("t", Path.FILE_TYPE) {
+            @Override
+            public Session getSession() {
+                return new NullSession(new Host("t")) {
+                    @Override
+                    public boolean isRenameSupported(final Path file) {
+                        return true;
+                    }
+                };
+            }
+        })
+        );
+        assertFalse(f.accept(new NullPath("t", Path.FILE_TYPE) {
+            @Override
+            public Session getSession() {
+                return new NullSession(new Host("t")) {
+                    @Override
+                    public boolean isRenameSupported(final Path file) {
+                        return false;
+                    }
+                };
+            }
+        })
+        );
+    }
+
+    @Test
+    public void testPrepare() throws Exception {
+        MoveRemoteFilter f = new MoveRemoteFilter(new NullSymlinkResolver());
+        final NullPath p = new NullPath("t", Path.FILE_TYPE) {
+            @Override
+            public Session getSession() {
+                return new NullSession(new Host(new NullProtocol(), "t"));
+            }
+
+            @Override
+            public Path getParent() {
+                return new NullPath("p", Path.DIRECTORY_TYPE) {
+                    @Override
+                    protected AttributedList<Path> list(final AttributedList<Path> children) {
+                        final AttributedList<Path> l = new AttributedList<Path>();
+                        l.add(new NullPath("t", Path.FILE_TYPE));
+                        return l;
+                    }
+
+                    @Override
+                    public void rename(final AbstractPath renamed) {
+                        assertNotSame(this.getName(), renamed.getName());
+                    }
+                };
+            }
+        };
+        f.prepare(p);
+    }
+}
