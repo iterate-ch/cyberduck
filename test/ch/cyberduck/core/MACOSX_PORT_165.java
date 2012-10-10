647d5b4f2e5e979de7937ed02ecc76f793fbeb47
David Kocher <dkocher@iterate.ch>
2012-10-10 10:03:17 +0000

    Add test.
    Former-commit-id: ee977d98a2c7a702198f5d2ebd937c2a7447d2b9

---
+++ test/ch/cyberduck/core/MACOSX_PORT_165.java
@@ -0,0 +1,30 @@
+package ch.cyberduck.core;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class MACOSX_PORT_165 {
+
+    @Test
+    public void testListFiles() throws IOException {
+        final String u = UUID.randomUUID().toString();
+        final File file = File.createTempFile(u, "-ä");
+        for(File f : file.getParentFile().listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(final File dir, final String name) {
+                return name.startsWith(u);
+            }
+        })) {
+            assertEquals(f.getName(), file.getName());
+        }
+    }
+}
