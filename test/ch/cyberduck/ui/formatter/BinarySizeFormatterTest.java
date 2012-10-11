4635b875e89cf2a73f3e9520fe9335a5b37cb037
David Kocher <dkocher@iterate.ch>
2012-10-11 13:17:12 +0000

    Former-commit-id: 18175ef818f51482abdd4458a51d9378314a532d

--- test/ch/cyberduck/ui/formatter/BinarySizeFormatterTest.java
+++ test/ch/cyberduck/ui/formatter/BinarySizeFormatterTest.java
@@ -5,18 +5,19 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class BinarySizeFormatterTest {
 
     @Test
     public void testFormat() throws Exception {
-        DecimalSizeFormatter f = new DecimalSizeFormatter();
+        BinarySizeFormatter f = new BinarySizeFormatter();
         assertEquals("1.0 KB", f.format(1024));
-        assertEquals("1.4 KB", f.format(1500));
-        assertEquals("1.9 KB", f.format(2000));
+        assertEquals("1.5 KB", f.format(1500));
+        assertEquals("1.4 KB", f.format(1480));
+        assertEquals("2.0 KB", f.format(2000));
         assertEquals("1.0 MB", f.format(1048576));
         assertEquals("1.0 GB", f.format(1073741824));
-        assertEquals("375.2 MB", f.format(393495974));
+        assertEquals("375.3 MB", f.format(393495974));
     }
 }
--- test/ch/cyberduck/ui/formatter/DecimalSizeFormatterTest.java
+++ test/ch/cyberduck/ui/formatter/DecimalSizeFormatterTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class DecimalSizeFormatterTest {
 
@@ -16,7 +16,7 @@ public class DecimalSizeFormatterTest {
         assertEquals("1.5 KB", f.format(1500));
         assertEquals("2.0 KB", f.format(2000));
         assertEquals("1.0 MB", f.format(1048576));
-        assertEquals("1.0 GB", f.format(1073741824));
-        assertEquals("375.3 MB", f.format(393495974));
+        assertEquals("1.1 GB", f.format(1073741824));
+        assertEquals("393.5 MB", f.format(393495974));
     }
 }
