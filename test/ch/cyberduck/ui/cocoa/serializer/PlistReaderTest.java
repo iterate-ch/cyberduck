1266c3b8616d899c088aecb78bdfea0963d124c6
David Kocher <dkocher@iterate.ch>
2012-10-14 15:35:14 +0000

    Former-commit-id: 32324ad59857f0aa85d3eb92984a5f4b1b3eeb68

--- test/ch/cyberduck/ui/cocoa/serializer/PlistReaderTest.java
+++ test/ch/cyberduck/ui/cocoa/serializer/PlistReaderTest.java
@@ -1,8 +1,10 @@
 package ch.cyberduck.ui.cocoa.serializer;
 
+import ch.cyberduck.core.AbstractTestCase;
 import ch.cyberduck.core.Serializable;
 import ch.cyberduck.core.local.FinderLocal;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertNotNull;
@@ -10,7 +12,12 @@ import static junit.framework.Assert.assertNotNull;
 /**
  * @version $Id$
  */
-public class PlistReaderTest {
+public class PlistReaderTest extends AbstractTestCase {
+
+    @BeforeClass
+    public static void register() {
+        HostPlistReader.register();
+    }
 
     @Test
     public void testRead() throws Exception {
