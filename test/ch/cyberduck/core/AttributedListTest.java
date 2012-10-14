ad270ddd922a802e2a8d5824ef0be8a107d49d06
David Kocher <dkocher@iterate.ch>
2012-10-14 15:25:40 +0000

    Add test.
    Former-commit-id: 324a53d4b7a37f831c4bfed88c9db90b4d181728

--- test/ch/cyberduck/core/AttributedListTest.java
+++ test/ch/cyberduck/core/AttributedListTest.java
@@ -4,6 +4,7 @@ import ch.cyberduck.ui.cocoa.foundation.NSString;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -16,5 +17,23 @@ public class AttributedListTest extends AbstractTestCase {
         AttributedList<Path> list = new AttributedList<Path>();
         assertTrue(list.add(new NullPath("/a", Path.DIRECTORY_TYPE)));
         assertTrue(list.contains(new NSObjectPathReference(NSString.stringWithString("/a"))));
+    }
+
+    @Test
+    public void testFilter() throws Exception {
+        AttributedList<Path> list = new AttributedList<Path>();
+        assertTrue(list.add(new NullPath("/a", Path.DIRECTORY_TYPE)));
+        assertTrue(list.filter(new NullComparator(), new PathFilter() {
+            @Override
+            public boolean accept(final AbstractPath file) {
+                return !file.getName().equals("a");
+            }
+        }).isEmpty());
+        assertFalse(list.filter(new NullComparator(), new PathFilter() {
+            @Override
+            public boolean accept(final AbstractPath file) {
+                return !file.getName().equals("b");
+            }
+        }).isEmpty());
     }
 }
