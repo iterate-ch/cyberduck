9020387f1291d5cc18b9ee8fdb74e6298e94b64a
David Kocher <dkocher@iterate.ch>
2012-10-15 18:45:57 +0000

    Copyright.
    Former-commit-id: 677dfbbb07d2db5e33fd432a356a2ee47fe282fb

--- source/ch/cyberduck/core/synchronization/CombinedComparisionService.java
+++ source/ch/cyberduck/core/synchronization/CombinedComparisionService.java
@@ -1,12 +1,30 @@
 package ch.cyberduck.core.synchronization;
 
+/*
+ * Copyright (c) 2012 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * dkocher@cyberduck.ch
+ */
+
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.Preferences;
 
 import org.apache.log4j.Logger;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class CombinedComparisionService implements ComparisonService {
     private static Logger log = Logger.getLogger(CombinedComparisionService.class);
