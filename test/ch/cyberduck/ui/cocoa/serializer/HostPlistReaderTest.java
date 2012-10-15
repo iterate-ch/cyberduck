16840d8bcaecc60bbbdedb54408f3343953e0584
David Kocher <dkocher@iterate.ch>
2012-10-15 16:09:09 +0000

    Add test.
    Former-commit-id: b732cc0003ea79aac6a9964e699c6d21fe7eac17

--- test/ch/cyberduck/ui/cocoa/serializer/HostPlistReaderTest.java
+++ test/ch/cyberduck/ui/cocoa/serializer/HostPlistReaderTest.java
@@ -1,22 +1,32 @@
 package ch.cyberduck.ui.cocoa.serializer;
 
 import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Protocol;
 import ch.cyberduck.core.Serializable;
 import ch.cyberduck.core.local.FinderLocal;
+import ch.cyberduck.ui.cocoa.foundation.NSDictionary;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 /**
- * @version $Id$
+ * @version $Id:$
  */
-public class PlistReaderTest extends AbstractTestCase {
+public class HostPlistReaderTest extends AbstractTestCase {
 
     @BeforeClass
     public static void register() {
         HostPlistReader.register();
+    }
+
+    @Test
+    public void testDeserializeDeprecatedProtocol() throws Exception {
+        assertEquals(Protocol.FTP, new HostPlistReader().deserialize(NSDictionary.dictionaryWithContentsOfFile(
+                "test/ch/cyberduck/ui/cocoa/serializer/1c158c34-db8a-4c32-a732-abd9447bb27c.duck"
+        )).getProtocol());
     }
 
     @Test
