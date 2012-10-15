01cbc09b487ba4f965bab5959d7aa8f3483f9e42
David Kocher <dkocher@iterate.ch>
2012-10-15 18:39:04 +0000

    Former-commit-id: e0b9aa4ae069278ce9e85d506bcc037427cb2b48

--- source/ch/cyberduck/core/transfer/upload/OverwriteFilter.java
+++ source/ch/cyberduck/core/transfer/upload/OverwriteFilter.java
@@ -2,13 +2,10 @@ package ch.cyberduck.core.transfer.upload;
 
 import ch.cyberduck.core.transfer.SymlinkResolver;
 
-import org.apache.log4j.Logger;
-
 /**
  * @version $Id$
  */
 public class OverwriteFilter extends AbstractUploadFilter {
-    private static final Logger log = Logger.getLogger(OverwriteFilter.class);
 
     public OverwriteFilter(final SymlinkResolver symlinkResolver) {
         super(symlinkResolver);
