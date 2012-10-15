54bd3c7eca95efd3298ee19191a656f0baad0719
David Kocher <dkocher@iterate.ch>
2012-10-15 18:25:28 +0000

    Former-commit-id: a16100e8cfaca01d33489366aeeac22b9caffb88

--- test/ch/cyberduck/core/editor/LaunchServicesApplicationFinderTest.java
+++ test/ch/cyberduck/core/editor/LaunchServicesApplicationFinderTest.java
@@ -22,7 +22,7 @@ public class LaunchServicesApplicationFinderTest extends AbstractTestCase {
 
     @Test
     public void testFindAll() throws Exception {
-        ApplicationFinder f = ApplicationFinderFactory.instance();
+        ApplicationFinder f = ApplicationFinderFactory.get();
         final List<Application> applications = f.findAll(new NullLocal(null, "file.txt"));
         assertFalse(applications.isEmpty());
         assertTrue(applications.contains(new Application("com.apple.TextEdit", "Preview")));
@@ -31,13 +31,13 @@ public class LaunchServicesApplicationFinderTest extends AbstractTestCase {
 
     @Test
     public void testFind() throws Exception {
-        ApplicationFinder f = ApplicationFinderFactory.instance();
+        ApplicationFinder f = ApplicationFinderFactory.get();
         assertEquals(new Application("com.apple.Preview", "Preview"), f.find(new NullLocal(null, "file.png")));
     }
 
     @Test
     public void testIsOpen() throws Exception {
-        ApplicationFinder f = ApplicationFinderFactory.instance();
+        ApplicationFinder f = ApplicationFinderFactory.get();
         assertTrue(f.isOpen(new Application("com.apple.finder", null)));
         assertFalse(f.isOpen(new Application("com.apple.Finder", null)));
     }
