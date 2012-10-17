1e2cea911ef7319c4ab6203882e643c95c9aeda2
David Kocher <dkocher@iterate.ch>
2012-10-17 21:54:03 +0000

    Delete unused.
    Former-commit-id: a087a0e4bcc78bfff9ad7a5876ee6d68afd5459c

--- source/de/zathras/UKSystemInfo.h
+++ source/de/zathras/UKSystemInfo.h
@@ -9,14 +9,6 @@
 #import <Cocoa/Cocoa.h>
 
 
-unsigned	UKPhysicalRAMSize();						// RAM Size in MBs.
-NSString*	UKSystemVersionString();					// System version as a string MM.m.b
-unsigned	UKClockSpeed();								// CPU speed in MHz.
-unsigned	UKCountCores();								// Number of CPU cores. This is always >= number of CPUs.
-NSString*	UKMachineName();							// Name of Mac model, as best as we can determine.
-NSString*	UKCPUName();								// Same as UKAutoreleasedCPUName( NO );
-NSString*	UKAutoreleasedCPUName( BOOL releaseIt );	// Returns CPU name, i.e. "G3", "G4" etc. If releaseIt is YES, this will look up the name anew each time, otherwise it will cache the name for subsequent calls. Doesn't support the G5 :-(
-//NSString*	UKSystemSerialNumber();
 void		UKGetSystemVersionComponents( SInt32* outMajor, SInt32* outMinor, SInt32* outBugfix );	// System version as the separate components (Major.Minor.Bugfix).
 
 // Don't use the following for new code:
