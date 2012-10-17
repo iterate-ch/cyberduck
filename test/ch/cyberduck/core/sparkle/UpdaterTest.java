6741eba534c01bed4f3de8bae9467596fc037d9b
David Kocher <dkocher@iterate.ch>
2012-10-17 19:56:37 +0000

    Add test.
    Former-commit-id: bdc0052380c33dfdb61f19c242b804c256cd9d97

---
+++ test/ch/cyberduck/core/sparkle/UpdaterTest.java
@@ -0,0 +1,36 @@
+package ch.cyberduck.core.sparkle;
+
+/*
+ * Copyright (c) 2012 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * dkocher@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @version $Id:$
+ */
+public class UpdaterTest extends AbstractTestCase {
+
+    @Test
+    public void testCreate() throws Exception {
+        assertNotNull(Updater.create());
+    }
+}
