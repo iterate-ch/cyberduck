a0c50a4ee4cb2bbc781f1d9565ef0a4c320142e8
David Kocher <dkocher@iterate.ch>
2012-10-18 14:27:24 +0000

    Former-commit-id: 7d6a634ddc34e286220066702795b59ccb175688

--- test/ch/cyberduck/core/editor/MultipleEditorFactoryTest.java
+++ test/ch/cyberduck/core/editor/MultipleEditorFactoryTest.java
@@ -2,6 +2,7 @@ package ch.cyberduck.core.editor;
 
 import ch.cyberduck.core.AbstractTestCase;
 import ch.cyberduck.core.FactoryException;
+import ch.cyberduck.core.local.LaunchServicesApplicationFinder;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,6 +15,7 @@ public class MultipleEditorFactoryTest extends AbstractTestCase {
     @BeforeClass
     public static void register() {
         MultipleEditorFactory.register();
+        LaunchServicesApplicationFinder.register();
     }
 
     @Test(expected = FactoryException.class)
