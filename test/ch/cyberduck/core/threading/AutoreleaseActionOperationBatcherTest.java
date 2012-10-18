759cc05180c843efca256afc87a4a50b90f2c645
David Kocher <dkocher@iterate.ch>
2012-10-18 08:50:31 +0000

    Add test.
    Former-commit-id: db42a05c52006e8bd88335c0009f407d07601eb9

---
+++ test/ch/cyberduck/core/threading/AutoreleaseActionOperationBatcherTest.java
@@ -0,0 +1,48 @@
+package ch.cyberduck.core.threading;
+
+/*
+ * Copyright (c) 2012 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * dkocher@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @version $Id:$
+ */
+public class AutoreleaseActionOperationBatcherTest extends AbstractTestCase {
+
+    @BeforeClass
+    public static void register() {
+        AutoreleaseActionOperationBatcher.register();
+    }
+
+    @Test
+    public void testOperate() throws Exception {
+        this.repeat(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                ActionOperationBatcherFactory.get().operate();
+                return null;
+            }
+        }, 20);
+    }
+}
