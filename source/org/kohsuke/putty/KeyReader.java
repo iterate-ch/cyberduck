59f3274c3f261fb6783c0f991dcf02b20f8de413
Yves Langisch <yla@iterate.ch>
2012-10-18 20:41:41 +0000

    Fix #6394.
    Former-commit-id: 5c204a06d4e8cf3fe2a85284c293179779a7fa4b

--- source/org/kohsuke/putty/KeyReader.java
+++ source/org/kohsuke/putty/KeyReader.java
@@ -1,13 +1,10 @@
 package org.kohsuke.putty;
 
-import java.io.ByteArrayInputStream;
 import java.io.DataInput;
 import java.io.DataInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.Arrays;
-
-import ch.ethz.ssh2.crypto.PEMDecryptException;
 
 /**
  * Parses the putty key bit vector, which is an encoded sequence
@@ -15,30 +12,28 @@ import ch.ethz.ssh2.crypto.PEMDecryptException;
  *
  * @author Kohsuke Kawaguchi
  */
-public class KeyReader {
+class KeyReader {
     private final DataInput di;
 
-    public KeyReader(byte[] key) {
-        this.di = new DataInputStream(new ByteArrayInputStream(Arrays.copyOf(key, key.length)));
+    KeyReader(byte[] key) {
+        this.di = new DataInputStream(new ByteArrayInputStream(key));
     }
 
     /**
      * Skips an integer without reading it.
      */
-    public void skip() throws IOException {
-        final int n = di.readInt();
-        final int skipped = di.skipBytes(n);
-        if(skipped != n) {
-            throw new IOException(String.format("Skipped %d bytes instead of %d", skipped, n));
+    public void skip() {
+        try {
+            di.skipBytes(di.readInt());
+        }
+        catch(IOException e) {
+            throw new AssertionError(e);
         }
     }
 
-    private byte[] read() throws PEMDecryptException {
+    private byte[] read() {
         try {
             int len = di.readInt();
-            if(len <= 0 || len > 512) {
-                throw new PEMDecryptException("Invalid length " + len);
-            }
             byte[] r = new byte[len];
             di.readFully(r);
             return r;
@@ -48,7 +43,10 @@ public class KeyReader {
         }
     }
 
-    public BigInteger readInt() throws PEMDecryptException {
+    /**
+     * Reads the next integer.
+     */
+    public BigInteger readInt() {
         return new BigInteger(read());
     }
 }
