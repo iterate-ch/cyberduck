99329e4b2efb858bf828db6065ef38ffb89e8a55
David Kocher <dkocher@iterate.ch>
2012-10-20 21:46:59 +0000

    Add tests.
    Former-commit-id: 748d897e581a0e136291b0f530e29c807cd530ec

--- test/ch/cyberduck/core/PathAttributesTest.java
+++ test/ch/cyberduck/core/PathAttributesTest.java
@@ -2,7 +2,7 @@ package ch.cyberduck.core;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * @version $Id$
@@ -15,5 +15,33 @@ public class PathAttributesTest extends AbstractTestCase {
         attributes.setSize(3L);
         attributes.setModificationDate(5343L);
         assertEquals(attributes, new PathAttributes(attributes.getAsDictionary()));
+    }
+
+    @Test
+    public void testTypeVolume() throws Exception {
+        PathAttributes attributes = new PathAttributes(Path.VOLUME_TYPE | Path.DIRECTORY_TYPE);
+        assertTrue(attributes.isVolume());
+        assertTrue(attributes.isDirectory());
+        assertFalse(attributes.isFile());
+        assertFalse(attributes.isSymbolicLink());
+    }
+
+    @Test
+    public void testTypeFile() throws Exception {
+        PathAttributes attributes = new PathAttributes(Path.FILE_TYPE | Path.SYMBOLIC_LINK_TYPE);
+        assertFalse(attributes.isVolume());
+        assertFalse(attributes.isDirectory());
+        assertTrue(attributes.isFile());
+        assertTrue(attributes.isSymbolicLink());
+    }
+
+    @Test
+    public void testPermissions() throws Exception {
+        PathAttributes attributes = new PathAttributes(Path.FILE_TYPE);
+        assertNotNull(attributes.getOwner());
+        assertNotNull(attributes.getGroup());
+        assertNotNull(attributes.getPermission());
+        assertEquals(Permission.EMPTY, attributes.getPermission());
+        assertEquals(Acl.EMPTY, attributes.getAcl());
     }
 }
