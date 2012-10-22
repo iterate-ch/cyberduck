f98a464c8fc6e9b961bdc12de1ab23684861564a
David Kocher <dkocher@iterate.ch>
2012-10-22 12:39:53 +0000

    Add test.
    Former-commit-id: 29a064a86d21b8e46c8089304eb02592031d191e

---
+++ test/ch/cyberduck/core/AbstractLoginControllerTest.java
@@ -0,0 +1,63 @@
+package ch.cyberduck.core;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @version $Id:$
+ */
+public class AbstractLoginControllerTest extends AbstractTestCase {
+
+    @BeforeClass
+    public static void register() {
+        NullKeychain.register();
+    }
+
+    @Test(expected = LoginCanceledException.class)
+    public void testCheck() throws Exception {
+        LoginController c = new AbstractLoginController() {
+            @Override
+            public void warn(String title, String message, String continueButton, String disconnectButton, String preference) throws LoginCanceledException {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void prompt(Protocol protocol, Credentials credentials, String title, String reason, boolean enableKeychain, boolean enablePublicKey, boolean enableAnonymous) throws LoginCanceledException {
+                assertEquals(Protocol.SFTP, protocol);
+                assertEquals("t", title);
+                assertEquals("r. No login credentials could be found in the Keychain.", reason);
+                assertTrue(enableKeychain);
+                assertTrue(enablePublicKey);
+                assertFalse(enableAnonymous);
+                throw new LoginCanceledException();
+            }
+        };
+        c.check(new Host(Protocol.SFTP, "h"), "t", "r", true, true, false);
+    }
+
+    @Test(expected = LoginCanceledException.class)
+    public void testFail() throws Exception {
+        final Credentials user = new Credentials("t", "p");
+        LoginController c = new AbstractLoginController() {
+            @Override
+            public void warn(String title, String message, String continueButton, String disconnectButton, String preference) throws LoginCanceledException {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void prompt(Protocol protocol, Credentials credentials, String title, String reason, boolean enableKeychain, boolean enablePublicKey, boolean enableAnonymous) throws LoginCanceledException {
+                assertEquals(Protocol.WEBDAV, protocol);
+                assertEquals(user, credentials);
+                assertEquals("r", reason);
+                assertEquals("Login failed", title);
+                assertTrue(enableKeychain);
+                assertFalse(enablePublicKey);
+                assertTrue(enableAnonymous);
+                throw new LoginCanceledException();
+            }
+        };
+        c.fail(Protocol.WEBDAV, user, "r");
+    }
+}
