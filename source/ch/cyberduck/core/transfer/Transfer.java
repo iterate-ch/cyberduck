7dfd9ada5721553ba5dc1559103d5540c7e90f08
David Kocher <dkocher@iterate.ch>
2012-11-30 15:15:45 +0000

    Former-commit-id: d3b58ac79d2c14bb06f949b9852ebfc92ad47d9b

--- source/ch/cyberduck/core/transfer/Transfer.java
+++ source/ch/cyberduck/core/transfer/Transfer.java
@@ -625,7 +625,6 @@ public abstract class Transfer implements Serializable {
             // We have our own session independent of any browser.
             this.cache().clear();
         }
-        reset = false;
     }
 
     /**
