a4c6fde5dab0ea051bd9b820712a8e304fbb6497
David Kocher <dkocher@iterate.ch>
2013-03-23 16:07:41 +0000

    Add tests.
    Former-commit-id: 5f87fed8d9286741ee593fcb824f2c11b837299b

--- test/ch/cyberduck/core/analytics/QloudstatAnalyticsProviderTest.java
+++ test/ch/cyberduck/core/analytics/QloudstatAnalyticsProviderTest.java
@@ -14,14 +14,28 @@ import org.junit.Test;
 public class QloudstatAnalyticsProviderTest extends AbstractTestCase {
 
     @Test
-    public void testGetSetupS3() throws Exception {
+    public void testGetSetupS3() {
         QloudstatAnalyticsProvider q = new QloudstatAnalyticsProvider();
         Assert.assertEquals("https://qloudstat.com/configuration/add?setup=cHJvdmlkZXI9czMuYW1hem9uYXdzLmNvbSxwcm90b2NvbD1odHRwLGVuZHBvaW50PWN5YmVyZHVjay10ZXN0aW5nLGtleT1xbG91ZHN0YXQsc2VjcmV0PXNlY3JldA%3D%3D",
                 q.getSetup(Protocol.S3_SSL, Scheme.http, "cyberduck-testing", new Credentials("qloudstat", "secret")));
     }
 
     @Test
-    public void testGetSetupCloudFrontStreaming() throws Exception {
+    public void testGetSetupGoogleStorage() {
+        QloudstatAnalyticsProvider q = new QloudstatAnalyticsProvider();
+        Assert.assertEquals("https://qloudstat.com/configuration/add?setup=cHJvdmlkZXI9c3RvcmFnZS5nb29nbGVhcGlzLmNvbSxwcm90b2NvbD1odHRwLGVuZHBvaW50PXRlc3QuY3liZXJkdWNrLmNoLGtleT1xbG91ZHN0YXQsc2VjcmV0PXNlY3JldA%3D%3D",
+                q.getSetup(Protocol.GOOGLESTORAGE_SSL, Scheme.http, "test.cyberduck.ch", new Credentials("qloudstat", "secret")));
+    }
+
+    @Test
+    public void testGetSetupRackspace() {
+        QloudstatAnalyticsProvider q = new QloudstatAnalyticsProvider();
+        Assert.assertEquals("https://qloudstat.com/configuration/add?setup=cHJvdmlkZXI9YXV0aC5hcGkucmFja3NwYWNlY2xvdWQuY29tLHByb3RvY29sPWh0dHAsZW5kcG9pbnQ9dGVzdC5jeWJlcmR1Y2suY2gsa2V5PXFsb3Vkc3RhdCxzZWNyZXQ9c2VjcmV0",
+                q.getSetup(Protocol.CLOUDFILES, Scheme.http, "test.cyberduck.ch", new Credentials("qloudstat", "secret")));
+    }
+
+    @Test
+    public void testGetSetupCloudFrontStreaming() {
         QloudstatAnalyticsProvider q = new QloudstatAnalyticsProvider();
         Assert.assertEquals("https://qloudstat.com/configuration/add?setup=cHJvdmlkZXI9Y2xvdWRmcm9udC5hbWF6b25hd3MuY29tLHByb3RvY29sPXJ0bXAsZW5kcG9pbnQ9Y3liZXJkdWNrLXRlc3Rpbmcsa2V5PXFsb3Vkc3RhdCxzZWNyZXQ9c2VjcmV0",
                 q.getSetup(Protocol.CLOUDFRONT, Scheme.rtmp, "cyberduck-testing", new Credentials("qloudstat", "secret")));
