b1eb6ebe62c3e19f9252b11cad83b7a82109bddb
David Kocher <dkocher@iterate.ch>
2013-04-04 14:47:17 +0000

    Status message for opening data connection
    Former-commit-id: 576d0798e9ea0a4511aea8cb39dd102af623551f

--- source/ch/cyberduck/core/ftp/FTPPath.java
+++ source/ch/cyberduck/core/ftp/FTPPath.java
@@ -106,9 +106,11 @@ public class FTPPath extends Path {
         try {
             // Make sure to always configure data mode because connect event sets defaults.
             if(this.getSession().getConnectMode().equals(FTPConnectMode.PASV)) {
+                this.getSession().message(Locale.localizedString("Opening PASV data connection", "Status"));
                 this.getSession().getClient().enterLocalPassiveMode();
             }
-            if(this.getSession().getConnectMode().equals(FTPConnectMode.PORT)) {
+            else if(this.getSession().getConnectMode().equals(FTPConnectMode.PORT)) {
+                this.getSession().message(Locale.localizedString("Opening PORT data connection", "Status"));
                 this.getSession().getClient().enterLocalActiveMode();
             }
             return action.run();
