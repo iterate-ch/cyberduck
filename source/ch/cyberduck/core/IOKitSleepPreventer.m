75ece52625d45b46167244260480562ffe6f296b
David Kocher <dkocher@iterate.ch>
2013-04-23 13:35:52 +0000

    Former-commit-id: 7cc44a851b09816e1e68abc8a2aea2b960b77cb3

--- source/ch/cyberduck/core/IOKitSleepPreventer.m
+++ source/ch/cyberduck/core/IOKitSleepPreventer.m
@@ -26,7 +26,7 @@ JNIEXPORT jstring JNICALL Java_ch_cyberduck_core_IOKitSleepPreventer_createAsser
      IOPMAssertionID assertionID;
      // Prevents the system from sleeping automatically due to a lack of user activity.
     IOReturn success;
-    if (IOPMAssertionCreateWithName) {
+    if (IOPMAssertionCreateWithName != NULL) {
         success = IOPMAssertionCreateWithName(kIOPMAssertionTypePreventUserIdleSystemSleep,
                                               kIOPMAssertionLevelOn, (CFStringRef)JNFJavaToNSString(env, reason), &assertionID);
     }
