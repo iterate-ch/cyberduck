9e7d51043e8d26ee2a4dd20d077173723b910760
David Kocher <dkocher@iterate.ch>
2013-04-23 13:41:54 +0000

    Fix 10.6 compatibility
    Former-commit-id: 410a4732541a3bc732d08fd3cc71317b62fcd189

--- source/ch/cyberduck/core/IOKitSleepPreventer.m
+++ source/ch/cyberduck/core/IOKitSleepPreventer.m
@@ -27,11 +27,11 @@ JNIEXPORT jstring JNICALL Java_ch_cyberduck_core_IOKitSleepPreventer_createAsser
      // Prevents the system from sleeping automatically due to a lack of user activity.
     IOReturn success;
     if (IOPMAssertionCreateWithName != NULL) {
-        success = IOPMAssertionCreateWithName(kIOPMAssertionTypePreventUserIdleSystemSleep,
+        success = IOPMAssertionCreateWithName(kIOPMAssertionTypeNoIdleSleep,
                                               kIOPMAssertionLevelOn, (CFStringRef)JNFJavaToNSString(env, reason), &assertionID);
     }
     else {
-        success = IOPMAssertionCreate(kIOPMAssertionTypePreventUserIdleSystemSleep,
+        success = IOPMAssertionCreate(kIOPMAssertionTypeNoIdleSleep,
                                       kIOPMAssertionLevelOn, &assertionID);
     }
     if (success == kIOReturnSuccess) {
