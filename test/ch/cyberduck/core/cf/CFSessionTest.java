7e3a585aac0774eda96b688f4601f1412a8b6f69
David Kocher <dkocher@iterate.ch>
2013-06-07 15:48:35 +0000

    Add tests.
    Former-commit-id: c7921b5ca4f46460b8ac7e4785ac8aac3989a50c

---
+++ test/ch/cyberduck/core/cf/CFSessionTest.java
@@ -0,0 +1,24 @@
+package ch.cyberduck.core.cf;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class CFSessionTest extends AbstractTestCase {
+
+    @Test
+    public void testFile() {
+        final CFSession session = new CFSession(new Host(Protocol.S3_SSL, "h"));
+        assertFalse(session.isCreateFileSupported(new CFPath(session, "/", Path.VOLUME_TYPE)));
+        assertTrue(session.isCreateFileSupported(new CFPath(session, "/container", Path.VOLUME_TYPE)));
+    }
+}
---
+++ test/ch/cyberduck/core/s3/S3SessionTest.java
@@ -0,0 +1,24 @@
+package ch.cyberduck.core.s3;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class S3SessionTest extends AbstractTestCase {
+
+    @Test
+    public void testFile() {
+        final S3Session session = new S3Session(new Host(Protocol.S3_SSL, "h"));
+        assertFalse(session.isCreateFileSupported(new S3Path(session, "/", Path.VOLUME_TYPE)));
+        assertTrue(session.isCreateFileSupported(new S3Path(session, "/container", Path.VOLUME_TYPE)));
+    }
+}
