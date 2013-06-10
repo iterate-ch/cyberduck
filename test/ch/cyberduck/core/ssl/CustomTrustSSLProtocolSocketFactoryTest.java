319410102bd062ab83c331399e6311cd8917c3fb
David Kocher <dkocher@iterate.ch>
2013-06-10 14:30:09 +0000

    Add test.
    Former-commit-id: 0b22cffedb2882b72236864376e944c0c348af92

--- test/ch/cyberduck/core/ssl/CustomTrustSSLProtocolSocketFactoryTest.java
+++ test/ch/cyberduck/core/ssl/CustomTrustSSLProtocolSocketFactoryTest.java
@@ -1,13 +1,15 @@
 package ch.cyberduck.core.ssl;
 
+import ch.cyberduck.core.AbstractTestCase;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
-public class CustomTrustSSLProtocolSocketFactoryTest {
+public class CustomTrustSSLProtocolSocketFactoryTest extends AbstractTestCase {
 
     @Test
     public void testGetSSLContext() throws Exception {
