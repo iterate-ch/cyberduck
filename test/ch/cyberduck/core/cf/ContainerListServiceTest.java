943d37db73f444c91c1b89f218251e185816df36
David Kocher <dkocher@iterate.ch>
2013-06-11 16:31:05 +0000

    Add test.
    Former-commit-id: 0a0a24af0ecfb7ca43387ce361463991c867e6a7

--- test/ch/cyberduck/core/cf/ContainerListServiceTest.java
+++ test/ch/cyberduck/core/cf/ContainerListServiceTest.java
@@ -1,19 +1,44 @@
 package ch.cyberduck.core.cf;
 
 import ch.cyberduck.core.AbstractTestCase;
-import ch.cyberduck.core.*;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
 
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
+
+import com.rackspacecloud.client.cloudfiles.FilesContainer;
+import com.rackspacecloud.client.cloudfiles.FilesRegion;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class ContainerListServiceTest extends AbstractTestCase {
 
     @Test(expected = IOException.class)
+    public void testListNoCredentials() throws Exception {
+        new ContainerListService().list(new CFSession(
+                new Host(Protocol.CLOUDFILES, Protocol.CLOUDFILES.getDefaultHostname())));
+    }
+
+    @Test
     public void testList() throws Exception {
-        new ContainerListService().list(new CFSession(new Host(Protocol.CLOUDFILES, Protocol.CLOUDFILES.getDefaultHostname())));
+        final CFSession session = new CFSession(
+                new Host(Protocol.CLOUDFILES, Protocol.CLOUDFILES.getDefaultHostname(),
+                        new Credentials(
+                                properties.getProperty("rackspace.key"), properties.getProperty("rackspace.secret")
+                        )));
+        session.connect();
+        final List<FilesContainer> list = new ContainerListService().list(session);
+        final CFPath container = new CFPath(session, "test.cyberduck.ch", Path.VOLUME_TYPE);
+        assertFalse(list.contains(new FilesContainer(new FilesRegion("ORD", null, null), "test.cyberduck.ch")));
+        assertTrue(list.contains(new FilesContainer(new FilesRegion("DFW", null, null), "test.cyberduck.ch")));
     }
 }
