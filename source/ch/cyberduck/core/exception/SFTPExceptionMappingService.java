3e864630c5fe8fa96149d4b7f7d8de920086e2ac
David Kocher <dkocher@iterate.ch>
2013-06-11 21:36:32 +0000

    Add exception mapper for SFTP.
    Former-commit-id: 38a2eea22451cfa244a496117e33af400c62fc48

---
+++ source/ch/cyberduck/core/exception/SFTPExceptionMappingService.java
@@ -0,0 +1,19 @@
+package ch.cyberduck.core.exception;
+
+import java.io.IOException;
+
+import ch.ethz.ssh2.SFTPException;
+
+/**
+ * @version $Id:$
+ */
+public class SFTPExceptionMappingService extends AbstractIOExceptionMappingService<SFTPException> {
+
+    @Override
+    public IOException map(final SFTPException e) {
+        final StringBuilder buffer = new StringBuilder();
+        this.append(buffer, e.getMessage());
+        this.append(buffer, e.getServerErrorMessage());
+        return new IOException(buffer.toString(), e);
+    }
+}
