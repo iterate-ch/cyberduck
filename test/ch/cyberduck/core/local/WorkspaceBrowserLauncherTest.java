1a74419fe34cce6e2078cb6b0ff1f99745971dd0
David Kocher <dkocher@iterate.ch>
2013-06-14 14:48:51 +0000

    Ignore test.
    Former-commit-id: db2f24076333bc515eb7f1c68af072d4783db3a8

--- test/ch/cyberduck/core/local/WorkspaceBrowserLauncherTest.java
+++ test/ch/cyberduck/core/local/WorkspaceBrowserLauncherTest.java
@@ -3,13 +3,14 @@ package ch.cyberduck.core.local;
 import ch.cyberduck.core.AbstractTestCase;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class WorkspaceBrowserLauncherTest extends AbstractTestCase {
 
@@ -19,6 +20,7 @@ public class WorkspaceBrowserLauncherTest extends AbstractTestCase {
     }
 
     @Test
+    @Ignore
     public void testOpen() throws Exception {
         assertTrue(new WorkspaceBrowserLauncher().open("http://cyberduck.ch"));
         assertFalse(new WorkspaceBrowserLauncher().open(""));
