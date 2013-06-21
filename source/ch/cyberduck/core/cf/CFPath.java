79ba7a4ea5d7b2ace38dbd46e2721a163079a910
David Kocher <dkocher@iterate.ch>
2013-06-21 19:56:00 +0000

    Update copyright.
    Former-commit-id: 8a74a39d2adfc6d39abf53b00e94a2f4ba8da5cf

--- source/ch/cyberduck/core/cf/CFPath.java
+++ source/ch/cyberduck/core/cf/CFPath.java
@@ -14,8 +14,7 @@ package ch.cyberduck.core.cf;
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * Bug fixes, suggestions and comments should be sent to:
- * dkocher@cyberduck.ch
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
  */
 
 import ch.cyberduck.core.AttributedList;
