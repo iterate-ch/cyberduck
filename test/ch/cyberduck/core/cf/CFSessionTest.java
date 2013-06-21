7b555273aa4a0c775120b17113f56b33a1b40b1c
David Kocher <dkocher@iterate.ch>
2013-06-21 23:18:58 +0000

    Add connection test.
    Former-commit-id: 5b705fe7078cd8feccd8c3546212bde34c12db67

--- test/ch/cyberduck/core/cf/CFSessionTest.java
+++ test/ch/cyberduck/core/cf/CFSessionTest.java
@@ -1,19 +1,54 @@
 package ch.cyberduck.core.cf;
 
 import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledLoginController;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.Protocol;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @version $Id$
  */
 public class CFSessionTest extends AbstractTestCase {
+
+    @Test
+    public void testConnectRackspace() throws Exception {
+        final Host host = new Host(Protocol.CLOUDFILES, Protocol.CLOUDFILES.getDefaultHostname(), new Credentials(
+                properties.getProperty("rackspace.key"), properties.getProperty("rackspace.secret")
+        ));
+        final CFSession session = new CFSession(host);
+        assertNotNull(session.open());
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        session.login(new DisabledLoginController());
+        assertNotNull(session.mount());
+        assertTrue(session.isConnected());
+        session.close();
+        assertNull(session.getClient());
+        assertFalse(session.isConnected());
+    }
+
+    @Test
+    public void testConnectHp() throws Exception {
+        final Host host = new Host(Protocol.SWIFT, "region-a.geo-1.identity.hpcloudsvc.com", 35357, new Credentials(
+                properties.getProperty("hpcloud.key"), properties.getProperty("hpcloud.secret")
+        ));
+        final CFSession session = new CFSession(host);
+        assertNotNull(session.open());
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        session.login(new DisabledLoginController());
+        assertNotNull(session.mount());
+        assertTrue(session.isConnected());
+        session.close();
+        assertNull(session.getClient());
+        assertFalse(session.isConnected());
+    }
 
     @Test
     public void testFile() {
