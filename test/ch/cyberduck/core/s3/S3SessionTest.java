15e34dabaeccb003188c54a4d8f2ede7fd19e23d
David Kocher <dkocher@iterate.ch>
2013-06-21 23:11:04 +0000

    Add connection test.
    Former-commit-id: 32592280601103aab6e1428c8ee3bd6ac1205c5e

--- test/ch/cyberduck/core/s3/S3SessionTest.java
+++ test/ch/cyberduck/core/s3/S3SessionTest.java
@@ -1,14 +1,17 @@
 package ch.cyberduck.core.s3;
 
 import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledLoginController;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Preferences;
 import ch.cyberduck.core.Protocol;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @version $Id$
@@ -24,7 +27,55 @@ public class S3SessionTest extends AbstractTestCase {
 
     @Test
     public void testConnect() throws Exception {
-        final S3Session session = new S3Session(new Host(Protocol.S3_SSL, Protocol.S3_SSL.getDefaultHostname()));
+        final Host host = new Host(Protocol.S3_SSL, Protocol.S3_SSL.getDefaultHostname(), new Credentials(
+                properties.getProperty("s3.key"), properties.getProperty("s3.secret")
+        ));
+        final S3Session session = new S3Session(host);
+        assertNotNull(session.open());
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        session.login(new DisabledLoginController());
+        assertNotNull(session.mount());
+        assertTrue(session.isConnected());
+        session.close();
+        assertNull(session.getClient());
+        assertFalse(session.isConnected());
+    }
+
+    @Test
+    @Ignore
+    public void testConnectCnameAnonymous() throws Exception {
+        final Host host = new Host(Protocol.S3_SSL, "dist.springframework.org", new Credentials(
+                Preferences.instance().getProperty("connection.login.anon.name"), null
+        ));
+        final S3Session session = new S3Session(host);
+        assertNotNull(session.open());
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        session.login(new DisabledLoginController());
+        assertNotNull(session.mount());
+        assertTrue(session.isConnected());
+        session.close();
+        assertNull(session.getClient());
+        assertFalse(session.isConnected());
+    }
+
+
+    @Test
+    public void testConnectBuckenameAnonymous() throws Exception {
+        final Host host = new Host(Protocol.S3_SSL, "dist.springframework.org.s3.amazonaws.com", new Credentials(
+                Preferences.instance().getProperty("connection.login.anon.name"), null
+        ));
+        final S3Session session = new S3Session(host);
+        assertNotNull(session.open());
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        session.login(new DisabledLoginController());
+        assertNotNull(session.mount());
+        assertTrue(session.isConnected());
+        session.close();
+        assertNull(session.getClient());
+        assertFalse(session.isConnected());
     }
 
     @Test
@@ -37,11 +88,11 @@ public class S3SessionTest extends AbstractTestCase {
         assertTrue(aws.isLocationSupported());
         assertTrue(aws.isRevertSupported());
         final S3Session o = new S3Session(new Host(Protocol.S3_SSL, "o"));
-        assertFalse(aws.isVersioningSupported());
-        assertFalse(aws.isAnalyticsSupported());
-        assertTrue(aws.isChecksumSupported());
-        assertFalse(aws.isLifecycleSupported());
-        assertFalse(aws.isLocationSupported());
-        assertFalse(aws.isRevertSupported());
+        assertFalse(o.isVersioningSupported());
+        assertFalse(o.isAnalyticsSupported());
+        assertTrue(o.isChecksumSupported());
+        assertFalse(o.isLifecycleSupported());
+        assertTrue(o.isLocationSupported());
+        assertFalse(o.isRevertSupported());
     }
 }
