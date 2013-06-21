536a7e4af954c0784c0cca662cc36e24db9e910c
David Kocher <dkocher@iterate.ch>
2013-06-21 16:53:19 +0000

    Make static.
    Former-commit-id: 7fbcbc9f4e792711e6ca2312796bfe8bd6445e8b

--- source/ch/cyberduck/core/PreferencesUseragentProvider.java
+++ source/ch/cyberduck/core/PreferencesUseragentProvider.java
@@ -1,11 +1,11 @@
 package ch.cyberduck.core;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class PreferencesUseragentProvider implements UseragentProvider {
 
-    private final String ua = Preferences.instance().getProperty("application.name") + "/"
+    private static final String ua = Preferences.instance().getProperty("application.name") + "/"
             + Preferences.instance().getProperty("application.version")
             + " (" + System.getProperty("os.name") + "/" + System.getProperty("os.version") + ")"
             + " (" + System.getProperty("os.arch") + ")";
