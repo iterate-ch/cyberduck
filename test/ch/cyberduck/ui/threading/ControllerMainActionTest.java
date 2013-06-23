8f43e2a6e421a8ce59012a1ff1681ee6eb2d8f88
David Kocher <dkocher@iterate.ch>
2013-06-23 20:10:29 +0000

    Add test.
    Former-commit-id: 54aa978f5cffb8493ecd90ab60c4a729ae6b8f72

---
+++ test/ch/cyberduck/ui/threading/ControllerMainActionTest.java
@@ -0,0 +1,32 @@
+package ch.cyberduck.ui.threading;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.threading.MainAction;
+import ch.cyberduck.ui.AbstractController;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class ControllerMainActionTest extends AbstractTestCase {
+
+    @Test
+    public void testLock() throws Exception {
+        final AbstractController c = new AbstractController() {
+            @Override
+            public void invoke(final MainAction runnable, final boolean wait) {
+                //
+            }
+        };
+        assertEquals(c, new ControllerMainAction(c) {
+
+            @Override
+            public void run() {
+                //
+            }
+        }.lock());
+    }
+}
