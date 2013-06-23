305a32ad7a96a55193667049a7400e70f3a8f32d
David Kocher <dkocher@iterate.ch>
2013-06-23 19:56:04 +0000

    Add test.
    Former-commit-id: 1a9ade2215bd038aae24c2e8460aec633d3bb786

---
+++ test/ch/cyberduck/core/io/MD5ChecksumComputeTest.java
@@ -0,0 +1,19 @@
+package ch.cyberduck.core.io;
+
+import ch.cyberduck.core.AbstractTestCase;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class MD5ChecksumComputeTest extends AbstractTestCase {
+
+    @Test
+    public void testCompute() throws Exception {
+        assertEquals("a43c1b0aa53a0c908810c06ab1ff3967", new MD5ChecksumCompute().compute(IOUtils.toInputStream("input")));
+    }
+}
