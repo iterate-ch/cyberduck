f6d53bfd88e5375c715320ea0b3057689dd95d4f
David Kocher <dkocher@iterate.ch>
2013-06-25 22:34:53 +0000

    Add test to read all profiles.
    Former-commit-id: 12978d01da2ef783bbcf61e36eb7a79a133f4d87

--- test/ch/cyberduck/core/serializer/impl/ProfilePlistReaderTest.java
+++ test/ch/cyberduck/core/serializer/impl/ProfilePlistReaderTest.java
@@ -1,27 +1,46 @@
 package ch.cyberduck.core.serializer.impl;
 
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Filter;
 import ch.cyberduck.core.Profile;
+import ch.cyberduck.core.local.Local;
 import ch.cyberduck.core.local.LocalFactory;
 import ch.cyberduck.core.serializer.ProfileReaderFactory;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @version $Id$
  */
-public class ProfilePlistReaderTest extends TestCase {
+public class ProfilePlistReaderTest extends AbstractTestCase {
 
     @BeforeClass
     public static void register() {
         ProfilePlistReader.register();
     }
 
+    @Test
     public void testDeserialize() throws Exception {
         final Profile profile = ProfileReaderFactory.get().read(
                 LocalFactory.createLocal("test/ch/cyberduck/core/serializer/impl/Dropbox.cyberduckprofile")
         );
         assertFalse(profile.isEnabled());
+    }
+
+    @Test
+    public void testAll() throws Exception {
+        for(Local l : LocalFactory.createLocal("profiles").list().filter(null, new Filter<Local>() {
+            @Override
+            public boolean accept(final Local file) {
+                return file.getName().endsWith(".cyberduckprofile");
+            }
+        })) {
+            final Profile profile = ProfileReaderFactory.get().read(l);
+            assertNotNull(profile);
+        }
     }
 }
