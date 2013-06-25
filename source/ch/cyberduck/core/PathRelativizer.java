580e78ae34f2441d9b0bdce4c63f2e9df59a5e18
David Kocher <dkocher@iterate.ch>
2013-06-25 14:59:55 +0000

    Change implementation.
    Former-commit-id: ef92a237fcd8443c3998489aa513b62f1cc90d0e

--- source/ch/cyberduck/core/PathRelativizer.java
+++ source/ch/cyberduck/core/PathRelativizer.java
@@ -3,7 +3,7 @@ package ch.cyberduck.core;
 import org.apache.commons.lang.StringUtils;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public final class PathRelativizer {
 
@@ -12,8 +12,8 @@ public final class PathRelativizer {
     }
 
     public static String relativize(final String root, String path) {
-        if(StringUtils.isNotBlank(root)) {
-            return StringUtils.removeStart(path, root);
+        if(StringUtils.contains(path, root)) {
+            return path.substring(path.indexOf(root) + root.length());
         }
         return path;
     }
