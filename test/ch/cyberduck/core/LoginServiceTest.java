ca936bb281ce422719c772bd34eaa0d7f9682fdc
David Kocher <dkocher@iterate.ch>
2013-06-25 16:25:33 +0000

    Add test for unsecure connection warning.
    Former-commit-id: c131bc5e51f6cdda49d610f1d76c89970469cf2c

--- test/ch/cyberduck/core/LoginServiceTest.java
+++ test/ch/cyberduck/core/LoginServiceTest.java
@@ -1,9 +1,16 @@
 package ch.cyberduck.core;
 
+import ch.cyberduck.core.ftp.FTPSession;
+import ch.cyberduck.core.threading.BackgroundException;
+
 import org.junit.Test;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.*;
+
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class LoginServiceTest extends AbstractTestCase {
 
@@ -11,5 +18,30 @@ public class LoginServiceTest extends AbstractTestCase {
     public void testCancel() throws Exception {
         LoginService l = new LoginService(new DisabledLoginController());
         l.login(new NullSession(new Host(Protocol.FTP, "h")));
+    }
+
+    @Test(expected = LoginCanceledException.class)
+    public void testConnectionWarning() throws Exception {
+        final Host host = new Host(Protocol.FTP, "test.cyberduck.ch", new Credentials(
+                "u", "p"
+        ));
+        final AtomicBoolean warned = new AtomicBoolean(false);
+        final FTPSession session = new FTPSession(host) {
+            @Override
+            protected void warn(final LoginController login) throws BackgroundException {
+                warned.set(true);
+                throw new LoginCanceledException();
+            }
+        };
+        session.open();
+        LoginService l = new LoginService(new DisabledLoginController());
+        try {
+            l.login(session);
+            fail();
+        }
+        catch(LoginCanceledException e) {
+            assertEquals("Unknown", e.getMessage());
+        }
+        assertTrue(warned.get());
     }
 }
