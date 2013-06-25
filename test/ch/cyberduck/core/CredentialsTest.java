04f8a3bffc98d42640691ad83f316603eb1d8a1a
David Kocher <dkocher@iterate.ch>
2013-06-25 16:36:51 +0000

    Adjust test.
    Former-commit-id: f8b98a7664466882ef3e01bb8db4c3ff7f7387ff

--- test/ch/cyberduck/core/CredentialsTest.java
+++ test/ch/cyberduck/core/CredentialsTest.java
@@ -48,7 +48,7 @@ public class CredentialsTest extends AbstractTestCase {
         Credentials c = new DefaultCredentials("user", "");
         assertTrue(c.validate(Protocol.FTP));
         assertFalse(c.validate(Protocol.WEBDAV));
-        assertFalse(c.validate(Protocol.SFTP));
+        assertTrue(c.validate(Protocol.SFTP));
     }
 
     @Test
