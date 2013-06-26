40491ed09a222e3914572c01270e65317e805441
David Kocher <dkocher@iterate.ch>
2013-06-26 08:08:28 +0000

    Add test.
    Former-commit-id: 87c6321b7e43e5e7181be2a94425d029b0be9efa

--- test/ch/cyberduck/core/DescriptiveUrlTest.java
+++ test/ch/cyberduck/core/DescriptiveUrlTest.java
@@ -2,15 +2,17 @@ package ch.cyberduck.core;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class DescriptiveUrlTest {
 
     @Test
     public void testEquals() throws Exception {
         assertTrue(new DescriptiveUrl("http://host.domain", "a").equals(new DescriptiveUrl("http://host.domain", "b")));
+        assertFalse(new DescriptiveUrl("http://host.domainb", "a").equals(new DescriptiveUrl("http://host.domain", "b")));
     }
 }
