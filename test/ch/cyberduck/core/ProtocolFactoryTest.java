8c379aa733af764c50a8cefb954777cddc98b5b0
David Kocher <dkocher@iterate.ch>
2013-06-26 12:19:04 +0000

    Add test.
    Former-commit-id: 679c8033256f2e80c2937050bcd042f5ba97c2bd

--- test/ch/cyberduck/core/ProtocolFactoryTest.java
+++ test/ch/cyberduck/core/ProtocolFactoryTest.java
@@ -3,14 +3,24 @@ package ch.cyberduck.core;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class ProtocolFactoryTest extends AbstractTestCase {
 
     @Test
     public void testRegister() throws Exception {
         assertFalse(ProtocolFactory.getKnownProtocols().isEmpty());
+    }
+
+    @Test
+    public void testUrl() throws Exception {
+        assertTrue(ProtocolFactory.isURL("ftp://h.name"));
+        assertTrue(ProtocolFactory.isURL("ftps://h.name"));
+        assertTrue(ProtocolFactory.isURL("sftp://h.name"));
+        assertTrue(ProtocolFactory.isURL("http://h.name"));
+        assertFalse(ProtocolFactory.isURL("h.name"));
     }
 }
