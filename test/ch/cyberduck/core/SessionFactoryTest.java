dab0b7a6fb3cae2d4a72faa64263cf021a80e1b6
David Kocher <dkocher@iterate.ch>
2013-06-26 12:40:44 +0000

    Add test.
    Former-commit-id: 87e9ef142d50968be0f3ee4b0bb7ae27b16c18c9

--- test/ch/cyberduck/core/SessionFactoryTest.java
+++ test/ch/cyberduck/core/SessionFactoryTest.java
@@ -2,13 +2,17 @@ package ch.cyberduck.core;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+
 /**
- * @version $Id:$
+ * @version $Id$
  */
-public class SessionFactoryTest {
+public class SessionFactoryTest extends AbstractTestCase {
 
     @Test
     public void testCreateSession() throws Exception {
-
+        for(Protocol protocol : ProtocolFactory.getKnownProtocols()) {
+            assertNotNull(SessionFactory.createSession(new Host(protocol, "h")));
+        }
     }
 }
