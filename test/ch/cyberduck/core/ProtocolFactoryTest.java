cb3dcb400247d0998621e85b39aad98fb0db0da1
David Kocher <dkocher@iterate.ch>
2013-06-26 12:22:40 +0000

    Add tests.
    Former-commit-id: c7dc14d3ca22700d16b0a634c25b21407bf5903d

--- test/ch/cyberduck/core/ProtocolFactoryTest.java
+++ test/ch/cyberduck/core/ProtocolFactoryTest.java
@@ -2,8 +2,7 @@ package ch.cyberduck.core;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @version $Id$
@@ -13,6 +12,22 @@ public class ProtocolFactoryTest extends AbstractTestCase {
     @Test
     public void testRegister() throws Exception {
         assertFalse(ProtocolFactory.getKnownProtocols().isEmpty());
+    }
+
+    @Test
+    public void testForName() throws Exception {
+        assertEquals(Protocol.FTP, ProtocolFactory.forName("ftp"));
+        assertEquals(Protocol.SFTP, ProtocolFactory.forName("sftp"));
+        assertEquals(Protocol.SWIFT, ProtocolFactory.forName("swift"));
+        assertEquals(Protocol.SFTP, ProtocolFactory.forName(String.valueOf(Protocol.SFTP.hashCode())));
+    }
+
+    @Test
+    public void testForScheme() throws Exception {
+        assertEquals(Protocol.WEBDAV, ProtocolFactory.forScheme("http"));
+        assertEquals(Protocol.WEBDAV_SSL, ProtocolFactory.forScheme("https"));
+        assertEquals(Protocol.FTP, ProtocolFactory.forScheme("ftp"));
+        assertEquals(Protocol.FTP_TLS, ProtocolFactory.forScheme("ftps"));
     }
 
     @Test
