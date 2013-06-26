7a1874e0308b0669ecd217e681ea60bd5d90bb65
David Kocher <dkocher@iterate.ch>
2013-06-26 08:31:46 +0000

    Former-commit-id: 1ce3579b61237f44b71a009ff9f13878778c1717

--- source/ch/cyberduck/core/DisabledLoginController.java
+++ source/ch/cyberduck/core/DisabledLoginController.java
@@ -1,9 +1,14 @@
 package ch.cyberduck.core;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class DisabledLoginController extends AbstractLoginController {
+
+    @Override
+    public void warn(final String title, final String message, final String continueButton, final String disconnectButton, final String preference) throws LoginCanceledException {
+        throw new LoginCanceledException();
+    }
 
     @Override
     public void prompt(final Protocol protocol, final Credentials credentials, final String title, final String reason,
