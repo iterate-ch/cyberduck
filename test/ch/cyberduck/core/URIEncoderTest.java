ec1075a803930bc59fd0c3f4eab0d3125243520a
David Kocher <dkocher@iterate.ch>
2013-06-26 12:29:27 +0000

    Add test.
    Former-commit-id: 6d97da9797fe265fde9436385a03f119c5fe7f07

---
+++ test/ch/cyberduck/core/URIEncoderTest.java
@@ -0,0 +1,17 @@
+package ch.cyberduck.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class URIEncoderTest {
+
+    @Test
+    public void testEncode() throws Exception {
+        assertEquals("/p", URIEncoder.encode("/p"));
+        assertEquals("/p%20d", URIEncoder.encode("/p d"));
+    }
+}
