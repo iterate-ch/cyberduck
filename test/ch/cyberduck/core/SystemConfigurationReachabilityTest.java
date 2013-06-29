60549dbb3650ceeea08adf1d2c2ac9c2baa72ebc
David Kocher <dkocher@iterate.ch>
2013-06-29 11:16:16 +0000

    Testability.
    Former-commit-id: bf18173adc3590c9d99a73ea49f2aa5307823f33

--- test/ch/cyberduck/core/SystemConfigurationReachabilityTest.java
+++ test/ch/cyberduck/core/SystemConfigurationReachabilityTest.java
@@ -1,6 +1,5 @@
 package ch.cyberduck.core;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -11,20 +10,16 @@ import static org.junit.Assert.assertTrue;
  */
 public class SystemConfigurationReachabilityTest extends AbstractTestCase {
 
-    @BeforeClass
-    public static void register() {
-        SystemConfigurationReachability.register();
-    }
-
     @Test
     public void testIsReachable() throws Exception {
-        assertTrue(ReachabilityFactory.get().isReachable(
+        final Reachability r = new SystemConfigurationReachability();
+        assertTrue(r.isReachable(
                 new Host("cyberduck.ch", 80)
         ));
-        assertTrue(ReachabilityFactory.get().isReachable(
+        assertTrue(r.isReachable(
                 new Host("cyberduck.ch", 22)
         ));
-        assertFalse(ReachabilityFactory.get().isReachable(
+        assertFalse(r.isReachable(
                 new Host("a.cyberduck.ch", 22)
         ));
     }
