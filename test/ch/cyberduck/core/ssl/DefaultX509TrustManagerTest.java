f56442b404ce504ec3e60115afd9edb2dee05041
David Kocher <dkocher@iterate.ch>
2013-06-29 12:45:15 +0000

    Enable test.
    Former-commit-id: 3d96b52eeeb7b026c3e72329b5614ac00f53ae36

--- test/ch/cyberduck/core/ssl/DefaultX509TrustManagerTest.java
+++ test/ch/cyberduck/core/ssl/DefaultX509TrustManagerTest.java
@@ -2,6 +2,8 @@ package ch.cyberduck.core.ssl;
 
 import ch.cyberduck.core.AbstractTestCase;
 
+import org.junit.Test;
+
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.security.cert.CertificateFactory;
@@ -12,6 +14,7 @@ import java.security.cert.X509Certificate;
  */
 public class DefaultX509TrustManagerTest extends AbstractTestCase {
 
+    @Test
     public void testCheckServerTrusted() throws Exception {
         final DefaultX509TrustManager m = new DefaultX509TrustManager();
         InputStream inStream = new FileInputStream("test/ch/cyberduck/core/ssl/OXxlRDVcWqdPEvFm.cer");
