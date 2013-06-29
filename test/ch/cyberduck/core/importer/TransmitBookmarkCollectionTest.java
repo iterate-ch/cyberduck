dfcce3f3de65dc15f3fe08042bd286f28438ae69
David Kocher <dkocher@iterate.ch>
2013-06-29 11:19:43 +0000

    Testability.
    Former-commit-id: f753a7c18e066736c72c9f9a609f485423638589

--- test/ch/cyberduck/core/importer/TransmitBookmarkCollectionTest.java
+++ test/ch/cyberduck/core/importer/TransmitBookmarkCollectionTest.java
@@ -1,11 +1,8 @@
 package ch.cyberduck.core.importer;
 
 import ch.cyberduck.core.AbstractTestCase;
-import ch.cyberduck.core.NullKeychain;
-import ch.cyberduck.core.local.LaunchServicesApplicationFinder;
 import ch.cyberduck.core.local.LocalFactory;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -14,12 +11,6 @@ import static org.junit.Assert.assertEquals;
  * @version $Id$
  */
 public class TransmitBookmarkCollectionTest extends AbstractTestCase {
-
-    @BeforeClass
-    public static void register() {
-        LaunchServicesApplicationFinder.register();
-        NullKeychain.register();
-    }
 
     @Test
     public void testParse() throws Exception {
