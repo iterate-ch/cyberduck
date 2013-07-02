6aef67e41f98b003b6dbdae01702101a46e183b1
David Kocher <dkocher@iterate.ch>
2013-07-02 10:33:07 +0000

    Add test.
    Former-commit-id: 073f4695a6743eedeb5d6c742a1528bf7493cf8b

---
+++ test/ch/cyberduck/core/gstorage/GSSessionTest.java
@@ -0,0 +1,57 @@
+package ch.cyberduck.core.gstorage;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.LoginCanceledException;
+import ch.cyberduck.core.LoginOptions;
+import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.exception.LoginFailureException;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @version $Id:$
+ */
+public class GSSessionTest extends AbstractTestCase {
+
+    @Test(expected = LoginFailureException.class)
+    public void testConnectRackspace() throws Exception {
+        final Host host = new Host(Protocol.GOOGLESTORAGE_SSL, Protocol.GOOGLESTORAGE_SSL.getDefaultHostname(), new Credentials(
+                properties.getProperty("googlestorage.key"), properties.getProperty("googlestorage.secret")
+        ));
+        final GSSession session = new GSSession(host);
+        assertNotNull(session.open());
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController() {
+            @Override
+            public void prompt(final Protocol protocol, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                assertEquals("OAuth2 Authentication", title);
+                throw new LoginCanceledException();
+            }
+        });
+    }
+
+    @Test(expected = LoginFailureException.class)
+    public void testLoginFailure() throws Exception {
+        final Host host = new Host(Protocol.GOOGLESTORAGE_SSL, Protocol.GOOGLESTORAGE_SSL.getDefaultHostname(), new Credentials(
+                "a", "s"
+        ));
+        final GSSession session = new GSSession(host);
+        assertNotNull(session.open());
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        try {
+            session.login(new DisabledPasswordStore(), new DisabledLoginController());
+            fail();
+        }
+        catch(LoginFailureException e) {
+            throw e;
+        }
+    }
+}
