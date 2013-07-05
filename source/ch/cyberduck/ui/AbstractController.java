7d63df240bc86b0e66295395c6b4b3b6ed3856b4
David Kocher <dkocher@iterate.ch>
2013-07-05 10:10:17 +0000

    Catch all.
    Former-commit-id: b2a6abf9a0de43ff0069a49d6aa8648157dc2094

--- source/ch/cyberduck/ui/AbstractController.java
+++ source/ch/cyberduck/ui/AbstractController.java
@@ -113,6 +113,9 @@ public abstract class AbstractController implements Controller {
                     catch(BackgroundException e) {
                         log.error(String.format("Unhandled exception running background task %s", e.getMessage()), e);
                     }
+                    catch(Exception e) {
+                        log.fatal(String.format("Unhandled exception running background task %s", e.getMessage()), e);
+                    }
                     finally {
                         // Increase the run counter
                         try {
@@ -123,6 +126,9 @@ public abstract class AbstractController implements Controller {
                         }
                         catch(BackgroundException e) {
                             log.error(String.format("Unhandled exception running background task %s", e.getMessage()), e);
+                        }
+                        catch(Exception e) {
+                            log.fatal(String.format("Unhandled exception running background task %s", e.getMessage()), e);
                         }
                         // Invoke the cleanup on the main thread to let the action synchronize the user interface
                         invoke(new ControllerMainAction(AbstractController.this) {
