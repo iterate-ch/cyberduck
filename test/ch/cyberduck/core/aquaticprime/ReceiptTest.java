02002a8fc4f0cfe9f5cb2ae02017812b0d86faa0
David Kocher <dkocher@iterate.ch>
2013-07-06 21:49:06 +0000

    Testability.
    Former-commit-id: 30a25352c0f47ee885459f2329fe8dc07a8be664

--- test/ch/cyberduck/core/aquaticprime/ReceiptTest.java
+++ test/ch/cyberduck/core/aquaticprime/ReceiptTest.java
@@ -14,14 +14,14 @@ public class ReceiptTest extends AbstractTestCase {
 
     @Test
     public void testVerify() throws Exception {
-        Receipt r = new Receipt(LocalFactory.createLocal("/Applications/Cyberduck.app/Contents/_MASReceipt/receipt"));
+        Receipt r = new Receipt(LocalFactory.createLocal("test/ch/cyberduck/core/aquaticprime/receipt"));
         assertTrue(r.verify());
-        assertEquals("c42c030b8670", r.getName());
+        assertEquals("040ccee30d02", r.getName());
     }
 
     @Test
     public void testVerifyFailure() throws Exception {
-        Receipt r = new Receipt(LocalFactory.createLocal("/Applications/Cyberduck.app/Contents/Info.plist"));
+        Receipt r = new Receipt(LocalFactory.createLocal("test/ch/cyberduck/core/aquaticprime/Info.plist"));
         assertFalse(r.verify());
         assertEquals("Unknown", r.getName());
     }
