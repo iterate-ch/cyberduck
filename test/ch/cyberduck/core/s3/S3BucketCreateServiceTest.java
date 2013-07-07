49f8757e492e95b83ede2968088a4909d98d1b34
David Kocher <dkocher@iterate.ch>
2013-07-07 17:00:11 +0000

    Add test.
    Former-commit-id: 6d98399660fdd6a08a683596fab565cfe5beed94

---
+++ test/ch/cyberduck/core/s3/S3BucketCreateServiceTest.java
@@ -0,0 +1,52 @@
+package ch.cyberduck.core.s3;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class S3BucketCreateServiceTest extends AbstractTestCase {
+
+    @Test
+    public void testCreate() throws Exception {
+        final S3Session session = new S3Session(
+                new Host(Protocol.S3_SSL, Protocol.S3_SSL.getDefaultHostname(),
+                        new Credentials(
+                                properties.getProperty("s3.key"), properties.getProperty("s3.secret")
+                        )));
+        assertNotNull(session.open(new DefaultHostKeyController()));
+        final S3Path bucket = new S3Path(UUID.randomUUID().toString(), Path.DIRECTORY_TYPE);
+        new S3BucketCreateService(session).create(
+                bucket);
+        assertTrue(session.exists(bucket));
+    }
+}
