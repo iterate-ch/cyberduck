06d04cf727fae9a574de01240821ddaf7940413b
David Kocher <dkocher@iterate.ch>
2013-07-08 15:42:18 +0000

    Add test.
    Former-commit-id: f3423eaa51c6246f51ff35eba9e9463560056190

---
+++ test/ch/cyberduck/core/sftp/SFTPCommandFeatureTest.java
@@ -0,0 +1,63 @@
+package ch.cyberduck.core.sftp;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.TranscriptListener;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class SFTPCommandFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testSend() throws Exception {
+        final Host host = new Host(Protocol.SFTP, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        assertNotNull(session.open(new DefaultHostKeyController()));
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final StringBuilder t = new StringBuilder();
+        session.addTranscriptListener(new TranscriptListener() {
+            @Override
+            public void log(final boolean request, final String message) {
+                if(!request) {
+                    t.append(message);
+                }
+            }
+        });
+        new SFTPCommandFeature(session).send("ps");
+        assertTrue("PID TTY          TIME CMD22417 ?        00:00:00 sshd22418 ?        00:00:00 sftp-server22427 ?        00:00:00 ps", t.toString().startsWith(
+                "  PID TTY          TIME"));
+        session.disconnect();
+    }
+}
