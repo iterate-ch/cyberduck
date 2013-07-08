b6e6ba4ad9b94275877c7c084a0aff62d2783f1e
David Kocher <dkocher@iterate.ch>
2013-07-08 17:27:55 +0000

    Add test.
    Former-commit-id: 4de2e855768b86f2a0cc99591a40a8d9e1982264

---
+++ test/ch/cyberduck/core/ftp/FTPCommandFeatureTest.java
@@ -0,0 +1,61 @@
+package ch.cyberduck.core.ftp;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.TranscriptListener;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @version $Id:$
+ */
+public class FTPCommandFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testSend() throws Exception {
+        final Host host = new Host(Protocol.FTP_TLS, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        assertNotNull(session.open(new DefaultHostKeyController()));
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final StringBuilder t = new StringBuilder();
+        session.addTranscriptListener(new TranscriptListener() {
+            @Override
+            public void log(final boolean request, final String message) {
+                if(!request) {
+                    t.append(message);
+                }
+            }
+        });
+        new FTPCommandFeature(session).send("HELP");
+        assertEquals("214 CHMOD UMASK HELP", t.toString());
+        session.close();
+    }
+}
