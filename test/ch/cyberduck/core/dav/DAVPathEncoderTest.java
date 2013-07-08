5d292b0fe6457b583dd512cb8722c8bebf42c358
David Kocher <dkocher@iterate.ch>
2013-07-08 17:23:02 +0000

    Add test.
    Former-commit-id: ae1196b376f4fd7831223947b42d26944a987343

---
+++ test/ch/cyberduck/core/dav/DAVPathEncoderTest.java
@@ -0,0 +1,38 @@
+package ch.cyberduck.core.dav;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Path;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class DAVPathEncoderTest extends AbstractTestCase {
+
+    @Test
+    public void testEncode() throws Exception {
+        assertEquals("/", new DAVPathEncoder().encode(new Path("/", Path.DIRECTORY_TYPE)));
+        assertEquals("/dav/", new DAVPathEncoder().encode(new Path("/dav", Path.DIRECTORY_TYPE)));
+        assertEquals("/dav", new DAVPathEncoder().encode(new Path("/dav", Path.FILE_TYPE)));
+    }
+}
