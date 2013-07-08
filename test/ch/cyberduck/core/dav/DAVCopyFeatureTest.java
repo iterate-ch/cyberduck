f2ae0dae3a7f78b918dff8a62b232eeea1318853
David Kocher <dkocher@iterate.ch>
2013-07-08 17:52:49 +0000

    Add test.
    Former-commit-id: a3066f5014afc2e1472104076e21400452647d1f

---
+++ test/ch/cyberduck/core/dav/DAVCopyFeatureTest.java
@@ -0,0 +1,57 @@
+package ch.cyberduck.core.dav;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class DAVCopyFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testCopy() throws Exception {
+        final Host host = new Host(Protocol.WEBDAV, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("webdav.user"), properties.getProperty("webdav.password")
+        ));
+        host.setDefaultPath("/dav/basic");
+        final DAVSession session = new DAVSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        session.touch(test);
+        final Path copy = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DAVCopyFeature(session).copy(test, copy);
+        assertTrue(session.exists(test));
+        assertTrue(session.exists(copy));
+        session.close();
+    }
+}
