6dbf4578279c2b84ce4dae81f25a6b8b99868d56
David Kocher <dkocher@iterate.ch>
2013-07-10 14:02:10 +0000

    Add tests.
    Former-commit-id: d0f7a51610ed02bf2c791bf0edaefcf526285165

--- test/ch/cyberduck/core/cdn/DistributionTest.java
+++ test/ch/cyberduck/core/cdn/DistributionTest.java
@@ -1,6 +1,7 @@
 package ch.cyberduck.core.cdn;
 
 import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.FactoryException;
 
 import org.junit.Test;
 
@@ -8,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class DistributionTest extends AbstractTestCase {
 
@@ -17,5 +18,19 @@ public class DistributionTest extends AbstractTestCase {
         assertEquals(new Distribution("o", Distribution.DOWNLOAD), new Distribution("o", Distribution.DOWNLOAD));
         assertFalse(new Distribution("a", Distribution.DOWNLOAD).equals(new Distribution("o", Distribution.DOWNLOAD)));
         assertFalse(new Distribution("o", Distribution.DOWNLOAD).equals(new Distribution("o", Distribution.STREAMING)));
+    }
+
+    @Test
+    public void testMethods() throws Exception {
+        assertEquals(Distribution.DOWNLOAD, Distribution.Method.forName(Distribution.DOWNLOAD.toString()));
+        assertEquals(Distribution.CUSTOM, Distribution.Method.forName(Distribution.CUSTOM.toString()));
+        assertEquals(Distribution.STREAMING, Distribution.Method.forName(Distribution.STREAMING.toString()));
+        assertEquals(Distribution.WEBSITE, Distribution.Method.forName(Distribution.WEBSITE.toString()));
+        assertEquals(Distribution.WEBSITE_CDN, Distribution.Method.forName(Distribution.WEBSITE_CDN.toString()));
+    }
+
+    @Test(expected = FactoryException.class)
+    public void testMethodInvalid() throws Exception {
+        Distribution.Method.forName("i");
     }
 }
