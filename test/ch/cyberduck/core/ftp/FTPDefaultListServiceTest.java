22b94f4faccaff7d89dd2be4ba8a1d3d013c984f
David Kocher <dkocher@iterate.ch>
2013-07-13 08:15:05 +0000

    Add tests.
    Former-commit-id: 52e377e6a6b1e18784ca45b8e761cbab32ef944b

---
+++ test/ch/cyberduck/core/ftp/FTPDefaultListServiceTest.java
@@ -0,0 +1,72 @@
+package ch.cyberduck.core.ftp;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.ListService;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.ftp.parser.CompositeFileEntryParser;
+
+import org.apache.commons.net.ftp.parser.UnixFTPEntryParser;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class FTPDefaultListServiceTest extends AbstractTestCase {
+
+    @Test
+    public void testListDefault() throws Exception {
+        final Host host = new Host(Protocol.FTP_TLS, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final ListService list = new FTPDefaultListService(session,
+                new CompositeFileEntryParser(Arrays.asList(new UnixFTPEntryParser())),
+                FTPListService.Command.list);
+        final Path directory = session.workdir();
+        assertTrue(list.list(directory).contains(new Path(directory, "test", Path.FILE_TYPE).getReference()));
+    }
+
+    @Test
+    public void testListDefaultFlag() throws Exception {
+        final Host host = new Host(Protocol.FTP_TLS, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final ListService list = new FTPDefaultListService(session,
+                new CompositeFileEntryParser(Arrays.asList(new UnixFTPEntryParser())),
+                FTPListService.Command.lista);
+        final Path directory = session.workdir();
+        assertTrue(list.list(directory).contains(new Path(directory, "test", Path.FILE_TYPE).getReference()));
+    }
+}
