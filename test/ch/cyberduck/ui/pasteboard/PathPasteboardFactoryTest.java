e6999c1237f3c78e371edfb0dc0eb55fc9c416c5
David Kocher <dkocher@iterate.ch>
2013-07-14 16:00:26 +0000

    Former-commit-id: 1744f0fb68930babb17d04dc4f10429c23db2815

--- test/ch/cyberduck/ui/pasteboard/PathPasteboardFactoryTest.java
+++ test/ch/cyberduck/ui/pasteboard/PathPasteboardFactoryTest.java
@@ -2,21 +2,22 @@ package ch.cyberduck.ui.pasteboard;
 
 import ch.cyberduck.core.AbstractTestCase;
 import ch.cyberduck.core.Host;
-import ch.cyberduck.core.NullSession;
 import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.Session;
+import ch.cyberduck.core.ftp.FTPSession;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class PathPasteboardFactoryTest extends AbstractTestCase {
 
     @Test
     public void testGetPasteboard() throws Exception {
-        final NullSession s = new NullSession(new Host(Protocol.SFTP, "l"));
+        final Session s = new FTPSession(new Host(Protocol.SFTP, "l"));
         final PathPasteboard pasteboard = PathPasteboardFactory.getPasteboard(s);
         assertNotNull(pasteboard);
         assertEquals(pasteboard, PathPasteboardFactory.getPasteboard(s));
