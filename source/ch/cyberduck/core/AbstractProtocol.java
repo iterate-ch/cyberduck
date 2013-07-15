675c33a9ef0ecaa1a8d6b0568ecbbb69c4fd93f7
David Kocher <dkocher@iterate.ch>
2013-07-15 22:35:15 +0000

    Former-commit-id: 0adcb8ae49d47b3856bf4ec6e12738faec2841b3

--- source/ch/cyberduck/core/AbstractProtocol.java
+++ source/ch/cyberduck/core/AbstractProtocol.java
@@ -23,6 +23,7 @@ import ch.cyberduck.core.i18n.Locale;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -33,6 +34,11 @@ public abstract class AbstractProtocol implements Protocol {
     @Override
     public String getProvider() {
         return this.getIdentifier();
+    }
+
+    @Override
+    public Set<String> getProviders() {
+        return new HashSet<String>(Collections.singletonList(this.getProvider()));
     }
 
     @Override
