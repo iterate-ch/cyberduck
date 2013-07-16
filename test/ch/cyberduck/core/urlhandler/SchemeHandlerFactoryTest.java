972a601e84499b638da3c58824aac1f4db32e4c7
David Kocher <dkocher@iterate.ch>
2013-07-16 11:06:20 +0000

    Add test.
    Former-commit-id: c89565856b01aaa969fdc643299751c2c03b0b54

---
+++ test/ch/cyberduck/core/urlhandler/SchemeHandlerFactoryTest.java
@@ -0,0 +1,19 @@
+package ch.cyberduck.core.urlhandler;
+
+import ch.cyberduck.core.AbstractTestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @version $Id:$
+ */
+public class SchemeHandlerFactoryTest extends AbstractTestCase {
+
+    @Test
+    public void testGet() throws Exception {
+        assertNotNull(SchemeHandlerFactory.get());
+    }
+}
