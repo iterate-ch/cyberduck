b73326b940efd549cad2cd59b7ad154b73770caa
David Kocher <dkocher@iterate.ch>
2013-07-18 12:42:38 +0000

    Former-commit-id: 8322af4a95a78f8147cfb5a641d4dc28a0b79140

--- source/ch/cyberduck/core/Profile.java
+++ source/ch/cyberduck/core/Profile.java
@@ -93,7 +93,7 @@ public final class Profile implements Protocol, Serializable {
 
     @Override
     public boolean isSecure() {
-        throw new UnsupportedOperationException();
+        return parent.isSecure();
     }
 
     private String getValue(final String key) {
