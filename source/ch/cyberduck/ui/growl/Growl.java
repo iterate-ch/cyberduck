d9b329e488c59eb85e762dce1b1f1bcffe3d02de
David Kocher <dkocher@iterate.ch>
2013-07-19 08:31:40 +0000

    Former-commit-id: 8b6639adb1950d4e6574b1e959e87bc356d1fa0f

--- source/ch/cyberduck/ui/growl/Growl.java
+++ source/ch/cyberduck/ui/growl/Growl.java
@@ -28,7 +28,16 @@ public interface Growl {
      */
     void setup();
 
+    /**
+     * @param title       Non localized title to be looked up in status table
+     * @param description Hostname
+     */
     void notify(String title, String description);
 
+    /**
+     * @param title       Non localized title to be looked up in status table
+     * @param description Hostname
+     * @param image       Custom icon instead of application icon
+     */
     void notifyWithImage(String title, String description, String image);
 }
