b176b708ba9440686aced6768b6e8e20810088e7
David Kocher <dkocher@iterate.ch>
2013-07-24 16:52:08 +0000

    Add tests.
    Former-commit-id: 5bbdcf8251104d95db54f4836786b1bd3c33a999

---
+++ test/ch/cyberduck/core/sftp/SFTPNoneAuthenticationTest.java
@@ -0,0 +1,45 @@
+package ch.cyberduck.core.sftp;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Protocol;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+/**
+ * @version $Id:$
+ */
+public class SFTPNoneAuthenticationTest extends AbstractTestCase {
+
+    @Test
+    public void testAuthenticate() throws Exception {
+        final Host host = new Host(Protocol.SFTP, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        session.open(new DefaultHostKeyController());
+        assertFalse(new SFTPNoneAuthentication(session).authenticate(host, new DisabledLoginController()));
+    }
+}
