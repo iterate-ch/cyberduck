81e20c9e336302d2f0dfd7a15a7c5215ffed0df1
David Kocher <dkocher@iterate.ch>
2013-07-28 21:04:58 +0000

    Former-commit-id: 442fd5edb88ca1a47dbf7f11a1221652266bfa26

--- test/ch/cyberduck/core/sftp/SFTPSessionTest.java
+++ test/ch/cyberduck/core/sftp/SFTPSessionTest.java
@@ -10,13 +10,9 @@ import ch.cyberduck.core.features.Symlink;
 import ch.cyberduck.core.features.Timestamp;
 import ch.cyberduck.core.features.Touch;
 import ch.cyberduck.core.features.UnixPermission;
-import ch.cyberduck.core.transfer.TransferStatus;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
@@ -95,33 +91,6 @@ public class SFTPSessionTest extends AbstractTestCase {
         final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.DIRECTORY_TYPE);
         session.mkdir(test, null);
         assertTrue(session.exists(test));
-        session.delete(test, new DisabledLoginController());
-        session.close();
-    }
-
-    @Test
-    public void testReadWrite() throws Exception {
-        final Host host = new Host(Protocol.SFTP, "test.cyberduck.ch", new Credentials(
-                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
-        ));
-        final SFTPSession session = new SFTPSession(host);
-        session.open(new DefaultHostKeyController());
-        session.login(new DisabledPasswordStore(), new DisabledLoginController());
-        final TransferStatus status = new TransferStatus();
-        final byte[] content = "test".getBytes("UTF-8");
-        status.setLength(content.length);
-        final Path test = new Path(session.mount(new DisabledListProgressListener()), UUID.randomUUID().toString(), Path.FILE_TYPE);
-        final OutputStream out = session.write(test, status);
-        assertNotNull(out);
-        IOUtils.write(content, out);
-        IOUtils.closeQuietly(out);
-        assertTrue(session.exists(test));
-        assertEquals(content.length, session.list(test.getParent(), new DisabledListProgressListener()).get(test.getReference()).attributes().getSize());
-        final byte[] buffer = new byte[content.length];
-        final InputStream in = session.read(test, new TransferStatus());
-        IOUtils.readFully(in, buffer);
-        IOUtils.closeQuietly(in);
-        assertArrayEquals(content, buffer);
         session.delete(test, new DisabledLoginController());
         session.close();
     }
