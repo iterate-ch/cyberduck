57b628d67dc791adbd06f3bb6367d8153696d1eb
David Kocher <dkocher@iterate.ch>
2013-07-28 23:42:04 +0000

    Former-commit-id: 4a0285f6ccd5203aa536a6d9ed2ca4bfe5a34ef1

--- test/ch/cyberduck/core/sftp/SFTPCompressFeatureTest.java
+++ test/ch/cyberduck/core/sftp/SFTPCompressFeatureTest.java
@@ -36,7 +36,7 @@ public class SFTPCompressFeatureTest extends AbstractTestCase {
                 }
             });
             assertTrue(session.exists(archive.getArchive(Collections.<Path>singletonList(test))));
-            session.delete(Collections.singletonList(test));
+            new SFTPDeleteFeature(session).delete(Collections.singletonList(test));
             assertFalse(session.exists(test));
             feature.unarchive(archive, archive.getArchive(Collections.<Path>singletonList(test)), new ProgressListener() {
                 @Override
@@ -45,10 +45,10 @@ public class SFTPCompressFeatureTest extends AbstractTestCase {
                 }
             });
             assertTrue(session.exists(test));
-            session.delete(Collections.singletonList(archive.getArchive(
+            new SFTPDeleteFeature(session).delete(Collections.singletonList(archive.getArchive(
                     Collections.<Path>singletonList(test)
             )));
-            session.delete(Collections.singletonList(test));
+            new SFTPDeleteFeature(session).delete(Collections.singletonList(test));
         }
         session.close();
     }
