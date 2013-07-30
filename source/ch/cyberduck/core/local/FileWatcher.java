958c548b3ce23d0c16483b292b2f5b8be92deaa4
David Kocher <dkocher@iterate.ch>
2013-07-30 12:46:25 +0000

    Former-commit-id: ff6cc9b8c3a0ab78101dcb2840cbf834af327780

--- source/ch/cyberduck/core/local/FileWatcher.java
+++ source/ch/cyberduck/core/local/FileWatcher.java
@@ -20,7 +20,7 @@ package ch.cyberduck.core.local;
 
 import ch.cyberduck.core.Local;
 import ch.cyberduck.core.LocalFactory;
-import ch.cyberduck.core.threading.NamedThreadFactory;
+import ch.cyberduck.core.threading.ThreadPool;
 
 import org.apache.log4j.Logger;
 
@@ -31,8 +31,6 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import com.barbarysoftware.watchservice.ClosedWatchServiceException;
 import com.barbarysoftware.watchservice.WatchEvent;
@@ -50,11 +48,11 @@ public class FileWatcher implements FileWatcherCallback {
 
     private WatchService monitor;
 
-    private ExecutorService pool;
+    private ThreadPool pool;
 
     public FileWatcher() {
         monitor = WatchService.newWatchService();
-        pool = Executors.newFixedThreadPool(1, new NamedThreadFactory());
+        pool = new ThreadPool(1);
     }
 
     public CountDownLatch register(final Local file) {
@@ -69,7 +67,7 @@ public class FileWatcher implements FileWatcherCallback {
             log.error(String.format("Failure registering file watcher monitor for %s", watchable.getFile()), e);
         }
         final CountDownLatch lock = new CountDownLatch(1);
-        pool.submit(new Callable<Boolean>() {
+        pool.execute(new Callable<Boolean>() {
             @Override
             public Boolean call() throws IOException {
                 while(true) {
