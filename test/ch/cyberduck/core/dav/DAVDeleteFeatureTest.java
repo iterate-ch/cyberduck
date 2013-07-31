e95dcb410113deaca67d23ab943fd45c11eea903
David Kocher <dkocher@iterate.ch>
2013-07-31 17:37:16 +0000

    Add tests.
    Former-commit-id: 27c6341c7cbdb85a59d4b1b5b7906b7ec4fdcc26

--- test/ch/cyberduck/core/dav/DAVDeleteFeatureTest.java
+++ test/ch/cyberduck/core/dav/DAVDeleteFeatureTest.java
@@ -8,6 +8,7 @@ import ch.cyberduck.core.DisabledPasswordStore;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.exception.NotfoundException;
 import ch.cyberduck.core.shared.DefaultTouchFeature;
 
 import org.junit.Test;
@@ -39,5 +40,18 @@ public class DAVDeleteFeatureTest extends AbstractTestCase {
         new DAVDeleteFeature(session).delete(Collections.singletonList(test));
         assertFalse(session.exists(test));
         session.close();
+    }
+
+    @Test(expected = NotfoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        final Host host = new Host(Protocol.WEBDAV, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("webdav.user"), properties.getProperty("webdav.password")
+        ));
+        host.setDefaultPath("/dav/basic");
+        final DAVSession session = new DAVSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DAVDeleteFeature(session).delete(Collections.singletonList(test));
     }
 }
---
+++ test/ch/cyberduck/core/ftp/FTPDeleteFeatureTest.java
@@ -0,0 +1,51 @@
+package ch.cyberduck.core.ftp;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.exception.NotfoundException;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.UUID;
+
+/**
+ * @version $Id:$
+ */
+public class FTPDeleteFeatureTest extends AbstractTestCase {
+
+    @Test(expected = NotfoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        final Host host = new Host(Protocol.FTP_TLS, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new FTPDeleteFeature(session).delete(Collections.singletonList(test));
+    }
+}
