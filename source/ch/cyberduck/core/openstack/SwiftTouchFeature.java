cc7747bb0265af42ba7b61dc15e8e3ab918ed89d
David Kocher <dkocher@iterate.ch>
2013-07-31 07:25:41 +0000

    Custom implementations.
    Former-commit-id: 8b645e9077e7fe33567383acfbf4afe599f9a7ea

--- source/ch/cyberduck/core/openstack/SwiftTouchFeature.java
+++ source/ch/cyberduck/core/openstack/SwiftTouchFeature.java
@@ -1,15 +1,37 @@
 package ch.cyberduck.core.openstack;
 
+import ch.cyberduck.core.DefaultIOExceptionMappingService;
 import ch.cyberduck.core.Path;
-import ch.cyberduck.core.shared.DefaultTouchFeature;
+import ch.cyberduck.core.PathContainerService;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Touch;
+
+import java.io.IOException;
 
 /**
  * @version $Id$
  */
-public class SwiftTouchFeature extends DefaultTouchFeature {
+public class SwiftTouchFeature implements Touch {
+
+    private SwiftSession session;
+
+    final PathContainerService containerService = new PathContainerService();
 
     public SwiftTouchFeature(final SwiftSession session) {
-        super(session);
+        this.session = session;
+    }
+
+    @Override
+    public void touch(final Path file) throws BackgroundException {
+        try {
+            session.getClient().createPath(session.getRegion(containerService.getContainer(file)),
+                    containerService.getContainer(file).getName(),
+                    containerService.getKey(file)
+            );
+        }
+        catch(IOException e) {
+            throw new DefaultIOExceptionMappingService().map("Cannot create file {0}", e, file);
+        }
     }
 
     @Override
--- source/ch/cyberduck/core/s3/S3TouchFeature.java
+++ source/ch/cyberduck/core/s3/S3TouchFeature.java
@@ -1,16 +1,35 @@
 package ch.cyberduck.core.s3;
 
 import ch.cyberduck.core.Path;
-import ch.cyberduck.core.Session;
-import ch.cyberduck.core.shared.DefaultTouchFeature;
+import ch.cyberduck.core.PathContainerService;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Touch;
+
+import org.jets3t.service.ServiceException;
+import org.jets3t.service.model.StorageObject;
 
 /**
  * @version $Id$
  */
-public class S3TouchFeature extends DefaultTouchFeature {
+public class S3TouchFeature implements Touch {
 
-    public S3TouchFeature(final Session session) {
-        super(session);
+    private S3Session session;
+
+    private PathContainerService containerService = new PathContainerService();
+
+    public S3TouchFeature(final S3Session session) {
+        this.session = session;
+    }
+
+    @Override
+    public void touch(final Path file) throws BackgroundException {
+        try {
+            session.getClient().putObject(containerService.getContainer(file).getName(),
+                    new StorageObject(containerService.getKey(file)));
+        }
+        catch(ServiceException e) {
+            throw new ServiceExceptionMappingService().map("Cannot create file {0}", e, file);
+        }
     }
 
     @Override
