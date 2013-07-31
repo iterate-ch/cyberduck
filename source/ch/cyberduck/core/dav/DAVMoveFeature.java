5786661894cfa55f1f76fc425a9e3daf3acff359
David Kocher <dkocher@iterate.ch>
2013-07-31 17:43:42 +0000

    Add tests.
    Former-commit-id: 0359aaf8205677329dad67062bab97b1174cb82c

--- source/ch/cyberduck/core/dav/DAVMoveFeature.java
+++ source/ch/cyberduck/core/dav/DAVMoveFeature.java
@@ -1,9 +1,11 @@
 package ch.cyberduck.core.dav;
 
 import ch.cyberduck.core.DefaultIOExceptionMappingService;
+import ch.cyberduck.core.DescriptiveUrl;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.Move;
+import ch.cyberduck.core.shared.DefaultUrlProvider;
 
 import java.io.IOException;
 
@@ -23,7 +25,8 @@ public class DAVMoveFeature implements Move {
     @Override
     public void move(final Path file, final Path renamed) throws BackgroundException {
         try {
-            session.getClient().move(new DAVPathEncoder().encode(file), new DAVPathEncoder().encode(renamed));
+            session.getClient().move(new DAVPathEncoder().encode(file),
+                    new DefaultUrlProvider(session.getHost()).get(renamed).find(DescriptiveUrl.Type.provider).getUrl());
         }
         catch(SardineException e) {
             throw new DAVExceptionMappingService().map("Cannot rename {0}", e, file);
---
+++ test/ch/cyberduck/core/dav/DAVMoveFeatureTest.java
@@ -0,0 +1,93 @@
+package ch.cyberduck.core.dav;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.shared.DefaultTouchFeature;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class DAVMoveFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testMove() throws Exception {
+        final Host host = new Host(Protocol.WEBDAV, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("webdav.user"), properties.getProperty("webdav.password")
+        ));
+        host.setDefaultPath("/dav/basic");
+        final DAVSession session = new DAVSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DefaultTouchFeature(session).touch(test);
+        final Path target = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DAVMoveFeature(session).move(test, target);
+        assertFalse(session.exists(test));
+        assertTrue(session.exists(target));
+        new DAVDeleteFeature(session).delete(Collections.<Path>singletonList(target));
+    }
+
+    @Test
+    public void testMoveOverride() throws Exception {
+        final Host host = new Host(Protocol.WEBDAV, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("webdav.user"), properties.getProperty("webdav.password")
+        ));
+        host.setDefaultPath("/dav/basic");
+        final DAVSession session = new DAVSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DefaultTouchFeature(session).touch(test);
+        final Path target = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DefaultTouchFeature(session).touch(target);
+        new DAVMoveFeature(session).move(test, target);
+        assertFalse(session.exists(test));
+        assertTrue(session.exists(target));
+        new DAVDeleteFeature(session).delete(Collections.<Path>singletonList(target));
+    }
+
+    @Test(expected = NotfoundException.class)
+    public void testMoveNotFound() throws Exception {
+        final Host host = new Host(Protocol.WEBDAV, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("webdav.user"), properties.getProperty("webdav.password")
+        ));
+        host.setDefaultPath("/dav/basic");
+        final DAVSession session = new DAVSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DAVMoveFeature(session).move(test, new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE));
+    }
+}
