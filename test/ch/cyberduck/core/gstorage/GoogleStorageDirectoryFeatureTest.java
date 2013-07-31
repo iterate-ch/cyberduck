666f603bc4d0e57530766c625764a6bdec47a2e3
David Kocher <dkocher@iterate.ch>
2013-07-31 17:50:59 +0000

    Add test.
    Former-commit-id: 24ca97a071d2306dffefb048ff655e3c76530604

---
+++ test/ch/cyberduck/core/gstorage/GoogleStorageDirectoryFeatureTest.java
@@ -0,0 +1,69 @@
+package ch.cyberduck.core.gstorage;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.Scheme;
+import ch.cyberduck.core.s3.S3DefaultDeleteFeature;
+import ch.cyberduck.core.s3.S3DirectoryFeature;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class GoogleStorageDirectoryFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testMakeDirectory() throws Exception {
+        final Host host = new Host(Protocol.GOOGLESTORAGE_SSL, Protocol.GOOGLESTORAGE_SSL.getDefaultHostname(), new Credentials(
+                properties.getProperty("google.projectid"), null
+        ));
+        final GoogleStorageSession session = new GoogleStorageSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore() {
+            @Override
+            public String getPassword(final Scheme scheme, final int port, final String hostname, final String user) {
+                if(user.equals("Google OAuth2 Access Token")) {
+                    return properties.getProperty("google.accesstoken");
+                }
+                if(user.equals("Google OAuth2 Refresh Token")) {
+                    return properties.getProperty("google.refreshtoken");
+                }
+                return null;
+            }
+        }, new DisabledLoginController());
+        final Path test = new Path(new Path("test.cyberduck.ch", Path.DIRECTORY_TYPE), UUID.randomUUID().toString(), Path.DIRECTORY_TYPE);
+        new S3DirectoryFeature(session).mkdir(test, null);
+        assertTrue(session.exists(test));
+        new S3DefaultDeleteFeature(session).delete(Collections.<Path>singletonList(test));
+        session.close();
+    }
+}
