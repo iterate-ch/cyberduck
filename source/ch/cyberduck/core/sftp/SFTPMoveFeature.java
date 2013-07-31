6e05f425705769c627bdc0f9a7aff47039d8fc50
David Kocher <dkocher@iterate.ch>
2013-07-31 17:52:25 +0000

    Add tests.
    Former-commit-id: c82dcdbf9a48a1c7de5f54ff69ca138e16d0b5f1

--- source/ch/cyberduck/core/sftp/SFTPMoveFeature.java
+++ source/ch/cyberduck/core/sftp/SFTPMoveFeature.java
@@ -27,7 +27,7 @@ public class SFTPMoveFeature implements Move {
     public void move(final Path file, final Path renamed) throws BackgroundException {
         try {
             if(session.exists(renamed)) {
-                new SFTPDeleteFeature(session).delete(Collections.singletonList(file));
+                new SFTPDeleteFeature(session).delete(Collections.singletonList(renamed));
             }
             session.sftp().mv(file.getAbsolute(), renamed.getAbsolute());
         }
