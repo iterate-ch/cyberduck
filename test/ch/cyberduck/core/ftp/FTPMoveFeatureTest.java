cba13bce13e16d915f29006f979e00bc1b937810
David Kocher <dkocher@iterate.ch>
2013-07-31 17:46:36 +0000

    Add tests.
    Former-commit-id: 61d27f846f414d554db11e31a76d67f652407ff0

---
+++ test/ch/cyberduck/core/ftp/FTPMoveFeatureTest.java
@@ -0,0 +1,90 @@
+package ch.cyberduck.core.ftp;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Protocol;
+import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.shared.DefaultTouchFeature;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class FTPMoveFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testMove() throws Exception {
+        final Host host = new Host(Protocol.FTP_TLS, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DefaultTouchFeature(session).touch(test);
+        final Path target = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new FTPMoveFeature(session).move(test, target);
+        assertFalse(session.exists(test));
+        assertTrue(session.exists(target));
+        new FTPDeleteFeature(session).delete(Collections.<Path>singletonList(target));
+    }
+
+    @Test
+    public void testMoveOverride() throws Exception {
+        final Host host = new Host(Protocol.FTP_TLS, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DefaultTouchFeature(session).touch(test);
+        final Path target = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DefaultTouchFeature(session).touch(target);
+        new FTPMoveFeature(session).move(test, target);
+        assertFalse(session.exists(test));
+        assertTrue(session.exists(target));
+        new FTPDeleteFeature(session).delete(Collections.<Path>singletonList(target));
+    }
+
+    @Test(expected = NotfoundException.class)
+    public void testMoveNotFound() throws Exception {
+        final Host host = new Host(Protocol.FTP_TLS, "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new FTPMoveFeature(session).move(test, new Path(session.home(), UUID.randomUUID().toString(), Path.FILE_TYPE));
+    }
+}
