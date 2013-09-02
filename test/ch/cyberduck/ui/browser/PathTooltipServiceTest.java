f9e57d150a30cc08721552ce73a5599a7bede992
David Kocher <dkocher@iterate.ch>
2013-09-02 15:39:33 +0000

    Add test.
    Former-commit-id: e8850e78c97ceabf43dc98109f97432771ea4d4a

---
+++ test/ch/cyberduck/ui/browser/PathTooltipServiceTest.java
@@ -0,0 +1,22 @@
+package ch.cyberduck.ui.browser;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Path;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class PathTooltipServiceTest extends AbstractTestCase {
+
+    @Test
+    public void testGetTooltip() throws Exception {
+        final PathTooltipService s = new PathTooltipService();
+        assertEquals("/p\n" +
+                "--\n" +
+                "Unknown", s.getTooltip(new Path("/p", Path.FILE_TYPE)));
+    }
+}
