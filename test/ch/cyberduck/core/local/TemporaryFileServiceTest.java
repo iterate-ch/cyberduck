add42a3aa49557196ce27a46d5870d664a28cbc1
David Kocher <dkocher@iterate.ch>
2013-09-06 11:38:34 +0000

    Former-commit-id: cbdaeb9defff0e04d63228ae5c009232c05afe96

--- test/ch/cyberduck/core/local/TemporaryFileServiceTest.java
+++ test/ch/cyberduck/core/local/TemporaryFileServiceTest.java
@@ -8,13 +8,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class TemporaryFileServiceTest extends AbstractTestCase {
 
     @Test
     public void testCreate() throws Exception {
-        assertEquals(System.getProperty("java.io.tmpdir") + "u/p-1",
-                new TemporaryFileService().create("u", new Path("/p", Path.FILE_TYPE)).getAbsolute());
+        assertEquals(System.getProperty("java.io.tmpdir") + "u/p/f-1",
+                new TemporaryFileService().create("u", new Path("/p/f", Path.FILE_TYPE)).getAbsolute());
     }
 }
