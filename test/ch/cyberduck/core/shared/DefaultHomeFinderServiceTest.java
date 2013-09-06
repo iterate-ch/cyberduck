a2c6f56f5bbda9bf0f88e3a0b347b1321d5f1a34
David Kocher <dkocher@iterate.ch>
2013-09-06 11:52:33 +0000

    Add test.
    Former-commit-id: 1e1249ead0767c1d8f0bc344eb8ffb64f03ea7d3

---
+++ test/ch/cyberduck/core/shared/DefaultHomeFinderServiceTest.java
@@ -0,0 +1,60 @@
+package ch.cyberduck.core.shared;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.ftp.FTPSession;
+import ch.cyberduck.core.ftp.FTPTLSProtocol;
+import ch.cyberduck.core.sftp.SFTPProtocol;
+import ch.cyberduck.core.sftp.SFTPSession;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class DefaultHomeFinderServiceTest extends AbstractTestCase {
+
+    @Test
+    public void testFindFtp() throws Exception {
+        final Host host = new Host(new FTPTLSProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        assertEquals(new Path("/", Path.DIRECTORY_TYPE | Path.VOLUME_TYPE), new DefaultHomeFinderService(session).find());
+        session.close();
+    }
+
+    @Test
+    public void testFindDefaultDirectory() throws Exception {
+        final Host host = new Host(new FTPTLSProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        host.setDefaultPath("/test.d");
+        final FTPSession session = new FTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        assertEquals(new Path("/test.d", Path.DIRECTORY_TYPE), new DefaultHomeFinderService(session).find());
+        session.close();
+    }
+
+    @Test
+    public void testFindSftp() throws Exception {
+        final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        assertEquals(new Path("/home/jenkins", Path.DIRECTORY_TYPE), new DefaultHomeFinderService(session).find());
+        session.close();
+    }
+}
