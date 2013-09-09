98ad65c661dca41b88ddb8fac6d0aa8b232b60f8
David Kocher <dkocher@iterate.ch>
2013-09-09 14:50:32 +0000

    Add test.
    Former-commit-id: 16bcf859397f6958ab31c2ed7bbbc0b161fda80c

---
+++ test/ch/cyberduck/core/shared/DefaultFileSizeFeatureTest.java
@@ -0,0 +1,34 @@
+package ch.cyberduck.core.shared;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.sftp.SFTPProtocol;
+import ch.cyberduck.core.sftp.SFTPSession;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class DefaultFileSizeFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testGetSize() throws Exception {
+        final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        assertEquals(0L, new DefaultFileSizeFeature(session).getSize(new Path(UUID.randomUUID().toString(), Path.FILE_TYPE)), 0L);
+    }
+}
