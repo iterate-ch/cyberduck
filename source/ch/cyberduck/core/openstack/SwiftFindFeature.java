81e501ee62d3c1ba8f3d865a25ba9933970c6e38
David Kocher <dkocher@iterate.ch>
2013-09-09 10:32:15 +0000

    Use HEAD to find object.
    Former-commit-id: ccf72c87b53ed11540142a1b2f0794708ac0d2dd

--- source/ch/cyberduck/core/openstack/SwiftFindFeature.java
+++ source/ch/cyberduck/core/openstack/SwiftFindFeature.java
@@ -1,19 +1,37 @@
 package ch.cyberduck.core.openstack;
 
+/*
+ * Copyright (c) 2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * feedback@cyberduck.ch
+ */
+
 import ch.cyberduck.core.DefaultIOExceptionMappingService;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathContainerService;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.exception.NotfoundException;
 import ch.cyberduck.core.features.Find;
-import ch.cyberduck.core.shared.DefaultFindFeature;
 
 import java.io.IOException;
+import java.util.Map;
 
 import ch.iterate.openstack.swift.exception.GenericException;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class SwiftFindFeature implements Find {
 
@@ -41,7 +59,13 @@ public class SwiftFindFeature implements Find {
                     throw new DefaultIOExceptionMappingService().map("Cannot read file attributes", e, file);
                 }
             }
-            return new DefaultFindFeature(session).find(file);
+            try {
+                final Map<String, String> headers = new SwiftMetadataFeature(session).getMetadata(file);
+                return true;
+            }
+            catch(NotfoundException e) {
+                return false;
+            }
         }
         catch(NotfoundException e) {
             return false;
