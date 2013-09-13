e21a91a59bcc1c6b47b91f9b35b72e30a1969ef1
David Kocher <dkocher@iterate.ch>
2013-09-13 13:17:42 +0000

    Fix test.
    Former-commit-id: d063ed7ee293f541152631ce4f5f106e9ed116e4

--- test/ch/cyberduck/core/AttributedListTest.java
+++ test/ch/cyberduck/core/AttributedListTest.java
@@ -17,16 +17,16 @@ public class AttributedListTest extends AbstractTestCase {
     public void testAdd() throws Exception {
         AttributedList<Path> list = new AttributedList<Path>();
         assertTrue(list.add(new Path("/a", Path.DIRECTORY_TYPE)));
-        assertTrue(list.contains(new NSObjectPathReference(NSString.stringWithString("/a-2"))));
+        assertTrue(list.contains(new NSObjectPathReference(NSString.stringWithString("2-/a"))));
     }
 
     @Test
     public void testRemove() throws Exception {
         AttributedList<Path> list = new AttributedList<Path>();
         assertTrue(list.add(new Path("/a", Path.DIRECTORY_TYPE)));
-        assertTrue(list.contains(new NSObjectPathReference(NSString.stringWithString("/a-2"))));
+        assertTrue(list.contains(new NSObjectPathReference(NSString.stringWithString("2-/a"))));
         list.remove(0);
-        assertFalse(list.contains(new NSObjectPathReference(NSString.stringWithString("/a-2"))));
+        assertFalse(list.contains(new NSObjectPathReference(NSString.stringWithString("2-/a"))));
     }
 
     @Test
