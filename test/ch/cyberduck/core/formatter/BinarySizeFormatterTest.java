04363968f864002dc5db5fdf622819ff5b76a89c
David Kocher <dkocher@iterate.ch>
2013-09-19 14:19:20 +0000

    Fix test.
    Former-commit-id: cf86b9b0b5d7c31fa00999c22a423b0826194c2c

--- test/ch/cyberduck/core/formatter/BinarySizeFormatterTest.java
+++ test/ch/cyberduck/core/formatter/BinarySizeFormatterTest.java
@@ -12,12 +12,12 @@ public class BinarySizeFormatterTest {
     @Test
     public void testFormat() throws Exception {
         ch.cyberduck.core.formatter.BinarySizeFormatter f = new ch.cyberduck.core.formatter.BinarySizeFormatter();
-        assertEquals("1.0 KB", f.format(1024));
-        assertEquals("1.5 KB", f.format(1500));
-        assertEquals("1.4 KB", f.format(1480));
-        assertEquals("2.0 KB", f.format(2000));
-        assertEquals("1.0 MB", f.format(1048576));
-        assertEquals("1.0 GB", f.format(1073741824));
-        assertEquals("375.3 MB", f.format(393495974));
+        assertEquals("1.0 KiB", f.format(1024));
+        assertEquals("1.5 KiB", f.format(1500));
+        assertEquals("1.4 KiB", f.format(1480));
+        assertEquals("2.0 KiB", f.format(2000));
+        assertEquals("1.0 MiB", f.format(1048576));
+        assertEquals("1.0 GiB", f.format(1073741824));
+        assertEquals("375.3 MiB", f.format(393495974));
     }
 }
