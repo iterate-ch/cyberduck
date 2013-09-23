54310f25fb0e0db5d2e2095405f38c5dee8ace34
David Kocher <dkocher@iterate.ch>
2013-09-23 11:56:22 +0000

    Add test.
    Former-commit-id: aea5155d33078e8236ca5481b295e61d3990fd87

--- test/ch/cyberduck/core/sftp/SFTPReadFeatureTest.java
+++ test/ch/cyberduck/core/sftp/SFTPReadFeatureTest.java
@@ -26,12 +26,24 @@ import ch.cyberduck.core.DisabledPasswordStore;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.shared.DefaultHomeFinderService;
+import ch.cyberduck.core.shared.DefaultTouchFeature;
 import ch.cyberduck.core.transfer.TransferStatus;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class SFTPReadFeatureTest extends AbstractTestCase {
 
@@ -45,5 +57,37 @@ public class SFTPReadFeatureTest extends AbstractTestCase {
         session.login(new DisabledPasswordStore(), new DisabledLoginController());
         final TransferStatus status = new TransferStatus();
         new SFTPReadFeature(session).read(new Path(session.workdir(), "nosuchname", Path.FILE_TYPE), status);
+    }
+
+    @Test
+    public void testReadRange() throws Exception {
+        final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path test = new Path(new DefaultHomeFinderService(session).find(), UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DefaultTouchFeature(session).touch(test);
+        final byte[] content = RandomStringUtils.random(1000).getBytes();
+        final OutputStream out = new SFTPWriteFeature(session).write(test, new TransferStatus().length(content.length));
+        assertNotNull(out);
+        IOUtils.write(content, out);
+        IOUtils.closeQuietly(out);
+        final TransferStatus status = new TransferStatus();
+        status.setLength(content.length);
+        status.setAppend(true);
+        status.setCurrent(100L);
+        final Path workdir = session.workdir();
+        final InputStream in = new SFTPReadFeature(session).read(test, status);
+        assertNotNull(in);
+        final byte[] download = new byte[content.length - 100];
+        IOUtils.readFully(in, download);
+        final byte[] reference = new byte[content.length - 100];
+        System.arraycopy(content, 100, reference, 0, content.length - 100);
+        assertArrayEquals(reference, download);
+        in.close();
+        new SFTPDeleteFeature(session).delete(Collections.<Path>singletonList(test), new DisabledLoginController());
+        session.close();
     }
 }
