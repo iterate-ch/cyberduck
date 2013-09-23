15177e8ec9986f6128b23c98b218fe1bac885897
David Kocher <dkocher@iterate.ch>
2013-09-23 11:59:49 +0000

    Add test.
    Former-commit-id: 5c335453e52cf8a64cf8ef96d353eed09de79246

--- test/ch/cyberduck/core/s3/S3ReadFeatureTest.java
+++ test/ch/cyberduck/core/s3/S3ReadFeatureTest.java
@@ -8,12 +8,23 @@ import ch.cyberduck.core.DisabledPasswordStore;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.shared.DefaultTouchFeature;
 import ch.cyberduck.core.transfer.TransferStatus;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class S3ReadFeatureTest extends AbstractTestCase {
 
@@ -28,5 +39,39 @@ public class S3ReadFeatureTest extends AbstractTestCase {
         final TransferStatus status = new TransferStatus();
         final Path container = new Path("test.cyberduck.ch", Path.VOLUME_TYPE);
         new S3ReadFeature(session).read(new Path(container, "nosuchname", Path.FILE_TYPE), status);
+    }
+
+    @Test
+    public void testReadRange() throws Exception {
+        final Host host = new Host(new S3Protocol(), new S3Protocol().getDefaultHostname(), new Credentials(
+                properties.getProperty("s3.key"), properties.getProperty("s3.secret")
+        ));
+        final S3Session session = new S3Session(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path container = new Path("test.cyberduck.ch", Path.VOLUME_TYPE);
+        container.attributes().setRegion("DFW");
+        final Path test = new Path(container, UUID.randomUUID().toString(), Path.FILE_TYPE);
+        new DefaultTouchFeature(session).touch(test);
+        final byte[] content = RandomStringUtils.random(1000).getBytes();
+        final OutputStream out = new S3WriteFeature(session).write(test, new TransferStatus().length(content.length));
+        assertNotNull(out);
+        IOUtils.write(content, out);
+        IOUtils.closeQuietly(out);
+        final TransferStatus status = new TransferStatus();
+        status.setLength(content.length);
+        status.setAppend(true);
+        status.setCurrent(100L);
+        final Path workdir = session.workdir();
+        final InputStream in = new S3ReadFeature(session).read(test, status);
+        assertNotNull(in);
+        final byte[] download = new byte[content.length - 100];
+        IOUtils.readFully(in, download);
+        final byte[] reference = new byte[content.length - 100];
+        System.arraycopy(content, 100, reference, 0, content.length - 100);
+        assertArrayEquals(reference, download);
+        in.close();
+        new S3DefaultDeleteFeature(session).delete(Collections.<Path>singletonList(test), new DisabledLoginController());
+        session.close();
     }
 }
