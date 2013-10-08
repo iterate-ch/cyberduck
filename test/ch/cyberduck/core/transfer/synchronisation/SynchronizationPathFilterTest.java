ddfea75e8d2398d937eb1470cefb0fc30eeb5653
David Kocher <dkocher@iterate.ch>
2013-10-08 09:25:36 +0000

    Add test.
    Former-commit-id: 663f6e2d33b2cfdde5dbe89efce0578fd491628a

---
+++ test/ch/cyberduck/core/transfer/synchronisation/SynchronizationPathFilterTest.java
@@ -0,0 +1,68 @@
+package ch.cyberduck.core.transfer.synchronisation;
+
+import ch.cyberduck.core.*;
+import ch.cyberduck.core.synchronization.CombinedComparisionService;
+import ch.cyberduck.core.transfer.SyncTransfer;
+import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.core.transfer.download.OverwriteFilter;
+import ch.cyberduck.core.transfer.symlink.DownloadSymlinkResolver;
+import ch.cyberduck.core.transfer.symlink.UploadSymlinkResolver;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.TimeZone;
+
+import static org.junit.Assert.*;
+
+/**
+ * @version $Id:$
+ */
+public class SynchronizationPathFilterTest extends AbstractTestCase {
+
+    @Test
+    public void testPrepare() throws Exception {
+        final Path test = new Path("/t/a", Path.FILE_TYPE);
+        test.setLocal(new NullLocal(System.getProperty("java.io.tmpdir"), "t") {
+            @Override
+            public Attributes attributes() {
+                return new LocalAttributes(this.getAbsolute()) {
+                    @Override
+                    public long getSize() {
+                        return 1L;
+                    }
+                };
+            }
+        });
+        Session session = new NullSession(new Host("t")) {
+            @Override
+            public AttributedList<Path> list(final Path file, final ListProgressListener listener) {
+                final AttributedList<Path> list = new AttributedList<Path>();
+                if(file.equals(test.getParent())) {
+                    final Path f = new Path("/t/a", Path.FILE_TYPE);
+                    f.attributes().setSize(3L);
+                    list.add(f);
+                }
+                return list;
+            }
+        };
+        final SynchronizationPathFilter mirror = new SynchronizationPathFilter(new CombinedComparisionService(session, TimeZone.getDefault()),
+                new OverwriteFilter(new DownloadSymlinkResolver(Collections.<Path>emptyList()), session),
+                new ch.cyberduck.core.transfer.upload.OverwriteFilter(new UploadSymlinkResolver(null, Collections.<Path>emptyList()), session),
+                SyncTransfer.ACTION_MIRROR);
+        assertTrue(mirror.accept(test, new TransferStatus().exists(true)));
+        final TransferStatus status = mirror.prepare(test, new TransferStatus().exists(true));
+        assertNotNull(status);
+        assertEquals(1L, status.getLength());
+        final SynchronizationPathFilter download = new SynchronizationPathFilter(new CombinedComparisionService(session, TimeZone.getDefault()),
+                new OverwriteFilter(new DownloadSymlinkResolver(Collections.<Path>emptyList()), session),
+                new ch.cyberduck.core.transfer.upload.OverwriteFilter(new UploadSymlinkResolver(null, Collections.<Path>emptyList()), session),
+                SyncTransfer.ACTION_DOWNLOAD);
+        assertFalse(download.accept(test, new TransferStatus().exists(true)));
+        final SynchronizationPathFilter upload = new SynchronizationPathFilter(new CombinedComparisionService(session, TimeZone.getDefault()),
+                new OverwriteFilter(new DownloadSymlinkResolver(Collections.<Path>emptyList()), session),
+                new ch.cyberduck.core.transfer.upload.OverwriteFilter(new UploadSymlinkResolver(null, Collections.<Path>emptyList()), session),
+                SyncTransfer.ACTION_UPLOAD);
+        assertTrue(upload.accept(test, new TransferStatus().exists(true)));
+    }
+}
