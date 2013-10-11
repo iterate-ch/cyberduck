6c16df930b0f0c42490c5f4ff211a7f14cbe8c50
David Kocher <dkocher@iterate.ch>
2013-10-11 09:03:55 +0000

    Fix test.
    Former-commit-id: 886ae6aa1e465fddf8b56b350d95dd63b3fe3964

--- test/ch/cyberduck/core/synchronization/ComparisionServiceFilterTest.java
+++ test/ch/cyberduck/core/synchronization/ComparisionServiceFilterTest.java
@@ -5,18 +5,21 @@ import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Local;
 import ch.cyberduck.core.LocalAttributes;
 import ch.cyberduck.core.NullLocal;
+import ch.cyberduck.core.NullSession;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathAttributes;
 import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Attributes;
 import ch.cyberduck.core.features.Find;
-import ch.cyberduck.core.ftp.FTPSession;
 
 import org.junit.Test;
 
 import java.util.Calendar;
 import java.util.TimeZone;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @version $Id$
@@ -24,15 +27,32 @@ import static org.junit.Assert.assertEquals;
 public class ComparisionServiceFilterTest extends AbstractTestCase {
 
     @Test
-    public void testCompare() throws Exception {
-        ComparisionServiceFilter s = new ComparisionServiceFilter(new FTPSession(new Host("t")) {
+    public void testCompareEqualResult() throws Exception {
+        final AtomicBoolean found = new AtomicBoolean();
+        final AtomicBoolean attr = new AtomicBoolean();
+        ComparisionServiceFilter s = new ComparisionServiceFilter(new NullSession(new Host("t")) {
             @Override
             public <T> T getFeature(final Class<T> type) {
                 if(type == Find.class) {
                     return (T) new Find() {
                         @Override
                         public boolean find(final Path file) throws BackgroundException {
+                            found.set(true);
                             return true;
+                        }
+                    };
+                }
+                if(type == Attributes.class) {
+                    return (T) new Attributes() {
+                        @Override
+                        public PathAttributes getAttributes(final Path file) throws BackgroundException {
+                            attr.set(true);
+                            return new PathAttributes(Path.FILE_TYPE) {
+                                @Override
+                                public String getChecksum() {
+                                    return "a";
+                                }
+                            };
                         }
                     };
                 }
@@ -59,17 +79,56 @@ public class ComparisionServiceFilterTest extends AbstractTestCase {
                     }
                 };
             }
-
-            @Override
-            public PathAttributes attributes() {
-                return new PathAttributes(Path.FILE_TYPE) {
-                    @Override
-                    public String getChecksum() {
-                        return "a";
-                    }
-                };
-            }
         }));
+        assertTrue(found.get());
+        assertTrue(attr.get());
+    }
+
+    @Test
+    public void testCompareLocalResult() throws Exception {
+        final AtomicBoolean found = new AtomicBoolean();
+        final AtomicBoolean attr = new AtomicBoolean();
+        ComparisionServiceFilter s = new ComparisionServiceFilter(new NullSession(new Host("t")) {
+            @Override
+            public <T> T getFeature(final Class<T> type) {
+                if(type == Find.class) {
+                    return (T) new Find() {
+                        @Override
+                        public boolean find(final Path file) throws BackgroundException {
+                            found.set(true);
+                            return true;
+                        }
+                    };
+                }
+                if(type == Attributes.class) {
+                    return (T) new Attributes() {
+                        @Override
+                        public PathAttributes getAttributes(final Path file) throws BackgroundException {
+                            attr.set(true);
+                            return new PathAttributes(Path.FILE_TYPE) {
+                                @Override
+                                public String getChecksum() {
+                                    return "b";
+                                }
+
+                                @Override
+                                public long getSize() {
+                                    return 2L;
+                                }
+
+                                @Override
+                                public long getModificationDate() {
+                                    final Calendar c = Calendar.getInstance(TimeZone.getDefault());
+                                    c.set(Calendar.HOUR_OF_DAY, 0);
+                                    return c.getTimeInMillis();
+                                }
+                            };
+                        }
+                    };
+                }
+                return super.getFeature(type);
+            }
+        }, TimeZone.getDefault());
         assertEquals(Comparison.local, s.compare(new Path("t", Path.FILE_TYPE) {
             @Override
             public Local getLocal() {
@@ -100,28 +159,8 @@ public class ComparisionServiceFilterTest extends AbstractTestCase {
                     }
                 };
             }
-
-            @Override
-            public PathAttributes attributes() {
-                return new PathAttributes(Path.FILE_TYPE) {
-                    @Override
-                    public String getChecksum() {
-                        return "b";
-                    }
-
-                    @Override
-                    public long getSize() {
-                        return 2L;
-                    }
-
-                    @Override
-                    public long getModificationDate() {
-                        final Calendar c = Calendar.getInstance(TimeZone.getDefault());
-                        c.set(Calendar.HOUR_OF_DAY, 0);
-                        return c.getTimeInMillis();
-                    }
-                };
-            }
         }));
+        assertTrue(found.get());
+        assertTrue(attr.get());
     }
 }
