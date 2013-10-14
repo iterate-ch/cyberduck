2983b091a79988bccdbee63c6e0ef6fc1d20357a
David Kocher <dkocher@iterate.ch>
2013-10-14 14:51:51 +0000

    Add test.
    Former-commit-id: 4b02f31268cc305c54b3180204a8de8a5f6d45ec

--- test/ch/cyberduck/core/CollectionTest.java
+++ test/ch/cyberduck/core/CollectionTest.java
@@ -1,10 +1,14 @@
 package ch.cyberduck.core;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class CollectionTest {
 
@@ -13,6 +17,21 @@ public class CollectionTest {
         Collection<Object> c = new Collection<Object>();
         c.add(new Object());
         c.clear();
-        Assert.assertTrue(c.isEmpty());
+        assertTrue(c.isEmpty());
+    }
+
+    @Test
+    public void testRemoveAll() throws Exception {
+        Collection<Object> collection = new Collection<Object>();
+        final Object a = new Object();
+        collection.add(a);
+        final Object b = new Object();
+        collection.add(b);
+        final Object c = new Object();
+        collection.add(c);
+        collection.removeAll(Arrays.asList(a, b));
+        assertFalse(collection.contains(a));
+        assertFalse(collection.contains(b));
+        assertTrue(collection.contains(c));
     }
 }
