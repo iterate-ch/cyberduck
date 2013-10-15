df893716108e89cb866bf89063f6597d9003c095
David Kocher <dkocher@iterate.ch>
2013-10-15 08:49:34 +0000

    Fix test.
    Former-commit-id: d3670fffb80f485115bea40e8a81aaf48315ed49

--- test/ch/cyberduck/core/DescriptiveUrlTest.java
+++ test/ch/cyberduck/core/DescriptiveUrlTest.java
@@ -17,7 +17,7 @@ public class DescriptiveUrlTest extends AbstractTestCase {
                 new DescriptiveUrl(URI.create("http://host.domain"), DescriptiveUrl.Type.provider, "b")));
         assertFalse(new DescriptiveUrl(URI.create("http://host.domainb"), DescriptiveUrl.Type.provider, "a").equals(
                 new DescriptiveUrl(URI.create("http://host.domain"), DescriptiveUrl.Type.provider, "b")));
-        assertTrue(new DescriptiveUrl(URI.create("http://host.domain"), DescriptiveUrl.Type.provider).equals(
+        assertTrue(new DescriptiveUrl(URI.create("http://host.domain"), DescriptiveUrl.Type.http).equals(
                 new DescriptiveUrl(URI.create("http://host.domain"), DescriptiveUrl.Type.http)));
         assertEquals("http://host.domain", new DescriptiveUrl(URI.create("http://host.domain"), DescriptiveUrl.Type.http).toString());
     }
