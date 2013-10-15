29a42174cc7c4b9df528f887c83c47bc38b22928
David Kocher <dkocher@iterate.ch>
2013-10-15 08:48:54 +0000

    Fix tests.
    Former-commit-id: a275986d2fdf404086f39ca5849ac14960c32947

--- test/ch/cyberduck/core/cdn/DistributionUrlProviderTest.java
+++ test/ch/cyberduck/core/cdn/DistributionUrlProviderTest.java
@@ -18,32 +18,32 @@ public class DistributionUrlProviderTest extends AbstractTestCase {
     @Test
     public void testDownload() throws Exception {
         final Distribution distribution = new Distribution(URI.create("https://test.cyberduck.ch.s3.amazonaws.com"), Distribution.DOWNLOAD, true);
-        assertEquals(new DescriptiveUrl(URI.create("https://test.cyberduck.ch.s3.amazonaws.com/p/f")),
+        assertEquals("https://test.cyberduck.ch.s3.amazonaws.com/p/f",
                 new DistributionUrlProvider(distribution).toUrl(
-                        new Path("/test.cyberduck.ch/p/f", Path.FILE_TYPE)).find(DescriptiveUrl.Type.origin));
+                        new Path("/test.cyberduck.ch/p/f", Path.FILE_TYPE)).find(DescriptiveUrl.Type.origin).getUrl());
     }
 
     @Test
     public void testStreaming() throws Exception {
         final Distribution distribution = new Distribution(URI.create("https://test.cyberduck.ch.s3.amazonaws.com"), Distribution.STREAMING, true);
         distribution.setUrl(URI.create("rtmp://d1f6cbdjcbzyiu.cloudfront.net/cfx/st"));
-        assertEquals(new DescriptiveUrl(URI.create("rtmp://d1f6cbdjcbzyiu.cloudfront.net/cfx/st/p/f")),
-                new DistributionUrlProvider(distribution).toUrl(new Path("/test.cyberduck.ch/p/f", Path.FILE_TYPE)).find(DescriptiveUrl.Type.cdn));
-        assertEquals(new DescriptiveUrl(URI.create("https://test.cyberduck.ch.s3.amazonaws.com/p/f")),
-                new DistributionUrlProvider(distribution).toUrl(new Path("/test.cyberduck.ch/p/f", Path.FILE_TYPE)).find(DescriptiveUrl.Type.origin));
+        assertEquals("rtmp://d1f6cbdjcbzyiu.cloudfront.net/cfx/st/p/f",
+                new DistributionUrlProvider(distribution).toUrl(new Path("/test.cyberduck.ch/p/f", Path.FILE_TYPE)).find(DescriptiveUrl.Type.cdn).getUrl());
+        assertEquals("https://test.cyberduck.ch.s3.amazonaws.com/p/f",
+                new DistributionUrlProvider(distribution).toUrl(new Path("/test.cyberduck.ch/p/f", Path.FILE_TYPE)).find(DescriptiveUrl.Type.origin).getUrl());
     }
 
     @Test
     public void testCustomOrigin() throws Exception {
         final Distribution distribution = new Distribution(URI.create("http://test.cyberduck.ch/"), Distribution.CUSTOM, true);
-        assertEquals(new DescriptiveUrl(URI.create("http://test.cyberduck.ch/p/f")),
-                new DistributionUrlProvider(distribution).toUrl(new Path("/p/f", Path.DIRECTORY_TYPE)).find(DescriptiveUrl.Type.origin));
+        assertEquals("http://test.cyberduck.ch/p/f",
+                new DistributionUrlProvider(distribution).toUrl(new Path("/p/f", Path.DIRECTORY_TYPE)).find(DescriptiveUrl.Type.origin).getUrl());
     }
 
     @Test
     public void testCustomOriginDefaultPath() throws Exception {
         final Distribution distribution = new Distribution(URI.create("http://test.cyberduck.ch/p"), Distribution.CUSTOM, true);
-        assertEquals(new DescriptiveUrl(URI.create("http://test.cyberduck.ch/f")),
-                new DistributionUrlProvider(distribution).toUrl(new Path("/p/f", Path.DIRECTORY_TYPE)).find(DescriptiveUrl.Type.origin));
+        assertEquals("http://test.cyberduck.ch/f",
+                new DistributionUrlProvider(distribution).toUrl(new Path("/p/f", Path.DIRECTORY_TYPE)).find(DescriptiveUrl.Type.origin).getUrl());
     }
 }
