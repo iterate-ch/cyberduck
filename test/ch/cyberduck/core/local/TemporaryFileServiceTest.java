1c028e2de4da37b59c77fdebcdc125c0557e2e2b
David Kocher <dkocher@iterate.ch>
2013-11-08 16:09:08 +0000

    Add tests.
    Former-commit-id: 2d65549b0ff34f25c4f2734268013d40f2446ae2

--- test/ch/cyberduck/core/local/TemporaryFileServiceTest.java
+++ test/ch/cyberduck/core/local/TemporaryFileServiceTest.java
@@ -13,15 +13,44 @@ import static org.junit.Assert.assertEquals;
 public class TemporaryFileServiceTest extends AbstractTestCase {
 
     @Test
-    public void testCreate() throws Exception {
-        assertEquals(System.getProperty("java.io.tmpdir") + "u/p/f",
+    public void testCreateFile() throws Exception {
+        final String temp = System.getProperty("java.io.tmpdir");
+        assertEquals(temp + "u/p/f",
                 new TemporaryFileService().create("u", new Path("/p/f", Path.FILE_TYPE)).getAbsolute());
         final Path file = new Path("/p/f", Path.FILE_TYPE);
         file.attributes().setRegion("region");
-        assertEquals(System.getProperty("java.io.tmpdir") + "u/p/region/f",
+        assertEquals(temp + "u/p/f",
+                new TemporaryFileService().create("u", file).getAbsolute());
+    }
+
+    @Test
+    public void testVersion() throws Exception {
+        final String temp = System.getProperty("java.io.tmpdir");
+        {
+            final Path file = new Path("/p/f", Path.FILE_TYPE);
+            file.attributes().setRegion("region");
+            file.attributes().setVersionId("2");
+            assertEquals(temp + "u/p/2/f",
+                    new TemporaryFileService().create("u", file).getAbsolute());
+        }
+        {
+            final Path file = new Path("/p", Path.DIRECTORY_TYPE);
+            file.attributes().setRegion("region");
+            file.attributes().setVersionId("2");
+            assertEquals(temp + "u/region2/p",
+                    new TemporaryFileService().create("u", file).getAbsolute());
+        }
+    }
+
+    @Test
+    public void testCreateContainer() throws Exception {
+        final String temp = System.getProperty("java.io.tmpdir");
+        final Path file = new Path("/container", Path.DIRECTORY_TYPE);
+        file.attributes().setRegion("region");
+        assertEquals(temp + "u/region/container",
                 new TemporaryFileService().create("u", file).getAbsolute());
         file.attributes().setVersionId("2");
-        assertEquals(System.getProperty("java.io.tmpdir") + "u/p/region2/f",
+        assertEquals(temp + "u/region2/container",
                 new TemporaryFileService().create("u", file).getAbsolute());
     }
 }
