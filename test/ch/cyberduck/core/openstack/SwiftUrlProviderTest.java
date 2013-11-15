d7a50cce1cf81b9020857d558da9501ab6617bca
David Kocher <dkocher@iterate.ch>
2013-11-15 12:38:19 +0000

    Fix test.
    Former-commit-id: bd99cc3e79b811b96592068db75187143b1992e2

--- test/ch/cyberduck/core/openstack/SwiftUrlProviderTest.java
+++ test/ch/cyberduck/core/openstack/SwiftUrlProviderTest.java
@@ -86,7 +86,7 @@ public class SwiftUrlProviderTest extends AbstractTestCase {
         final Path container = new Path("test w.cyberduck.ch", Path.VOLUME_TYPE);
         final Path file = new Path(container, "key", Path.FILE_TYPE);
         final SwiftUrlProvider provider = new SwiftUrlProvider(session, accounts);
-        assertEquals("https://storage101.hkg1.clouddrive.com/v1/MossoCloudFS_59113590-c679-46c3-bf62-9d7c3d5176ee/test%20w.cyberduck.ch/key?temp_url_sig=b1effc88c08ec3392776346c377aff3620c46a51&temp_url_expires=1379500716",
+        assertEquals("https://storage101.hkg1.clouddrive.com/v1/MossoCloudFS_59113590-c679-46c3-bf62-9d7c3d5176ee/test%20w.cyberduck.ch/key?temp_url_sig=0b08dd5b2b48aff5c0269cf4e3ca3afdeaf9c7a5&temp_url_expires=1379500716",
                 provider.createTempUrl(region, file, 1379500716L).getUrl());
     }
 }
