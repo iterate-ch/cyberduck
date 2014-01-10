9e3e4a5e1db188a50abac68690343dba524bae2a
David Kocher <dkocher@iterate.ch>
2014-01-10 11:11:28 +0000

    Add test.
    Former-commit-id: 91613088127cee4aacc1dc8594238b5a9d35c2fa

---
+++ test/ch/cyberduck/core/s3/S3AttributesFeatureTest.java
@@ -0,0 +1,62 @@
+package ch.cyberduck.core.s3;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.exception.NotfoundException;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @version $Id:$
+ */
+public class S3AttributesFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testFind() throws Exception {
+        final S3Session session = new S3Session(
+                new Host(new S3Protocol(), new S3Protocol().getDefaultHostname(),
+                        new Credentials(
+                                properties.getProperty("s3.key"), properties.getProperty("s3.secret")
+                        )));
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path container = new Path("test.cyberduck.ch", Path.VOLUME_TYPE);
+        final Path test = new Path(container, UUID.randomUUID().toString() + ".txt", Path.FILE_TYPE);
+        new S3TouchFeature(session).touch(test);
+        final String v = UUID.randomUUID().toString();
+        final PathAttributes attributes = new S3AttributesFeature(session).find(test);
+        assertEquals(0L, attributes.getSize());
+        assertEquals(Path.FILE_TYPE, attributes.getType());
+        assertEquals("d41d8cd98f00b204e9800998ecf8427e", attributes.getChecksum());
+        assertNotNull(attributes.getModificationDate());
+        new S3DefaultDeleteFeature(session).delete(Collections.singletonList(test), new DisabledLoginController());
+        session.close();
+    }
+
+    @Test(expected = NotfoundException.class)
+    public void testFindNotFound() throws Exception {
+        final S3Session session = new S3Session(
+                new Host(new S3Protocol(), new S3Protocol().getDefaultHostname(),
+                        new Credentials(
+                                properties.getProperty("s3.key"), properties.getProperty("s3.secret")
+                        )));
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final Path container = new Path("test.cyberduck.ch", Path.VOLUME_TYPE);
+        final Path test = new Path(container, UUID.randomUUID().toString(), Path.FILE_TYPE);
+        final S3AttributesFeature f = new S3AttributesFeature(session);
+        f.find(test);
+    }
+}
