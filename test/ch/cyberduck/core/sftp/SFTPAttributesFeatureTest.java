0fe90ef5bd6d719e049be01536b89d0b738bee31
David Kocher <dkocher@iterate.ch>
2014-01-27 21:25:04 +0000

    Add test.
    Former-commit-id: ff2332140811a747d0129f577058e1d0bcfaa9cf

---
+++ test/ch/cyberduck/core/sftp/SFTPAttributesFeatureTest.java
@@ -0,0 +1,49 @@
+package ch.cyberduck.core.sftp;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.exception.NotfoundException;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @version $Id:$
+ */
+public class SFTPAttributesFeatureTest extends AbstractTestCase {
+
+    @Test(expected = NotfoundException.class)
+    public void testFindNotFound() throws Exception {
+        final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        new SFTPAttributesFeature(session).find(new Path(UUID.randomUUID().toString(), Path.FILE_TYPE));
+    }
+
+    @Test
+    public void testFindDirectory() throws Exception {
+        final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController());
+        final PathAttributes attributes = new SFTPAttributesFeature(session).find(session.workdir());
+        assertNotNull(attributes);
+        assertEquals(Path.DIRECTORY_TYPE, attributes.getType());
+        session.close();
+    }
+}
