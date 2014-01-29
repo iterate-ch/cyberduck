e0804a88351a8c4001a188cb5008ab97757b9f93
David Kocher <dkocher@iterate.ch>
2014-01-29 20:39:23 +0000

    Former-commit-id: ed2af099646a4ae063f65445890aed5c0bdc7a7c

--- source/ch/cyberduck/core/fs/fuse/FuseFilesystem.java
+++ source/ch/cyberduck/core/fs/fuse/FuseFilesystem.java
@@ -344,7 +344,7 @@ public final class FuseFilesystem extends ProxyController implements Filesystem 
                         NSObject modificationNumber = attributes.objectForKey(NSFileManager.NSFileModificationDate);
                         if(null != modificationNumber) {
                             final long modification = (long) (Rococoa.cast(modificationNumber, NSDate.class).timeIntervalSince1970() * 1000);
-                            timestamp.setTimestamp(file, modification, modification, modification);
+                            timestamp.setTimestamp(file, modification);
                         }
                     }
                     return true;
