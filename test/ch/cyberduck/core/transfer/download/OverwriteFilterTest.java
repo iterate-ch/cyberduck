19ef6617a4d1db587bf2048f80bec81bb65f596a
David Kocher <dkocher@iterate.ch>
2014-02-12 09:29:52 +0000

    Add test.
    Former-commit-id: 982cec85de429b770ab2c53e966b8a312d2073dd

--- test/ch/cyberduck/core/transfer/download/OverwriteFilterTest.java
+++ test/ch/cyberduck/core/transfer/download/OverwriteFilterTest.java
@@ -3,9 +3,11 @@ package ch.cyberduck.core.transfer.download;
 import ch.cyberduck.core.AbstractTestCase;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Local;
+import ch.cyberduck.core.LocalAttributes;
 import ch.cyberduck.core.NullLocal;
 import ch.cyberduck.core.NullSession;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Permission;
 import ch.cyberduck.core.local.WorkspaceApplicationLauncher;
 import ch.cyberduck.core.transfer.TransferStatus;
 import ch.cyberduck.core.transfer.symlink.NullSymlinkResolver;
@@ -13,8 +15,7 @@ import ch.cyberduck.core.transfer.symlink.NullSymlinkResolver;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @version $Id$
@@ -70,5 +71,34 @@ public class OverwriteFilterTest extends AbstractTestCase {
         p.attributes().setSize(8L);
         final TransferStatus status = f.prepare(p, new TransferStatus());
         assertEquals(8L, status.getLength(), 0L);
+    }
+
+    @Test
+    public void testPrepareAttributes() throws Exception {
+        OverwriteFilter f = new OverwriteFilter(new NullSymlinkResolver(), new NullSession(new Host("h")));
+        final Path p = new Path("a", Path.FILE_TYPE);
+        p.setLocal(new NullLocal(null, "a") {
+            @Override
+            public LocalAttributes attributes() {
+                return new LocalAttributes("a") {
+                    @Override
+                    public void setModificationDate(long timestamp) {
+                        fail();
+                    }
+
+                    @Override
+                    public void setPermission(Permission permission) {
+                        fail();
+                    }
+                };
+            }
+        });
+        p.attributes().setSize(8L);
+        p.attributes().setModificationDate(1L);
+        p.attributes().setPermission(new Permission(777));
+        final TransferStatus status = f.prepare(p, new TransferStatus());
+        assertEquals(8L, status.getLength(), 0L);
+        assertEquals(1L, status.getTimestamp(), 0L);
+        assertEquals(new Permission(777), status.getPermission());
     }
 }
