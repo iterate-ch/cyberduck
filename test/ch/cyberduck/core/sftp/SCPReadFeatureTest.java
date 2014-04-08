c65aa32ba9cf587ad82a25fb4cfadf0254ed7af5
David Kocher <dkocher@iterate.ch>
2014-04-08 11:44:34 +0000

    Add test.
    Former-commit-id: cd312392b61d6039ee44998c18b12d1e1ec6a422

---
+++ test/ch/cyberduck/core/sftp/SCPReadFeatureTest.java
@@ -0,0 +1,101 @@
+package ch.cyberduck.core.sftp;
+
+/*
+ * Copyright (c) 2002-2014 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DefaultHostKeyController;
+import ch.cyberduck.core.DisabledCancelCallback;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.io.DisabledStreamListener;
+import ch.cyberduck.core.io.StreamCopier;
+import ch.cyberduck.core.shared.DefaultHomeFinderService;
+import ch.cyberduck.core.shared.DefaultTouchFeature;
+import ch.cyberduck.core.transfer.TransferStatus;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Random;
+import java.util.UUID;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @version $Id:$
+ */
+public class SCPReadFeatureTest extends AbstractTestCase {
+
+    @Test(expected = BackgroundException.class)
+    public void testReadNotFound() throws Exception {
+        final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController(), new DisabledCancelCallback());
+        final TransferStatus status = new TransferStatus();
+        new SCPReadFeature(session).read(new Path(session.workdir(), "nosuchname", EnumSet.of(Path.Type.file)), status);
+    }
+
+    @Test
+    public void testRead() throws Exception {
+        final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("sftp.user"), properties.getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        session.open(new DefaultHostKeyController());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController(), new DisabledCancelCallback());
+        final Path home = new DefaultHomeFinderService(session).find();
+        final Path test = new Path(home, UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
+        new DefaultTouchFeature(session).touch(test);
+        final byte[] content = new byte[39865];
+        new Random().nextBytes(content);
+        {
+            final TransferStatus status = new TransferStatus().length(content.length);
+            final OutputStream out = new SFTPWriteFeature(session).write(test, status);
+            assertNotNull(out);
+            new StreamCopier(status, status, 32768).transfer(new ByteArrayInputStream(content), 0, out, new DisabledStreamListener(), content.length);
+            out.close();
+        }
+        {
+            final TransferStatus status = new TransferStatus();
+            status.setLength(content.length);
+            final InputStream in = new SCPReadFeature(session).read(test, status);
+            assertNotNull(in);
+            final ByteArrayOutputStream buffer = new ByteArrayOutputStream(content.length);
+            new StreamCopier(status, status, 32768).transfer(in, 0, buffer, new DisabledStreamListener(), content.length);
+            in.close();
+            assertArrayEquals(content, buffer.toByteArray());
+        }
+        new SFTPDeleteFeature(session).delete(Collections.singletonList(test), new DisabledLoginController());
+        session.close();
+    }
+}
