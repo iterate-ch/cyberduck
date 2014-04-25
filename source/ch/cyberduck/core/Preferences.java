b9638d780b14e4db88c333f63c5664e542464f57
David Kocher <dkocher@iterate.ch>
2014-04-25 12:27:31 +0000

    Enable Windows Azure protocol support. See #6521.
    Former-commit-id: 43522e50bcd74c9dacd2e4e5464f0f438487566d

--- source/ch/cyberduck/core/Preferences.java
+++ source/ch/cyberduck/core/Preferences.java
@@ -759,7 +759,7 @@ public abstract class Preferences {
         defaults.put(String.format("connection.protocol.%s.enable", ProtocolFactory.SWIFT.getIdentifier()), String.valueOf(true));
         defaults.put(String.format("connection.protocol.%s.enable", ProtocolFactory.S3_SSL.getIdentifier()), String.valueOf(true));
         defaults.put(String.format("connection.protocol.%s.enable", ProtocolFactory.GOOGLESTORAGE_SSL.getIdentifier()), String.valueOf(true));
-        defaults.put(String.format("connection.protocol.%s.enable", ProtocolFactory.AZURE.getIdentifier()), String.valueOf(false));
+        defaults.put(String.format("connection.protocol.%s.enable", ProtocolFactory.AZURE.getIdentifier()), String.valueOf(true));
 
         /**
          * Socket timeout
