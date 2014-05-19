6b3a2b13a96d57a2dd4f9d89ae79f065c2638be7
David Kocher <dkocher@iterate.ch>
2014-05-19 19:13:23 +0000

    Add test.
    Former-commit-id: de68e47460f0daca965d93c940e74bba61c6f180

---
+++ test/ch/cyberduck/core/local/WorkspaceSymlinkFeatureTest.java
@@ -0,0 +1,45 @@
+package ch.cyberduck.core.local;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Local;
+import ch.cyberduck.core.Preferences;
+import ch.cyberduck.core.exception.NotfoundException;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version $Id:$
+ */
+public class WorkspaceSymlinkFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testSymlink() throws Exception {
+        final Local target = new FinderLocal(Preferences.instance().getProperty("tmp.dir"),
+                UUID.randomUUID().toString());
+        new DefaultLocalTouchFeature().touch(target);
+        final FinderLocal symlink = new FinderLocal(Preferences.instance().getProperty("tmp.dir"),
+                UUID.randomUUID().toString());
+        assertFalse(symlink.exists());
+        new WorkspaceSymlinkFeature().symlink(symlink, target.getAbsolute());
+        assertTrue(symlink.exists());
+        target.delete();
+        symlink.delete();
+    }
+
+    @Ignore
+    @Test(expected = NotfoundException.class)
+    public void testSymlinkNoTarget() throws Exception {
+        final Local target = new FinderLocal(Preferences.instance().getProperty("tmp.dir"),
+                UUID.randomUUID().toString());
+        final FinderLocal symlink = new FinderLocal(Preferences.instance().getProperty("tmp.dir"),
+                UUID.randomUUID().toString());
+        assertFalse(symlink.exists());
+        new WorkspaceSymlinkFeature().symlink(symlink, target.getAbsolute());
+    }
+}
