665e80ca45dafdb2ada7c873f17af8d4a70e5ac2
David Kocher <dkocher@iterate.ch>
2014-06-11 13:34:49 +0000

    Former-commit-id: c6041dc52d1b5d93c2e25448effd37b229765e32

--- source/ch/cyberduck/core/ftp/FTPExceptionMappingService.java
+++ source/ch/cyberduck/core/ftp/FTPExceptionMappingService.java
@@ -46,57 +46,47 @@ public class FTPExceptionMappingService extends AbstractExceptionMappingService<
             return new ConnectionCanceledException(e);
         }
         if(e instanceof FTPException) {
-            final int code = ((FTPException) e).getCode();
-            if(code == FTPReply.INSUFFICIENT_STORAGE) {
-                return new QuotaException(buffer.toString(), e);
-            }
-            if(code == FTPReply.STORAGE_ALLOCATION_EXCEEDED) {
-                return new QuotaException(buffer.toString(), e);
-            }
-            if(code == FTPReply.NOT_LOGGED_IN) {
-                return new LoginFailureException(buffer.toString(), e);
-            }
-            if(code == FTPReply.FAILED_SECURITY_CHECK) {
-                return new AccessDeniedException(buffer.toString(), e);
-            }
-            if(code == FTPReply.DENIED_FOR_POLICY_REASONS) {
-                return new AccessDeniedException(buffer.toString(), e);
-            }
-            if(code == FTPReply.NEED_ACCOUNT) {
-                return new AccessDeniedException(buffer.toString(), e);
-            }
-            if(code == FTPReply.NEED_ACCOUNT_FOR_STORING_FILES) {
-                return new AccessDeniedException(buffer.toString(), e);
-            }
-            if(code == FTPReply.FILE_NAME_NOT_ALLOWED) {
-                return new AccessDeniedException(buffer.toString(), e);
-            }
-            if(code == FTPReply.FILE_UNAVAILABLE) {
-                // Requested action not taken. File unavailable (e.g., file not found, no access)
-                return new NotfoundException(buffer.toString(), e);
-            }
-            if(code == FTPReply.UNAVAILABLE_RESOURCE) {
-                return new NotfoundException(buffer.toString(), e);
-            }
-            if(code == FTPReply.UNRECOGNIZED_COMMAND) {
-                return new InteroperabilityException(buffer.toString(), e);
-            }
-            if(code == FTPReply.SYNTAX_ERROR_IN_ARGUMENTS) {
-                return new InteroperabilityException(buffer.toString(), e);
-            }
-            if(code == FTPReply.BAD_COMMAND_SEQUENCE) {
-                return new InteroperabilityException(buffer.toString(), e);
-            }
-            if(code == FTPReply.COMMAND_NOT_IMPLEMENTED) {
-                return new InteroperabilityException(buffer.toString(), e);
-            }
-            if(code == FTPReply.COMMAND_NOT_IMPLEMENTED_FOR_PARAMETER) {
-                return new InteroperabilityException(buffer.toString(), e);
-            }
+            return this.handle((FTPException) e, buffer);
         }
         if(e instanceof MalformedServerReplyException) {
             return new InteroperabilityException(buffer.toString(), e);
         }
         return new DefaultIOExceptionMappingService().map(e);
+    }
+
+    private BackgroundException handle(final FTPException e, final StringBuilder buffer) {
+        final int status = e.getCode();
+        if(status == FTPReply.INSUFFICIENT_STORAGE) {
+            return new QuotaException(buffer.toString(), e);
+        }
+        if(status == FTPReply.STORAGE_ALLOCATION_EXCEEDED) {
+            return new QuotaException(buffer.toString(), e);
+        }
+        if(status == FTPReply.NOT_LOGGED_IN) {
+            return new LoginFailureException(buffer.toString(), e);
+        }
+        if(status == FTPReply.FAILED_SECURITY_CHECK) {
+            return new AccessDeniedException(buffer.toString(), e);
+        }
+        if(status == FTPReply.DENIED_FOR_POLICY_REASONS) {
+            return new AccessDeniedException(buffer.toString(), e);
+        }
+        if(status == FTPReply.NEED_ACCOUNT) {
+            return new AccessDeniedException(buffer.toString(), e);
+        }
+        if(status == FTPReply.NEED_ACCOUNT_FOR_STORING_FILES) {
+            return new AccessDeniedException(buffer.toString(), e);
+        }
+        if(status == FTPReply.FILE_NAME_NOT_ALLOWED) {
+            return new AccessDeniedException(buffer.toString(), e);
+        }
+        if(status == FTPReply.FILE_UNAVAILABLE) {
+            // Requested action not taken. File unavailable (e.g., file not found, no access)
+            return new NotfoundException(buffer.toString(), e);
+        }
+        if(status == FTPReply.UNAVAILABLE_RESOURCE) {
+            return new NotfoundException(buffer.toString(), e);
+        }
+        return new InteroperabilityException(buffer.toString(), e);
     }
 }
