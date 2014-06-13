defc26f740bbd47154a3c25a4f0686aff154b207
David Kocher <dkocher@iterate.ch>
2014-06-13 14:34:17 +0000

    Human readable error descriptions for SSL alerts.
    Former-commit-id: f1f9fb103bb1c6474bc4615b01285632b344bcc5

--- source/ch/cyberduck/core/ssl/SSLExceptionMappingService.java
+++ source/ch/cyberduck/core/ssl/SSLExceptionMappingService.java
@@ -23,6 +23,7 @@ import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.exception.ConnectionCanceledException;
 import ch.cyberduck.core.exception.InteroperabilityException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import javax.net.ssl.SSLException;
@@ -32,10 +33,115 @@ import javax.net.ssl.SSLProtocolException;
 import java.security.cert.CertificateException;
 
 /**
- * @version $Id:$
+ * @version $Id$
  */
 public class SSLExceptionMappingService extends AbstractExceptionMappingService<SSLException> {
     private static final Logger log = Logger.getLogger(SSLExceptionMappingService.class);
+
+    private enum Alert {
+        close_notify(0),
+        unexpected_message(10) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. An inappropriate message was received.", super.getDescription());
+            }
+        },
+        bad_record_mac(20) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. A record is received with an incorrect MAC.", super.getDescription());
+            }
+        },
+        decryption_failed_RESERVED(21),
+        record_overflow(22),
+        decompression_failure(30),
+        handshake_failure(40) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. Unable to negotiate an acceptable set of security parameters.", super.getDescription());
+            }
+        },
+        no_certificate_RESERVED(41),
+        bad_certificate(42) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. The certificate provided could not be verified by the server.", super.getDescription());
+            }
+        },
+        unsupported_certificate(43) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. The certificate type provided is not supported by the server.", super.getDescription());
+            }
+        },
+        certificate_revoked(44) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. The certificate provided has been revoked by its signer.", super.getDescription());
+            }
+        },
+        certificate_expired(45) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. The certificate provided has expired.", super.getDescription());
+            }
+        },
+        certificate_unknown(46) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. The certificate provided was not accepted by the server.", super.getDescription());
+            }
+        },
+        illegal_parameter(47),
+        unknown_ca(48) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. A valid certificate chain or partial chain was received, but " +
+                        "the certificate was not accepted because the certificate authority certificate could not be located " +
+                        "or couldn't be matched with a known, trusted certificate authority.", super.getDescription());
+            }
+        },
+        access_denied(49) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. A valid certificate was received, but when access control was " +
+                        "applied, the server decided not to proceed with negotiation.", super.getDescription());
+            }
+        },
+        decode_error(50),
+        decrypt_error(51),
+        export_restriction_RESERVED(60),
+        protocol_version(70) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. The protocol version attempted to negotiate is recognized but not supported.", super.getDescription());
+            }
+        },
+        insufficient_security(71) {
+            @Override
+            public String getDescription() {
+                return String.format("%s. The server requires ciphers more secure than those supported.", super.getDescription());
+            }
+        },
+        internal_error(80),
+        user_canceled(90),
+        no_renegotiation(100),
+        unsupported_extension(110);
+
+        private int code;
+
+        private Alert(int code) {
+            this.code = code;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getDescription() {
+            return StringUtils.capitalize(StringUtils.replaceChars(this.name(), '_', ' '));
+        }
+    }
 
     /**
      * close_notify(0),
@@ -74,7 +180,15 @@ public class SSLExceptionMappingService extends AbstractExceptionMappingService<
             }
         }
         final StringBuilder buffer = new StringBuilder();
-        this.append(buffer, failure.getMessage());
+        final String message = failure.getMessage();
+        for(Alert alert : Alert.values()) {
+            if(StringUtils.contains(message, alert.name())) {
+                this.append(buffer, alert.getDescription());
+            }
+        }
+        if(buffer.length() == 0) {
+            this.append(buffer, message);
+        }
         if(failure instanceof SSLProtocolException) {
             return new InteroperabilityException(buffer.toString(), failure);
         }
