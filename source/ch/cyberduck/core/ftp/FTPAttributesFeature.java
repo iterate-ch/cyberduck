093419439b66f38c294c795b6d0a0cce03664a13
David Kocher <dkocher@iterate.ch>
2014-06-18 12:19:37 +0000

    Add implementation using MLST.
    Former-commit-id: 2a7f45c8456874f11500c8890548f481b0c52d94

---
+++ source/ch/cyberduck/core/ftp/FTPAttributesFeature.java
@@ -0,0 +1,77 @@
+package ch.cyberduck.core.ftp;
+
+/*
+ * Copyright (c) 2002-2014 David Kocher. All rights reserved.
+ * http://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * feedback@cyberduck.io
+ */
+
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.Cache;
+import ch.cyberduck.core.DisabledListProgressListener;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.features.Attributes;
+
+import org.apache.commons.net.ftp.FTPCmd;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @version $Id:$
+ */
+public class FTPAttributesFeature implements Attributes {
+
+    private FTPSession session;
+
+    public FTPAttributesFeature(FTPSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public PathAttributes find(final Path file) throws BackgroundException {
+        try {
+            final List<String> list = new FTPDataFallback(session).data(file, new DataConnectionAction<List<String>>() {
+                @Override
+                public List<String> execute() throws BackgroundException {
+                    try {
+                        return session.getClient().list(FTPCmd.MLST);
+                    }
+                    catch(IOException e) {
+                        throw new FTPExceptionMappingService().map(e);
+                    }
+                }
+            });
+            final FTPDataResponseReader reader = new FTPMlsdListResponseReader(session);
+            final AttributedList<Path> attributes
+                    = reader.read(file.getParent(), list, new DisabledListProgressListener());
+            if(attributes.size() == 1) {
+                return attributes.iterator().next().attributes();
+            }
+            throw new NotfoundException();
+        }
+        catch(IOException e) {
+            throw new FTPExceptionMappingService().map(e);
+        }
+    }
+
+    @Override
+    public Attributes withCache(Cache cache) {
+        return this;
+    }
+}
---
+++ test/ch/cyberduck/core/ftp/FTPAttributesFeatureTest.java
@@ -0,0 +1,73 @@
+package ch.cyberduck.core.ftp;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.Attributes;
+import ch.cyberduck.core.Cache;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledCancelCallback;
+import ch.cyberduck.core.DisabledHostKeyCallback;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.ListProgressListener;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Permission;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.shared.DefaultAttributesFeature;
+
+import org.junit.Test;
+
+import java.util.EnumSet;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.*;
+
+/**
+ * @version $Id:$
+ */
+public class FTPAttributesFeatureTest extends AbstractTestCase {
+
+    @Test(expected = NotfoundException.class)
+    public void testNotFound() throws Exception {
+        final Host host = new Host(new FTPProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController(), new DisabledCancelCallback());
+        new DefaultAttributesFeature(session).find(new Path(UUID.randomUUID().toString(), EnumSet.of(Path.Type.file)));
+    }
+
+    @Test
+    public void testAttributes() throws Exception {
+        final Host host = new Host(new FTPProtocol(), "test.cyberduck.ch", new Credentials(
+                properties.getProperty("ftp.user"), properties.getProperty("ftp.password")
+        ));
+        final AtomicBoolean set = new AtomicBoolean();
+        final FTPSession session = new FTPSession(host) {
+            @Override
+            public AttributedList<Path> list(final Path file, final ListProgressListener listener) throws BackgroundException {
+                assertFalse(set.get());
+                final AttributedList<Path> list = super.list(file, listener);
+                set.set(true);
+                return list;
+            }
+        };
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController(), new DisabledCancelCallback());
+        final Cache cache = new Cache(1);
+        final DefaultAttributesFeature f = new DefaultAttributesFeature(session, cache);
+        final Path file = new Path(session.workdir(), "test", EnumSet.of(Path.Type.file));
+        final Attributes attributes = f.find(file);
+        assertEquals(0L, attributes.getSize());
+        assertEquals("1106", attributes.getOwner());
+        assertEquals(new Permission("-rw-rw-rw-"), attributes.getPermission());
+        // Test cache
+        assertEquals(0L, f.find(file).getSize());
+        assertTrue(cache.containsKey(file.getParent().getReference()));
+    }
+
+}
