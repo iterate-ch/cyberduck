19eb069475c0bd41c196b5c2eefccf3d6970dcbc
Yves Langisch <yla@iterate.ch>
2014-08-14 11:41:04 +0000

    Add diagnose option.
    Former-commit-id: f1c90a29b60abee83b231c9057c73e5891a50989

--- source/ch/cyberduck/ui/winforms/threading/DialogAlertCallback.cs
+++ source/ch/cyberduck/ui/winforms/threading/DialogAlertCallback.cs
@@ -1,5 +1,5 @@
 ﻿// 
-// Copyright (c) 2010-2013 Yves Langisch. All rights reserved.
+// Copyright (c) 2010-2014 Yves Langisch. All rights reserved.
 // http://cyberduck.ch/
 // 
 // This program is free software; you can redistribute it and/or modify
@@ -16,7 +16,6 @@
 // yves@cyberduck.ch
 // 
 
-using System.Windows.Forms;
 using Ch.Cyberduck.Ui.Controller;
 using ch.cyberduck.core;
 using ch.cyberduck.core.exception;
@@ -43,16 +42,27 @@ namespace Ch.Cyberduck.Ui.Winforms.Threading
                     String provider = host.getProtocol().getProvider();
                     string footer = String.Format("{0}/{1}", Preferences.instance().getProperty("website.help"),
                                                   provider);
-                    DialogResult result = _controller.WarningBox(LocaleFactory.localizedString("Error"),
-                                                                 failure.getMessage() ??
-                                                                 LocaleFactory.localizedString("Unknown"),
-                                                                 failure.getDetail() ??
-                                                                 LocaleFactory.localizedString("Unknown"),
-                                                                 log.length() > 0 ? log.toString() : null,
-                                                                 String.Format("{0}",
-                                                                               LocaleFactory.localizedString(
-                                                                                   "Try Again", "Alert")), true, footer);
-                    r = DialogResult.OK == result;
+                    string title = LocaleFactory.localizedString("Error");
+                    string message = failure.getMessage() ?? LocaleFactory.localizedString("Unknown");
+                    string detail = failure.getDetail() ?? LocaleFactory.localizedString("Unknown");
+                    string expanded = log.length() > 0 ? log.toString() : null;
+                    string commandButtons = String.Format("{0}|{1}", LocaleFactory.localizedString("Try Again", "Alert"),
+                                                          LocaleFactory.localizedString("Network Diagnostics", "Alert"));
+
+                    _controller.WarningBox(title, message, detail, expanded, commandButtons, true, footer,
+                                           delegate(int option, bool @checked)
+                                               {
+                                                   switch (option)
+                                                   {
+                                                       case 0:
+                                                           r = true;
+                                                           break;
+                                                       case 1:
+                                                           ReachabilityFactory.get().diagnose(host);
+                                                           r = false;
+                                                           break;
+                                                   }
+                                               });
                 }, true);
             return r;
         }
