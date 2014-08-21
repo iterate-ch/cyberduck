c9eb9e3ffbcb0ccee844ca302ac613b1a5935bd7
David Kocher <dkocher@iterate.ch>
2014-08-21 07:46:31 +0000

    Logging
    Former-commit-id: 4dfdf0f014d6bc32d395b5aa704e04d8affae45b

--- source/ch/cyberduck/core/sparkle/Sandbox.java
+++ source/ch/cyberduck/core/sparkle/Sandbox.java
@@ -33,6 +33,9 @@ public final class Sandbox {
     }
 
     public static Sandbox get() {
+        if(log.isDebugEnabled()) {
+            log.debug("Obtain sandbox environment with requirement com.apple.security.app-sandbox");
+        }
         return new Sandbox();
     }
 
