71cee9d14855d5b85510d0c6de8d224cf3a51e7a
Yves Langisch <yla@iterate.ch>
2014-09-02 18:11:34 +0000

    Fix #8201.
    Former-commit-id: 89f8f2d6b0a20ada66b96de9dc5446a1c21ec27a

--- source/ch/cyberduck/ui/controller/TransferPromptModel.cs
+++ source/ch/cyberduck/ui/controller/TransferPromptModel.cs
@@ -1,5 +1,5 @@
-﻿//
-// Copyright (c) 2010-2013 Yves Langisch. All rights reserved.
+﻿// 
+// Copyright (c) 2010-2014 Yves Langisch. All rights reserved.
 // http://cyberduck.ch/
 // 
 // This program is free software; you can redistribute it and/or modify
@@ -274,7 +274,7 @@ namespace Ch.Cyberduck.Ui.Controller
 
                 public override void cleanup(object list)
                 {
-                    _cache.put(_directory.getReference(), (AttributedList) list);
+                    _cache.put(_directory.getReference(), new AttributedList((List) list));
                     _model.Filter();
                 }
             }
