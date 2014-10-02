1955419af8ef1ccd917b26b757bde9ea8e8867de
David Kocher <dkocher@iterate.ch>
2014-10-02 09:42:04 +0000

    Add test.
    Former-commit-id: f1fa13dc2f46732f78a9a2f12efa441c1a1c0eb3

---
+++ test/ch/cyberduck/core/s3/S3MultipartServiceTest.java
@@ -0,0 +1,70 @@
+package ch.cyberduck.core.s3;
+
+/*
+ * Copyright (c) 2002-2014 David Kocher. All rights reserved.
+ * http://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * feedback@cyberduck.io
+ */
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledCancelCallback;
+import ch.cyberduck.core.DisabledHostKeyCallback;
+import ch.cyberduck.core.DisabledLoginController;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+
+import org.jets3t.service.model.MultipartUpload;
+import org.junit.Test;
+
+import java.util.EnumSet;
+import java.util.UUID;
+
+import static org.junit.Assert.assertNull;
+
+public class S3MultipartServiceTest extends AbstractTestCase {
+
+    @Test
+    public void testFind() throws Exception {
+        final S3Session session = new S3Session(
+                new Host(new S3Protocol(), new S3Protocol().getDefaultHostname(),
+                        new Credentials(
+                                properties.getProperty("s3.key"), properties.getProperty("s3.secret")
+                        )));
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController(), new DisabledCancelCallback());
+        final Path container = new Path("test.cyberduck.ch", EnumSet.of(Path.Type.directory, Path.Type.volume));
+        final Path test = new Path(container, UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
+        final MultipartUpload part = new S3MultipartService(session).find(test);
+        assertNull(part);
+    }
+
+
+    @Test
+    public void testFindKeyWithSpace() throws Exception {
+        final S3Session session = new S3Session(
+                new Host(new S3Protocol(), new S3Protocol().getDefaultHostname(),
+                        new Credentials(
+                                properties.getProperty("s3.key"), properties.getProperty("s3.secret")
+                        )));
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginController(), new DisabledCancelCallback());
+        final Path container = new Path("test.cyberduck.ch", EnumSet.of(Path.Type.directory, Path.Type.volume));
+        final Path test = new Path(container, "t f", EnumSet.of(Path.Type.file));
+        final MultipartUpload part = new S3MultipartService(session).find(test);
+        assertNull(part);
+    }
+}
