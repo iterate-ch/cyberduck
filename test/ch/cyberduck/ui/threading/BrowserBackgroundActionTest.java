f2dc2b6bc8a95a2c99283b4b3a1abe3bb6be9ab3
David Kocher <dkocher@iterate.ch>
2014-10-22 13:26:34 +0000

    Former-commit-id: 7d22fc5cb3aa89d044032759b35e1e6d985220eb

--- test/ch/cyberduck/ui/threading/BrowserBackgroundActionTest.java
+++ test/ch/cyberduck/ui/threading/BrowserBackgroundActionTest.java
@@ -2,21 +2,13 @@ package ch.cyberduck.ui.threading;
 
 import ch.cyberduck.core.AbstractTestCase;
 import ch.cyberduck.core.Cache;
-import ch.cyberduck.core.Factory;
 import ch.cyberduck.core.Host;
-import ch.cyberduck.core.HostKeyCallback;
-import ch.cyberduck.core.LoginCallback;
 import ch.cyberduck.core.NullSession;
 import ch.cyberduck.core.Path;
-import ch.cyberduck.core.Protocol;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.threading.MainAction;
 import ch.cyberduck.ui.AbstractController;
-import ch.cyberduck.ui.Controller;
-import ch.cyberduck.ui.HostKeyCallbackFactory;
-import ch.cyberduck.ui.LoginCallbackFactory;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
@@ -25,32 +17,6 @@ import static org.junit.Assert.assertNotNull;
  * @version $Id$
  */
 public class BrowserBackgroundActionTest extends AbstractTestCase {
-
-    @Before
-    public void factory() {
-        LoginCallbackFactory.addFactory(Factory.NATIVE_PLATFORM, new LoginCallbackFactory() {
-            @Override
-            public LoginCallback create(final Controller c) {
-                return null;
-            }
-
-            @Override
-            protected LoginCallback create() {
-                return null;
-            }
-        });
-        HostKeyCallbackFactory.addFactory(Factory.NATIVE_PLATFORM, new HostKeyCallbackFactory() {
-            @Override
-            public HostKeyCallback create(final Controller c, final Protocol protocol) {
-                return null;
-            }
-
-            @Override
-            protected HostKeyCallback create() {
-                return null;
-            }
-        });
-    }
 
     @Test
     public void testGetSessions() throws Exception {
