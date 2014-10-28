bffd0898b29db0c1c1678acdc2bcc954aa35daaa
David Kocher <dkocher@iterate.ch>
2014-10-28 10:28:59 +0000

    Doc
    Former-commit-id: e73fe146384f6e5320be9cada0af7524e61b875c

--- source/ch/cyberduck/core/openstack/SwiftObjectListService.java
+++ source/ch/cyberduck/core/openstack/SwiftObjectListService.java
@@ -92,7 +92,8 @@ public class SwiftObjectListService implements ListService {
                             .equals(object.getMimeType()) ? EnumSet.of(Path.Type.directory) : EnumSet.of(Path.Type.file);
                     if(StringUtils.endsWith(object.getName(), String.valueOf(Path.DELIMITER))) {
                         if(children.contains(new Path(directory, PathNormalizer.name(object.getName()), EnumSet.of(Path.Type.directory), attributes))) {
-                            // There is already a placeholder object
+                            // There is already a real placeholder file with application/directory MIME type. Only
+                            // add virtual directory if the placeholder object is missing
                             continue;
                         }
                         types.add(Path.Type.placeholder);
