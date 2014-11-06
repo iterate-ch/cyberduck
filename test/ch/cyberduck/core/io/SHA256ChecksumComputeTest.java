e96c4d8cfe85c93edaa3c7e4a8a18f0e11ff7e52
David Kocher <dkocher@iterate.ch>
2014-11-06 14:36:24 +0000

    Add test.
    Former-commit-id: f2aabbc9d7df018efb17aa16e8c38f978350c947

---
+++ test/ch/cyberduck/core/io/SHA256ChecksumComputeTest.java
@@ -0,0 +1,32 @@
+package ch.cyberduck.core.io;
+
+/*
+ * Copyright (c) 2002-2014 David Kocher. All rights reserved.
+ * http://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * feedback@cyberduck.io
+ */
+
+import org.apache.commons.io.input.NullInputStream;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SHA256ChecksumComputeTest {
+
+    @Test
+    public void testCompute() throws Exception {
+        assertEquals("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", new SHA256ChecksumCompute().compute(new NullInputStream(0)));
+    }
+}
