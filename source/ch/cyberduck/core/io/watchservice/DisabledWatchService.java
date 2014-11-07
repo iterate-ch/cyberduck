63481aa6de70e514b9a52855afa0b0f16b4b7d1f
David Kocher <dkocher@iterate.ch>
2014-11-07 14:37:39 +0000

    Add disabled implementation.
    Former-commit-id: f55213c390807554ba62a255602f547f092b4bd2

---
+++ source/ch/cyberduck/core/io/watchservice/DisabledWatchService.java
@@ -0,0 +1,61 @@
+package ch.cyberduck.core.io.watchservice;
+
+/*
+ * Copyright (c) 2002-2014 David Kocher. All rights reserved.
+ * http://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * feedback@cyberduck.io
+ */
+
+import java.io.IOException;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.Watchable;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @version $Id$
+ */
+public class DisabledWatchService implements RegisterWatchService {
+
+    @Override
+    public WatchKey register(final Watchable file, final WatchEvent.Kind<?>[] events, final WatchEvent.Modifier... modifiers) throws IOException {
+        return null;
+    }
+
+    @Override
+    public void release() throws IOException {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+
+    @Override
+    public WatchKey poll() {
+        return null;
+    }
+
+    @Override
+    public WatchKey poll(final long timeout, final TimeUnit unit) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public WatchKey take() throws InterruptedException {
+        return null;
+    }
+}
--- test/ch/cyberduck/core/local/FileWatcherTest.java
+++ test/ch/cyberduck/core/local/FileWatcherTest.java
@@ -2,7 +2,7 @@ package ch.cyberduck.core.local;
 
 import ch.cyberduck.core.AbstractTestCase;
 import ch.cyberduck.core.Local;
-import ch.cyberduck.core.io.watchservice.NIOEventWatchService;
+import ch.cyberduck.core.io.watchservice.DisabledWatchService;
 
 import org.junit.Test;
 
@@ -18,22 +18,22 @@ public class FileWatcherTest extends AbstractTestCase {
     @Test
     public void testMatch() throws IOException {
         assertTrue(
-                new FileWatcher(new NIOEventWatchService()).matches(
+                new FileWatcher(new DisabledWatchService()).matches(
                         new Local("/private/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"),
                         new Local("/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"))
         );
         assertTrue(
-                new FileWatcher(new NIOEventWatchService()).matches(
+                new FileWatcher(new DisabledWatchService()).matches(
                         new Local("/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"),
                         new Local("/private/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"))
         );
         assertTrue(
-                new FileWatcher(new NIOEventWatchService()).matches(
+                new FileWatcher(new DisabledWatchService()).matches(
                         new Local("edit.html"),
                         new Local("/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"))
         );
         assertTrue(
-                new FileWatcher(new NIOEventWatchService()).matches(
+                new FileWatcher(new DisabledWatchService()).matches(
                         new Local("edit.html"),
                         new Local("/private/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"))
         );
