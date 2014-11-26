a8302eba765a0d0918ae5f6aa5cd9064714be541
David Kocher <dkocher@iterate.ch>
2014-11-26 15:49:03 +0000

    Add homebrew formula.
    Former-commit-id: 3d30a5df37a29c23ce59534384d71eac5e9b523c

---
+++ www/update/duck.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+class Duck < Formula
+  homepage "https://cyberduck.io/"
+  # Reference to the CLI version of Cyberduck
+  url "https://update.cyberduck.io/cli/duck-_VERSION_.tar.gz"
+  version "_VERSION_"
+  md5 "_MD5SUM_"
+
+  def install
+    # Because compiling would need a JDK and xcodebuild we just use the pre-compiled binary.
+    libexec.install Dir['*']
+    bin.install_symlink "#{libexec}/Contents/MacOS/duck" => "duck"
+  end
+
+  test do
+    system "#{bin}/duck", "-version"
+  end
+
+  def caveats;
+    <<-EOS.undent
+    For more information refer to
+      https://trac.cyberduck.io/wiki/help/en/howto/cli
+    EOS
+  end
+end
