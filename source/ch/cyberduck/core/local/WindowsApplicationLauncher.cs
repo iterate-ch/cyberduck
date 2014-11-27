5d4ca072291544f6f06ed3b3b582ef6634fd9d19
Yves Langisch <yla@iterate.ch>
2014-11-27 16:00:32 +0000

    Fix #8359. Process exit detection does not work reliably for .NET apps.
    Former-commit-id: 899ff5c4ce4f36b9a0b00e1708b064ae7ef149af

--- source/ch/cyberduck/core/local/WindowsApplicationLauncher.cs
+++ source/ch/cyberduck/core/local/WindowsApplicationLauncher.cs
@@ -50,11 +50,6 @@ namespace Ch.Cyberduck.Core.Local
                 process.StartInfo.FileName = GetExecutableCommand(application.getIdentifier());
                 process.StartInfo.Arguments = "\"" + local.getAbsolute() + "\"";
             }
-            if (callback != null)
-            {
-                process.EnableRaisingEvents = true;
-                process.Exited += delegate { callback.callback(); };
-            }
             return Utils.StartProcess(process);
         }
 
