2e9edc588472de5b728a8736b41987e9effdcb6d
David Kocher <dkocher@iterate.ch>
2014-12-01 15:54:18 +0000

    Fix path
    Former-commit-id: 74a8dc87d6b80fa976ddfd0d048121a2f5c84044

--- debug.sh
+++ debug.sh
@@ -25,11 +25,11 @@ usage() {
 enable() {
 	# When enabled, you can connect to the running application using 
 	# -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
-	cp Info.plist.debug build/Release/Cyberduck.app/Contents/Info.plist;
+	cp Info.plist.debug build/Cyberduck.app/Contents/Info.plist;
 }
 
 disable() {
-	cp Info.plist build/Release/Cyberduck.app/Contents/Info.plist;
+	cp Info.plist build/Cyberduck.app/Contents/Info.plist;
 }
 
 while [ "$1" != "" ] # When there are arguments...
