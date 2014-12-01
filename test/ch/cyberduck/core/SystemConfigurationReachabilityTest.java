8abb25cfd5af5267d29b08bb40b5bc25c5d59a08
David Kocher <dkocher@iterate.ch>
2014-12-01 20:58:30 +0000

    Former-commit-id: 52812831210a69816f6042f1884d06f5f1e61aea

--- test/ch/cyberduck/core/SystemConfigurationReachabilityTest.java
+++ test/ch/cyberduck/core/SystemConfigurationReachabilityTest.java
@@ -11,14 +11,24 @@ import static org.junit.Assert.assertTrue;
 public class SystemConfigurationReachabilityTest extends AbstractTestCase {
 
     @Test
-    public void testIsReachable() throws Exception {
+    public void testIsReachablePort80() throws Exception {
         final Reachability r = new SystemConfigurationReachability();
         assertTrue(r.isReachable(
                 new Host("cyberduck.ch", 80)
         ));
+    }
+
+    @Test
+    public void testIsReachablePort22() throws Exception {
+        final Reachability r = new SystemConfigurationReachability();
         assertTrue(r.isReachable(
                 new Host("cyberduck.ch", 22)
         ));
+    }
+
+    @Test
+    public void testNotReachablePort22() throws Exception {
+        final Reachability r = new SystemConfigurationReachability();
         assertFalse(r.isReachable(
                 new Host("a.cyberduck.ch", 22)
         ));
