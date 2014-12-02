d825483237dbee61553d147f45c975d172c58cf7
David Kocher <dkocher@iterate.ch>
2014-12-02 15:59:22 +0000

    Add implementations.
    Former-commit-id: 323603d4a237904c054b80f627a4cbe66a239438

---
+++ source/ch/cyberduck/core/preferences/TemporarySupportDirectoryFinder.java
@@ -0,0 +1,36 @@
+package ch.cyberduck.core.preferences;
+
+/*
+ * Copyright (c) 2002-2014 David Kocher. All rights reserved.
+ * http://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * feedback@cyberduck.io
+ */
+
+import ch.cyberduck.core.Local;
+import ch.cyberduck.core.LocalFactory;
+import ch.cyberduck.core.Preferences;
+
+/**
+ * @version $Id$
+ */
+public class TemporarySupportDirectoryFinder implements SupportDirectoryFinder {
+
+    private final Preferences preferences = Preferences.instance();
+
+    @Override
+    public Local find() {
+        return LocalFactory.get(preferences.getProperty("tmp.dir"));
+    }
+}
