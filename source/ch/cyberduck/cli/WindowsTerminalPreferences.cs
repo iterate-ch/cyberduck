7a55fdb838dd02e2c8ff92aa5853554a65094387
David Kocher <dkocher@iterate.ch>
2014-12-05 11:33:58 +0000

    Disable custom certificate store. Use default terminal callback.
    Former-commit-id: 90792ca9bf0101fbf7a6df757f75653fadf04d18

--- source/ch/cyberduck/cli/WindowsTerminalPreferences.cs
+++ source/ch/cyberduck/cli/WindowsTerminalPreferences.cs
@@ -40,7 +40,6 @@ namespace Ch.Cyberduck.Cli
             defaults.put("factory.applicationfinder.class", typeof (RegistryApplicationFinder).AssemblyQualifiedName);
             defaults.put("factory.local.class", typeof (SystemLocal).AssemblyQualifiedName);
             defaults.put("factory.passwordstore.class", typeof (Keychain).AssemblyQualifiedName);
-            defaults.put("factory.certificatestore.class", typeof (Keychain).AssemblyQualifiedName);
             defaults.put("factory.proxy.class", typeof (SystemProxy).AssemblyQualifiedName);
             defaults.put("factory.reachability.class", typeof (TcpReachability).AssemblyQualifiedName);
             defaults.put("factory.filedescriptor.class", typeof (Win32FileDescriptor).AssemblyQualifiedName);
