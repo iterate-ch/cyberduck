c83e767cd179360fa8c1280373b681fd22693671
David Kocher <dkocher@iterate.ch>
2014-12-08 11:09:04 +0000

    Fix #8270.
    Former-commit-id: 4a5efec844c68e4d56645611dac2ad455342fdf0

--- source/de/zathras/UKSystemInfo.h
+++ source/de/zathras/UKSystemInfo.h
@@ -6,11 +6,6 @@
 //  Copyright 2004 M. Uli Kusterer. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#include <Carbon/Carbon.h>
 
-
-void		UKGetSystemVersionComponents( SInt32* outMajor, SInt32* outMinor, SInt32* outBugfix );	// System version as the separate components (Major.Minor.Bugfix).
-
-// Don't use the following for new code:
-//	(Since the number is in BCD, the maximum for minor and bugfix revisions is 9, so this returns 1049 for 10.4.10)
-long		UKSystemVersion();							// System version as BCD number, I.e. 0xMMmb
+void UKGetSystemVersionComponents( SInt32* outMajor, SInt32* outMinor, SInt32* outBugfix );	// System version as the separate components (Major.Minor.Bugfix).
--- source/de/zathras/UKSystemInfo.m
+++ source/de/zathras/UKSystemInfo.m
@@ -7,33 +7,12 @@
 //
 
 #import "UKSystemInfo.h"
-#include <Carbon/Carbon.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
 
-void	UKGetSystemVersionComponents( SInt32* outMajor, SInt32* outMinor, SInt32* outBugfix )
+void UKGetSystemVersionComponents( SInt32* outMajor, SInt32* outMinor, SInt32* outBugfix )
 {
-	SInt32		sysVersion = UKSystemVersion();
-	if( sysVersion >= MAC_OS_X_VERSION_10_4 )
-	{
-		Gestalt( gestaltSystemVersionMajor, outMajor );
-		Gestalt( gestaltSystemVersionMinor, outMinor );
-		Gestalt( gestaltSystemVersionBugFix, outBugfix );
-	}
-	else
-	{
-		*outMajor = ((sysVersion & 0x0000F000) >> 12) * 10 + ((sysVersion & 0x00000F00) >> 8);
-		*outMinor = (sysVersion & 0x000000F0) >> 4;
-		*outBugfix = sysVersion & 0x0000000F;
-	}
-}
-
-long	UKSystemVersion()
-{
-	SInt32		sysVersion;
-
-	if( Gestalt( gestaltSystemVersion, &sysVersion ) != noErr )
-		return 0;
-
-	return sysVersion;
+	Gestalt( gestaltSystemVersionMajor, outMajor );
+	Gestalt( gestaltSystemVersionMinor, outMinor );
+	Gestalt( gestaltSystemVersionBugFix, outBugfix );
 }
