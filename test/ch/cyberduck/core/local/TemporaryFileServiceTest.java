a15628b868bda4cf43afcdeb4ae48a6f2ce0f3e7
David Kocher <dkocher@iterate.ch>
2014-12-08 19:05:08 +0000

    Fix test.
    Former-commit-id: 1553ad3e5fd8e56a3b081e4092aaedce0efe914c

--- test/ch/cyberduck/core/local/TemporaryFileServiceTest.java
+++ test/ch/cyberduck/core/local/TemporaryFileServiceTest.java
@@ -2,6 +2,7 @@ package ch.cyberduck.core.local;
 
 import ch.cyberduck.core.AbstractTestCase;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathNormalizer;
 
 import org.junit.Test;
 
@@ -21,7 +22,7 @@ public class TemporaryFileServiceTest extends AbstractTestCase {
                 new TemporaryFileService().create("u", new Path("/p/f", EnumSet.of(Path.Type.file))).getAbsolute());
         final Path file = new Path("/p/f", EnumSet.of(Path.Type.file));
         file.attributes().setRegion("region");
-        assertEquals(temp + "u/p/f",
+        assertEquals(PathNormalizer.normalize(temp + "/u/p/f"),
                 new TemporaryFileService().create("u", file).getAbsolute());
     }
 
@@ -32,14 +33,14 @@ public class TemporaryFileServiceTest extends AbstractTestCase {
             final Path file = new Path("/p/f", EnumSet.of(Path.Type.file));
             file.attributes().setRegion("region");
             file.attributes().setVersionId("2");
-            assertEquals(temp + "u/p/2/f",
+            assertEquals(PathNormalizer.normalize(temp + "/u/p/2/f"),
                     new TemporaryFileService().create("u", file).getAbsolute());
         }
         {
             final Path file = new Path("/p", EnumSet.of(Path.Type.directory));
             file.attributes().setRegion("region");
             file.attributes().setVersionId("2");
-            assertEquals(temp + "u/region2/p",
+            assertEquals(PathNormalizer.normalize(temp + "/u/region2/p"),
                     new TemporaryFileService().create("u", file).getAbsolute());
         }
     }
@@ -49,10 +50,10 @@ public class TemporaryFileServiceTest extends AbstractTestCase {
         final String temp = System.getProperty("java.io.tmpdir");
         final Path file = new Path("/container", EnumSet.of(Path.Type.directory));
         file.attributes().setRegion("region");
-        assertEquals(temp + "u/region/container",
+        assertEquals(PathNormalizer.normalize(temp + "/u/region/container"),
                 new TemporaryFileService().create("u", file).getAbsolute());
         file.attributes().setVersionId("2");
-        assertEquals(temp + "u/region2/container",
+        assertEquals(PathNormalizer.normalize(temp + "/u/region2/container"),
                 new TemporaryFileService().create("u", file).getAbsolute());
     }
 }
