93548be44b3389ea3cd5f9d97e286c3c8721768f
David Kocher <dkocher@iterate.ch>
2015-01-07 20:40:33 +0000

    Include *.ps1 when replacing tokens.
    Former-commit-id: a39fa5d82bdd03aef6df6f0199e4d7f569a8d7be

--- setup/chocolatey/tools/chocolateyInstall.ps1
+++ setup/chocolatey/tools/chocolateyInstall.ps1
@@ -1,49 +1,7 @@
-﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+﻿$packageName = 'duck'
+$installerType = 'EXE'
+$url = 'https://dist.duck.sh/duck-${VERSION}.${REVISION}.exe'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
 
-$packageName = 'duck' # arbitrary name for the package, used in messages
-$installerType = 'EXE' #only one of these: exe, msi, msu
-$url = 'https://update.cyberduck.io/cli/nightly/duck-4.6.2.16262.exe' # download url
-$silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
-# main helpers - these have error handling tucked into them already
-# installer, will assert administrative rights
-
-# if removing $url64, please remove from here
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
-# download and unpack a zip file
-
-# if removing $url64, please remove from here
-#Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
-
-#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
-  # downloader that the main helpers use to download items
-
-  # if removing $url64, please remove from here
-  #Get-ChocolateyWebFile "$packageName" 'DOWNLOAD_TO_FILE_FULL_PATH' "$url" "$url64"
-  # installer, will assert administrative rights - used by Install-ChocolateyPackage
-  #Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" '_FULLFILEPATH_' -validExitCodes $validExitCodes
-  # unzips a file to the specified location - auto overwrites existing content
-  #Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  # Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-  #Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
-  # add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
-  #Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
-  # add specific files as shortcuts to the desktop
-  #$target = Join-Path $MyInvocation.MyCommand.Definition "$($packageName).exe"
-  #Install-ChocolateyDesktopLink $target
-
-  #------- ADDITIONAL SETUP -------#
-  # make sure to uncomment the error handling if you have additional setup to do
-
-  # outputs the bitness of the OS (either "32" or "64")
-  #$osBitness = Get-ProcessorBits
-
-
-  # the following is all part of error handling
-  #Write-ChocolateySuccess "$packageName"
-#} catch {
-  #Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  #throw
-#}
