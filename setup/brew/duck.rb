fe34e1ae38c5be2f349877e457ff90d2bd72bca1
David Kocher <dkocher@iterate.ch>
2015-01-07 08:20:10 +0000

    Use install name tool to link against libcrypto from brew.
    Former-commit-id: f8f97e426e2bcfd424386acaffa91254f4915c66

--- setup/brew/duck.rb
+++ setup/brew/duck.rb
@@ -6,10 +6,12 @@ class Duck < Formula
   depends_on :java => :build
   depends_on :xcode => :build
   depends_on "ant" => :build
-  depends_on "openssl" => :recommended
+  depends_on "openssl"
 
   def install
-    system "ant", "-Drevision=${REVISION}", "cli"
+    system "ant", "-Dbuild.compile.target=1.7", "-Drevision=${REVISION}", "cli"
+    system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/libPrime.dylib"
+    system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/librococoa.dylib"
     libexec.install Dir["build/duck.bundle/*"]
     bin.install_symlink "#{libexec}/Contents/MacOS/duck" => "duck"
   end
