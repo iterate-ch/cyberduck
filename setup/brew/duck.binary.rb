5bc85599bfa73af1c4fb55793ced9c81fca4901a
David Kocher <dkocher@iterate.ch>
2015-01-08 13:36:27 +0000

    %x-literals should be delimited by ( and )
    Former-commit-id: da81bbf9ab71925ede31e1f571cab1f2f8a19ef8

--- setup/brew/duck.binary.rb
+++ setup/brew/duck.binary.rb
@@ -13,7 +13,7 @@ class Duck < Formula
   end
 
   test do
-    unless "Cyberduck ${VERSION} (${REVISION})\n".eql? %x[#{bin}/duck -version]
+    unless "Cyberduck ${VERSION} (${REVISION})\n".eql? %x(#{bin}/duck -version)
       raise "Version mismatch"
     end
     filename = (testpath/"test")
--- setup/brew/duck.source.rb
+++ setup/brew/duck.source.rb
@@ -21,7 +21,7 @@ class Duck < Formula
   end
 
   test do
-    unless "Cyberduck ${VERSION} (${REVISION})\n".eql? %x[#{bin}/duck -version]
+    unless "Cyberduck ${VERSION} (${REVISION})\n".eql? %x(#{bin}/duck -version)
       raise "Version mismatch"
     end
     filename = (testpath/"test")
