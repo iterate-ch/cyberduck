14108635dbece4a0d4524b1ca452d1989ceaf4fc
David Kocher <dkocher@iterate.ch>
2015-01-11 10:09:40 +0000

    Fix dependency.
    Former-commit-id: 296c0d450597a472c8879ca4c80a2843c5745f04

--- setup/brew/duck.source.rb
+++ setup/brew/duck.source.rb
@@ -12,7 +12,7 @@ class Duck < Formula
   def install
     system "ant", "-Dbuild.compile.target=1.7", "-Drevision=#{version.to_str[/(\d\.\d(\.\d)?)\.(\d+)/, 3]}", "cli"
     system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/libPrime.dylib"
-    system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/librococoa.dylib"
+    system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/libLocal.dylib"
     libexec.install Dir["build/duck.bundle/*"]
     bin.install_symlink "#{libexec}/Contents/MacOS/duck" => "duck"
   end
