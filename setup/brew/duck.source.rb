d398a7ce22d5a975fd908215ee098325c5efb8fa
David Kocher <dkocher@iterate.ch>
2015-01-11 23:06:34 +0000

    Remove `openssl` dependency. See r16364 and r16365.
    Former-commit-id: 589ec43248a11cb320e93700a5fd6528defa39ec

--- setup/brew/duck.source.rb
+++ setup/brew/duck.source.rb
@@ -7,12 +7,9 @@ class Duck < Formula
   depends_on :java => [:build, "1.7"]
   depends_on :xcode => :build
   depends_on "ant" => :build
-  depends_on "openssl"
 
   def install
     system "ant", "-Dbuild.compile.target=1.7", "-Drevision=#{version.to_str[/(\d\.\d(\.\d)?)\.(\d+)/, 3]}", "cli"
-    system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/libPrime.dylib"
-    system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/libLocal.dylib"
     libexec.install Dir["build/duck.bundle/*"]
     bin.install_symlink "#{libexec}/Contents/MacOS/duck" => "duck"
   end
