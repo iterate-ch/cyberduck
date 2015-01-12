e934ec70342bcb60fe93d98aadbd4c8109180b79
David Kocher <dkocher@iterate.ch>
2015-01-12 11:08:52 +0000

    Former-commit-id: d6abe0960efa47c122e264ecbab4005e3e709dfc

--- source/ch/cyberduck/core/AbstractController.java
+++ source/ch/cyberduck/core/AbstractController.java
@@ -88,7 +88,7 @@ public abstract class AbstractController implements Controller {
     /**
      * Will queue up the <code>BackgroundAction</code> to be run in a background thread. Will be executed
      * as soon as no other previous <code>BackgroundAction</code> is pending.
-     * Will return immediatly but not run the runnable before the lock of the runnable is acquired.
+     * Will return immediately but not run the runnable before the lock of the runnable is acquired.
      *
      * @param action The runnable to execute in a secondary Thread
      * @see java.lang.Thread
