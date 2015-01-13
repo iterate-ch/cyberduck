7fc011399367e988f635ab7080ceeeb4f4ee49c8
David Kocher <dkocher@iterate.ch>
2015-01-13 09:02:52 +0000

    Fix line feed.
    Former-commit-id: 30769a9dfb8c4886e39202d0c6f0b30f7d09d8b6

--- source/ch/cyberduck/cli/TerminalHostKeyVerifier.java
+++ source/ch/cyberduck/cli/TerminalHostKeyVerifier.java
@@ -46,7 +46,7 @@ public class TerminalHostKeyVerifier extends OpenSSHHostKeyVerifier {
 
     @Override
     protected boolean isUnknownKeyAccepted(final String hostname, final PublicKey key) throws ConnectionCanceledException, ChecksumException {
-        final String input = console.readLine("%s. %s %s? (y/n): ", LocaleFactory.localizedString("Unknown fingerprint", "Sftp"),
+        final String input = console.readLine("%n%s. %s %s? (y/n): ", LocaleFactory.localizedString("Unknown fingerprint", "Sftp"),
                 MessageFormat.format(LocaleFactory.localizedString("The fingerprint for the {1} key sent by the server is {0}.", "Sftp"),
                         new MD5ChecksumCompute().fingerprint(key),
                         KeyType.fromKey(key).name()),
@@ -64,7 +64,7 @@ public class TerminalHostKeyVerifier extends OpenSSHHostKeyVerifier {
 
     @Override
     protected boolean isChangedKeyAccepted(final String hostname, final PublicKey key) throws ConnectionCanceledException, ChecksumException {
-        final String input = console.readLine("%s. %s %s? (y/n): ", LocaleFactory.localizedString("Changed fingerprint", "Sftp"),
+        final String input = console.readLine("%n%s. %s %s? (y/n): ", LocaleFactory.localizedString("Changed fingerprint", "Sftp"),
                 MessageFormat.format(LocaleFactory.localizedString("The fingerprint for the {1} key sent by the server is {0}.", "Sftp"),
                         new MD5ChecksumCompute().fingerprint(key),
                         KeyType.fromKey(key).name()),
