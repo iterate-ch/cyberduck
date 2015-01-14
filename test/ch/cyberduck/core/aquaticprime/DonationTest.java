1e5661f934c4fd013ecc17d33781a9d8ae68ec43
David Kocher <dkocher@iterate.ch>
2015-01-14 09:12:17 +0000

    Ignore tests that require framework in @executable_path.
    Former-commit-id: 34cf7312bf59c720829d5557f2a68ef5c7406272

--- test/ch/cyberduck/core/aquaticprime/DonationTest.java
+++ test/ch/cyberduck/core/aquaticprime/DonationTest.java
@@ -7,6 +7,7 @@ import ch.cyberduck.core.local.FinderLocal;
 import ch.cyberduck.core.local.LocalTouchFactory;
 import ch.cyberduck.core.test.Depends;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -15,6 +16,7 @@ import static org.junit.Assert.assertFalse;
  * @version $Id$
  */
 @Depends(platform = Factory.Platform.Name.mac)
+@Ignore
 public class DonationTest extends AbstractTestCase {
 
     @Test
