8d091035439a4e5f17edd9a8d9258f340aabc447
David Kocher <dkocher@iterate.ch>
2015-01-26 15:56:19 +0000

    Switch to use `HEAD` only to read attributes of file. Fix #8523.
    Former-commit-id: a20400431705c7a9395f817bb0417e9125e690de

--- source/ch/cyberduck/core/dav/DAVAttributesFeature.java
+++ source/ch/cyberduck/core/dav/DAVAttributesFeature.java
@@ -24,10 +24,7 @@ import ch.cyberduck.core.PathCache;
 import ch.cyberduck.core.date.InvalidDateException;
 import ch.cyberduck.core.date.RFC1123DateFormatter;
 import ch.cyberduck.core.exception.BackgroundException;
-import ch.cyberduck.core.exception.InteroperabilityException;
-import ch.cyberduck.core.exception.NotfoundException;
 import ch.cyberduck.core.features.Attributes;
-import ch.cyberduck.core.shared.DefaultAttributesFeature;
 
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.http.HttpHeaders;
@@ -35,10 +32,8 @@ import org.apache.http.client.methods.HttpHead;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 
-import com.github.sardine.DavResource;
 import com.github.sardine.impl.SardineException;
 import com.github.sardine.impl.handler.HeadersResponseHandler;
 
@@ -63,51 +58,22 @@ public class DAVAttributesFeature implements Attributes {
     @Override
     public PathAttributes find(final Path file) throws BackgroundException {
         try {
-            final List<DavResource> resources = session.getClient().list(new DAVPathEncoder().encode(file));
-            for(final DavResource resource : resources) {
-                final PathAttributes attributes = new PathAttributes();
-                if(resource.getModified() != null) {
-                    attributes.setModificationDate(resource.getModified().getTime());
-                }
-                if(resource.getCreation() != null) {
-                    attributes.setCreationDate(resource.getCreation().getTime());
-                }
-                if(resource.getContentLength() != null) {
-                    attributes.setSize(resource.getContentLength());
-                }
-                attributes.setChecksum(resource.getEtag());
-                attributes.setETag(resource.getEtag());
-                return attributes;
+            final Map<String, String> headers = session.getClient().execute(
+                    new HttpHead(new DAVPathEncoder().encode(file)), new HeadersResponseHandler());
+            final PathAttributes attributes = new PathAttributes();
+            try {
+                attributes.setModificationDate(dateParser.parse(headers.get(HttpHeaders.LAST_MODIFIED)).getTime());
             }
-            throw new NotfoundException(file.getAbsolute());
+            catch(InvalidDateException p) {
+                log.warn(String.format("%s is not RFC 1123 format %s", headers.get(HttpHeaders.LAST_MODIFIED), p.getMessage()));
+            }
+            attributes.setSize(NumberUtils.toLong(headers.get(HttpHeaders.CONTENT_LENGTH), -1));
+            attributes.setChecksum(headers.get(HttpHeaders.CONTENT_MD5));
+            attributes.setETag(headers.get(HttpHeaders.ETAG));
+            return attributes;
         }
         catch(SardineException e) {
-            try {
-                throw new DAVExceptionMappingService().map("Failure to read attributes of {0}", e, file);
-            }
-            catch(InteroperabilityException i) {
-                try {
-                    final Map<String, String> headers = session.getClient().execute(
-                            new HttpHead(new DAVPathEncoder().encode(file)), new HeadersResponseHandler());
-                    final PathAttributes attributes = new PathAttributes();
-                    try {
-                        attributes.setModificationDate(dateParser.parse(headers.get(HttpHeaders.LAST_MODIFIED)).getTime());
-                    }
-                    catch(InvalidDateException p) {
-                        log.warn(String.format("%s is not RFC 1123 format %s", headers.get(HttpHeaders.LAST_MODIFIED), e.getMessage()));
-                    }
-                    attributes.setSize(NumberUtils.toLong(headers.get(HttpHeaders.CONTENT_LENGTH), -1));
-                    attributes.setChecksum(headers.get(HttpHeaders.CONTENT_MD5));
-                    attributes.setETag(headers.get(HttpHeaders.ETAG));
-                    return attributes;
-                }
-                catch(SardineException f) {
-                    return new DefaultAttributesFeature(session).withCache(cache).find(file);
-                }
-                catch(IOException f) {
-                    throw new DefaultIOExceptionMappingService().map(e, file);
-                }
-            }
+            throw new DAVExceptionMappingService().map("Failure to read attributes of {0}", e, file);
         }
         catch(IOException e) {
             throw new DefaultIOExceptionMappingService().map(e, file);
