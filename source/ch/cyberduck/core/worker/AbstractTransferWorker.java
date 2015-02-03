1946a68eeade344ff89ecb34565dfbe852f56be7
David Kocher <dkocher@iterate.ch>
2015-02-03 20:51:39 +0000

    Revert: Fix to run #apply and #complete of filter with renamed files. Former-commit-id: f0e1f96b6546f648bcb2c0ed3d0459eacfe44b80

--- source/ch/cyberduck/core/worker/AbstractTransferWorker.java
+++ source/ch/cyberduck/core/worker/AbstractTransferWorker.java
@@ -267,10 +267,7 @@ public abstract class AbstractTransferWorker extends Worker<Boolean> implements 
                             final TransferStatus status = filter.prepare(file, local, parent);
                             table.put(file, status);
                             // Apply filter
-                            filter.apply(
-                                    status.getRename().remote != null ? status.getRename().remote : item.remote,
-                                    status.getRename().local != null ? status.getRename().local : item.local,
-                                    status, progressListener);
+                            filter.apply(item.remote, item.local, status, progressListener);
                             // Add transfer length to total bytes
                             transfer.addSize(status.getLength());
                             // Add skipped bytes
@@ -394,10 +391,7 @@ public abstract class AbstractTransferWorker extends Worker<Boolean> implements 
                         if(!status.isFailure()) {
                             // Post process of file.
                             try {
-                                filter.complete(
-                                        status.getRename().remote != null ? status.getRename().remote : item.remote,
-                                        status.getRename().local != null ? status.getRename().local : item.local,
-                                        options, status, progressListener);
+                                filter.complete(item.remote, item.local, options, status, progressListener);
                             }
                             catch(BackgroundException e) {
                                 log.warn(String.format("Ignore failure in completion filter for %s", item));
