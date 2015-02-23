dd39e28bc35a7f802ec711081ca6cf2dabc4ed7c
David Kocher <dkocher@iterate.ch>
2015-02-23 16:45:26 +0000

    Add byte range end for range retrieval requests requests.
    Former-commit-id: 8e16906402e643e0d0372f2ba65da6c562a388d0

--- source/ch/cyberduck/core/s3/S3ReadFeature.java
+++ source/ch/cyberduck/core/s3/S3ReadFeature.java
@@ -59,7 +59,7 @@ public class S3ReadFeature implements Read {
                         null, // ifUnmodifiedSince
                         null, // ifMatch
                         null, // ifNoneMatch
-                        status.isAppend() ? status.getSkip() : null, null);
+                        status.isAppend() ? status.getSkip() : null, status.getLength());
                 return object.getDataInputStream();
             }
             else {
@@ -70,7 +70,7 @@ public class S3ReadFeature implements Read {
                         null, // ifUnmodifiedSince
                         null, // ifMatch
                         null, // ifNoneMatch
-                        status.isAppend() ? status.getSkip() : null, null);
+                        status.isAppend() ? status.getSkip() : null, status.getLength());
             }
             if(log.isDebugEnabled()) {
                 log.debug(String.format("Reading stream with content length %d", object.getContentLength()));
