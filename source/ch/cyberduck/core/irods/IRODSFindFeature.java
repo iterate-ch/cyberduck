030aae3773b7efa164448727102a49d793b6b3d8
markobosancic <markobosancic@iterate.ch>
2015-02-25 01:43:19 +0000

    [add:IRODS-4] Find feature for iRODS implemented and unit tested
    Former-commit-id: ee7f7c23067fe2edf3ac99a8296124e7758bfba8

---
+++ source/ch/cyberduck/core/irods/IRODSFindFeature.java
@@ -0,0 +1,84 @@
+package ch.cyberduck.core.irods;
+
+/*
+ * Copyright (c) 2002-2013 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to feedback@cyberduck.ch
+ */
+
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathCache;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Find;
+
+import org.irods.jargon.core.exception.JargonException;
+import org.irods.jargon.core.pub.io.IRODSFile;
+
+/**
+ * @version $Id$
+ */
+public class IRODSFindFeature implements Find {
+
+    private IRODSSession session;
+
+    private PathCache cache;
+
+    public IRODSFindFeature(IRODSSession session) {
+        this.session = session;
+        this.cache = PathCache.empty();
+    }
+
+    @Override
+    public boolean find(final Path file) throws BackgroundException {
+        if(file.isRoot()) {
+            return true;
+        }
+        final AttributedList<Path> list;
+        if(cache.containsKey(file.getParent())) {
+            list = cache.get(file.getParent());
+        }
+        else {
+            list = new AttributedList<Path>();
+            cache.put(file.getParent(), list);
+        }
+        if(list.contains(file)) {
+            // Previously found
+            return true;
+        }
+        if(cache.isHidden(file)) {
+            // Previously not found
+            return false;
+        }
+        try {
+            final IRODSFile irodsFile = session.getIrodsFileSystemAO().getIRODSFileFactory().instanceIRODSFile(file.getAbsolute());
+            final boolean found = session.getIrodsFileSystemAO().isFileExists(irodsFile);
+            if(found) {
+                list.add(file);
+            }
+            else {
+                list.attributes().addHidden(file);
+            }
+            return found;
+        } catch(JargonException e) {
+            throw new IRODSExceptionMappingService().map(e);
+        }
+    }
+
+    @Override
+    public Find withCache(final PathCache cache) {
+        this.cache = cache;
+        return this;
+    }
+}
