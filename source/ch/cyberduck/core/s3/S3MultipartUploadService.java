7e85b9f09163c47594eb45ab2d9e57bddc9dc1e8
David Kocher <dkocher@iterate.ch>
2015-04-15 13:37:10 +0000

    Fix error message.
    Former-commit-id: caf020f18e98dd4cba5727fd0509b36d6fdea64e

--- source/ch/cyberduck/core/s3/S3MultipartUploadService.java
+++ source/ch/cyberduck/core/s3/S3MultipartUploadService.java
@@ -194,7 +194,7 @@ public class S3MultipartUploadService extends HttpUploadFeature<StorageObject, M
                     reference = complete.getEtag();
                 }
                 if(!expected.equals(reference)) {
-                    throw new ChecksumException("Upload {0} failed",
+                    throw new ChecksumException(MessageFormat.format(LocaleFactory.localizedString("Upload {0} failed", "Error"), file.getName()),
                             MessageFormat.format("Mismatch between MD5 hash {0} of uploaded data and ETag {1} returned by the server",
                                     expected, reference));
                 }
