02ef4f82dc6fc7a7f581e5532d39c821e8a4a139
David Kocher <dkocher@iterate.ch>
2015-04-16 13:16:24 +0000

    Disable download segments. Set the preference `queue.download.segments.threshold` to `0` to enable.
    Former-commit-id: c1ce5d90713de252fd7b9792a88a4a07513d4403

--- source/ch/cyberduck/core/preferences/Preferences.java
+++ source/ch/cyberduck/core/preferences/Preferences.java
@@ -470,7 +470,7 @@ public abstract class Preferences {
         defaults.put("queue.download.wherefrom", String.valueOf(true));
 
         // Segmented concurrent downloads
-        defaults.put("queue.download.segments.threshold", String.valueOf(0L));
+        defaults.put("queue.download.segments.threshold", String.valueOf(Long.MAX_VALUE));
         defaults.put("queue.download.segments.size", String.valueOf(100L * 1024L * 1024L));
 
         /**
