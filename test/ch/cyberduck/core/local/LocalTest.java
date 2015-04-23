8148c7dd83d7d9d57d875bd25294dbefdb022bf5
David Kocher <dkocher@iterate.ch>
2015-04-23 11:07:35 +0000

    Add test.
    Former-commit-id: fbd795b00bfe2df812cc2a832d779de67304b821

--- test/ch/cyberduck/core/local/LocalTest.java
+++ test/ch/cyberduck/core/local/LocalTest.java
@@ -3,6 +3,7 @@ package ch.cyberduck.core.local;
 import ch.cyberduck.core.AbstractTestCase;
 import ch.cyberduck.core.Local;
 import ch.cyberduck.core.exception.AccessDeniedException;
+import ch.cyberduck.core.preferences.PreferencesFactory;
 
 import org.junit.Test;
 
@@ -52,5 +53,50 @@ public class LocalTest extends AbstractTestCase {
         final TestLocal l = new TestLocal("/p/1");
         assertNotNull(l.attributes());
         assertSame(l.attributes(), l.attributes());
+    }
+
+    @Test
+    public void testDelimiter() {
+        PreferencesFactory.get().setProperty("local.delimiter", "\\");
+        try {
+            Local l = new WindowsLocal("G:\\");
+            assertEquals("G:\\", l.getAbsolute());
+            assertEquals("", l.getName());
+
+            l = new WindowsLocal("C:\\path\\relative");
+            assertEquals("relative", l.getName());
+            assertEquals("C:\\path\\relative", l.getAbsolute());
+
+            l = new WindowsLocal("C:\\path", "cyberduck.log");
+            assertEquals("cyberduck.log", l.getName());
+            assertEquals("C:\\path\\cyberduck.log", l.getAbsolute());
+
+            l = new WindowsLocal("C:\\path", "Sessions");
+            assertEquals("Sessions", l.getName());
+            assertEquals("C:\\path\\Sessions", l.getAbsolute());
+        }
+        finally {
+            PreferencesFactory.get().deleteProperty("local.delimiter");
+        }
+    }
+
+    private static class WindowsLocal extends Local {
+
+        public WindowsLocal(final String parent, final String name) {
+            super(parent, name);
+        }
+
+        public WindowsLocal(final Local parent, final String name) {
+            super(parent, name);
+        }
+
+        public WindowsLocal(final String name) {
+            super(name);
+        }
+
+        @Override
+        public char getDelimiter() {
+            return '\\';
+        }
     }
 }
