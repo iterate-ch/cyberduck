189222309056a45c631a7a55c0ad3a28f04578b3
David Kocher <dkocher@iterate.ch>
2015-05-22 08:53:51 +0000

    Former-commit-id: 18d3161b40fe50316254f6621c8f2c4ef48fc885

--- source/ch/cyberduck/core/http/HttpUploadFeature.java
+++ source/ch/cyberduck/core/http/HttpUploadFeature.java
@@ -156,7 +156,7 @@ public class HttpUploadFeature<Output, Digest> implements Upload<Output> {
         // Obtain locally-calculated MD5 hash.
         final String expected = Hex.encodeHexString(digest.digest());
         // Compare our locally-calculated hash with the ETag returned by S3.
-        if(!expected.equals(checksum.hash)) {
+        if(!checksum.equals(Checksum.parse(expected))) {
             throw new ChecksumException(MessageFormat.format(LocaleFactory.localizedString("Upload {0} failed", "Error"), file.getName()),
                     MessageFormat.format("Mismatch between MD5 hash {0} of uploaded data and ETag {1} returned by the server",
                             expected, checksum.hash));
