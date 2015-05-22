cc96d0f8ad41b5df9c82f4864624cf31e8624a83
David Kocher <dkocher@iterate.ch>
2015-05-22 12:34:34 +0000

    Null implementation.
    Former-commit-id: 907c8488a5808c6d61e53643250423678ef6eec0

--- source/ch/cyberduck/core/resources/DisabledIconCache.java
+++ source/ch/cyberduck/core/resources/DisabledIconCache.java
@@ -7,43 +7,44 @@ import ch.cyberduck.core.local.Application;
  * @version $Id$
  */
 public class DisabledIconCache<I> extends AbstractIconCache {
+
     @Override
     public Object iconNamed(final String name, final Integer width, final Integer height) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Object documentIcon(final String extension, final Integer size) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Object documentIcon(final String extension, final Integer size, final Object badge) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Object folderIcon(final Integer size) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Object folderIcon(final Integer size, final Object badge) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Object fileIcon(final Path item, final Integer size) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Object applicationIcon(final Application app, final Integer size) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     protected Object badge(final Object badge, final Object icon) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 }
