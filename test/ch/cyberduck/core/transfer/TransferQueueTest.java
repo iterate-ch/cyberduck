c57fd41ea3100f85fad6da5975ab9b4b3a60400e
David Kocher <dkocher@iterate.ch>
2015-06-19 12:26:09 +0000

    Add test.
    Former-commit-id: 452f658c3eb8a87f23e48a2329f5b620aa83814b

--- test/ch/cyberduck/core/transfer/TransferQueueTest.java
+++ test/ch/cyberduck/core/transfer/TransferQueueTest.java
@@ -107,7 +107,51 @@ public class TransferQueueTest extends AbstractTestCase {
         assertTrue(c.getCount() == 1);
         assertFalse(c.await(1, TimeUnit.SECONDS));
         queue.resize(2);
-        c.await(1, TimeUnit.SECONDS);
+        assertTrue(c.await(1, TimeUnit.SECONDS));
         assertTrue(set.get());
+    }
+
+    @Test
+    public void testResizeMultipleWaiting() throws Exception {
+        final TransferQueue queue = new TransferQueue(1);
+        final DownloadTransfer d1 = new DownloadTransfer(new Host("t"), new Path("/t1", EnumSet.of(Path.Type.directory)), null);
+        final DownloadTransfer d2 = new DownloadTransfer(new Host("t"), new Path("/t2", EnumSet.of(Path.Type.directory)), null);
+        final DownloadTransfer d3 = new DownloadTransfer(new Host("t"), new Path("/t3", EnumSet.of(Path.Type.directory)), null);
+        queue.add(d1, new DisabledProgressListener());
+        final CountDownLatch c = new CountDownLatch(1);
+        final AtomicBoolean set1 = new AtomicBoolean();
+        final AtomicBoolean set2 = new AtomicBoolean();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                queue.add(d2, new DisabledProgressListener() {
+                    @Override
+                    public void message(final String message) {
+                        assertEquals("Maximum allowed connections exceeded. Waiting", message);
+                        set1.set(true);
+                    }
+                });
+                c.countDown();
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                queue.add(d3, new DisabledProgressListener() {
+                    @Override
+                    public void message(final String message) {
+                        assertEquals("Maximum allowed connections exceeded. Waiting", message);
+                        set2.set(true);
+                    }
+                });
+            }
+        }).start();
+        assertTrue(c.getCount() == 1);
+        assertFalse(c.await(1, TimeUnit.SECONDS));
+        assertTrue(set1.get());
+        assertTrue(set2.get());
+        queue.resize(2);
+        assertTrue(c.await(1, TimeUnit.SECONDS));
+        assertTrue(c.getCount() == 0);
     }
 }
