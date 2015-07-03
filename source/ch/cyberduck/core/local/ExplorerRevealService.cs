7d600ca93b733f6c8a6b9b8e87b6ca4697dad44f
Yves Langisch <yla@iterate.ch>
2015-07-03 11:08:46 +0000

    Fix reveal service for drives.
    Former-commit-id: 0fa4cea7aa0e23ff3a8eaa3c39bbc0fbf282c278

--- source/ch/cyberduck/core/local/ExplorerRevealService.cs
+++ source/ch/cyberduck/core/local/ExplorerRevealService.cs
@@ -1,21 +1,21 @@
 ﻿// 
-// Copyright (c) 2010-2013 Yves Langisch. All rights reserved.
+// Copyright (c) 2010-2015 Yves Langisch. All rights reserved.
 // http://cyberduck.ch/
-// 
+//
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // Bug fixes, suggestions and comments should be sent to:
 // yves@cyberduck.ch
-// 
-
+//
+using System.IO;
 using ch.cyberduck.core.local;
 
 namespace Ch.Cyberduck.Core.Local
@@ -26,9 +26,15 @@ namespace Ch.Cyberduck.Core.Local
         {
             if (l.exists())
             {
-                //select first file downloaded. We could just open the containing folder alternatively.
-                return ApplicationLauncherFactory.get().open(new Application("explorer.exe", null),
-                                                             "/select, " + l.getAbsolute());
+                string path = l.getAbsolute();
+                DirectoryInfo d = new DirectoryInfo(path);
+                string parameter = "";
+                if (d.Parent != null)
+                {
+                    parameter = "/select,";
+                }
+                return ApplicationLauncherFactory.get()
+                    .open(new Application("explorer.exe", null), parameter + l.getAbsolute());
             }
             return false;
         }
