35f751ba96104deb1542f5bffa8ab8e3094ce077
David Kocher <dkocher@iterate.ch>
2015-08-27 18:45:05 +0000

    Fix test.
    Former-commit-id: 909856730ac979905ce710852bcd37a9e648f863

--- test/ch/cyberduck/core/worker/ReadMetadataWorkerTest.java
+++ test/ch/cyberduck/core/worker/ReadMetadataWorkerTest.java
@@ -3,12 +3,16 @@ package ch.cyberduck.core.worker;
 import ch.cyberduck.core.AbstractTestCase;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Headers;
 import ch.cyberduck.core.test.NullSession;
 
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,7 +32,25 @@ public class ReadMetadataWorkerTest extends AbstractTestCase {
                 fail();
             }
         };
-        assertTrue(worker.run(new NullSession(new Host(""))).isEmpty());
+        assertTrue(worker.run(new NullSession(new Host("")) {
+            @Override
+            public <T> T getFeature(final Class<T> type) {
+                if(type == Headers.class) {
+                    return (T) new Headers() {
+                        @Override
+                        public Map<String, String> getMetadata(final Path file) throws BackgroundException {
+                            throw new UnsupportedOperationException();
+                        }
+
+                        @Override
+                        public void setMetadata(final Path file, final Map<String, String> metadata) throws BackgroundException {
+                            throw new UnsupportedOperationException();
+                        }
+                    };
+                }
+                return super.getFeature(type);
+            }
+        }).isEmpty());
     }
 
     @Test
@@ -43,7 +65,37 @@ public class ReadMetadataWorkerTest extends AbstractTestCase {
                 fail();
             }
         };
-        final Map<String, String> map = worker.run(new NullSession(new Host("")));
+        final Map<String, String> map = worker.run(new NullSession(new Host("")) {
+            @Override
+            public <T> T getFeature(final Class<T> type) {
+                if(type == Headers.class) {
+                    return (T) new Headers() {
+                        @Override
+                        public Map<String, String> getMetadata(final Path file) throws BackgroundException {
+                            if(file.getName().equals("a")) {
+                                return Collections.singletonMap("key1", "value1");
+                            }
+                            else if(file.getName().equals("b")) {
+                                return Collections.singletonMap("key2", "value2");
+                            }
+                            else if(file.getName().equals("c")) {
+                                return Collections.singletonMap("key2", "value2");
+                            }
+                            else {
+                                fail();
+                            }
+                            throw new UnsupportedOperationException();
+                        }
+
+                        @Override
+                        public void setMetadata(final Path file, final Map<String, String> metadata) throws BackgroundException {
+                            throw new UnsupportedOperationException();
+                        }
+                    };
+                }
+                return super.getFeature(type);
+            }
+        });
         assertFalse(map.containsKey("key1"));
         assertFalse(map.containsKey("key2"));
     }
@@ -60,7 +112,45 @@ public class ReadMetadataWorkerTest extends AbstractTestCase {
                 fail();
             }
         };
-        final Map<String, String> map = worker.run(new NullSession(new Host("")));
+        final Map<String, String> map = worker.run(new NullSession(new Host("")) {
+            @Override
+            public <T> T getFeature(final Class<T> type) {
+                if(type == Headers.class) {
+                    return (T) new Headers() {
+                        @Override
+                        public Map<String, String> getMetadata(final Path file) throws BackgroundException {
+                            final HashMap<String, String> map = new HashMap<String, String>();
+                            if(file.getName().equals("a")) {
+                                map.put("key1", "v1");
+                                map.put("key2", "v");
+                                map.put("key3", "v");
+                                return map;
+                            }
+                            else if(file.getName().equals("b")) {
+                                map.put("key2", "v");
+                                map.put("key3", "v");
+                                return map;
+                            }
+                            else if(file.getName().equals("c")) {
+                                map.put("key2", "v2");
+                                map.put("key3", "v");
+                                return map;
+                            }
+                            else {
+                                fail();
+                            }
+                            return map;
+                        }
+
+                        @Override
+                        public void setMetadata(final Path file, final Map<String, String> metadata) throws BackgroundException {
+                            throw new UnsupportedOperationException();
+                        }
+                    };
+                }
+                return super.getFeature(type);
+            }
+        });
         assertFalse(map.containsKey("key1"));
         assertTrue(map.containsKey("key2"));
         assertNull(map.get("key2"));
