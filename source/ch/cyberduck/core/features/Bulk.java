376c71b46491aeb0cb53e9a70c6e8f50afd0b1aa
David Kocher <dkocher@iterate.ch>
2015-11-24 18:11:14 +0100

    Add transfer type to bulk interface.
    Former-commit-id: ac90f23ed5059e3c7ad13d8128d024bee4b08731

--- source/ch/cyberduck/core/features/Bulk.java
+++ source/ch/cyberduck/core/features/Bulk.java
@@ -2,6 +2,7 @@ package ch.cyberduck.core.features;
 
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.transfer.Transfer;
 import ch.cyberduck.core.transfer.TransferStatus;
 
 import java.util.Map;
@@ -9,6 +10,6 @@ import java.util.Map;
 /**
  * @version $Id:$
  */
-public interface Bulk {
-    void pre(Map<Path, TransferStatus> files) throws BackgroundException;
+public interface Bulk<R> {
+    R pre(Transfer.Type type, Map<Path, TransferStatus> files) throws BackgroundException;
 }
