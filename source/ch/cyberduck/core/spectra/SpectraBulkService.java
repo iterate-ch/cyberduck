d67b2902530f39820d5d7b24830b5332a319a2af
David Kocher <dkocher@iterate.ch>
2015-11-24 18:09:26 +0100

    Rewrite as single service.
    Former-commit-id: 837e74018b2b598375083c83c6f11277685512fe

--- source/ch/cyberduck/core/spectra/SpectraBulkService.java
+++ source/ch/cyberduck/core/spectra/SpectraBulkService.java
@@ -6,6 +6,7 @@ import ch.cyberduck.core.PathContainerService;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.Bulk;
 import ch.cyberduck.core.s3.S3PathContainerService;
+import ch.cyberduck.core.transfer.Transfer;
 import ch.cyberduck.core.transfer.TransferStatus;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import com.spectralogic.ds3client.helpers.Ds3ClientHelpers;
 import com.spectralogic.ds3client.models.bulk.Ds3Object;
@@ -23,19 +25,19 @@ import com.spectralogic.ds3client.serializer.XmlProcessingException;
 /**
  * @version $Id:$
  */
-public class SpectraBulkUploadService implements Bulk {
+public class SpectraBulkService implements Bulk<UUID> {
 
     private final SpectraSession session;
 
     private final PathContainerService containerService
             = new S3PathContainerService();
 
-    public SpectraBulkUploadService(final SpectraSession session) {
+    public SpectraBulkService(final SpectraSession session) {
         this.session = session;
     }
 
     @Override
-    public void pre(final Map<Path, TransferStatus> files) throws BackgroundException {
+    public UUID pre(final Transfer.Type type, final Map<Path, TransferStatus> files) throws BackgroundException {
         final Ds3ClientHelpers helper = Ds3ClientHelpers.wrap(session.getClient());
         final Map<Path, List<Ds3Object>> jobs = new HashMap<Path, List<Ds3Object>>();
         for(Map.Entry<Path, TransferStatus> item : files.entrySet()) {
@@ -48,9 +50,20 @@ public class SpectraBulkUploadService implements Bulk {
         }
         try {
             for(Map.Entry<Path, List<Ds3Object>> container : jobs.entrySet()) {
-                final Ds3ClientHelpers.Job job = helper.startWriteJob(
-                        container.getKey().getName(), container.getValue());
+                switch(type) {
+                    case download:
+                        final Ds3ClientHelpers.Job read = helper.startReadJob(
+                                container.getKey().getName(), container.getValue());
+                        return read.getJobId();
+                    case upload:
+                        final Ds3ClientHelpers.Job write = helper.startWriteJob(
+                                container.getKey().getName(), container.getValue());
+                        return write.getJobId();
+                    default:
+                        throw new BackgroundException();
+                }
             }
+            return null;
         }
         catch(XmlProcessingException | SignatureException e) {
             throw new BackgroundException(e);
--- test/ch/cyberduck/core/spectra/SpectraBulkServiceTest.java
+++ test/ch/cyberduck/core/spectra/SpectraBulkServiceTest.java
@@ -9,6 +9,7 @@ import ch.cyberduck.core.Path;
 import ch.cyberduck.core.exception.NotfoundException;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DisabledX509TrustManager;
+import ch.cyberduck.core.transfer.Transfer;
 import ch.cyberduck.core.transfer.TransferStatus;
 
 import org.junit.Test;
@@ -20,17 +21,31 @@ import java.util.UUID;
 /**
  * @version $Id:$
  */
-public class SpectraBulkDownloadServiceTest extends AbstractTestCase {
+public class SpectraBulkServiceTest extends AbstractTestCase {
 
-    @Test(expected = NotfoundException.class)
-    public void testPre() throws Exception {
+    @Test
+    public void testPreUpload() throws Exception {
         final Host host = new Host(new SpectraProtocol(), "192.168.56.101", 8080, new Credentials(
                 "aXRlcmF0ZQ==", "sVYKkwL9"
         ));
         final SpectraSession session = new SpectraSession(host, new DisabledX509TrustManager(),
                 new DefaultX509KeyManager());
         session.open(new DisabledHostKeyCallback(), new DisabledTranscriptListener());
-        new SpectraBulkDownloadService(session).pre(Collections.singletonMap(
+        new SpectraBulkService(session).pre(Transfer.Type.upload, Collections.singletonMap(
+                new Path(String.format("/cyberduck/%s", UUID.randomUUID().toString()), EnumSet.of(Path.Type.directory, Path.Type.volume)), new TransferStatus().length(1L)
+        ));
+        session.close();
+    }
+
+    @Test(expected = NotfoundException.class)
+    public void testPreDownload() throws Exception {
+        final Host host = new Host(new SpectraProtocol(), "192.168.56.101", 8080, new Credentials(
+                "aXRlcmF0ZQ==", "sVYKkwL9"
+        ));
+        final SpectraSession session = new SpectraSession(host, new DisabledX509TrustManager(),
+                new DefaultX509KeyManager());
+        session.open(new DisabledHostKeyCallback(), new DisabledTranscriptListener());
+        new SpectraBulkService(session).pre(Transfer.Type.download, Collections.singletonMap(
                 new Path(String.format("/cyberduck/%s", UUID.randomUUID().toString()), EnumSet.of(Path.Type.directory, Path.Type.volume)), new TransferStatus().length(1L)
         ));
         session.close();
