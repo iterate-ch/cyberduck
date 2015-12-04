86e5cf449084874a18a9e0235540f86c11d2d665
David Kocher <dkocher@iterate.ch>
2015-12-04 14:22:13 +0100

    Fix #3813. Default to lax hostname verification for HTTPS sessions.
    Former-commit-id: 903723727f16e6c8dae39f704e4c98bc44581a29

---
+++ test/ch/cyberduck/core/ssl/ThreadLocalHostnameDelegatingTrustManagerTest.java
@@ -0,0 +1,21 @@
+package ch.cyberduck.core.ssl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @version $Id:$
+ */
+public class ThreadLocalHostnameDelegatingTrustManagerTest {
+
+    @Test
+    public void testSetTarget() throws Exception {
+        assertEquals("s3.amazonaws.com",
+                new ThreadLocalHostnameDelegatingTrustManager(new DisabledX509TrustManager(), "s3.amazonaws.com").getTarget());
+        assertEquals("cyberduck.s3.amazonaws.com",
+                new ThreadLocalHostnameDelegatingTrustManager(new DisabledX509TrustManager(), "cyberduck.s3.amazonaws.com").getTarget());
+        assertEquals("duck.s3.amazonaws.com",
+                new ThreadLocalHostnameDelegatingTrustManager(new DisabledX509TrustManager(), "cyber.duck.s3.amazonaws.com").getTarget());
+    }
+}
