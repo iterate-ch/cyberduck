18580be756ed1494574d879a255c06fdbc04bb01
David Kocher <dkocher@iterate.ch>
2015-12-23 12:50:56 +0100

    Add test.
    Former-commit-id: 5b1408e7fd4e9767dfc06e4c01cc97b8f0881b19

---
+++ spectra/src/test/java/ch/cyberduck/core/spectra/SpectraWriteFeatureTest.java
@@ -0,0 +1,89 @@
+package ch.cyberduck.core.spectra;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.PathCache;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Attributes;
+import ch.cyberduck.core.features.Find;
+import ch.cyberduck.core.features.Write;
+import ch.cyberduck.core.s3.S3Session;
+import ch.cyberduck.core.s3.S3WriteFeature;
+import ch.cyberduck.core.ssl.DefaultX509KeyManager;
+import ch.cyberduck.core.ssl.DisabledX509TrustManager;
+
+import org.junit.Test;
+
+import java.util.EnumSet;
+
+import static org.junit.Assert.*;
+
+/**
+ * @version $Id$
+ */
+public class SpectraWriteFeatureTest extends AbstractTestCase {
+
+    @Test
+    public void testAppendBelowLimit() throws Exception {
+        final SpectraSession session = new SpectraSession(new Host(new SpectraProtocol()), new DisabledX509TrustManager(),
+                new DefaultX509KeyManager());
+        final S3WriteFeature feature = new S3WriteFeature(session, null, new Find() {
+            @Override
+            public boolean find(final Path file) throws BackgroundException {
+                return true;
+            }
+
+            @Override
+            public Find withCache(final PathCache cache) {
+                return this;
+            }
+        }, new Attributes() {
+            @Override
+            public PathAttributes find(final Path file) throws BackgroundException {
+                return new PathAttributes();
+            }
+
+            @Override
+            public Attributes withCache(final PathCache cache) {
+                return this;
+            }
+        });
+        final Write.Append append = feature.append(new Path("/p", EnumSet.of(Path.Type.file)), 0L, PathCache.empty());
+        assertFalse(append.append);
+    }
+
+    @Test
+    public void testSize() throws Exception {
+        final S3Session session = new SpectraSession(new Host(new SpectraProtocol()), new DisabledX509TrustManager(),
+                new DefaultX509KeyManager());
+        final S3WriteFeature feature = new S3WriteFeature(session, null, new Find() {
+            @Override
+            public boolean find(final Path file) throws BackgroundException {
+                return true;
+            }
+
+            @Override
+            public Find withCache(final PathCache cache) {
+                return this;
+            }
+        }, new Attributes() {
+            @Override
+            public PathAttributes find(final Path file) throws BackgroundException {
+                final PathAttributes attributes = new PathAttributes();
+                attributes.setSize(3L);
+                return attributes;
+            }
+
+            @Override
+            public Attributes withCache(final PathCache cache) {
+                return this;
+            }
+        });
+        final Write.Append append = feature.append(new Path("/p", EnumSet.of(Path.Type.file)), 0L, PathCache.empty());
+        assertFalse(append.append);
+        assertTrue(append.override);
+        assertEquals(3L, append.size, 0L);
+    }
+}
