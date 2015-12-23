27281594721e94eca1d2f2f7d53b640a7ce936a9
David Kocher <dkocher@iterate.ch>
2015-12-23 11:39:52 +0100

    Fix PUT of `0` byte files with `start_bulk_put` operation.
    Former-commit-id: 323ac6f91d222aa8b601caae74f11ade5713a949

--- spectra/src/main/java/ch/cyberduck/core/spectra/SpectraTouchFeature.java
+++ spectra/src/main/java/ch/cyberduck/core/spectra/SpectraTouchFeature.java
@@ -2,7 +2,7 @@ package ch.cyberduck.core.spectra;
 
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.exception.BackgroundException;
-import ch.cyberduck.core.s3.S3TouchFeature;
+import ch.cyberduck.core.features.Touch;
 import ch.cyberduck.core.transfer.Transfer;
 import ch.cyberduck.core.transfer.TransferStatus;
 
@@ -11,12 +11,11 @@ import java.util.Collections;
 /**
  * @version $Id:$
  */
-public class SpectraTouchFeature extends S3TouchFeature {
+public class SpectraTouchFeature implements Touch {
 
     private final SpectraSession session;
 
     public SpectraTouchFeature(final SpectraSession session) {
-        super(session);
         this.session = session;
     }
 
@@ -24,6 +23,12 @@ public class SpectraTouchFeature extends S3TouchFeature {
     public void touch(final Path file) throws BackgroundException {
         final SpectraBulkService bulk = new SpectraBulkService(session);
         bulk.pre(Transfer.Type.upload, Collections.singletonMap(file, new TransferStatus().length(0L)));
-        super.touch(file);
+    }
+
+
+    @Override
+    public boolean isSupported(final Path workdir) {
+        // Creating files is only possible inside a bucket.
+        return !workdir.isRoot();
     }
 }
