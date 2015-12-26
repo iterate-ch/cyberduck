5c7899b9ca545fd6f9092a1556f374519a4289d6
David Kocher <dkocher@iterate.ch>
2015-12-26 16:20:53 +0100

    Fix test.
    Former-commit-id: fe72d499c8834fc88a8989f6b4695d4a43af5e55

--- core/dylib/src/test/java/ch/cyberduck/core/local/WorkspaceIconServiceTest.java
+++ core/dylib/src/test/java/ch/cyberduck/core/local/WorkspaceIconServiceTest.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 package ch.cyberduck.core.local;
 
 import ch.cyberduck.binding.application.NSImage;
@@ -21,7 +35,7 @@ public class WorkspaceIconServiceTest {
         final WorkspaceIconService s = new WorkspaceIconService();
         final Local file = new Local(PreferencesFactory.get().getProperty("tmp.dir"),
                 UUID.randomUUID().toString());
-        assertFalse(s.update(file, NSImage.imageWithContentsOfFile("../img/download0.icns")));
+        assertFalse(s.update(file, NSImage.imageWithContentsOfFile("../../img/download0.icns")));
     }
 
     @Test
@@ -30,7 +44,7 @@ public class WorkspaceIconServiceTest {
         final Local file = new Local(PreferencesFactory.get().getProperty("tmp.dir"),
                 UUID.randomUUID().toString());
         file.mkdir();
-        assertTrue(s.update(file, NSImage.imageWithContentsOfFile("../img/download0.icns")));
+        assertTrue(s.update(file, NSImage.imageWithContentsOfFile("../../img/download0.icns")));
     }
 
     @Test
@@ -39,7 +53,7 @@ public class WorkspaceIconServiceTest {
         final Local file = new Local(PreferencesFactory.get().getProperty("tmp.dir"),
                 UUID.randomUUID().toString());
         LocalTouchFactory.get().touch(file);
-        assertTrue(s.update(file, NSImage.imageWithContentsOfFile("../img/download0.icns")));
+        assertTrue(s.update(file, NSImage.imageWithContentsOfFile("../../img/download0.icns")));
         file.delete();
     }
 
@@ -51,7 +65,7 @@ public class WorkspaceIconServiceTest {
         assertFalse(s.remove(file));
         LocalTouchFactory.get().touch(file);
         assertFalse(s.remove(file));
-        assertTrue(s.update(file, NSImage.imageWithContentsOfFile("../img/download0.icns")));
+        assertTrue(s.update(file, NSImage.imageWithContentsOfFile("../../img/download0.icns")));
         assertTrue(s.remove(file));
         file.delete();
     }
