2a373b8a384f8a09173b5bea993df3416ca03bbf
David Kocher <dkocher@iterate.ch>
2015-12-26 15:46:51 +0100

    Move tests to platform module.
    Former-commit-id: 8dbe9e3eefe190abbb407f643fa047fbeecd50b0

--- core/dylib/src/test/java/ch/cyberduck/core/local/FileWatcherTest.java
+++ core/dylib/src/test/java/ch/cyberduck/core/local/FileWatcherTest.java
@@ -30,6 +30,40 @@ import static org.junit.Assert.assertTrue;
 public class FileWatcherTest {
 
     @Test
+    public void testMatchDefaultLocal() throws IOException {
+        assertTrue(
+                new FileWatcher(new DisabledWatchService()).matches(
+                        new Local("/private/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"),
+                        new Local("/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"))
+        );
+        assertTrue(
+                new FileWatcher(new DisabledWatchService()).matches(
+                        new Local("/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"),
+                        new Local("/private/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"))
+        );
+        assertFalse(
+                new FileWatcher(new DisabledWatchService()).matches(
+                        new Local("/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"),
+                        new Local("/private/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/d/edit.html"))
+        );
+        assertFalse(
+                new FileWatcher(new DisabledWatchService()).matches(
+                        new Local("/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/d/edit.html"),
+                        new Local("/private/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"))
+        );
+        assertTrue(
+                new FileWatcher(new DisabledWatchService()).matches(
+                        new Local("edit.html"),
+                        new Local("/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"))
+        );
+        assertTrue(
+                new FileWatcher(new DisabledWatchService()).matches(
+                        new Local("edit.html"),
+                        new Local("/private/var/folders/cl/622z57616532npsw3xs1ndyc0000gp/T/1022b1a9-c21c-4a79-9162-59990c75aaa8/usr/home/dkocher/sandbox/edit.html"))
+        );
+    }
+
+    @Test
     public void testMatchFinderLocal() throws IOException {
         assertTrue(
                 new FileWatcher(new DisabledWatchService()).matches(
