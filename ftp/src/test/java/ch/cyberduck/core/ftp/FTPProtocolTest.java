ff2b9715200e426ca1e4e6016fecb46b4f1f591d
David Kocher <dkocher@iterate.ch>
2015-12-26 20:57:15 +0100

    Move tests.
    Former-commit-id: 8b4e848ee20123f70203d56e1085d26d85d500e9

--- ftp/src/test/java/ch/cyberduck/core/ftp/FTPProtocolTest.java
+++ ftp/src/test/java/ch/cyberduck/core/ftp/FTPProtocolTest.java
@@ -1,22 +1,34 @@
-package ch.cyberduck.core;
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
+package ch.cyberduck.core.ftp;
+
+import ch.cyberduck.core.Protocol;
 import ch.cyberduck.core.dav.DAVProtocol;
-import ch.cyberduck.core.ftp.FTPProtocol;
-import ch.cyberduck.core.ftp.FTPTLSProtocol;
 import ch.cyberduck.core.gstorage.GoogleStorageProtocol;
-import ch.cyberduck.core.irods.IRODSProtocol;
-import ch.cyberduck.core.openstack.SwiftProtocol;
 import ch.cyberduck.core.s3.S3Protocol;
-import ch.cyberduck.core.sftp.SFTPProtocol;
 
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 /**
  * @version $Id$
  */
-public class ProtocolTest extends AbstractTestCase {
+public class FTPProtocolTest {
 
     @Test
     public void testEquals() {
@@ -28,24 +40,17 @@ public class ProtocolTest extends AbstractTestCase {
     public void testConfigurable() {
         assertTrue(new S3Protocol().isHostnameConfigurable());
         assertFalse(new S3Protocol().isPortConfigurable());
-        assertTrue(new SwiftProtocol().isHostnameConfigurable());
-        assertTrue(new SwiftProtocol().isPortConfigurable());
         assertTrue(new FTPProtocol().isHostnameConfigurable());
         assertTrue(new FTPProtocol().isPortConfigurable());
-        assertTrue(new SFTPProtocol().isHostnameConfigurable());
-        assertTrue(new SFTPProtocol().isPortConfigurable());
         assertTrue(new DAVProtocol().isHostnameConfigurable());
         assertTrue(new DAVProtocol().isPortConfigurable());
-        assertTrue(new IRODSProtocol().isHostnameConfigurable());
-        assertTrue(new IRODSProtocol().isPortConfigurable());
-
         assertFalse(new GoogleStorageProtocol().isHostnameConfigurable());
         assertFalse(new GoogleStorageProtocol().isPortConfigurable());
     }
 
     @Test
     public void testIcons() {
-        for(Protocol p : ProtocolFactory.getEnabledProtocols()) {
+        for(Protocol p : Arrays.asList(new FTPProtocol(), new FTPTLSProtocol())) {
             assertNotNull(p.disk());
             assertNotNull(p.icon());
             assertNotNull(p.getDefaultPort());
--- openstack/src/test/java/ch/cyberduck/core/openstack/SwiftProtocolTest.java
+++ openstack/src/test/java/ch/cyberduck/core/openstack/SwiftProtocolTest.java
@@ -1,8 +1,12 @@
 package ch.cyberduck.core.openstack;
 
+import ch.cyberduck.core.Protocol;
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
 
 /**
  * @version $Id:$
@@ -12,5 +16,23 @@ public class SwiftProtocolTest {
     @Test
     public void testPrefix() {
         assertEquals("ch.cyberduck.core.openstack.Swift", new SwiftProtocol().getPrefix());
+    }
+
+    @Test
+    public void testConfigurable() {
+        assertTrue(new SwiftProtocol().isHostnameConfigurable());
+        assertTrue(new SwiftProtocol().isPortConfigurable());
+    }
+
+    @Test
+    public void testIcons() {
+        for(Protocol p : Arrays.asList(new SwiftProtocol())) {
+            assertNotNull(p.disk());
+            assertNotNull(p.icon());
+            assertNotNull(p.getDefaultPort());
+            assertNotNull(p.getDefaultHostname());
+            assertNotNull(p.getDescription());
+            assertNotNull(p.getIdentifier());
+        }
     }
 }
