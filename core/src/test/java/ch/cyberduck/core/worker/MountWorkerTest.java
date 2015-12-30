d0102872ad8105ca243c151058a910d2c7b29eb4
David Kocher <dkocher@iterate.ch>
2015-12-30 10:04:56 +0100

    Add test.
    Former-commit-id: 2b248ff9fded768ea28b9c6e1d1b202bda7b40df

---
+++ core/src/test/java/ch/cyberduck/core/worker/MountWorkerTest.java
@@ -0,0 +1,48 @@
+package ch.cyberduck.core.worker;
+
+import ch.cyberduck.core.AbstractTestCase;
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.Cache;
+import ch.cyberduck.core.DisabledListProgressListener;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.ListProgressListener;
+import ch.cyberduck.core.NullSession;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathCache;
+import ch.cyberduck.core.Session;
+import ch.cyberduck.core.TestProtocol;
+import ch.cyberduck.core.exception.NotfoundException;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.EnumSet;
+
+import static org.junit.Assert.*;
+
+/**
+ * @version $Id$
+ */
+public class MountWorkerTest extends AbstractTestCase {
+
+    @Test
+    public void testRunInvalidDefaultPath() throws Exception {
+        final Host host = new Host(new TestProtocol());
+        host.setDefaultPath("/notfound");
+        final Session<?> session = new NullSession(host) {
+            @Override
+            public AttributedList<Path> list(final Path file, final ListProgressListener listener) throws NotfoundException {
+                if(file.equals(new Path("/notfound", EnumSet.of(Path.Type.volume, Path.Type.directory)))) {
+                    throw new NotfoundException("");
+                }
+                return new AttributedList<>(Collections.emptyList());
+            }
+        };
+        final Cache<Path> cache = new PathCache(2);
+        final MountWorker worker = new MountWorker(host, cache, new DisabledListProgressListener());
+        assertEquals(new Path("/", EnumSet.of(Path.Type.directory)), worker.run(session));
+        assertTrue(cache.containsKey(new Path("/", EnumSet.of(Path.Type.directory))));
+        assertFalse(cache.containsKey(new Path("/notfound", EnumSet.of(Path.Type.directory))));
+        session.close();
+    }
+}
