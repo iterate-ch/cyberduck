7be345225e1c14ef7ad7b213218ff19730057446
yla <coke4you>
2016-01-25 17:48:42 +0100

    Fix disable localization.
    Former-commit-id: f11186355eb821a84776e8fb110f5225194c8d44

--- core/src/main/csharp/ch/cyberduck/core/i18n/DictionaryLocale.cs
+++ core/src/main/csharp/ch/cyberduck/core/i18n/DictionaryLocale.cs
@@ -1,17 +1,17 @@
 ﻿// 
-// Copyright (c) 2010-2015 Yves Langisch. All rights reserved.
+// Copyright (c) 2010-2016 Yves Langisch. All rights reserved.
 // http://cyberduck.io/
-// 
+//
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-//  
+//
 // Bug fixes, suggestions and comments should be sent to:
 // feedback@cyberduck.io
 // 
@@ -37,8 +37,11 @@ namespace Ch.Cyberduck.Core.I18n
 
         public string localize(string key, string table)
         {
+            if (!PreferencesFactory.get().getBoolean("application.localization.enable"))
+            {
+                return key;
+            }
             Dictionary<string, string> bundle;
-
             if (!_cache.TryGetValue(table, out bundle))
             {
                 lock (this)
