c476752a41607743e3cb04ad93339ade4f91b2da
David Kocher <dkocher@iterate.ch>
2016-01-26 23:19:40 +0100

    Fix build.
    Former-commit-id: 03d0a0f052c1283e689b04b1680fb24beb6f0f9a

--- setup/brew/duck.source.rb
+++ setup/brew/duck.source.rb
@@ -9,12 +9,12 @@ class Duck < Formula
   depends_on :java => ["1.8+", :build]
   depends_on :xcode => :build
   depends_on "ant" => :build
-  depends_on "maven32" => :build
+  depends_on "maven" => :build
 
   def install
     revision = version.to_s.rpartition(".").last
-    system "mvn", "-Drevision=#{revision}", "package"
-    libexec.install Dir["cli/target/duck.bundle/*"]
+    system "mvn", "-DskipTests", "-Drevision=#{revision}", "--projects", "cli/osx", "--also-make", "verify"
+    libexec.install Dir["cli/osx/target/duck.bundle/*"]
     bin.install_symlink "#{libexec}/Contents/MacOS/duck" => "duck"
   end
 
