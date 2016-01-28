8fb0eba8baa0744997288320802c229c374c89a5
David Kocher <dkocher@iterate.ch>
2016-01-28 13:52:54 +0100

    Add feature interface.
    Former-commit-id: b12781c88887632d315a0f9aa032c906e73f3d49

---
+++ spectra/src/main/java/ch/cyberduck/core/spectra/SpectraSearchFeature.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015-2016 Spectra Logic Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package ch.cyberduck.core.spectra;
+
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.ListProgressListener;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.features.Search;
+
+import com.spectralogic.ds3client.helpers.Ds3ClientHelpers;
+
+public class SpectraSearchFeature implements Search {
+
+    private final SpectraSession session;
+
+    public SpectraSearchFeature(final SpectraSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public AttributedList<Path> search(final String regex, final ListProgressListener listener) {
+        final Ds3ClientHelpers helper = Ds3ClientHelpers.wrap(new SpectraClientBuilder().wrap(session));
+        return AttributedList.emptyList();
+    }
+}
