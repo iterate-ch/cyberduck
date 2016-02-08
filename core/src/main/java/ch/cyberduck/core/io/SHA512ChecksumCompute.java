622bf8eb3b129616013175f29be22fe138deb057
David Kocher <dkocher@iterate.ch>
2016-02-08 16:32:46 +0100

    Add SHA-512.
    Former-commit-id: eeec25696b8b7234ae96be55b5bc6a0e8661e717

---
+++ core/src/main/java/ch/cyberduck/core/io/SHA512ChecksumCompute.java
@@ -0,0 +1,30 @@
+package ch.cyberduck.core.io;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.exception.ChecksumException;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.io.InputStream;
+
+public class SHA512ChecksumCompute extends AbstractChecksumCompute {
+
+    @Override
+    public Checksum compute(final InputStream in) throws ChecksumException {
+        return new Checksum(HashAlgorithm.sha512, Hex.encodeHexString(this.digest("SHA-512", in)));
+    }
+}
