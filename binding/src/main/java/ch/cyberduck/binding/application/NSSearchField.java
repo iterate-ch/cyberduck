2d30bc30eb99dd126d9a552142d6808a534185a3
David Kocher <dkocher@iterate.ch>
2016-02-25 14:11:04 +0100

    Javadoc
    Former-commit-id: fe930c17ccb4d2f4de5f9c9c659f698c806ab058

--- binding/src/main/java/ch/cyberduck/binding/application/NSSearchField.java
+++ binding/src/main/java/ch/cyberduck/binding/application/NSSearchField.java
@@ -30,11 +30,28 @@ public abstract class NSSearchField extends NSTextField {
 
     public abstract NSSearchField init();
 
+    /**
+     * When the value of this property is YES, the cell calls its action method immediately upon notification of any
+     * changes to the search field. When the value is NO, the cell pauses briefly after receiving a notification and
+     * then calls its action method. Pausing gives the user an opportunity to type more text into the search field
+     * and minimize the number of searches that are performed.
+     *
+     * @param flag A Boolean value indicating whether the cell calls its action method immediately when an
+     *             appropriate action occurs.
+     */
     public abstract void setSendsSearchStringImmediately(boolean flag);
 
     public abstract boolean sendsSearchStringImmediately();
 
-    public abstract void setSendsWholeSearchString(final boolean sendsWholeSearchString);
+    /**
+     * When the value of this property is YES, the cell calls its action method when the user clicks the search button
+     * or presses Return. When the value is NO, the cell calls the action method after each keystroke.
+     * The default value of this property is NO.
+     *
+     * @param flag A Boolean value indicating whether the cell calls its search action method
+     *             when the user clicks the search button (or presses Return) or after each keystroke.
+     */
+    public abstract void setSendsWholeSearchString(final boolean flag);
 
     public abstract boolean sendsWholeSearchString();
 }
