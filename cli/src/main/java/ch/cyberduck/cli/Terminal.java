5750dc21e9c06258a5500b5d121724aa17a48230
David Kocher <dkocher@iterate.ch>
2016-03-01 16:05:12 +0100

    Revert.
    Former-commit-id: 0462b37b305748b8f0270d5f187613476b0a2a65

--- cli/src/main/java/ch/cyberduck/cli/Terminal.java
+++ cli/src/main/java/ch/cyberduck/cli/Terminal.java
@@ -41,6 +41,7 @@ import ch.cyberduck.core.preferences.Preferences;
 import ch.cyberduck.core.preferences.PreferencesFactory;
 import ch.cyberduck.core.s3.S3Protocol;
 import ch.cyberduck.core.sftp.SFTPProtocol;
+import ch.cyberduck.core.spectra.SpectraProtocol;
 import ch.cyberduck.core.ssl.CertificateStoreX509TrustManager;
 import ch.cyberduck.core.ssl.DefaultTrustManagerHostnameCallback;
 import ch.cyberduck.core.ssl.PreferencesX509KeyManager;
@@ -112,7 +113,7 @@ public class Terminal {
     public Terminal(final Preferences defaults, final Options options, final CommandLine input) {
         this.preferences = defaults;
         ProtocolFactory.register(new FTPProtocol(), new FTPTLSProtocol(), new SFTPProtocol(), new DAVProtocol(), new DAVSSLProtocol(), new SwiftProtocol(), new S3Protocol(),
-                new GoogleStorageProtocol(), new AzureProtocol(), new IRODSProtocol());
+                new GoogleStorageProtocol(), new AzureProtocol(), new IRODSProtocol(), new SpectraProtocol());
         this.options = options;
         if(log.isInfoEnabled()) {
             log.info(String.format("Parsed options %s from input %s", options, input));
