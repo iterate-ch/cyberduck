f35a265915e866e7c17329c45572a5884607899b
David Kocher <dkocher@iterate.ch>
2016-03-03 15:56:09 +0100

    Add protocol definition.
    Former-commit-id: cd9e5b0c832fa847c6d5370838af01d9aaee8fe5

---
+++ backblaze/src/main/java/ch/cyberduck/core/b2/B2Protocol.java
@@ -0,0 +1,46 @@
+package ch.cyberduck.core.b2;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AbstractProtocol;
+import ch.cyberduck.core.Scheme;
+
+public class B2Protocol extends AbstractProtocol {
+    @Override
+    public String getIdentifier() {
+        return "b2";
+    }
+
+    @Override
+    public String getDescription() {
+        return "B2 Cloud Storage";
+    }
+
+    @Override
+    public Scheme getScheme() {
+        return Scheme.https;
+    }
+
+    @Override
+    public String getUsernamePlaceholder() {
+        return "Account ID";
+    }
+
+    @Override
+    public String getPasswordPlaceholder() {
+        return "Application Key";
+    }
+}
