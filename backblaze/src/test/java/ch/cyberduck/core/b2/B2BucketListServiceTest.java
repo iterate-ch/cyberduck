9a5d171f45082cd44c504df4402f514b7edad83c
David Kocher <dkocher@iterate.ch>
2016-03-04 17:01:58 +0100

    Add test.
    Former-commit-id: c438656408ea79eb3a62cee721253717c97e0880

---
+++ backblaze/src/test/java/ch/cyberduck/core/b2/B2BucketListServiceTest.java
@@ -0,0 +1,54 @@
+package ch.cyberduck.core.b2;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledCancelCallback;
+import ch.cyberduck.core.DisabledHostKeyCallback;
+import ch.cyberduck.core.DisabledListProgressListener;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.DisabledTranscriptListener;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+
+import org.junit.Test;
+
+import java.util.EnumSet;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class B2BucketListServiceTest {
+
+    @Test
+    public void testList() throws Exception {
+        final B2Session session = new B2Session(
+                new Host(new B2Protocol(), new B2Protocol().getDefaultHostname(),
+                        new Credentials(
+                                System.getProperties().getProperty("b2.user"), System.getProperties().getProperty("b2.key")
+                        )));
+        session.open(new DisabledHostKeyCallback(), new DisabledTranscriptListener());
+        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback());
+        final List<Path> list = new B2BucketListService(session).list(new DisabledListProgressListener());
+        assertFalse(list.isEmpty());
+        final Path reference = new Path("test-cyberduck", EnumSet.of(Path.Type.directory, Path.Type.volume));
+        reference.attributes().setVersionId("da4d79164ba64e2a553e0d10");
+        assertTrue(list.contains(reference));
+        session.close();
+    }
+}
