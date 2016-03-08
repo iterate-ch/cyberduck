04c8555a86245c8c9baf066dac0544dc353f6d73
David Kocher <dkocher@iterate.ch>
2016-03-08 10:31:07 +0100

    Map generic I/O failures.
    Former-commit-id: ab70ef7742ffa978b62700ab067a2b257e79eaf6

--- backblaze/src/main/java/ch/cyberduck/core/b2/B2ExceptionMappingService.java
+++ backblaze/src/main/java/ch/cyberduck/core/b2/B2ExceptionMappingService.java
@@ -16,6 +16,7 @@ package ch.cyberduck.core.b2;
  */
 
 import ch.cyberduck.core.AbstractExceptionMappingService;
+import ch.cyberduck.core.DefaultIOExceptionMappingService;
 import ch.cyberduck.core.exception.AccessDeniedException;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.exception.ChecksumException;
@@ -27,6 +28,7 @@ import ch.cyberduck.core.exception.RetriableAccessDeniedException;
 
 import org.apache.http.HttpStatus;
 
+import java.io.IOException;
 import java.time.Duration;
 
 import synapticloop.b2.exception.B2ApiException;
@@ -81,6 +83,9 @@ public class B2ExceptionMappingService extends AbstractExceptionMappingService<B
                         return new RetriableAccessDeniedException(buffer.toString(), Duration.ofSeconds(1));
                 }
                 return new InteroperabilityException(buffer.toString(), e);
+        }
+        if(e.getCause() instanceof IOException) {
+            return new DefaultIOExceptionMappingService().map((IOException) e.getCause());
         }
         return new BackgroundException(buffer.toString(), e);
     }
