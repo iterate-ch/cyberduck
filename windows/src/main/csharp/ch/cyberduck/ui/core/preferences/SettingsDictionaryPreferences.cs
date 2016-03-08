8c8736af2360794b995e270aabbea69d45d62d2e
David Kocher <dkocher@iterate.ch>
2016-03-08 09:11:03 +0100

    Set property for spectra.
    Former-commit-id: 7f056eb741e488fc26f2d2a4e7af2581e46d9f36

--- windows/src/main/csharp/ch/cyberduck/ui/core/preferences/SettingsDictionaryPreferences.cs
+++ windows/src/main/csharp/ch/cyberduck/ui/core/preferences/SettingsDictionaryPreferences.cs
@@ -33,6 +33,7 @@ using ch.cyberduck.core.openstack;
 using ch.cyberduck.core.s3;
 using ch.cyberduck.core.sftp;
 using ch.cyberduck.core.b2;
+using ch.cyberduck.core.spectra;
 using Ch.Cyberduck.Core;
 using Ch.Cyberduck.Core.Bonjour;
 using Ch.Cyberduck.Core.Diagnostics;
@@ -367,6 +368,8 @@ namespace Ch.Cyberduck.Ui.Core.Preferences
             defaults.put(String.Format("connection.protocol.{0}.enable", new IRODSProtocol().getIdentifier()),
                 false.ToString());
             defaults.put(String.Format("connection.protocol.{0}.enable", new B2Protocol().getIdentifier()),
+                false.ToString());
+            defaults.put(String.Format("connection.protocol.{0}.enable", new SpectraProtocol().getIdentifier()),
                 false.ToString());
 
              // NTLM Windows Domain
