053b64ee92d5b3439205d3634ca83aafdb95cc06
David Kocher <dkocher@iterate.ch>
2016-03-14 17:27:08 +0100

    Add test.
    Former-commit-id: b8346d7780cc5184869e0b4ab9a6e7d0b615c830

--- googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveMoveFeatureTest.java
+++ googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveMoveFeatureTest.java
@@ -15,15 +15,81 @@ package ch.cyberduck.core.googledrive;
  * GNU General Public License for more details.
  */
 
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledHostKeyCallback;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.DisabledProgressListener;
+import ch.cyberduck.core.DisabledTranscriptListener;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.LoginConnectionService;
+import ch.cyberduck.core.LoginOptions;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathCache;
+import ch.cyberduck.core.Scheme;
+import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.features.Find;
+import ch.cyberduck.core.shared.DefaultHomeFinderService;
+import ch.cyberduck.core.shared.DefaultTouchFeature;
+import ch.cyberduck.core.ssl.DefaultX509KeyManager;
+import ch.cyberduck.core.ssl.DefaultX509TrustManager;
 import ch.cyberduck.test.IntegrationTest;
 
 import org.junit.Test;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
 
 @IntegrationTest
 public class DriveMoveFeatureTest {
 
     @Test
-    public void testMove() {
+    public void testMove() throws Exception {
+        final Host host = new Host(new DriveProtocol(), "www.googleapis.com", new Credentials());
+        final DriveSession session = new DriveSession(host, new DefaultX509TrustManager(), new DefaultX509KeyManager());
+        new LoginConnectionService(new DisabledLoginCallback() {
+            @Override
+            public void prompt(final Host bookmark, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                fail(reason);
+            }
+        }, new DisabledHostKeyCallback(),
+                new DisabledPasswordStore() {
+                    @Override
+                    public String getPassword(Scheme scheme, int port, String hostname, String user) {
+                        if(user.equals("Google Drive OAuth2 Access Token")) {
+                            return System.getProperties().getProperty("googledrive.accesstoken");
+                        }
+                        if(user.equals("Google Drive OAuth2 Refresh Token")) {
+                            return System.getProperties().getProperty("googledrive.refreshtoken");
+                        }
+                        fail();
+                        return null;
+                    }
 
+                    @Override
+                    public String getPassword(String hostname, String user) {
+                        return super.getPassword(hostname, user);
+                    }
+                }, new DisabledProgressListener(),
+                new DisabledTranscriptListener()).connect(session, PathCache.empty());
+        final Path test = new Path(new DriveHomeFinderService(session).find(), UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
+        new DefaultTouchFeature(session).touch(test);
+        final Path target = new Path(new DefaultHomeFinderService(session).find(), UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
+        new DriveMoveFeature(session).move(test, target, false, new Delete.Callback() {
+            @Override
+            public void delete(final Path file) {
+            }
+        });
+        assertFalse(session.getFeature(Find.class).find(test));
+        assertTrue(session.getFeature(Find.class).find(target));
+        new DriveDeleteFeature(session).delete(Collections.<Path>singletonList(target), new DisabledLoginCallback(), new Delete.Callback() {
+            @Override
+            public void delete(final Path file) {
+            }
+        });
     }
 }
