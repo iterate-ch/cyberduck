4dc5b0976cde65130f5292201c02f723c89fc5ef
David Kocher <dkocher@iterate.ch>
2016-03-27 20:54:29 +0200

    Notify listener.
    Former-commit-id: f3d6024a17a7044f327e58c10c122d138c6c50d5

--- dropbox/src/main/java/ch/cyberduck/core/dropbox/DropBoxListService.java
+++ dropbox/src/main/java/ch/cyberduck/core/dropbox/DropBoxListService.java
@@ -15,36 +15,43 @@ package ch.cyberduck.core.dropbox;
  * GNU General Public License for more details.
  */
 
-import ch.cyberduck.core.*;
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.ListProgressListener;
+import ch.cyberduck.core.ListService;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.PathNormalizer;
 import ch.cyberduck.core.exception.BackgroundException;
-import com.dropbox.core.DbxException;
-import com.dropbox.core.v2.files.FileMetadata;
-import com.dropbox.core.v2.files.ListFolderResult;
-import com.dropbox.core.v2.files.Metadata;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import java.util.EnumSet;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
 
 public class DropBoxListService implements ListService {
     private static final Logger log = Logger.getLogger(DropBoxListService.class);
 
     private final DropboxSession session;
 
-    public DropBoxListService(DropboxSession session) {
+    public DropBoxListService(final DropboxSession session) {
         this.session = session;
     }
 
     @Override
-    public AttributedList<Path> list(Path directory, ListProgressListener listener) throws BackgroundException {
+    public AttributedList<Path> list(final Path directory, final ListProgressListener listener) throws BackgroundException {
         try {
             final AttributedList<Path> children = new AttributedList<>();
             final String path = directory.isRoot() ? StringUtils.EMPTY : directory.getAbsolute();
-            ListFolderResult result = session.getClient().getDbxClient().files().listFolder(path);
-            for (Metadata md : result.getEntries()) {
+            ListFolderResult result = session.getClient().files().listFolder(path);
+            for(Metadata md : result.getEntries()) {
                 final PathAttributes attributes = new PathAttributes();
-                if (md instanceof FileMetadata) {
-                    FileMetadata fmd = (FileMetadata)md;
+                if(md instanceof FileMetadata) {
+                    FileMetadata fmd = (FileMetadata) md;
                     attributes.setSize(fmd.getSize());
                     attributes.setVersionId(fmd.getId());
                     attributes.setModificationDate(fmd.getClientModified().getTime());
@@ -53,12 +60,13 @@ public class DropBoxListService implements ListService {
                 }
 
                 final Path child = new Path(directory, PathNormalizer.name(md.getName()), EnumSet.of(Path.Type.file), attributes);
+                listener.chunk(directory, children);
                 children.add(child);
             }
             return children;
-        } catch (DbxException e) {
-            e.printStackTrace();
-            return null;
+        }
+        catch(DbxException e) {
+            throw new DropboxExceptionMappingService().map(e);
         }
     }
 }
