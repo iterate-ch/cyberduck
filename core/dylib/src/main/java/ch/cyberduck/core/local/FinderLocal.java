6fd4bf587dda56fbd6e925098e857192785b34e6
David Kocher <dkocher@iterate.ch>
2016-04-12 10:43:51 +0200

    Throw failure if start accessing bookmark fails.
    Former-commit-id: 3cd6b395e06e72a2fa9cad978164a8f768cfdd20

--- core/dylib/src/main/java/ch/cyberduck/core/local/FinderLocal.java
+++ core/dylib/src/main/java/ch/cyberduck/core/local/FinderLocal.java
@@ -170,7 +170,9 @@ public class FinderLocal extends Local {
     public NSURL lock() throws AccessDeniedException {
         final NSURL resolved = new PanelSandboxBookmarkResolver().resolve(this);
         if(resolved.respondsToSelector(Foundation.selector("startAccessingSecurityScopedResource"))) {
-            resolved.startAccessingSecurityScopedResource();
+            if(!resolved.startAccessingSecurityScopedResource()) {
+                throw new LocalAccessDeniedException(String.format("Failure accessing security scoped resource for %s", this));
+            }
         }
         return resolved;
     }
