42492981b08aab7a8a335eeedbfbf2452923f9e1
David Kocher <dkocher@iterate.ch>
2016-04-19 10:34:00 +0200

    Fix null pointer in disabled updater.
    Former-commit-id: 90b1d67bff19591db387b6416be16ea45360d8f7

--- core/dylib/src/main/java/ch/cyberduck/core/sparkle/SparklePeriodicUpdateChecker.java
+++ core/dylib/src/main/java/ch/cyberduck/core/sparkle/SparklePeriodicUpdateChecker.java
@@ -16,21 +16,35 @@ package ch.cyberduck.core.sparkle;
  */
 
 import ch.cyberduck.binding.Outlet;
+import ch.cyberduck.core.FactoryException;
 import ch.cyberduck.core.updater.AbstractPeriodicUpdateChecker;
 
+import org.apache.log4j.Logger;
+
 public class SparklePeriodicUpdateChecker extends AbstractPeriodicUpdateChecker {
+    private static final Logger log = Logger.getLogger(SparklePeriodicUpdateChecker.class);
 
     @Outlet
-    private Updater updater
-            = Updater.create();
+    private Updater updater;
+
+    public SparklePeriodicUpdateChecker() {
+        try {
+            updater = Updater.create();
+        }
+        catch(FactoryException e) {
+            log.warn(String.format("Updater is disabled. %s", e.getMessage()));
+        }
+    }
 
     @Override
     public void check(boolean background) {
-        if(background) {
-            updater.checkForUpdatesInBackground();
-        }
-        else {
-            updater.checkForUpdates(null);
+        if(this.hasUpdatePrivileges()) {
+            if(background) {
+                updater.checkForUpdatesInBackground();
+            }
+            else {
+                updater.checkForUpdates(null);
+            }
         }
     }
 
