9336ecde313608904149d4e1bf1a2728601f7c55
David Kocher <dkocher@iterate.ch>
2016-04-22 13:36:00 +0200

    Fix tests.
    Former-commit-id: 350f9f7d5245b085c9e173b8b893e051993d20f6

--- core/src/test/java/ch/cyberduck/core/local/TemporaryFileServiceTest.java
+++ core/src/test/java/ch/cyberduck/core/local/TemporaryFileServiceTest.java
@@ -1,10 +1,11 @@
 package ch.cyberduck.core.local;
 
 import ch.cyberduck.core.Path;
-import ch.cyberduck.core.PathNormalizer;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.EnumSet;
 
 import static org.junit.Assert.assertEquals;
@@ -13,46 +14,46 @@ public class TemporaryFileServiceTest {
 
     @Test
     public void testCreateFile() throws Exception {
-        final String temp = System.getProperty("java.io.tmpdir");
+        final String temp = StringUtils.removeEnd(System.getProperty("java.io.tmpdir"), File.separator);
         final String s = System.getProperty("file.separator");
-        assertEquals(String.format("%su%sp%sf", temp, s, s),
+        assertEquals(String.format("%s%su%sp%sf", temp, s, s, s),
                 new TemporaryFileService().create("u", new Path("/p/f", EnumSet.of(Path.Type.file))).getAbsolute());
         final Path file = new Path("/p/f", EnumSet.of(Path.Type.file));
         file.attributes().setRegion("region");
-        assertEquals(String.format("%su%sp%sf", temp, s, s),
+        assertEquals(String.format("%s%su%sp%sf", temp, s, s, s),
                 new TemporaryFileService().create("u", file).getAbsolute());
     }
 
     @Test
     public void testVersion() throws Exception {
-        final String temp = System.getProperty("java.io.tmpdir");
+        final String temp = StringUtils.removeEnd(System.getProperty("java.io.tmpdir"), File.separator);
         final String s = System.getProperty("file.separator");
         {
             final Path file = new Path("/p/f", EnumSet.of(Path.Type.file));
             file.attributes().setRegion("region");
             file.attributes().setVersionId("2");
-            assertEquals(String.format("%su%sp%s2%sf", temp, s, s, s),
+            assertEquals(String.format("%s%su%sp%s2%sf", temp, s, s, s, s),
                     new TemporaryFileService().create("u", file).getAbsolute());
         }
         {
             final Path file = new Path("/p", EnumSet.of(Path.Type.directory));
             file.attributes().setRegion("region");
             file.attributes().setVersionId("2");
-            assertEquals(String.format("%su%sregion2%sp", temp, s, s),
+            assertEquals(String.format("%s%su%sregion2%sp", temp, s, s, s),
                     new TemporaryFileService().create("u", file).getAbsolute());
         }
     }
 
     @Test
     public void testCreateContainer() throws Exception {
-        final String temp = System.getProperty("java.io.tmpdir");
+        final String temp = StringUtils.removeEnd(System.getProperty("java.io.tmpdir"), File.separator);
         final String s = System.getProperty("file.separator");
         final Path file = new Path("/container", EnumSet.of(Path.Type.directory));
         file.attributes().setRegion("region");
-        assertEquals(String.format("%su%sregion%scontainer", temp, s, s),
+        assertEquals(String.format("%s%su%sregion%scontainer", temp, s, s, s),
                 new TemporaryFileService().create("u", file).getAbsolute());
         file.attributes().setVersionId("2");
-        assertEquals(String.format("%su%sregion2%scontainer", temp, s, s),
+        assertEquals(String.format("%s%su%sregion2%scontainer", temp, s, s, s),
                 new TemporaryFileService().create("u", file).getAbsolute());
     }
 }
