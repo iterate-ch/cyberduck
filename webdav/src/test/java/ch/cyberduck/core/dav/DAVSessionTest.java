cdf9265c609fb30d18871d73fc62dc04448a1a4a
David Kocher <dkocher@iterate.ch>
2016-04-22 20:56:06 +0200

    Delete tests.
    Former-commit-id: 938cf315d31b3dc6af1d56e423a1367e33f53d9f

--- webdav/src/test/java/ch/cyberduck/core/dav/DAVSessionTest.java
+++ webdav/src/test/java/ch/cyberduck/core/dav/DAVSessionTest.java
@@ -373,50 +373,14 @@ public class DAVSessionTest {
         }, null);
     }
 
-    @Test(expected = LoginFailureException.class)
-    @Ignore
-    public void testLoginErrorBasicFallback() throws Exception {
-        final Host host = new Host(new DAVProtocol(), "prod.lattusdemo.com", new Credentials(
-                "u", "p"
-        ));
-        host.setDefaultPath("/namespace");
-        final DAVSession session = new DAVSession(host);
-        PreferencesFactory.get().setProperty("webdav.basic.preemptive", true);
-        session.open(new DisabledHostKeyCallback(), new DisabledTranscriptListener());
-        try {
-            session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback());
-        }
-        catch(LoginFailureException e) {
-            assertEquals("Unauthorized. Please contact your web hosting service provider for assistance.", e.getDetail());
-            throw e;
-        }
-    }
-
     @Test
-    @Ignore
     public void testFeatures() throws Exception {
         final Session session = new DAVSession(new Host(new DAVProtocol(), "h"));
         assertNull(session.getFeature(UnixPermission.class));
-        assertNull(session.getFeature(Timestamp.class));
+        assertNotNull(session.getFeature(Timestamp.class));
         assertNotNull(session.getFeature(Copy.class));
         assertNotNull(session.getFeature(Headers.class));
         assertNotNull(session.getFeature(DistributionConfiguration.class));
-    }
-
-    @Test
-    @Ignore
-    public void testdavpiximegallery() throws Exception {
-        final Host host = new Host(new DAVSSLProtocol(), "g2.pixi.me", new Credentials(
-                "webdav", "webdav"
-        ));
-        host.setDefaultPath("/w/webdav/");
-        final DAVSession session = new DAVSession(host);
-        session.open(new DisabledHostKeyCallback(), new DisabledTranscriptListener());
-        assertTrue(session.isConnected());
-        assertTrue(session.isSecured());
-        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback());
-        assertFalse(session.getAcceptedIssuers().isEmpty());
-        session.close();
     }
 
     @Test(expected = LoginFailureException.class)
@@ -445,70 +409,6 @@ public class DAVSessionTest {
         session.close();
     }
 
-    @Test
-    public void testInteroperabilitydavpixime() throws Exception {
-        // no SSLv3, supports TLSv1-1.2 - Apache/mod_dav framework)
-        final Host host = new Host(new DAVSSLProtocol(), "dav.pixi.me", new Credentials(
-                "webdav", "webdav"
-        ));
-        final DAVSession session = new DAVSession(host, new DefaultX509TrustManager() {
-            @Override
-            public void verify(final String hostname, final X509Certificate[] certs, final String cipher) throws CertificateException {
-                assertEquals("ECDHE_RSA", cipher);
-                super.verify(hostname, certs, cipher);
-            }
-        }, new DefaultX509KeyManager());
-        session.open(new DisabledHostKeyCallback(), new DisabledTranscriptListener());
-        assertTrue(session.isConnected());
-        assertTrue(session.isSecured());
-        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback());
-        assertFalse(session.getAcceptedIssuers().isEmpty());
-        session.close();
-    }
-
-    @Test
-    public void testInteroperabilitytlsv11pixime() throws Exception {
-        // no SSLv3, supports TLSv1.1 only -Apache/mod_dav framework
-        final Host host = new Host(new DAVSSLProtocol(), "tlsv11.pixi.me", new Credentials(
-                "webdav", "webdav"
-        ));
-        final DAVSession session = new DAVSession(host, new DefaultX509TrustManager() {
-            @Override
-            public void verify(final String hostname, final X509Certificate[] certs, final String cipher) throws CertificateException {
-                assertEquals("ECDHE_RSA", cipher);
-                super.verify(hostname, certs, cipher);
-            }
-        }, new DefaultX509KeyManager());
-        session.open(new DisabledHostKeyCallback(), new DisabledTranscriptListener());
-        assertTrue(session.isConnected());
-        assertTrue(session.isSecured());
-        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback());
-        assertFalse(session.getAcceptedIssuers().isEmpty());
-        session.close();
-    }
-
-    @Test
-    public void testInteroperabilitytlsv12pixime() throws Exception {
-        // no SSLv3, supports TLSv1.2 only - Apache/mod_dav framework
-        final Host host = new Host(new DAVSSLProtocol(), "tlsv12.pixi.me", new Credentials(
-                "webdav", "webdav"
-        ));
-        final DAVSession session = new DAVSession(host, new DefaultX509TrustManager() {
-            @Override
-            public void verify(final String hostname, final X509Certificate[] certs, final String cipher) throws CertificateException {
-                assertEquals("ECDHE_RSA", cipher);
-                super.verify(hostname, certs, cipher);
-            }
-        }, new DefaultX509KeyManager());
-        session.open(new DisabledHostKeyCallback(), new DisabledTranscriptListener());
-        assertTrue(session.isConnected());
-        assertTrue(session.isSecured());
-        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback());
-        assertFalse(session.getAcceptedIssuers().isEmpty());
-        session.close();
-    }
-
-    @Test
     public void testLoginChangeUsername() throws Exception {
         final Host host = new Host(new DAVProtocol(), "test.cyberduck.ch");
         host.setDefaultPath("/dav/basic");
@@ -646,34 +546,6 @@ public class DAVSessionTest {
             assertEquals("Handshake failure. Unable to negotiate an acceptable set of security parameters. Please contact your web hosting service provider for assistance.", e.getDetail());
             throw e;
         }
-    }
-
-    @Test(expected = LoginCanceledException.class)
-    public void testTrustChain1() throws Exception {
-        final Host host = new Host(new DAVSSLProtocol(), "dav.pixi.me");
-        final AtomicBoolean verified = new AtomicBoolean();
-        final DAVSession session = new DAVSession(host, new DefaultX509TrustManager() {
-            @Override
-            public void verify(final String hostname, final X509Certificate[] certs, final String cipher) throws CertificateException {
-                assertEquals(2, certs.length);
-                assertEquals("CN=RapidSSL SHA256 CA - G3,O=GeoTrust Inc.,C=US",
-                        certs[certs.length - 1].getSubjectX500Principal().getName());
-                assertEquals("OU=GT79990730,OU=See www.rapidssl.com/resources/cps (c)15,OU=Domain Control Validated - RapidSSL(R),CN=*.pixi.me",
-                        certs[0].getSubjectDN().getName());
-                verified.set(true);
-                super.verify(hostname, certs, cipher);
-            }
-        },
-                new KeychainX509KeyManager(new DisabledCertificateStore()));
-        final LoginConnectionService c = new LoginConnectionService(
-                new DisabledLoginCallback(),
-                new DisabledHostKeyCallback(),
-                new DisabledPasswordStore(),
-                new DisabledProgressListener(),
-                new DisabledTranscriptListener());
-        c.connect(session, PathCache.empty());
-        assertTrue(verified.get());
-        session.close();
     }
 
     @Test
