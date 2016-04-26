5199f27d05cb1197bbace8ab4876d59fe949e6bc
David Kocher <dkocher@iterate.ch>
2016-04-26 16:27:12 +0200

    Update copyright header.
    Former-commit-id: 119e05a99d12e05f0b12642882a99d30f8b10096

--- osx/src/main/objc/CDToolbarItem.h
+++ osx/src/main/objc/CDToolbarItem.h
@@ -1,19 +1,16 @@
 /*
- *  Copyright (c) 2016 David Kocher. All rights reserved.
- *  https://cyberduck.io/
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  Bug fixes, suggestions and comments should be sent to:
- *  dkocher@cyberduck.ch
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #import <Cocoa/Cocoa.h>
