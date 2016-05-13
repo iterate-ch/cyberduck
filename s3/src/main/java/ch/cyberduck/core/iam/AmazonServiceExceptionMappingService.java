a878263534cdfd15f77e08e8724f51c59e612ace
David Kocher <dkocher@iterate.ch>
2016-05-13 11:12:44 +0200

    Shorten error message.
    Former-commit-id: 4614f6db0f2ca5406051af6b506d28dc41e12cfa

--- s3/src/main/java/ch/cyberduck/core/iam/AmazonServiceExceptionMappingService.java
+++ s3/src/main/java/ch/cyberduck/core/iam/AmazonServiceExceptionMappingService.java
@@ -35,9 +35,9 @@ public class AmazonServiceExceptionMappingService extends AbstractExceptionMappi
     @Override
     public BackgroundException map(final AmazonClientException e) {
         final StringBuilder buffer = new StringBuilder();
-        this.append(buffer, e.getMessage());
         if(e instanceof AmazonServiceException) {
             final AmazonServiceException failure = (AmazonServiceException) e;
+            this.append(buffer, failure.getErrorMessage());
             switch(failure.getStatusCode()) {
                 case HttpStatus.SC_BAD_REQUEST:
                     switch(failure.getErrorCode()) {
@@ -71,6 +71,7 @@ public class AmazonServiceExceptionMappingService extends AbstractExceptionMappi
                     return new ConnectionRefusedException(buffer.toString(), e);
             }
         }
+        this.append(buffer, e.getMessage());
         return this.wrap(e, buffer);
     }
 }
