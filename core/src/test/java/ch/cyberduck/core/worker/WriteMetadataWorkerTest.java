c4de93467352629ef468657153900fc1a0ebe140
David Kocher <dkocher@iterate.ch>
2016-05-16 10:49:14 +0200

    Unchecked warnings.
    Former-commit-id: 4ee4812adcf515ac7e31918cc44ef7207f96f10f

--- core/src/test/java/ch/cyberduck/core/worker/WriteMetadataWorkerTest.java
+++ core/src/test/java/ch/cyberduck/core/worker/WriteMetadataWorkerTest.java
@@ -77,6 +77,7 @@ public class WriteMetadataWorkerTest {
         };
         worker.run(new NullSession(new Host(new TestProtocol())) {
             @Override
+            @SuppressWarnings("unchecked")
             public <T> T getFeature(final Class<T> type) {
                 if(type == Headers.class) {
                     return (T) new Headers() {
@@ -121,6 +122,7 @@ public class WriteMetadataWorkerTest {
         };
         worker.run(new NullSession(new Host(new TestProtocol())) {
             @Override
+            @SuppressWarnings("unchecked")
             public <T> T getFeature(final Class<T> type) {
                 if(type == Headers.class) {
                     return (T) new Headers() {
