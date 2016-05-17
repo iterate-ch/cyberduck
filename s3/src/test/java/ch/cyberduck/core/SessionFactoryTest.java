02b29d3f3ed6b010e390230efe09c8bf62dc0d81
David Kocher <dkocher@iterate.ch>
2016-05-17 14:42:22 +0200

    Fix test.
    Former-commit-id: 26d28cb3cecfbcc50653038cfc1fc50c561c9d5f

--- s3/src/test/java/ch/cyberduck/core/SessionFactoryTest.java
+++ s3/src/test/java/ch/cyberduck/core/SessionFactoryTest.java
@@ -1,6 +1,5 @@
 package ch.cyberduck.core;
 
-import ch.cyberduck.core.googlestorage.GoogleStorageProtocol;
 import ch.cyberduck.core.s3.S3Protocol;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DefaultX509TrustManager;
@@ -9,16 +8,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
-/**
- * @version $Id$
- */
 public class SessionFactoryTest {
 
     @Test
     public void testCreateSession() throws Exception {
         assertNotNull(SessionFactory.create(new Host(new S3Protocol()),
-                new DefaultX509TrustManager(), new DefaultX509KeyManager()));
-        assertNotNull(SessionFactory.create(new Host(new GoogleStorageProtocol()),
                 new DefaultX509TrustManager(), new DefaultX509KeyManager()));
     }
 }
