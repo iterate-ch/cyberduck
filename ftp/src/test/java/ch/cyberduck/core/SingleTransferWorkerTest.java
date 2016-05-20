e61cee7b0b76acfa63fc1bf7810adc9fb7f2b475
David Kocher <dkocher@iterate.ch>
2016-05-20 16:16:22 +0200

    Add test.
    Former-commit-id: 82a9e0694f4ca6236bc5d3485e9854615ae8d985

---
+++ ftp/src/test/java/ch/cyberduck/core/SingleTransferWorkerTest.java
@@ -0,0 +1,125 @@
+package ch.cyberduck.core;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.features.Write;
+import ch.cyberduck.core.ftp.FTPDeleteFeature;
+import ch.cyberduck.core.ftp.FTPSession;
+import ch.cyberduck.core.ftp.FTPTLSProtocol;
+import ch.cyberduck.core.ftp.FTPWriteFeature;
+import ch.cyberduck.core.io.DisabledStreamListener;
+import ch.cyberduck.core.shared.DefaultHomeFinderService;
+import ch.cyberduck.core.transfer.DisabledTransferErrorCallback;
+import ch.cyberduck.core.transfer.DisabledTransferItemCallback;
+import ch.cyberduck.core.transfer.DisabledTransferPrompt;
+import ch.cyberduck.core.transfer.Transfer;
+import ch.cyberduck.core.transfer.TransferAction;
+import ch.cyberduck.core.transfer.TransferItem;
+import ch.cyberduck.core.transfer.TransferOptions;
+import ch.cyberduck.core.transfer.TransferSpeedometer;
+import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.core.transfer.UploadTransfer;
+import ch.cyberduck.core.worker.SingleTransferWorker;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.CountingOutputStream;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.SocketTimeoutException;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SingleTransferWorkerTest {
+
+    @Test
+    public void testTransferredSizeRepeat() throws Exception {
+        final Local local = new Local(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
+        final byte[] content = new byte[62768];
+        new Random().nextBytes(content);
+        final OutputStream out = local.getOutputStream(false);
+        IOUtils.write(content, out);
+        out.close();
+        final Host host = new Host(new FTPTLSProtocol(), "test.cyberduck.ch", new Credentials(
+                System.getProperties().getProperty("ftp.user"), System.getProperties().getProperty("ftp.password")
+        ));
+        final AtomicBoolean failed = new AtomicBoolean();
+        final FTPSession session = new FTPSession(host) {
+            final FTPWriteFeature write = new FTPWriteFeature(this) {
+                @Override
+                public OutputStream write(final Path file, final TransferStatus status) throws BackgroundException {
+                    final OutputStream out = super.write(file, status);
+                    if(failed.get()) {
+                        // Second attempt successful
+                        return out;
+                    }
+                    return new CountingOutputStream(out) {
+                        @Override
+                        protected void afterWrite(final int n) throws IOException {
+                            super.afterWrite(n);
+                            if(this.getByteCount() >= 42768L) {
+                                // Buffer size
+                                assertEquals(32768L, status.getOffset());
+                                failed.set(true);
+                                throw new SocketTimeoutException();
+                            }
+                        }
+                    };
+                }
+            };
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public <T> T getFeature(final Class<T> type) {
+                if(type == Write.class) {
+                    return (T) write;
+                }
+                return super.getFeature(type);
+            }
+        };
+        session.open(new DisabledHostKeyCallback(), new DisabledTranscriptListener());
+        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback());
+        final Path test = new Path(new DefaultHomeFinderService(session).find(), UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
+        final Transfer t = new UploadTransfer(new Host(new TestProtocol()), test, local);
+        final BytecountStreamListener counter = new BytecountStreamListener(new DisabledStreamListener());
+        assertTrue(new SingleTransferWorker(session, t, new TransferOptions(), new TransferSpeedometer(t), new DisabledTransferPrompt() {
+            @Override
+            public TransferAction prompt(final TransferItem file) {
+                return TransferAction.overwrite;
+            }
+        }, new DisabledTransferErrorCallback(), new DisabledTransferItemCallback(),
+                new DisabledProgressListener(), counter, new DisabledLoginCallback(), TransferItemCache.empty()) {
+
+        }.run(session));
+        new FTPDeleteFeature(session).delete(Collections.singletonList(test), new DisabledLoginCallback(), new Delete.Callback() {
+            @Override
+            public void delete(final Path file) {
+            }
+        });
+        local.delete();
+        assertEquals(62768L, counter.getSent(), 0L);
+        assertTrue(failed.get());
+    }
+}
