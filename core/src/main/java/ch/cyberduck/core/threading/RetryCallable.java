f2cb7ee7ea99f8463ae33af753e97dbbe077479b
David Kocher <dkocher@iterate.ch>
2016-05-24 21:44:37 +0200

    Logging.
    Former-commit-id: 7c44d08922bdb1453b6aa441ea754a3263387f46

--- core/src/main/java/ch/cyberduck/core/threading/RetryCallable.java
+++ core/src/main/java/ch/cyberduck/core/threading/RetryCallable.java
@@ -23,10 +23,13 @@ import ch.cyberduck.core.io.StreamCancelation;
 import ch.cyberduck.core.preferences.Preferences;
 import ch.cyberduck.core.preferences.PreferencesFactory;
 
+import org.apache.log4j.Logger;
+
 import java.text.MessageFormat;
 import java.util.concurrent.Callable;
 
 public abstract class RetryCallable<T> implements Callable<T> {
+    private static final Logger log = Logger.getLogger(RetryCallable.class);
 
     private final Preferences preferences = PreferencesFactory.get();
 
@@ -41,28 +44,29 @@ public abstract class RetryCallable<T> implements Callable<T> {
     private int backoff = preferences.getInteger("connection.retry.delay");
 
     /**
-     * @param e        Failure
+     * @param failure  Failure
      * @param progress Listener
      * @param cancel   Progress callback
      * @return Increment counter and return true if retry attempt should be made for a failed transfer
      */
-    public boolean retry(final BackgroundException e, final ProgressListener progress, final StreamCancelation cancel) {
+    public boolean retry(final BackgroundException failure, final ProgressListener progress, final StreamCancelation cancel) {
         if(++count > preferences.getInteger("connection.retry")) {
+            log.warn(String.format("Cancel retry for failure %s", failure));
             return false;
-
         }
-        switch(diagnostics.determine(e)) {
+        switch(diagnostics.determine(failure)) {
             case network:
             case application:
                 final int delay;
-                if(e instanceof RetriableAccessDeniedException) {
-                    delay = (int) ((RetriableAccessDeniedException) e).getRetry().getSeconds();
+                if(failure instanceof RetriableAccessDeniedException) {
+                    delay = (int) ((RetriableAccessDeniedException) failure).getRetry().getSeconds();
                 }
                 else {
                     delay = backoff;
                     // Exponential backoff
                     backoff *= 2;
                 }
+                log.warn(String.format("Retry for failure %s with delay of %ds", failure, delay));
                 final BackgroundActionPauser pause = new BackgroundActionPauser(new BackgroundActionPauser.Callback() {
                     @Override
                     public boolean isCanceled() {
@@ -77,6 +81,7 @@ public abstract class RetryCallable<T> implements Callable<T> {
                 pause.await(progress);
                 return true;
         }
+        log.warn(String.format("No retry for failure %s", failure));
         return false;
     }
 }
