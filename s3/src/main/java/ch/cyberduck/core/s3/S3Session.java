62000d1dff8e4f80c4be7204befe8125d63ee60b
David Kocher <dkocher@iterate.ch>
2016-08-05 11:31:08 +0200

    Remove target check for multipart write feature.
    Former-commit-id: aa8c0de479a18c939a8cde6efc039707bf70ffdf

--- s3/src/main/java/ch/cyberduck/core/s3/S3Session.java
+++ s3/src/main/java/ch/cyberduck/core/s3/S3Session.java
@@ -266,10 +266,7 @@ public class S3Session extends HttpSession<RequestEntityRestStorageService> {
             return (T) new S3ReadFeature(this);
         }
         if(type == Write.class) {
-            if(host.getHostname().endsWith(preferences.getProperty("s3.hostname.default"))) {
-                return (T) new S3MultipartWriteFeature(this);
-            }
-            return (T) new S3WriteFeature(this);
+            return (T) new S3MultipartWriteFeature(this);
         }
         if(type == Download.class) {
             return (T) new S3ThresholdDownloadService(this, trust, key, new QloudsonicTransferOption());
