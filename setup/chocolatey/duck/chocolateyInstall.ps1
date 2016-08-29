82159023ff1b5a66984d0976bd8f31bf4c5a40b1
Yves Langisch <yla@iterate.ch>
2016-08-29 17:42:39 +0200

    Add chocolatey package for Cyberduck.
    Former-commit-id: 28b0ee649db6da4fb1a45b9792bdd06e2f6511ac

---
+++ setup/chocolatey/duck/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'duck'
+$installerType = 'exe'
+$url = 'https://dist.duck.sh/${FEED}/duck-${VERSION}.${REVISION}.exe'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+$checksum = '${CHECKSUM}'
+$checksumType = 'sha256'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType"
--- setup/chocolatey/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'duck'
-$installerType = 'EXE'
-$url = 'https://dist.duck.sh/${FEED}/duck-${VERSION}.${REVISION}.exe'
-$silentArgs = '/quiet'
-$validExitCodes = @(0)
-$checksum = '${CHECKSUM}'
-$checksumType = 'sha256'
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType"
