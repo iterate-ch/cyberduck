562c1b9c524c7d9ed3e0610687399bc93ac0e0b1
David Kocher <dkocher@iterate.ch>
2016-08-30 23:37:14 +0200

    Implement shared link.
    Former-commit-id: fad69d1ee66132d20c8a0057f23ecbfcabda7ead

--- dropbox/src/main/java/ch/cyberduck/core/dropbox/DropboxUrlProvider.java
+++ dropbox/src/main/java/ch/cyberduck/core/dropbox/DropboxUrlProvider.java
@@ -1,12 +1,21 @@
 package ch.cyberduck.core.dropbox;
 
+import ch.cyberduck.core.DescriptiveUrl;
 import ch.cyberduck.core.DescriptiveUrlBag;
+import ch.cyberduck.core.LocaleFactory;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Scheme;
 import ch.cyberduck.core.UrlProvider;
 
-/**
- * Created by sebi on 01.04.16.
- */
+import java.net.URI;
+import java.text.MessageFormat;
+import java.util.Locale;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.sharing.RequestedVisibility;
+import com.dropbox.core.v2.sharing.SharedLinkMetadata;
+import com.dropbox.core.v2.sharing.SharedLinkSettings;
+
 public class DropboxUrlProvider implements UrlProvider {
 
     private final DropboxSession session;
@@ -17,6 +26,18 @@ public class DropboxUrlProvider implements UrlProvider {
 
     @Override
     public DescriptiveUrlBag toUrl(final Path file) {
-        return null;
+        final DescriptiveUrlBag list = new DescriptiveUrlBag();
+        if(file.isFile()) {
+            try {
+                final SharedLinkMetadata shared = session.getClient().sharing().createSharedLinkWithSettings(file.getAbsolute(),
+                        new SharedLinkSettings(RequestedVisibility.PUBLIC, null, null));
+                list.add(new DescriptiveUrl(URI.create(shared.getUrl()), DescriptiveUrl.Type.http,
+                        MessageFormat.format(LocaleFactory.localizedString("{0} URL"), Scheme.https.name().toUpperCase(Locale.ROOT))));
+            }
+            catch(DbxException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 }
