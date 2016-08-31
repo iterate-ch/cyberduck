3275067bf64ff80a2b7c5df28ec3a119785313ab
David Kocher <dkocher@iterate.ch>
2016-08-31 16:43:01 +0200

    Implement feature.
    Former-commit-id: 7e756175fc8c041a886261c12ee1792a8d562846

--- dropbox/src/main/java/ch/cyberduck/core/dropbox/DropboxUrlProvider.java
+++ dropbox/src/main/java/ch/cyberduck/core/dropbox/DropboxUrlProvider.java
@@ -15,11 +15,27 @@ package ch.cyberduck.core.dropbox;
  * GNU General Public License for more details.
  */
 
+import ch.cyberduck.core.DescriptiveUrl;
 import ch.cyberduck.core.DescriptiveUrlBag;
+import ch.cyberduck.core.LocaleFactory;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Scheme;
 import ch.cyberduck.core.UrlProvider;
 
+import org.apache.log4j.Logger;
+
+import java.net.URI;
+import java.text.MessageFormat;
+import java.util.Locale;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.sharing.DbxUserSharingRequests;
+import com.dropbox.core.v2.sharing.RequestedVisibility;
+import com.dropbox.core.v2.sharing.SharedLinkMetadata;
+import com.dropbox.core.v2.sharing.SharedLinkSettings;
+
 public class DropboxUrlProvider implements UrlProvider {
+    private static final Logger log = Logger.getLogger(DropboxUrlProvider.class);
 
     private final DropboxSession session;
 
@@ -30,17 +46,17 @@ public class DropboxUrlProvider implements UrlProvider {
     @Override
     public DescriptiveUrlBag toUrl(final Path file) {
         final DescriptiveUrlBag list = new DescriptiveUrlBag();
-//        if(file.isFile()) {
-//            try {
-//                final SharedLinkMetadata shared = session.getClient().sharing().createSharedLinkWithSettings(file.getAbsolute(),
-//                        new SharedLinkSettings(RequestedVisibility.PUBLIC, null, null));
-//                list.add(new DescriptiveUrl(URI.create(shared.getUrl()), DescriptiveUrl.Type.http,
-//                        MessageFormat.format(LocaleFactory.localizedString("{0} URL"), Scheme.https.name().toUpperCase(Locale.ROOT))));
-//            }
-//            catch(DbxException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        if(file.isFile()) {
+            try {
+                final SharedLinkMetadata shared = new DbxUserSharingRequests(session.getClient()).createSharedLinkWithSettings(file.getAbsolute(),
+                        new SharedLinkSettings(RequestedVisibility.PUBLIC, null, null));
+                list.add(new DescriptiveUrl(URI.create(shared.getUrl()), DescriptiveUrl.Type.http,
+                        MessageFormat.format(LocaleFactory.localizedString("{0} URL"), Scheme.https.name().toUpperCase(Locale.ROOT))));
+            }
+            catch(DbxException e) {
+                log.warn(String.format("Failure retrieving shared link. %s", e.getMessage()));
+            }
+        }
         return list;
     }
 }
