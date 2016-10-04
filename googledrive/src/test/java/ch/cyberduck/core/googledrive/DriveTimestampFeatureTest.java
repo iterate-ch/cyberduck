e4b25336315dfb0d7968d439858e04593a0777a7
David Kocher <dkocher@iterate.ch>
2016-10-04 15:09:42 +0200

    Add tests.
    Former-commit-id: 04283dc9277b60b1b2136cc870c8e5e843d475a6

---
+++ googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveTimestampFeatureTest.java
@@ -0,0 +1,122 @@
+package ch.cyberduck.core.googledrive;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledHostKeyCallback;
+import ch.cyberduck.core.DisabledListProgressListener;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.DisabledProgressListener;
+import ch.cyberduck.core.DisabledTranscriptListener;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.LoginConnectionService;
+import ch.cyberduck.core.LoginOptions;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathCache;
+import ch.cyberduck.core.Scheme;
+import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.ssl.DefaultX509KeyManager;
+import ch.cyberduck.core.ssl.DefaultX509TrustManager;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class DriveTimestampFeatureTest {
+
+    @Test
+    public void testSetTimestamp() throws Exception {
+        final Host host = new Host(new DriveProtocol(), "www.googleapis.com", new Credentials());
+        final DriveSession session = new DriveSession(host, new DefaultX509TrustManager(), new DefaultX509KeyManager());
+        new LoginConnectionService(new DisabledLoginCallback() {
+            @Override
+            public void prompt(final Host bookmark, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                fail(reason);
+            }
+        }, new DisabledHostKeyCallback(),
+                new DisabledPasswordStore() {
+                    @Override
+                    public String getPassword(Scheme scheme, int port, String hostname, String user) {
+                        if(user.equals("Google Drive OAuth2 Access Token")) {
+                            return System.getProperties().getProperty("googledrive.accesstoken");
+                        }
+                        if(user.equals("Google Drive OAuth2 Refresh Token")) {
+                            return System.getProperties().getProperty("googledrive.refreshtoken");
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    public String getPassword(String hostname, String user) {
+                        return super.getPassword(hostname, user);
+                    }
+                }, new DisabledProgressListener(),
+                new DisabledTranscriptListener()).connect(session, PathCache.empty());
+        final Path home = new DriveHomeFinderService(session).find();
+        final Path test = new Path(home, UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
+        new DriveTouchFeature(session).touch(test);
+        final long modified = System.currentTimeMillis();
+        new DriveTimestampFeature(session).setTimestamp(test, modified);
+        assertEquals(modified / 1000 * 1000, session.list(home, new DisabledListProgressListener()).get(test).attributes().getModificationDate(), 0L);
+        new DriveDeleteFeature(session).delete(Collections.<Path>singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
+        session.close();
+    }
+
+    @Test
+    public void testSetTimestampDirectory() throws Exception {
+        final Host host = new Host(new DriveProtocol(), "www.googleapis.com", new Credentials());
+        final DriveSession session = new DriveSession(host, new DefaultX509TrustManager(), new DefaultX509KeyManager());
+        new LoginConnectionService(new DisabledLoginCallback() {
+            @Override
+            public void prompt(final Host bookmark, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                fail(reason);
+            }
+        }, new DisabledHostKeyCallback(),
+                new DisabledPasswordStore() {
+                    @Override
+                    public String getPassword(Scheme scheme, int port, String hostname, String user) {
+                        if(user.equals("Google Drive OAuth2 Access Token")) {
+                            return System.getProperties().getProperty("googledrive.accesstoken");
+                        }
+                        if(user.equals("Google Drive OAuth2 Refresh Token")) {
+                            return System.getProperties().getProperty("googledrive.refreshtoken");
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    public String getPassword(String hostname, String user) {
+                        return super.getPassword(hostname, user);
+                    }
+                }, new DisabledProgressListener(),
+                new DisabledTranscriptListener()).connect(session, PathCache.empty());
+        final Path home = new DriveHomeFinderService(session).find();
+        final Path test = new Path(home, UUID.randomUUID().toString(), EnumSet.of(Path.Type.directory));
+        new DriveDirectoryFeature(session).mkdir(test);
+        final long modified = System.currentTimeMillis();
+        new DriveTimestampFeature(session).setTimestamp(test, modified);
+        assertEquals(modified / 1000 * 1000, session.list(home, new DisabledListProgressListener()).get(test).attributes().getModificationDate(), 0L);
+        new DriveDeleteFeature(session).delete(Collections.<Path>singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
+        session.close();
+    }
+}
