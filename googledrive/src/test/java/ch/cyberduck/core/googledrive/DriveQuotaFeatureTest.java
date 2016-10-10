038aca9ec1b85bcaa8aacb89c1d77376d1d857a6
David Kocher <dkocher@iterate.ch>
2016-10-10 16:19:47 +0200

    Add test annotations. (cherry picked from commit 56f0050)
    Former-commit-id: 8427d5e8de9ca6f535cae806a5e894720954d52d

--- googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveQuotaFeatureTest.java
+++ googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveQuotaFeatureTest.java
@@ -30,11 +30,14 @@ import ch.cyberduck.core.exception.LoginCanceledException;
 import ch.cyberduck.core.features.Quota;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DefaultX509TrustManager;
+import ch.cyberduck.test.IntegrationTest;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.junit.Assert.*;
 
+@Category(IntegrationTest.class)
 public class DriveQuotaFeatureTest {
     @Test
     public void testGet() throws Exception {
