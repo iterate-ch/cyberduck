1ae1773426e352dc6d4b57c7d90be9fd7b11bb33
Jöran Malek <joeran3@gmail.com>
2016-11-04 22:34:42 +0100

    Extract PermissionOverwriteAction
    Former-commit-id: b5cf2b73331a19f6d830c94324bc199b26df20fe

--- core/src/main/java/ch/cyberduck/core/PermissionOverwrite.java
+++ core/src/main/java/ch/cyberduck/core/PermissionOverwrite.java
@@ -7,19 +7,19 @@ import org.apache.commons.lang3.StringUtils;
  * Created by alive on 04.11.2016.
  */
 public class PermissionOverwrite {
-    public final Action user, group, other;
+    public final PermissionOverwriteAction user, group, other;
 
     public PermissionOverwrite() {
         this((Boolean) null, (Boolean) null, (Boolean) null);
     }
 
     public PermissionOverwrite(Boolean read, Boolean write, Boolean execute) {
-        this.user = new Action(read, write, execute);
-        this.group = new Action(read, write, execute);
-        this.other = new Action(read, write, execute);
+        this.user = new PermissionOverwriteAction(read, write, execute);
+        this.group = new PermissionOverwriteAction(read, write, execute);
+        this.other = new PermissionOverwriteAction(read, write, execute);
     }
 
-    public PermissionOverwrite(Action user, Action group, Action other) {
+    public PermissionOverwrite(PermissionOverwriteAction user, PermissionOverwriteAction group, PermissionOverwriteAction other) {
         this.user = user;
         this.group = group;
         this.other = other;
@@ -42,42 +42,5 @@ public class PermissionOverwrite {
         symbolic.append(other);
 
         return symbolic.toString();
-    }
-
-    public class Action {
-        public Boolean read;
-        public Boolean write;
-        public Boolean execute;
-
-        public Action(Boolean read, Boolean write, Boolean execute) {
-            this.read = read;
-            this.write = write;
-            this.execute = execute;
-        }
-
-        public Permission.Action Resolve(Permission.Action original) {
-            Permission.Action result = Permission.Action.none;
-
-            result = SolvePermission(result, Permission.Action.read, read == null ? original.implies(Permission.Action.read) : read.booleanValue());
-            result = SolvePermission(result, Permission.Action.write, write == null ? original.implies(Permission.Action.write) : write.booleanValue());
-            result = SolvePermission(result, Permission.Action.execute, execute == null ? original.implies(Permission.Action.execute) : execute.booleanValue());
-
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            final StringBuilder symbolic = new StringBuilder();
-
-            symbolic.append(read != null ? read ? 'r' : '-' : '?');
-            symbolic.append(write != null ? write ? 'w' : '-' : '?');
-            symbolic.append(execute != null ? execute ? 'x' : '-' : '?');
-
-            return symbolic.toString();
-        }
-    }
-
-    private static Permission.Action SolvePermission(Permission.Action base, Permission.Action permission, boolean value) {
-        return value ? base.or(permission) : base.and(permission.not());
     }
 }
