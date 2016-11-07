9f138fcc27acda74305e8a6f3b09d6fcdb3586d9
David Kocher <dkocher@iterate.ch>
2016-11-07 10:59:24 +0100

    Fix tests.
    Former-commit-id: 3edefb8b71536154975cbe7c920be137eca6448d

--- s3/src/test/java/ch/cyberduck/core/cloudfront/CustomOriginCloudFrontDistributionConfigurationTest.java
+++ s3/src/test/java/ch/cyberduck/core/cloudfront/CustomOriginCloudFrontDistributionConfigurationTest.java
@@ -8,8 +8,6 @@ import ch.cyberduck.core.Path;
 import ch.cyberduck.core.TestProtocol;
 import ch.cyberduck.core.cdn.Distribution;
 import ch.cyberduck.core.exception.LoginCanceledException;
-import ch.cyberduck.core.s3.S3Protocol;
-import ch.cyberduck.core.s3.S3Session;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DefaultX509TrustManager;
 import ch.cyberduck.test.IntegrationTest;
@@ -19,7 +17,7 @@ import org.junit.experimental.categories.Category;
 
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumSet;
 
 import static org.junit.Assert.assertEquals;
@@ -30,54 +28,51 @@ public class CustomOriginCloudFrontDistributionConfigurationTest {
 
     @Test
     public void testGetMethods() throws Exception {
-        assertEquals(Arrays.asList(Distribution.CUSTOM),
+        assertEquals(Collections.singletonList(Distribution.CUSTOM),
                 new CustomOriginCloudFrontDistributionConfiguration(new Host(new TestProtocol()), new DefaultX509TrustManager(), new DefaultX509KeyManager(), new DisabledTranscriptListener()).getMethods(
                         new Path("/bbb", EnumSet.of(Path.Type.directory, Path.Type.volume))));
     }
 
     @Test
     public void testGetOrigin() throws Exception {
-        final Host h = new Host(new TestProtocol());
+        final Host origin = new Host(new TestProtocol(), "m");
         final Path container = new Path("/", EnumSet.of(Path.Type.directory, Path.Type.volume));
-        h.setWebURL("http://w.example.net");
-        final S3Session session = new S3Session(new Host(new S3Protocol(), new S3Protocol().getDefaultHostname()));
+        origin.setWebURL("http://w.example.net");
         final CustomOriginCloudFrontDistributionConfiguration configuration
-                = new CustomOriginCloudFrontDistributionConfiguration(h, new DefaultX509TrustManager(), new DefaultX509KeyManager(),
+                = new CustomOriginCloudFrontDistributionConfiguration(origin, new DefaultX509TrustManager(), new DefaultX509KeyManager(),
                 new DisabledTranscriptListener());
         assertEquals("w.example.net", configuration.getOrigin(container, Distribution.CUSTOM).getHost());
-        h.setWebURL(null);
+        origin.setWebURL(null);
         assertEquals("m", configuration.getOrigin(container, Distribution.CUSTOM).getHost());
-        h.setWebURL("f");
+        origin.setWebURL("f");
         assertEquals("f", configuration.getOrigin(container, Distribution.CUSTOM).getHost());
     }
 
     @Test
     public void testGetOriginCustomHttpPort() throws Exception {
-        final Host h = new Host(new TestProtocol());
+        final Host origin = new Host(new TestProtocol(), "m");
         final Path container = new Path("/", EnumSet.of(Path.Type.directory, Path.Type.volume));
-        h.setWebURL("http://w.example.net:8080");
-        final S3Session session = new S3Session(new Host(new S3Protocol(), new S3Protocol().getDefaultHostname()));
+        origin.setWebURL("http://w.example.net:8080");
         final CustomOriginCloudFrontDistributionConfiguration configuration
-                = new CustomOriginCloudFrontDistributionConfiguration(h, new DefaultX509TrustManager(), new DefaultX509KeyManager(), new DisabledTranscriptListener());
+                = new CustomOriginCloudFrontDistributionConfiguration(origin, new DefaultX509TrustManager(), new DefaultX509KeyManager(), new DisabledTranscriptListener());
         assertEquals("w.example.net", configuration.getOrigin(container, Distribution.CUSTOM).getHost());
         assertEquals(8080, configuration.getOrigin(container, Distribution.CUSTOM).getPort());
-        h.setWebURL(null);
+        origin.setWebURL(null);
         assertEquals("m", configuration.getOrigin(container, Distribution.CUSTOM).getHost());
         assertEquals(-1, configuration.getOrigin(container, Distribution.CUSTOM).getPort());
     }
 
     @Test
     public void testGetOriginCustomHttpsPort() throws Exception {
-        final Host h = new Host(new TestProtocol());
+        final Host origin = new Host(new TestProtocol(), "m");
         final Path container = new Path("/", EnumSet.of(Path.Type.directory, Path.Type.volume));
-        h.setWebURL("https://w.example.net:4444");
-        final S3Session session = new S3Session(new Host(new S3Protocol(), new S3Protocol().getDefaultHostname()));
+        origin.setWebURL("https://w.example.net:4444");
         final CustomOriginCloudFrontDistributionConfiguration configuration
-                = new CustomOriginCloudFrontDistributionConfiguration(h, new DefaultX509TrustManager(), new DefaultX509KeyManager(), new DisabledTranscriptListener());
+                = new CustomOriginCloudFrontDistributionConfiguration(origin, new DefaultX509TrustManager(), new DefaultX509KeyManager(), new DisabledTranscriptListener());
         assertEquals("w.example.net", configuration.getOrigin(container, Distribution.CUSTOM).getHost());
         assertEquals("https", configuration.getOrigin(container, Distribution.CUSTOM).getScheme());
         assertEquals(4444, configuration.getOrigin(container, Distribution.CUSTOM).getPort());
-        h.setWebURL(null);
+        origin.setWebURL(null);
         assertEquals("m", configuration.getOrigin(container, Distribution.CUSTOM).getHost());
         assertEquals(-1, configuration.getOrigin(container, Distribution.CUSTOM).getPort());
     }
