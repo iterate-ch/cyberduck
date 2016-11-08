8b34f586be4cbad5796ca36609c50c7eb1d77cdc
David Kocher <dkocher@iterate.ch>
2016-11-08 14:49:42 +0100

    Review.
    Former-commit-id: 9c41cc3608ccb1c2530980597beefd4a070c4088

--- core/src/main/java/ch/cyberduck/core/stream/ExtendedCollectors.java
+++ core/src/main/java/ch/cyberduck/core/stream/ExtendedCollectors.java
@@ -1,26 +1,28 @@
 package ch.cyberduck.core.stream;
 
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collector;
 
-/**
- * Created by alive on 08.11.2016.
- */
 public final class ExtendedCollectors {
-    private ExtendedCollectors() throws Exception {
-        throw new Exception("Invalid operation");
-    }
-
     /**
      * http://stackoverflow.com/a/32648397
-     * @param keyMapper
-     * @param valueMapper
-     * @param <T>
-     * @param <K>
-     * @param <U>
-     * @return
      */
     public static <T, K, U> Collector<T, ?, Map<K, U>> toMap(
             Function<? super T, ? extends K> keyMapper,
@@ -29,9 +31,9 @@ public final class ExtendedCollectors {
                 java.util.stream.Collectors.toList(),
                 list -> {
                     Map<K, U> result = new HashMap<>();
-                    for (T item : list) {
+                    for(T item : list) {
                         K key = keyMapper.apply(item);
-                        if (result.putIfAbsent(key, valueMapper.apply(item)) != null) {
+                        if(result.putIfAbsent(key, valueMapper.apply(item)) != null) {
                             throw new IllegalStateException(String.format("Duplicate key %s", key));
                         }
                     }
