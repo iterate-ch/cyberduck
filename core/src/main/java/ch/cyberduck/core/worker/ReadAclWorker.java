b21a232fa5b1b1f5a76e374f674d797ee69e9ca3
Jöran Malek <joeran3@gmail.com>
2016-11-09 15:15:35 +0100

    ReadAclWorker throw BackgroundExceptions (no silent catch)
    Former-commit-id: ebcb704b16fb4b5ef0fe06a3640626c8dd96f879

--- core/src/main/java/ch/cyberduck/core/worker/ReadAclWorker.java
+++ core/src/main/java/ch/cyberduck/core/worker/ReadAclWorker.java
@@ -19,12 +19,21 @@ package ch.cyberduck.core.worker;
  * dkocher@cyberduck.ch
  */
 
-import ch.cyberduck.core.*;
+import ch.cyberduck.core.Acl;
+import ch.cyberduck.core.AclOverwrite;
+import ch.cyberduck.core.LocaleFactory;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Session;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.AclPermission;
 
 import java.text.MessageFormat;
-import java.util.*;
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -41,17 +50,12 @@ public class ReadAclWorker extends Worker<AclOverwrite> {
     public AclOverwrite run(final Session<?> session) throws BackgroundException {
         final AclPermission feature = session.getFeature(AclPermission.class);
 
-        Map<Path, List<Acl.UserAndRole>> onlineAcl = files.stream().collect(Collectors.toMap(x -> x, x -> {
-            List<Acl.UserAndRole> acl = null;
-            try {
-                Acl tempAcl = feature.getPermission(x);
-                x.attributes().setAcl(tempAcl);
-                acl = tempAcl.asList();
-            } catch (BackgroundException e) {
-                acl = Collections.emptyList();
-            }
-            return acl;
-        }));
+        Map<Path, List<Acl.UserAndRole>> onlineAcl = new HashMap<>();
+        for(Path file : files) {
+            Acl tempAcl = feature.getPermission(file);
+            file.attributes().setAcl(tempAcl);
+            onlineAcl.put(file, tempAcl.asList());
+        }
 
         Supplier<Stream<Map.Entry<Path, Acl.UserAndRole>>> flatAcl = () -> onlineAcl.entrySet().stream().flatMap(
                 x -> x.getValue().stream().map(
@@ -80,7 +84,8 @@ public class ReadAclWorker extends Worker<AclOverwrite> {
 
                     try {
                         roleSupplier.get().count();
-                    } catch (Exception e) {
+                    }
+                    catch(Exception e) {
                         ; // empty
                     }
 
@@ -103,14 +108,14 @@ public class ReadAclWorker extends Worker<AclOverwrite> {
 
     @Override
     public boolean equals(final Object o) {
-        if (this == o) {
+        if(this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if(o == null || getClass() != o.getClass()) {
             return false;
         }
         final ReadAclWorker that = (ReadAclWorker) o;
-        if (files != null ? !files.equals(that.files) : that.files != null) {
+        if(files != null ? !files.equals(that.files) : that.files != null) {
             return false;
         }
         return true;
