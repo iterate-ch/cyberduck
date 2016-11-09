f4466fcbc7d5b8be8e1eda8ae25e0343fb9bfa1a
David Kocher <dkocher@iterate.ch>
2016-11-09 11:24:06 +0100

    Add mapping.
    Former-commit-id: 8d1e947de656c905c2f3552208e2ed7e73b139e0

--- core/src/main/java/ch/cyberduck/core/http/HttpResponseExceptionMappingService.java
+++ core/src/main/java/ch/cyberduck/core/http/HttpResponseExceptionMappingService.java
@@ -21,6 +21,7 @@ package ch.cyberduck.core.http;
 import ch.cyberduck.core.AbstractExceptionMappingService;
 import ch.cyberduck.core.exception.AccessDeniedException;
 import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.ConflictException;
 import ch.cyberduck.core.exception.ConnectionRefusedException;
 import ch.cyberduck.core.exception.ConnectionTimeoutException;
 import ch.cyberduck.core.exception.InteroperabilityException;
@@ -43,6 +44,8 @@ public class HttpResponseExceptionMappingService extends AbstractExceptionMappin
                 return new LoginFailureException(buffer.toString(), failure);
             case HttpStatus.SC_FORBIDDEN:
                 return new AccessDeniedException(buffer.toString(), failure);
+            case HttpStatus.SC_CONFLICT:
+                return new ConflictException(buffer.toString(), failure);
             case HttpStatus.SC_NOT_FOUND:
                 return new NotfoundException(buffer.toString(), failure);
             case HttpStatus.SC_UNPROCESSABLE_ENTITY:
