d1f41de9dcea9ccb2a9e9d571c376257d1fa4741
David Kocher <dkocher@iterate.ch>
2016-11-14 14:31:51 +0100

    Fix param.
    Former-commit-id: acdcc88b7f9cdf7f565ecb8e621f39b1992e9fa6

--- osx/src/main/java/ch/cyberduck/ui/cocoa/BookmarkControllerFactory.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/BookmarkControllerFactory.java
@@ -18,7 +18,7 @@ package ch.cyberduck.ui.cocoa;
  */
 
 import ch.cyberduck.binding.application.NSApplication;
-import ch.cyberduck.core.BookmarkCollection;
+import ch.cyberduck.core.AbstractHostCollection;
 import ch.cyberduck.core.Host;
 
 import java.util.HashMap;
@@ -33,7 +33,7 @@ public final class BookmarkControllerFactory {
         //
     }
 
-    public static BookmarkController create(final BookmarkCollection collection, final Host host) {
+    public static BookmarkController create(final AbstractHostCollection collection, final Host host) {
         synchronized(NSApplication.sharedApplication()) {
             if(!open.containsKey(host)) {
                 final BookmarkController c = new ExtendedBookmarkController(host) {
