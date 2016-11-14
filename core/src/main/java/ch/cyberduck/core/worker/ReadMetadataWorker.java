173168ce1de900d2e3ef0a987186014e780e1669
David Kocher <dkocher@iterate.ch>
2016-11-14 13:38:33 +0100

    Review. Use iterators.
    Former-commit-id: 9a25348f8760bb1e185c0da2fd6f22e948f37d44

--- core/src/main/java/ch/cyberduck/core/worker/ReadMetadataWorker.java
+++ core/src/main/java/ch/cyberduck/core/worker/ReadMetadataWorker.java
@@ -28,9 +28,9 @@ import ch.cyberduck.core.features.Headers;
 import org.apache.log4j.Logger;
 
 import java.text.MessageFormat;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
@@ -52,72 +52,61 @@ public class ReadMetadataWorker extends Worker<Map<String, String>> {
     public Map<String, String> run(final Session<?> session) throws BackgroundException {
         final Headers feature = session.getFeature(Headers.class);
 
-        // create a list for temporarily storing entries
-        List<String> removedEntries = new ArrayList<>();
-        // create two maps, one for Map->Meta, and one for Key->Path:Value
-        Map<Path, Map<String, String>> onlineMetadata = new HashMap<>();
-        Map<String, Map<Path, String>> metaGraph = new HashMap<>();
+        // Map for File > Metadata Set
+        Map<Path, Map<String, String>> fullMetadata = new HashMap<>();
+        // Map for metadata entry key > File & Metadata Values
+        Map<String, Map<Path, String>> graphMetadata = new HashMap<>();
 
-        // iterate through all files
         for(Path file : files) {
-            // read online metadata
-            Map<String, String> metadata = feature.getMetadata(file);
-            // put it into onlineMetadata (do nothing with it)
-            onlineMetadata.put(file, new HashMap<>(metadata));
+            // Read online metadata
+            final Map<String, String> metadata = feature.getMetadata(file);
+            fullMetadata.put(file, new HashMap<>(metadata));
             // take every entry of current metadata and store it in metaGraph
             for(Map.Entry<String, String> entry : metadata.entrySet()) {
-                if(metaGraph.containsKey(entry.getKey())) {
+                if(graphMetadata.containsKey(entry.getKey())) {
                     // if existing, get map, put value
-                    metaGraph.get(entry.getKey()).put(file, entry.getValue());
+                    graphMetadata.get(entry.getKey()).put(file, entry.getValue());
                 }
                 else {
                     // if not existent create hashmap and put it back
                     Map<Path, String> map = new HashMap<>();
-                    metaGraph.put(entry.getKey(), map);
+                    graphMetadata.put(entry.getKey(), map);
                     map.put(file, entry.getValue());
                 }
             }
         }
 
-        for(Map.Entry<String, Map<Path, String>> entry : metaGraph.entrySet()) {
-            // if current key does not have equal to files amount items remove it
+        for(Iterator<Map.Entry<String, Map<Path, String>>> iter = graphMetadata.entrySet().iterator(); iter.hasNext(); ) {
+            Map.Entry<String, Map<Path, String>> entry = iter.next();
+            // Remove if current key is not  equal to number of files
             if(entry.getValue().size() != files.size()) {
-                removedEntries.add(entry.getKey());
+                iter.remove();
             }
         }
-        // deferred removing of files due to InvalidOperationException if changed while iteration
-        for(String key : removedEntries) {
-            metaGraph.remove(key);
-        }
-        // iterate all Path->Meta values
-        for(Map.Entry<Path, Map<String, String>> entry : onlineMetadata.entrySet()) {
-            // before continue, clear removedEntries
-            removedEntries.clear();
-            for(Map.Entry<String, String> metaPair : entry.getValue().entrySet()) {
-                // if filtered metaGraph does not contain this key, remove it
-                if(!metaGraph.containsKey(metaPair.getKey())) {
-                    removedEntries.add(metaPair.getKey());
+        // Iterate all Path->Metadata values
+        for(Map.Entry<Path, Map<String, String>> entry : fullMetadata.entrySet()) {
+            for(Iterator<Map.Entry<String, String>> iter = entry.getValue().entrySet().iterator(); iter.hasNext(); ) {
+                Map.Entry<String, String> pair = iter.next();
+                // if merged metadata does not contain this key, remove it
+                if(!graphMetadata.containsKey(pair.getKey())) {
+                    iter.remove();
                 }
             }
-            // deferred removing of entries due to InvalidOperationException on change while iteration
-            for(String key : removedEntries) {
-                entry.getValue().remove(key);
-            }
-            // put filtered metadata to file attributes
-            entry.getKey().attributes().setMetadata(entry.getValue());
+            // Set filtered metadata to file attributes
+            final Path file = entry.getKey();
+            file.attributes().setMetadata(entry.getValue());
         }
 
-        // store result metadata in hashmap
+        // Store result metadata in hashmap
         Map<String, String> metadata = new HashMap<>();
-        for(Map.Entry<String, Map<Path, String>> entry : metaGraph.entrySet()) {
+        for(Map.Entry<String, Map<Path, String>> entry : graphMetadata.entrySet()) {
             // single use of streams, reason: distinct is easier in Streams than it would be writing it manually
             Supplier<Stream<String>> valueSupplier = () -> entry.getValue().entrySet().stream().map(y -> y.getValue()).distinct();
-            // check count against 1, if it is use that value, otherwise use null
+            // Check count against 1, if it is use that value, otherwise use null
             String value = valueSupplier.get().count() == 1 ? valueSupplier.get().findAny().get() : null;
             // store it
             metadata.put(entry.getKey(), value);
         }
-
         return metadata;
     }
 
