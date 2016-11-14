d6d5067033d9f605dc843e1679dff0b1617c10d5
David Kocher <dkocher@iterate.ch>
2016-11-14 14:21:52 +0100

    Rewrite test to only use cached attributes.
    Former-commit-id: dfbf8881ffd98aa5b75b41c3a56aee1029f5081b

--- core/src/test/java/ch/cyberduck/core/worker/WriteMetadataWorkerTest.java
+++ core/src/test/java/ch/cyberduck/core/worker/WriteMetadataWorkerTest.java
@@ -1,11 +1,24 @@
 package ch.cyberduck.core.worker;
 
-import ch.cyberduck.core.*;
+import ch.cyberduck.core.DisabledProgressListener;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Local;
+import ch.cyberduck.core.NullSession;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.TestProtocol;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.Headers;
+
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -143,14 +156,26 @@ public class WriteMetadataWorkerTest {
 
     @Test
     public void testRunDifferent() throws Exception {
-        final List<Path> files = Arrays.asList(new Path("a", EnumSet.of(Path.Type.file)), new Path("b", EnumSet.of(Path.Type.file)));
-
-        final Map<String, String> merged = new HashMap<String, String>();
-        merged.put("equal", "equal");
-        merged.put("different", null);
-
-        files.get(0).attributes().setMetadata(merged);
-        files.get(1).attributes().setMetadata(merged);
+        final PathAttributes attributesA = new PathAttributes();
+        {
+            final Map<String, String> map = new HashMap<String, String>();
+            map.put("equal", "equal");
+            map.put("different", "diff1");
+            map.put("unique", "unique");
+            attributesA.setMetadata(map);
+        }
+        final PathAttributes attributesB = new PathAttributes();
+        {
+            final Map<String, String> map = new HashMap<String, String>();
+            map.put("equal", "equal");
+            map.put("different", "diff2");
+            attributesB.setMetadata(map);
+        }
+        final List<Path> files = Arrays.asList(
+                new Path("a", EnumSet.of(Path.Type.file),
+                        attributesA),
+                new Path("b", EnumSet.of(Path.Type.file),
+                        attributesB));
 
         final Map<String, String> updated = new HashMap<String, String>();
         updated.put("equal", "equal-changed");
@@ -166,7 +191,7 @@ public class WriteMetadataWorkerTest {
             @Override
             @SuppressWarnings("unchecked")
             public <T> T getFeature(final Class<T> type) {
-                if (type == Headers.class) {
+                if(type == Headers.class) {
                     return (T) new Headers() {
                         @Override
                         public Map<String, String> getDefault(final Local local) {
@@ -175,43 +200,26 @@ public class WriteMetadataWorkerTest {
 
                         @Override
                         public Map<String, String> getMetadata(final Path file) throws BackgroundException {
-                            Map<String, String> map = new HashMap<>();
-                            map.put("equal", "equal");
-                            switch (file.getName()) {
-                                case "a":
-                                    map.put("different", "diff1");
-                                    map.put("unique", "unique");
-                                    break;
-
-                                case "b":
-                                    map.put("different", "diff2");
-                                    break;
-
-                                default:
-                                    fail();
-                                    break;
-                            }
-                            return map;
+                            throw new UnsupportedOperationException();
                         }
 
                         @Override
-                        public void setMetadata(final Path file, final Map<String, String> meta) throws BackgroundException {
-                            assertTrue(meta.containsKey("equal"));
-                            assertTrue(meta.containsKey("different"));
-                            assertEquals("equal-changed", meta.get("equal"));
+                        public void setMetadata(final Path file, final Map<String, String> metadata) throws BackgroundException {
+                            assertTrue(metadata.containsKey("equal"));
+                            assertTrue(metadata.containsKey("different"));
+                            assertEquals("equal-changed", metadata.get("equal"));
 
-                            switch(file.getName())
-                            {
+                            switch(file.getName()) {
                                 case "a":
-                                    assertTrue(meta.containsKey("unique"));
+                                    assertTrue(metadata.containsKey("unique"));
 
-                                    assertEquals("diff1", meta.get("different"));
-                                    assertEquals("unique", meta.get("unique"));
+                                    assertEquals("diff1", metadata.get("different"));
+                                    assertEquals("unique", metadata.get("unique"));
                                     break;
                                 case "b":
-                                    assertFalse(meta.containsKey("unique"));
+                                    assertFalse(metadata.containsKey("unique"));
 
-                                    assertEquals("diff2", meta.get("different"));
+                                    assertEquals("diff2", metadata.get("different"));
                                     break;
                                 default:
                                     fail();
