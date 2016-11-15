2c9e5c6dffbfef9a450480d30e1f38a9afee2feb
Jöran Malek <joeran3@gmail.com>
2016-11-15 13:17:29 +0100

    Add UAP Browser Launcher
    Former-commit-id: 1b3f01457b9a9f4679a0ccafaf318b4fe7f4ac28

---
+++ core/src/main/csharp/ch/cyberduck/core/local/WindowsUAPBrowserLauncher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ch.cyberduck.core.local;
+using Windows.System;
+
+namespace Ch.Cyberduck.Core.Local
+{
+	public class WindowsUAPBrowserLauncher : BrowserLauncher
+	{
+		public bool open(string str)
+		{
+			return Launcher.LaunchUriAsync(new Uri(str)).GetResults();
+		}
+	}
+}
