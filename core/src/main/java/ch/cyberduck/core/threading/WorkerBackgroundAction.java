e86e828015a219062846aee3748dd3e94e8f0c6c
David Kocher <dkocher@iterate.ch>
2016-11-18 22:30:42 +0100

    Make worker final.
    Former-commit-id: b0983c510ca62acee54bbcd1ca85577ecb8174ac

--- core/src/main/java/ch/cyberduck/core/threading/WorkerBackgroundAction.java
+++ core/src/main/java/ch/cyberduck/core/threading/WorkerBackgroundAction.java
@@ -31,7 +31,7 @@ import org.apache.log4j.Logger;
 public class WorkerBackgroundAction<T> extends RegistryBackgroundAction<T> {
     private static final Logger log = Logger.getLogger(WorkerBackgroundAction.class);
 
-    protected Worker<T> worker;
+    protected final Worker<T> worker;
 
     private T result;
 
