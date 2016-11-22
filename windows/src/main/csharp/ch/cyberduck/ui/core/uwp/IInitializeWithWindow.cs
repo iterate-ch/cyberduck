25c2048afb4081ee5ccbb1567e06634b1b2a36fb
Jöran Malek <joeran3@gmail.com>
2016-11-22 22:20:32 +0100

    Add IInitializeWithWindow (ref https://msdn.microsoft.com/en-us/windows/uwp/monetize/in-app-purchases-and-trials#desktop)
    Former-commit-id: fc32d698aacd726c81661100158540d0c1a05044

---
+++ windows/src/main/csharp/ch/cyberduck/ui/core/uwp/IInitializeWithWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch.Cyberduck.windows.src.main.csharp.ch.cyberduck.ui.core.uwp
+{
+    [ComImport]
+    [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IInitializeWithWindow
+    {
+        void Initialize(IntPtr hwnd);
+    }
+}
