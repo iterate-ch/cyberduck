7bd1ee491b6a1e512c917ca0a00b958cc6b69e03
David Kocher <dkocher@iterate.ch>
2016-11-23 15:18:06 +0100

    Add test.
    Former-commit-id: 20ef61e717da6d2807bc75e60211e7ee7c72a77f

--- core/src/test/java/ch/cyberduck/core/pool/DefaultSessionPoolTest.java
+++ core/src/test/java/ch/cyberduck/core/pool/DefaultSessionPoolTest.java
@@ -15,12 +15,17 @@ package ch.cyberduck.core.pool;
  * GNU General Public License for more details.
  */
 
+import ch.cyberduck.core.Cache;
 import ch.cyberduck.core.DisabledProgressListener;
 import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathCache;
+import ch.cyberduck.core.Session;
 import ch.cyberduck.core.TestLoginConnectionService;
 import ch.cyberduck.core.TestProtocol;
+import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.exception.ConnectionCanceledException;
+import ch.cyberduck.core.exception.ConnectionRefusedException;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DisabledX509TrustManager;
 import ch.cyberduck.core.threading.BackgroundActionState;
@@ -34,6 +39,18 @@ public class DefaultSessionPoolTest {
         final DefaultSessionPool pool = new DefaultSessionPool(new TestLoginConnectionService(), new DisabledX509TrustManager(), new DefaultX509KeyManager(),
                 PathCache.empty(), new DisabledProgressListener(), new Host(new TestProtocol()));
         pool.shutdown();
+        pool.borrow(BackgroundActionState.running);
+    }
+
+    @Test(expected = ConnectionRefusedException.class)
+    public void testConnectRefuse() throws Exception {
+        final DefaultSessionPool pool = new DefaultSessionPool(new TestLoginConnectionService() {
+            @Override
+            public boolean check(final Session session, final Cache<Path> cache) throws BackgroundException {
+                throw new ConnectionRefusedException("t", new RuntimeException());
+            }
+        }, new DisabledX509TrustManager(), new DefaultX509KeyManager(),
+                PathCache.empty(), new DisabledProgressListener(), new Host(new TestProtocol(), "t"));
         pool.borrow(BackgroundActionState.running);
     }
 }
