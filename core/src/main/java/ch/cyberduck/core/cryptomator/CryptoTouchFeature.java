26b2514eaddfc870eaca24a05c20250b88df7cc0
David Kocher <dkocher@iterate.ch>
2016-11-25 22:56:24 +0100

    Stubs.
    Former-commit-id: db919d11a6c781fe4d461a99bd2b97a19a137423

---
+++ core/src/main/java/ch/cyberduck/core/cryptomator/CryptoTouchFeature.java
@@ -0,0 +1,41 @@
+package ch.cyberduck.core.cryptomator;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Touch;
+
+public class CryptoTouchFeature implements Touch {
+
+    private final Touch delegate;
+    private final SessionCryptomatorLoader cryptomator;
+
+    public CryptoTouchFeature(final Touch delegate, final SessionCryptomatorLoader cryptomator) {
+        this.delegate = delegate;
+        this.cryptomator = cryptomator;
+    }
+
+    @Override
+    public void touch(final Path file) throws BackgroundException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isSupported(final Path workdir) {
+        return delegate.isSupported(workdir);
+    }
+}
