acf3509c6d8361bc2bea77edb51ec9c77cc1c33d
David Kocher <dkocher@iterate.ch>
2016-11-27 00:20:59 +0100

    Add worker to create new vault.
    Former-commit-id: 1fc71977b5ddcda361154cfc1c28350887d48b89

---
+++ core/src/main/java/ch/cyberduck/core/cryptomator/CreateVaultWorker.java
@@ -0,0 +1,49 @@
+package ch.cyberduck.core.cryptomator;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.LoginCallback;
+import ch.cyberduck.core.PasswordStore;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Session;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.features.Vault;
+import ch.cyberduck.core.worker.Worker;
+
+public class CreateVaultWorker extends Worker<Boolean> {
+
+    private final Path directory;
+    private final PasswordStore keychain;
+    private final LoginCallback login;
+
+    public CreateVaultWorker(final Path directory, final PasswordStore keychain, final LoginCallback login) {
+        this.directory = directory;
+        this.keychain = keychain;
+        this.login = login;
+    }
+
+    @Override
+    public Boolean run(final Session<?> session) throws BackgroundException {
+        try {
+            session.getFeature(Vault.class).create(directory, keychain, login);
+        }
+        catch(LoginCanceledException e) {
+            return false;
+        }
+        return true;
+    }
+}
