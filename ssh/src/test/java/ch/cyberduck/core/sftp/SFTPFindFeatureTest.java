ee14591218a2ad8087c8b06b0ff4659e736698aa
David Kocher <dkocher@iterate.ch>
2016-11-27 22:16:02 +0100

    Add test.
    Former-commit-id: 82ed3ed89c5eb997fd89b86892163dc431b1b3a1

--- ssh/src/test/java/ch/cyberduck/core/sftp/SFTPFindFeatureTest.java
+++ ssh/src/test/java/ch/cyberduck/core/sftp/SFTPFindFeatureTest.java
@@ -7,8 +7,12 @@ import ch.cyberduck.core.DisabledHostKeyCallback;
 import ch.cyberduck.core.DisabledLoginCallback;
 import ch.cyberduck.core.DisabledPasswordStore;
 import ch.cyberduck.core.Host;
+import ch.cyberduck.core.LoginOptions;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathCache;
+import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.features.Find;
+import ch.cyberduck.core.features.Vault;
 import ch.cyberduck.test.IntegrationTest;
 
 import org.junit.Test;
@@ -17,8 +21,7 @@ import org.junit.experimental.categories.Category;
 import java.util.EnumSet;
 import java.util.UUID;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @Category(IntegrationTest.class)
 public class SFTPFindFeatureTest {
@@ -63,5 +66,29 @@ public class SFTPFindFeatureTest {
     @Test
     public void testFindRoot() throws Exception {
         assertTrue(new SFTPFindFeature(new SFTPSession(new Host(new SFTPProtocol()))).find(new Path("/", EnumSet.of(Path.Type.directory))));
+    }
+
+    @Test
+    public void testListCryptomator() throws Exception {
+        final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
+                System.getProperties().getProperty("sftp.user"), System.getProperties().getProperty("sftp.password")
+        ));
+        final SFTPSession session = new SFTPSession(host);
+        assertNotNull(session.open(new DisabledHostKeyCallback()));
+        assertTrue(session.isConnected());
+        assertNotNull(session.getClient());
+        final PathCache cache = new PathCache(1);
+        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), cache);
+        final Path home = new SFTPHomeDirectoryService(session).find();
+        final Path vault = new Path(home, "/cryptomator-vault/test", EnumSet.of(Path.Type.directory));
+        session.getFeature(Vault.class).load(vault, new DisabledPasswordStore(), new DisabledLoginCallback() {
+            @Override
+            public void prompt(final Host bookmark, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                credentials.setPassword("coke4you");
+            }
+        });
+        assertTrue(session.getFeature(Find.class).find(new Path(vault, "blabal", EnumSet.of(Path.Type.directory))));
+        assertFalse(session.getFeature(Find.class).find(new Path(vault, "bla", EnumSet.of(Path.Type.directory))));
+        session.close();
     }
 }
