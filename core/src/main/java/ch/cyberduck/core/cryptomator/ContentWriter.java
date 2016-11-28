9c6b472fe746f4cbaf48ba1649b8814ed3057aed
Yves Langisch <yla@iterate.ch>
2016-11-28 15:54:16 +0100

    Add create vault implementation.
    Former-commit-id: 79f6ccd71b95aafaa37c6ddf3262ec2ec42f77e9

---
+++ core/src/main/java/ch/cyberduck/core/cryptomator/ContentWriter.java
@@ -0,0 +1,51 @@
+package ch.cyberduck.core.cryptomator;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.DefaultIOExceptionMappingService;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Session;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Write;
+import ch.cyberduck.core.transfer.TransferStatus;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ContentWriter {
+
+    private final Session<?> session;
+
+    public ContentWriter(final Session<?> session) {
+        this.session = session;
+    }
+
+    public void write(final Path file, final byte[] content) throws BackgroundException {
+        final Write write = session._getFeature(Write.class);
+        final OutputStream stream = write.write(file, new TransferStatus());
+        try {
+            IOUtils.write(content, stream);
+        }
+        catch(IOException e) {
+            throw new DefaultIOExceptionMappingService().map(e);
+        }
+        finally {
+            IOUtils.closeQuietly(stream);
+        }
+    }
+}
