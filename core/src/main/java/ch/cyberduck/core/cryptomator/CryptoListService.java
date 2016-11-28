3ecbdb96d2ce4bd6102c0c695a157a7dcceacbff
David Kocher <dkocher@iterate.ch>
2016-11-28 12:21:06 +0100

    Remove catch.
    Former-commit-id: f1036fb35c27491eb9f59a7fd573288bb89ecf64

--- core/src/main/java/ch/cyberduck/core/cryptomator/CryptoListService.java
+++ core/src/main/java/ch/cyberduck/core/cryptomator/CryptoListService.java
@@ -16,14 +16,11 @@ package ch.cyberduck.core.cryptomator;
  */
 
 import ch.cyberduck.core.AttributedList;
-import ch.cyberduck.core.DefaultIOExceptionMappingService;
 import ch.cyberduck.core.ListProgressListener;
 import ch.cyberduck.core.ListService;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.Vault;
-
-import java.io.IOException;
 
 public class CryptoListService implements ListService {
 
@@ -37,12 +34,7 @@ public class CryptoListService implements ListService {
 
     @Override
     public AttributedList<Path> list(final Path directory, final ListProgressListener listener) throws BackgroundException {
-        try {
-            return delegate.list(vault.encrypt(directory),
-                    new DecryptingListProgressListener(vault, directory, listener));
-        }
-        catch(IOException e) {
-            throw new DefaultIOExceptionMappingService().map(e);
-        }
+        return delegate.list(vault.encrypt(directory),
+                new DecryptingListProgressListener(vault, directory, listener));
     }
 }
