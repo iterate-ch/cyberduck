cb61a4926e58d3cc691124ff5743b0e07d5d65a0
David Kocher <dkocher@iterate.ch>
2016-11-30 10:48:21 +0100

    Make test self contained.
    Former-commit-id: 48ea40afc59d6e6487b5bb36a37d8f2c81a403dd

--- ssh/src/test/java/ch/cyberduck/core/sftp/SFTPFindFeatureTest.java
+++ ssh/src/test/java/ch/cyberduck/core/sftp/SFTPFindFeatureTest.java
@@ -12,12 +12,15 @@ import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathCache;
 import ch.cyberduck.core.cryptomator.impl.CryptoVault;
 import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.features.Delete;
 import ch.cyberduck.core.features.Find;
+import ch.cyberduck.core.features.Touch;
 import ch.cyberduck.test.IntegrationTest;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.UUID;
 
@@ -69,7 +72,7 @@ public class SFTPFindFeatureTest {
     }
 
     @Test
-    public void testListCryptomator() throws Exception {
+    public void testFindCryptomator() throws Exception {
         final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
                 System.getProperties().getProperty("sftp.user"), System.getProperties().getProperty("sftp.password")
         ));
@@ -80,15 +83,18 @@ public class SFTPFindFeatureTest {
         final PathCache cache = new PathCache(1);
         session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), cache);
         final Path home = new SFTPHomeDirectoryService(session).find();
-        final Path vault = new Path(home, "/cryptomator-vault/test", EnumSet.of(Path.Type.directory));
+        final Path vault = new Path(home, UUID.randomUUID().toString(), EnumSet.of(Path.Type.directory));
+        final Path test = new Path(vault, UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
         session.withVault(new CryptoVault(session, vault, new DisabledPasswordStore(), new DisabledLoginCallback() {
             @Override
             public void prompt(final Host bookmark, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
-                credentials.setPassword("coke4you");
+                credentials.setPassword("vault");
             }
-        }).load());
-        assertTrue(session.getFeature(Find.class).find(new Path(vault, "blabal", EnumSet.of(Path.Type.directory))));
-        assertFalse(session.getFeature(Find.class).find(new Path(vault, "bla", EnumSet.of(Path.Type.directory))));
+        }).create());
+        assertFalse(session.getFeature(Find.class).find(new Path(vault, "a", EnumSet.of(Path.Type.directory))));
+        session.getFeature(Touch.class).touch(test);
+        assertTrue(session.getFeature(Find.class).find(test));
+        session.getFeature(Delete.class).delete(Arrays.asList(test, vault), new DisabledLoginCallback(), new Delete.DisabledCallback());
         session.close();
     }
 }
