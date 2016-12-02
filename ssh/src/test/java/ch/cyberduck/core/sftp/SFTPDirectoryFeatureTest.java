1bd0e8b9adc9e47fed44b5b517f39057b563eaf7
David Kocher <dkocher@iterate.ch>
2016-12-02 11:04:54 +0100

    Add test.
    Former-commit-id: 133c2c2fcd7fdc73ffc72533632e1a21513cc13c

--- ssh/src/test/java/ch/cyberduck/core/sftp/SFTPDirectoryFeatureTest.java
+++ ssh/src/test/java/ch/cyberduck/core/sftp/SFTPDirectoryFeatureTest.java
@@ -40,6 +40,7 @@ import ch.cyberduck.core.pool.SingleSessionPool;
 import ch.cyberduck.core.threading.BackgroundActionState;
 import ch.cyberduck.test.IntegrationTest;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -82,6 +83,31 @@ public class SFTPDirectoryFeatureTest {
         final Path home = session.getFeature(Home.class).find();
         final Path vault = new Path(home, UUID.randomUUID().toString(), EnumSet.of(Path.Type.directory));
         final Path test = new Path(vault, UUID.randomUUID().toString(), EnumSet.of(Path.Type.directory));
+        final CryptoVault cryptomator = new CryptoVault(vault, new DisabledPasswordStore(), new DisabledLoginCallback() {
+            @Override
+            public void prompt(final Host bookmark, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                credentials.setPassword("vault");
+            }
+        }).create(session);
+        session.withVault(cryptomator);
+        session.getFeature(Directory.class).mkdir(test);
+        assertTrue(session.getFeature(Find.class).find(test));
+        session.getFeature(Delete.class).delete(Collections.singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
+        session.close();
+    }
+
+    @Test
+    public void testMakeDirectoryLongFilenameEncrypted() throws Exception {
+        final Host host = new Host(new SFTPProtocol(), "test.cyberduck.ch", new Credentials(
+                System.getProperties().getProperty("sftp.user"), System.getProperties().getProperty("sftp.password")
+        ));
+        final SingleSessionPool pool = new SingleSessionPool(new LoginConnectionService(
+                new DisabledLoginCallback(), new DisabledHostKeyCallback(), new DisabledPasswordStore(), new DisabledProgressListener(), new DisabledTranscriptListener()
+        ), new SFTPSession(host), PathCache.empty());
+        final Session<?> session = pool.borrow(BackgroundActionState.running);
+        final Path home = session.getFeature(Home.class).find();
+        final Path vault = new Path(home, UUID.randomUUID().toString(), EnumSet.of(Path.Type.directory));
+        final Path test = new Path(vault, RandomStringUtils.random(130), EnumSet.of(Path.Type.directory));
         final CryptoVault cryptomator = new CryptoVault(vault, new DisabledPasswordStore(), new DisabledLoginCallback() {
             @Override
             public void prompt(final Host bookmark, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
