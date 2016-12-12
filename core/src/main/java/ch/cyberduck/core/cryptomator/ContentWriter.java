556fe37d8ad4d5dcc3b4700cc1cad96e68dacfe6
David Kocher <dkocher@iterate.ch>
2016-12-12 17:12:45 +0100

    Set content checksum.
    Former-commit-id: 802477c0c557939eec7d5fce4e17844704c8ed78

--- core/src/main/java/ch/cyberduck/core/cryptomator/ContentWriter.java
+++ core/src/main/java/ch/cyberduck/core/cryptomator/ContentWriter.java
@@ -20,10 +20,12 @@ import ch.cyberduck.core.Path;
 import ch.cyberduck.core.Session;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.Write;
+import ch.cyberduck.core.io.ChecksumCompute;
 import ch.cyberduck.core.transfer.TransferStatus;
 
 import org.apache.commons.io.IOUtils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -37,7 +39,12 @@ public class ContentWriter {
 
     public void write(final Path file, final byte[] content) throws BackgroundException {
         final Write write = session._getFeature(Write.class);
-        final OutputStream out = write.write(file, new TransferStatus().length(content.length));
+        final TransferStatus status = new TransferStatus().length(content.length);
+        final ChecksumCompute checksum = session._getFeature(ChecksumCompute.class);
+        if(null != checksum) {
+            status.setChecksum(checksum.compute(new ByteArrayInputStream(content), new TransferStatus()));
+        }
+        final OutputStream out = write.write(file, status);
         try {
             IOUtils.write(content, out);
         }
