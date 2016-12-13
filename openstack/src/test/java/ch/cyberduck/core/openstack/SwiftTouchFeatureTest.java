7a4d9ec92ae8515b7b78044a4995f667fa9b72bf
David Kocher <dkocher@iterate.ch>
2016-12-13 14:20:31 +0100

    Add tests.
    Former-commit-id: 06801d2459836bd22b0af3e3452dcf21a2620a19

--- openstack/src/test/java/ch/cyberduck/core/openstack/SwiftTouchFeatureTest.java
+++ openstack/src/test/java/ch/cyberduck/core/openstack/SwiftTouchFeatureTest.java
@@ -1,13 +1,34 @@
 package ch.cyberduck.core.openstack;
 
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledCancelCallback;
+import ch.cyberduck.core.DisabledHostKeyCallback;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.DisabledPasswordCallback;
+import ch.cyberduck.core.DisabledPasswordStore;
 import ch.cyberduck.core.Host;
+import ch.cyberduck.core.LoginOptions;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathCache;
+import ch.cyberduck.core.cryptomator.CryptoTouchFeature;
+import ch.cyberduck.core.cryptomator.DisabledVaultLookupListener;
+import ch.cyberduck.core.cryptomator.impl.CryptoVault;
+import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.features.Find;
+import ch.cyberduck.core.features.Home;
+import ch.cyberduck.core.features.Write;
+import ch.cyberduck.core.shared.DefaultTouchFeature;
+import ch.cyberduck.core.transfer.TransferStatus;
 import ch.cyberduck.test.IntegrationTest;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.util.Collections;
 import java.util.EnumSet;
+import java.util.UUID;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -20,5 +41,55 @@ public class SwiftTouchFeatureTest {
         final SwiftSession session = new SwiftSession(new Host(new SwiftProtocol(), "h"));
         assertFalse(new SwiftTouchFeature(session).isSupported(new Path("/", EnumSet.of(Path.Type.directory, Path.Type.volume))));
         assertTrue(new SwiftTouchFeature(session).isSupported(new Path("/container", EnumSet.of(Path.Type.directory, Path.Type.volume))));
+    }
+
+
+    @Test
+    public void testTouchLongFilenameEncrypted() throws Exception {
+        final Host host = new Host(new SwiftProtocol(), "identity.api.rackspacecloud.com", new Credentials(
+                System.getProperties().getProperty("rackspace.key"), System.getProperties().getProperty("rackspace.secret")
+        ));
+        final SwiftSession session = new SwiftSession(host).withAccountPreload(false).withCdnPreload(false).withContainerPreload(false);
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
+        final Path home = session.getFeature(Home.class).find();
+        final Path vault = new Path(home, UUID.randomUUID().toString(), EnumSet.of(Path.Type.directory));
+        final Path test = new Path(vault, RandomStringUtils.random(130), EnumSet.of(Path.Type.file));
+        final CryptoVault cryptomator = new CryptoVault(vault, new DisabledPasswordStore(), new DisabledPasswordCallback() {
+            @Override
+            public void prompt(final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                credentials.setPassword("vault");
+            }
+        }, new DisabledVaultLookupListener()).create(session, null);
+        session.withVault(cryptomator);
+        final SwiftRegionService regionService = new SwiftRegionService(session);
+        new CryptoTouchFeature(session, new SwiftTouchFeature(session, session.getFeature(Write.class, new SwiftWriteFeature(session, regionService))), cryptomator).touch(test, new TransferStatus());
+        assertTrue(session.getFeature(Find.class).find(test));
+        session.getFeature(Delete.class).delete(Collections.singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
+        session.close();
+    }
+
+    @Test
+    public void testTouchEncryptedDefaultFeature() throws Exception {
+        final Host host = new Host(new SwiftProtocol(), "identity.api.rackspacecloud.com", new Credentials(
+                System.getProperties().getProperty("rackspace.key"), System.getProperties().getProperty("rackspace.secret")
+        ));
+        final SwiftSession session = new SwiftSession(host).withAccountPreload(false).withCdnPreload(false).withContainerPreload(false);
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
+        final Path home = session.getFeature(Home.class).find();
+        final Path vault = new Path(home, UUID.randomUUID().toString(), EnumSet.of(Path.Type.directory));
+        final Path test = new Path(vault, UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
+        final CryptoVault cryptomator = new CryptoVault(vault, new DisabledPasswordStore(), new DisabledPasswordCallback() {
+            @Override
+            public void prompt(final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                credentials.setPassword("vault");
+            }
+        }, new DisabledVaultLookupListener()).create(session, null);
+        session.withVault(cryptomator);
+        new CryptoTouchFeature(session, new DefaultTouchFeature(session), cryptomator).touch(test, new TransferStatus());
+        assertTrue(session.getFeature(Find.class).find(test));
+        session.getFeature(Delete.class).delete(Collections.singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
+        session.close();
     }
 }
