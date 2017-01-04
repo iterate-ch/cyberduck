40d911b241b14a3006b3700515cfc437b4864748
David Kocher <dkocher@iterate.ch>
2017-01-04 11:49:17 +0100

    Add test.
    Former-commit-id: bdb395f6e5396f99f1807408163aa66518175614

--- cryptomator/src/test/java/ch/cyberduck/core/cryptomator/impl/CryptoVaultTest.java
+++ cryptomator/src/test/java/ch/cyberduck/core/cryptomator/impl/CryptoVaultTest.java
@@ -29,6 +29,7 @@ import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.exception.LoginCanceledException;
 import ch.cyberduck.core.features.Directory;
 import ch.cyberduck.core.features.Read;
+import ch.cyberduck.core.features.Vault;
 import ch.cyberduck.core.transfer.TransferStatus;
 
 import org.apache.commons.io.IOUtils;
@@ -85,6 +86,9 @@ public class CryptoVaultTest {
             }
         });
         assertNotSame(f, vault.encrypt(session, f));
+        assertEquals(Vault.State.open, vault.getState());
+        vault.close();
+        assertEquals(Vault.State.closed, vault.getState());
     }
 
     @Test
