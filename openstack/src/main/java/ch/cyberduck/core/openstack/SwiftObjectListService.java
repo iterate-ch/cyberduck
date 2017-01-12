81eb77004ab42ed8dfcf55281dc43c276111500a
David Kocher <dkocher@iterate.ch>
2017-01-12 11:04:08 +0100

    Remove superfluous type.
    Former-commit-id: f6a83b82473ad7242f6450279dbcbab661623bd6

--- openstack/src/main/java/ch/cyberduck/core/openstack/SwiftObjectListService.java
+++ openstack/src/main/java/ch/cyberduck/core/openstack/SwiftObjectListService.java
@@ -104,7 +104,6 @@ public class SwiftObjectListService implements ListService {
                             // add virtual directory if the placeholder object is missing
                             continue;
                         }
-                        types.add(Path.Type.placeholder);
                     }
                     children.add(new Path(directory, PathNormalizer.name(object.getName()), types, attributes));
                     marker = object.getName();
