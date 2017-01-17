ada99820b1a54c8498b223085d58612aa2149ca2
David Kocher <dkocher@iterate.ch>
2017-01-17 15:04:32 +0100

    Implement logout to shutdown connection pool.
    (cherry picked from commit d926dbf)
    
    
    Former-commit-id: bd41d714d57566cc63c271e7c6978b910bfac7ca

--- dropbox/src/main/java/ch/cyberduck/core/dropbox/DropboxCommonsHttpRequestExecutor.java
+++ dropbox/src/main/java/ch/cyberduck/core/dropbox/DropboxCommonsHttpRequestExecutor.java
@@ -167,4 +167,8 @@ public class DropboxCommonsHttpRequestExecutor extends HttpRequestor {
             }
         };
     }
+
+    public void shutdown() throws IOException {
+        client.close();
+    }
 }
