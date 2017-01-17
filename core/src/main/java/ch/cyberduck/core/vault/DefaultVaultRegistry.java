bb79d2d69fdc10fa75ee5c2d6200f7541e7c3e83
David Kocher <dkocher@iterate.ch>
2017-01-17 13:35:11 +0100

    Register features.
    Former-commit-id: 6dbd8fcba457857bcbe5b7ff3c7324567838165c

--- core/src/main/java/ch/cyberduck/core/vault/DefaultVaultRegistry.java
+++ core/src/main/java/ch/cyberduck/core/vault/DefaultVaultRegistry.java
@@ -185,6 +185,21 @@ public class DefaultVaultRegistry extends CopyOnWriteArraySet<Vault> implements 
         if(type == Encryption.class) {
             return (T) new VaultRegistryEncryptionFeature(session, (Encryption) proxy, this);
         }
+        if(type == Lifecycle.class) {
+            return (T) new VaultRegistryLifecycleFeature(session, (Lifecycle) proxy, this);
+        }
+        if(type == Location.class) {
+            return (T) new VaultRegistryLocationFeature(session, (Location) proxy, this);
+        }
+        if(type == Lock.class) {
+            return (T) new VaultRegistryLockFeature<>(session, (Lock) proxy, this);
+        }
+        if(type == Logging.class) {
+            return (T) new VaultRegistryLoggingFeature(session, (Logging) proxy, this);
+        }
+        if(type == Redundancy.class) {
+            return (T) new VaultRegistryRedundancyFeature(session, (Redundancy) proxy, this);
+        }
         return proxy;
     }
 }
---
+++ core/src/main/java/ch/cyberduck/core/vault/registry/VaultRegistryLoggingFeature.java
@@ -0,0 +1,45 @@
+package ch.cyberduck.core.vault.registry;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Session;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Logging;
+import ch.cyberduck.core.logging.LoggingConfiguration;
+import ch.cyberduck.core.vault.DefaultVaultRegistry;
+
+public class VaultRegistryLoggingFeature implements Logging {
+    private final Session<?> session;
+    private final Logging proxy;
+    private final DefaultVaultRegistry registry;
+
+    public VaultRegistryLoggingFeature(final Session<?> session, final Logging proxy, final DefaultVaultRegistry registry) {
+        this.session = session;
+        this.proxy = proxy;
+        this.registry = registry;
+    }
+
+    @Override
+    public LoggingConfiguration getConfiguration(final Path container) throws BackgroundException {
+        return registry.find(session, container).getFeature(session, Logging.class, proxy).getConfiguration(container);
+    }
+
+    @Override
+    public void setConfiguration(final Path container, final LoggingConfiguration configuration) throws BackgroundException {
+        registry.find(session, container).getFeature(session, Logging.class, proxy).setConfiguration(container, configuration);
+    }
+}
