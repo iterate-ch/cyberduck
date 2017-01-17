366bb19d0995439ba07a4b3cc317b670b2231226
David Kocher <dkocher@iterate.ch>
2017-01-17 22:56:20 +0100

    Register feature.
    Former-commit-id: f1cbdeed3dabe747a06586b1fce0578228d3ee3c

--- cryptomator/src/main/java/ch/cyberduck/core/cryptomator/CryptoVault.java
+++ cryptomator/src/main/java/ch/cyberduck/core/cryptomator/CryptoVault.java
@@ -419,7 +419,7 @@ public class CryptoVault implements Vault {
                 return (T) new CryptoWriteFeature(session, (Write) delegate, this);
             }
             if(type == MultipartWrite.class) {
-                return (T) new CryptoWriteFeature(session, (Write) delegate, this);
+                return (T) new CryptoMultipartWriteFeature(session, (Write) delegate, this);
             }
             if(type == Move.class) {
                 return (T) new CryptoMoveFeature(session, (Move) delegate, session._getFeature(Delete.class), session._getFeature(ListService.class), this);
