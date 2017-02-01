daa7d1f6844eaef8493f14c26641add7491af372
David Kocher <dkocher@iterate.ch>
2017-02-01 10:34:05 +0100

    Fix test.
    Former-commit-id: 67c9e97a8c86238a216c78d05de0835da4877008

--- ssh/src/test/java/ch/cyberduck/core/sftp/SFTPListServiceTest.java
+++ ssh/src/test/java/ch/cyberduck/core/sftp/SFTPListServiceTest.java
@@ -19,11 +19,14 @@ package ch.cyberduck.core.sftp;
 
 import ch.cyberduck.core.*;
 import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.transfer.TransferStatus;
 import ch.cyberduck.test.IntegrationTest;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.UUID;
 
@@ -43,15 +46,27 @@ public class SFTPListServiceTest {
         assertNotNull(session.getClient());
         session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
         final Path home = new SFTPHomeDirectoryService(session).find();
+        final Path file = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file));
+        final Path symlinkRelative = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file, AbstractPath.Type.symboliclink));
+        final Path symlinkAbsolute = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file, AbstractPath.Type.symboliclink));
+        final Path directory = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory));
+        new SFTPTouchFeature(session).touch(file, new TransferStatus());
+        new SFTPSymlinkFeature(session).symlink(symlinkRelative, file.getName());
+        new SFTPSymlinkFeature(session).symlink(symlinkAbsolute, file.getAbsolute());
+        new SFTPDirectoryFeature(session).mkdir(directory);
+        final Permission permission = new Permission(Permission.Action.read_write, Permission.Action.read_write, Permission.Action.read_write);
+        new SFTPUnixPermissionFeature(session).setUnixPermission(file, permission);
+
         final AttributedList<Path> list = new SFTPListService(session).list(home, new DisabledListProgressListener());
-        assertTrue(list.contains(new Path(home, "test", EnumSet.of(Path.Type.file))));
-        assertEquals(new Permission(Permission.Action.read_write, Permission.Action.read_write, Permission.Action.read_write),
-                list.get(new Path(home, "test", EnumSet.of(Path.Type.file))).attributes().getPermission());
-        assertTrue(list.contains(new Path(home, "test.directory", EnumSet.of(Path.Type.directory))));
-        assertTrue(list.contains(new Path(home, "test.symlink", EnumSet.of(Path.Type.file, AbstractPath.Type.symboliclink))));
-        assertEquals(new Path(home, "test", EnumSet.of(Path.Type.file)), list.get(new Path(home, "test.symlink", EnumSet.of(Path.Type.file, AbstractPath.Type.symboliclink))).getSymlinkTarget());
-        assertTrue(list.contains(new Path(home, "test.symlink-absolute", EnumSet.of(Path.Type.file, AbstractPath.Type.symboliclink))));
-        assertEquals(new Path(home, "test", EnumSet.of(Path.Type.file)), list.get(new Path(home, "test.symlink-absolute", EnumSet.of(Path.Type.file, AbstractPath.Type.symboliclink))).getSymlinkTarget());
+        assertTrue(list.contains(file));
+        assertEquals(permission, list.get(file).attributes().getPermission());
+        assertTrue(list.contains(directory));
+        assertTrue(list.contains(symlinkRelative));
+        assertEquals(file, list.get(symlinkRelative).getSymlinkTarget());
+        assertTrue(list.contains(symlinkAbsolute));
+        assertEquals(file, list.get(symlinkAbsolute).getSymlinkTarget());
+
+        new SFTPDeleteFeature(session).delete(Arrays.asList(file, symlinkAbsolute, symlinkRelative, directory), new DisabledLoginCallback(), new Delete.DisabledCallback());
         session.close();
     }
 
