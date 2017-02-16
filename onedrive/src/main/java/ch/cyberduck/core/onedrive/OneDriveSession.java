bda52d57c0f995d58ee420d94358702ef2b2b5d2
Jöran Malek <joeran3@gmail.com>
2017-02-16 22:56:33 +0100

    Replace offline_access with corrected wl.offline_access scope
    Former-commit-id: 7254e2eef10d95d121e9a8981d7ec95189d384a9

--- onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveSession.java
+++ onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveSession.java
@@ -63,7 +63,7 @@ public class OneDriveSession extends HttpSession<OneDriveAPI> {
                 "https://login.live.com/oauth20_token.srf", "https://login.live.com/oauth20_authorize.srf",
                 "372770ba-bb24-436b-bbd4-19bc86310c0e",
                 "mJjWVkmfD9FVHNFTpbrdowv",
-                Arrays.asList("onedrive.readwrite", "offline_access"))
+                Arrays.asList("onedrive.readwrite", "wl.offline_access"))
                 .withRedirectUri("https://cyberduck.io/oauth");
 
         return new OneDriveAPI() {
