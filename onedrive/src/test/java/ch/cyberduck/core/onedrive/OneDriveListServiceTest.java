fd135ec48882f83ed9894583865a2f5b3a6836e5
Jöran Malek <joeran3@gmail.com>
2017-02-16 22:05:13 +0100

    Add Test for List Service
    Former-commit-id: bb8226174ef2008d4984d01b57788f68a4f05228

---
+++ onedrive/src/test/java/ch/cyberduck/core/onedrive/OneDriveListServiceTest.java
@@ -0,0 +1,70 @@
+package ch.cyberduck.core.onedrive;
+
+import ch.cyberduck.core.*;
+import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.ssl.DefaultX509KeyManager;
+import ch.cyberduck.core.ssl.DefaultX509TrustManager;
+import ch.cyberduck.test.IntegrationTest;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.EnumSet;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+@Category(IntegrationTest.class)
+public class OneDriveListServiceTest {
+    @Test
+    public void testList() throws Exception {
+        final Host host = new Host(new OneDriveProtocol(), "api.onedrive.com", new Credentials());
+        final OneDriveSession session = new OneDriveSession(host, new DefaultX509TrustManager(), new DefaultX509KeyManager());
+        new LoginConnectionService(new DisabledLoginCallback() {
+            @Override
+            public void prompt(final Host bookmark, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                fail(reason);
+            }
+        }, new DisabledHostKeyCallback(),
+                new DisabledPasswordStore() {
+                    @Override
+                    public String getPassword(Scheme scheme, int port, String hostname, String user) {
+                        if(user.equals("OneDrive OAuth2 Access Token")) {
+                            return System.getProperties().getProperty("onedrive.accesstoken");
+                        }
+                        if(user.equals("OneDrive OAuth2 Refresh Token")) {
+                            return System.getProperties().getProperty("onedrive.refreshtoken");
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    public String getPassword(String hostname, String user) {
+                        return super.getPassword(hostname, user);
+                    }
+                }, new DisabledProgressListener(),
+                new DisabledTranscriptListener()).connect(session, PathCache.empty(), new DisabledCancelCallback());
+        final AttributedList<Path> list = new OneDriveListService(session).list(new Path("/", EnumSet.of(Path.Type.directory)), new DisabledListProgressListener());
+        assertFalse(list.isEmpty());
+        for(Path f : list) {
+            assertEquals(new Path("/", EnumSet.of(Path.Type.directory)), f.getParent());
+            assertNotNull(f.attributes().getVersionId());
+            assertNotNull(f.attributes().getModificationDate());
+        }
+    }
+}
