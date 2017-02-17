5e6abcc05c1e86cd9f053738d4969d4d66e46632
Jöran Malek <joeran3@gmail.com>
2017-02-17 19:47:08 +0100

    Add publicly available JsonObjectIterator for use in OneDrive protocol
    Former-commit-id: 2b8b2c4ed46d17a3ec5cb52e7b61a13556800800

---
+++ onedrive/src/main/java/ch/cyberduck/core/onedrive/JsonObjectIteratorPort.java
@@ -0,0 +1,95 @@
+package ch.cyberduck.core.onedrive;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import org.nuxeo.onedrive.client.OneDriveAPI;
+import org.nuxeo.onedrive.client.OneDriveAPIException;
+import org.nuxeo.onedrive.client.OneDriveJsonRequest;
+import org.nuxeo.onedrive.client.OneDriveJsonResponse;
+import org.nuxeo.onedrive.client.OneDriveRuntimeException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+
+public class JsonObjectIteratorPort implements Iterator<JsonValue> {
+    private final OneDriveAPI api;
+    private URL url;
+    private boolean hasMorePages;
+    private Iterator<JsonValue> currentPage;
+
+    public JsonObjectIteratorPort(OneDriveAPI api, URL url) {
+        this.api = api;
+        this.url = url;
+        this.hasMorePages = true;
+    }
+
+    public boolean hasNext() throws OneDriveRuntimeException {
+        if(this.currentPage != null && this.currentPage.hasNext()) {
+            return true;
+        }
+        else if(!this.hasMorePages) {
+            return false;
+        }
+        else {
+            this.loadNextPage();
+            return this.currentPage != null && this.currentPage.hasNext();
+        }
+    }
+
+    public JsonObject next() throws OneDriveRuntimeException {
+        if(this.hasNext()) {
+            return ((JsonValue) this.currentPage.next()).asObject();
+        }
+        else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    private void loadNextPage() throws OneDriveRuntimeException {
+        try {
+            OneDriveJsonRequest e = new OneDriveJsonRequest(this.api, this.url, "GET");
+            OneDriveJsonResponse response = (OneDriveJsonResponse) e.send();
+            JsonObject json = response.getContent();
+            JsonValue values = json.get("value");
+            if(values.isNull()) {
+                this.currentPage = Collections.emptyIterator();
+            }
+            else {
+                this.currentPage = values.asArray().iterator();
+            }
+
+            JsonValue nextUrl = json.get("@odata.nextLink");
+            this.hasMorePages = nextUrl != null && !nextUrl.isNull();
+            if(this.hasMorePages) {
+                this.url = new URL(nextUrl.asString());
+            }
+
+        }
+        catch(OneDriveAPIException var6) {
+            throw new OneDriveRuntimeException("An error occurred during connection with OneDrive API.", var6);
+        }
+        catch(MalformedURLException var7) {
+            this.hasMorePages = false;
+            throw new OneDriveRuntimeException("Next url returned from OneDrive API is malformed.", var7);
+        }
+    }
+}
