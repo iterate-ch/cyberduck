5e3f1e17d71b1637627fdce75c2f23cedb2c8a3a
Jöran Malek <joeran3@gmail.com>
2017-02-17 21:00:06 +0100

    Implement drive/path logic (every drive is now an own directory in filesystem)
    Former-commit-id: efb37040d0e5d5216729158903358c61f798c1e0

--- onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveListService.java
+++ onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveListService.java
@@ -20,19 +20,23 @@ import ch.cyberduck.core.AttributedList;
 import ch.cyberduck.core.ListProgressListener;
 import ch.cyberduck.core.ListService;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathContainerService;
 import ch.cyberduck.core.exception.BackgroundException;
 
-import org.nuxeo.onedrive.client.OneDriveItem;
-import org.nuxeo.onedrive.client.OneDriveItemIterator;
 import org.nuxeo.onedrive.client.OneDriveRuntimeException;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.EnumSet;
+import java.util.Iterator;
+
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
 
 public class OneDriveListService implements ListService {
 
     private final OneDriveSession session;
+    private final PathContainerService pathContainerService = new PathContainerService();
 
     public OneDriveListService(final OneDriveSession session) {
         this.session = session;
@@ -41,35 +45,80 @@ public class OneDriveListService implements ListService {
     @Override
     public AttributedList<Path> list(final Path directory, final ListProgressListener listener) throws BackgroundException {
         final AttributedList<Path> children = new AttributedList<>();
-        OneDriveItemIterator iterator = null;
+
+        // evaluating query
+        StringBuilder builder = new StringBuilder();
+        builder.append(session.getClient().getBaseURL());
+        builder.append("/drives"); // query drives
+
+        if(!directory.isRoot()) {
+            Path driveId = pathContainerService.getContainer(directory); // using pathContainerService for retrieving current drive id
+            builder.append(String.format("/%s", driveId.getName())); // append with format /drive-id
+
+            if(!pathContainerService.isContainer(directory)) {
+                // append path to item via pathContainerService with format :/path:
+                builder.append(String.format(":/%s:", pathContainerService.getKey(directory)));
+            }
+
+            builder.append("/children");
+        }
+
+        Iterator<JsonObject> iterator = null;
         try {
-            iterator = new OneDriveItemIterator(session.getClient(), new URL(String.format("%s/drive/root:%s:/children", session.getClient().getBaseURL(), directory.getAbsolute())));
+            iterator = new JsonObjectIteratorPort(session.getClient(), new URL(builder.toString()));
         }
         catch(MalformedURLException e) {
             throw new BackgroundException(e);
         }
-        while(iterator.hasNext()) {
-            final OneDriveItem.Metadata metadata;
-            try {
-                metadata = iterator.next();
-            }
-            catch(OneDriveRuntimeException e) {
-                continue;
-            }
 
-            final EnumSet<AbstractPath.Type> type;
-            if(metadata.isFile()) {
-                type = EnumSet.of(Path.Type.file);
-            }
-            else if(metadata.isFolder()) {
-                type = EnumSet.of(Path.Type.directory);
-            }
-            else {
-                continue; // ignore !file && !folder
-            }
+        try {
+            while(iterator.hasNext()) {
+                try {
+                    final String name;
+                    final EnumSet<AbstractPath.Type> type;
 
-            children.add(new Path(directory, metadata.getName(), type));
+                    JsonObject jsonObject = iterator.next();
+
+                    JsonValue driveType = jsonObject.get("driveType");
+                    if(driveType != null && !driveType.isNull()) {
+                        // this is drive object we are on /drives hierarchy
+                        name = jsonObject.get("id").asString(); // this may not fail
+                        type = EnumSet.of(AbstractPath.Type.volume, AbstractPath.Type.directory);
+                    }
+                    else {
+                        // try evaluating
+                        JsonValue nameValue = jsonObject.get("name");
+                        if(nameValue == null || nameValue.isNull() || !nameValue.isString()) {
+                            // got null name (not found) or empty name (should not happen)
+                            continue;
+                        }
+                        name = nameValue.asString();
+
+                        JsonValue fileValue = jsonObject.get("file");
+                        JsonValue folderValue = jsonObject.get("folder");
+                        if(fileValue != null && !fileValue.isNull()) {
+                            type = EnumSet.of(AbstractPath.Type.file);
+                        }
+                        else if(folderValue != null && !folderValue.isNull()) {
+                            type = EnumSet.of(AbstractPath.Type.directory);
+                        }
+                        else {
+                            // if everything else fails: ignore and continue
+                            continue;
+                        }
+                    }
+
+                    children.add(new Path(directory, name, type));
+                }
+                catch(OneDriveRuntimeException e) { // this catches iterator.next() whicht may not cause hasNext() to fail
+                    continue; // silent ignore any OneDriveRuntimeException in next(). Might redirect to log!
+                }
+            }
         }
+        catch(OneDriveRuntimeException e) { // this catches iterator.hasNext() which in return should fail fast
+            throw new BackgroundException(e);
+        }
+
         return children;
     }
 }
