81d1a32a2f245c67b0248acaa9e594f9b6b3ebbf
David Kocher <dkocher@iterate.ch>
2017-03-28 22:38:35 +0200

    Mark as integration test.
    Former-commit-id: e8c3eb029159acccae005997bb0b544c46482be8

--- cryptomator/src/test/java/ch/cyberduck/core/azure/AzureWriteFeatureTest.java
+++ cryptomator/src/test/java/ch/cyberduck/core/azure/AzureWriteFeatureTest.java
@@ -41,10 +41,12 @@ import ch.cyberduck.core.shared.DefaultAttributesFinderFeature;
 import ch.cyberduck.core.shared.DefaultFindFeature;
 import ch.cyberduck.core.transfer.TransferStatus;
 import ch.cyberduck.core.vault.DefaultVaultRegistry;
+import ch.cyberduck.test.IntegrationTest;
 
 import org.cryptomator.cryptolib.api.Cryptor;
 import org.cryptomator.cryptolib.api.FileHeader;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -58,6 +60,7 @@ import com.microsoft.azure.storage.OperationContext;
 
 import static org.junit.Assert.*;
 
+@Category(IntegrationTest.class)
 public class AzureWriteFeatureTest {
 
     @Test
