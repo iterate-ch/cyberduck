a648cf0f0002d24fc80834ac40ec8aead7110cfa
Jöran Malek <joeran3@gmail.com>
2017-04-05 21:01:38 +0200

    Fix tests (not relying on /temp)
    Former-commit-id: 487ee09e31c8d5c6acf01ab0646b4e5a1f4e89e4

--- core/src/test/java/ch/cyberduck/core/local/DefaultTemporaryFileServiceTest.java
+++ core/src/test/java/ch/cyberduck/core/local/DefaultTemporaryFileServiceTest.java
@@ -3,7 +3,6 @@ package ch.cyberduck.core.local;
 import ch.cyberduck.core.AlphanumericRandomStringService;
 import ch.cyberduck.core.Local;
 import ch.cyberduck.core.Path;
-import ch.cyberduck.core.preferences.PreferencesFactory;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -71,35 +70,33 @@ public class DefaultTemporaryFileServiceTest {
 
     @Test
     public void testPathTooLong() {
-        final String replace = new File("/").getAbsolutePath();
+        final String temp = StringUtils.removeEnd(System.getProperty("java.io.tmpdir"), File.separator);
         final String testPathDirectory = "/Lorem/ipsum/dolor/sit/amet/consetetur/sadipscing/elitr/sed/diam/nonumy/eirmod/tempor/invidunt/ut/labore/et/dolore/magna/aliquyam/erat/sed/diam/voluptua/At/vero/eos/et/accusam/et/justo/duo/dolores/et/ea/rebum/Stet/clita/kasd/gubergren/no/sea";
         final String testPathFile = "takimata.sanc";
         final String testPath = String.format("%s/%s", testPathDirectory, testPathFile);
         final String testPathMD5 = DigestUtils.md5Hex(testPathDirectory);
 
-        PreferencesFactory.get().setProperty("tmp.dir", "/temp"); // overwrite tmp.dir for easier tests
         final Path file = new Path(testPath, EnumSet.of(Path.Type.file));
         file.attributes().setVersionId("2");
         final Local local = new DefaultTemporaryFileService().create("UID", file);
-        final String localFile = local.getAbsolute().replace(replace, "/").replace('/', File.separatorChar);
-        assertNotEquals(String.format("/%s/%s%s/2/%s", "temp", "UID", testPathDirectory, testPathFile).replace('/', File.separatorChar), localFile);
-        assertEquals(String.format("/%s/%s/%s/2/%s", "temp", "UID", testPathMD5, testPathFile).replace('/', File.separatorChar), localFile);
+        final String localFile = local.getAbsolute();
+        assertNotEquals(String.format("%s/%s%s/2/%s", temp, "UID", testPathDirectory, testPathFile).replace('/', File.separatorChar), localFile);
+        assertEquals(String.format("%s/%s/%s/2/%s", temp, "UID", testPathMD5, testPathFile).replace('/', File.separatorChar), localFile);
     }
 
     @Test
     public void testPathNotTooLong() {
-        final String replace = new File("/").getAbsolutePath();
+        final String temp = StringUtils.removeEnd(System.getProperty("java.io.tmpdir"), File.separator);
         final String testPathDirectory = "/Lorem/ipsum/dolor/sit/amet/consetetur/sadipscing/elitr/sed/diam/nonumy/eirmod/tempor";
         final String testPathFile = "takimata.sanc";
         final String testPath = String.format("%s/%s", testPathDirectory, testPathFile);
         final String testPathMD5 = DigestUtils.md5Hex(testPathDirectory);
 
-        PreferencesFactory.get().setProperty("tmp.dir", "/temp"); // overwrite tmp.dir for easier tests
         Path file = new Path(testPath, EnumSet.of(Path.Type.file));
         file.attributes().setVersionId("2");
         final Local local = new DefaultTemporaryFileService().create("UID", file);
-        final String localFile = local.getAbsolute().replace(replace, "/").replace('/', File.separatorChar);
-        assertEquals(String.format("/%s/%s%s/2/%s", "temp", "UID", testPathDirectory, testPathFile).replace('/', File.separatorChar), localFile);
-        assertNotEquals(String.format("/%s/%s%s/2/%s", "temp", "UID", testPathMD5, testPathFile).replace('/', File.separatorChar), localFile);
+        final String localFile = local.getAbsolute();
+        assertEquals(String.format("%s/%s%s/2/%s", temp, "UID", testPathDirectory, testPathFile).replace('/', File.separatorChar), localFile);
+        assertNotEquals(String.format("%s/%s%s/2/%s", temp, "UID", testPathMD5, testPathFile).replace('/', File.separatorChar), localFile);
     }
 }
