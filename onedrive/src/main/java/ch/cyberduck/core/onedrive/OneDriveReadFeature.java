c81f2942842cb7da59b1a3948abb35e5cbd835cf
Jöran Malek <jmalek@iterate.ch>
2017-04-08 10:24:16 +0200

    OneDriveReadFeature does not use custom request
    Former-commit-id: a0ac9978bdf43545d3eba52dba80c53332ed0961

--- onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveReadFeature.java
+++ onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveReadFeature.java
@@ -18,11 +18,14 @@ package ch.cyberduck.core.onedrive;
 import ch.cyberduck.core.ConnectionCallback;
 import ch.cyberduck.core.DefaultIOExceptionMappingService;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathContainerService;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.Read;
 import ch.cyberduck.core.transfer.TransferStatus;
 
 import org.nuxeo.onedrive.client.OneDriveAPIException;
+import org.nuxeo.onedrive.client.OneDriveDrive;
+import org.nuxeo.onedrive.client.OneDriveFile;
 import org.nuxeo.onedrive.client.OneDriveRequest;
 import org.nuxeo.onedrive.client.OneDriveResponse;
 
@@ -33,20 +36,19 @@ public class OneDriveReadFeature implements Read {
 
     private final OneDriveSession session;
 
+    private final PathContainerService containerService
+            = new PathContainerService();
+
     public OneDriveReadFeature(final OneDriveSession session) {
         this.session = session;
     }
 
     @Override
     public InputStream read(final Path file, final TransferStatus status, final ConnectionCallback callback) throws BackgroundException {
-        final OneDriveUrlBuilder builder = new OneDriveUrlBuilder(session)
-                .resolveDriveQueryPath(file)
-                .resolveContentPath(file);
+        OneDriveFile oneDriveFile = session.getFile(file);
 
         try {
-            OneDriveRequest request = new OneDriveRequest(builder.build(), "GET");
-            OneDriveResponse response = request.sendRequest(session.getClient().getExecutor());
-            return response.getContent();
+            return oneDriveFile.download();
         }
         catch(OneDriveAPIException e) {
             throw new OneDriveExceptionMappingService().map(e);
