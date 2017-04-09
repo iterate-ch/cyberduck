e21d16060dd40956066de6180bb841ae5953650b
Jöran Malek <joeran3@gmail.com>
2017-04-09 19:17:25 +0200

    Add OneDriveDirectoryFeatureTest
    Former-commit-id: a928f6245848b322ba056d6882d556400bbed246

---
+++ onedrive/src/test/java/ch/cyberduck/core/onedrive/OneDriveDirectoryFeatureTest.java
@@ -0,0 +1,55 @@
+package ch.cyberduck.core.onedrive;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.DisabledListProgressListener;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.test.IntegrationTest;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.UUID;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+@Category(IntegrationTest.class)
+public class OneDriveDirectoryFeatureTest extends AbstractOneDriveTest {
+    private static final Logger log = Logger.getLogger(OneDriveDirectoryFeatureTest.class);
+
+    @Test
+    public void testMkdir() throws Exception {
+        final OneDriveListService listService = new OneDriveListService(session);
+        final OneDriveAttributesFinderFeature attributesFinderFeature = new OneDriveAttributesFinderFeature(session);
+        final OneDriveDirectoryFeature directoryFeature = new OneDriveDirectoryFeature(session);
+        final OneDriveDeleteFeature deleteFeature = new OneDriveDeleteFeature(session);
+        final AttributedList<Path> list = listService.list(new Path("/", EnumSet.of(Path.Type.directory)), new DisabledListProgressListener());
+        assertFalse(list.isEmpty());
+        for(Path f : list) {
+            log.info(f);
+            final Path target = directoryFeature.mkdir(new Path(f, UUID.randomUUID().toString(), EnumSet.of(Path.Type.directory)), null, null);
+            assertNotNull(attributesFinderFeature.find(target).getVersionId());
+            deleteFeature.delete(Collections.singletonList(target), new DisabledLoginCallback(), new Delete.DisabledCallback());
+        }
+    }
+}
