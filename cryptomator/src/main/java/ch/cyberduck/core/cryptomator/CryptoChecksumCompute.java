2322ef83dce340bd7b1371a6b8577daa49182686
David Kocher <dkocher@iterate.ch>
2017-04-11 11:24:44 +0200

    Revert pipe size
    Former-commit-id: 6dd96c778ba1cc9d9f72806522c460a224cfd724

--- cryptomator/src/main/java/ch/cyberduck/core/cryptomator/CryptoChecksumCompute.java
+++ cryptomator/src/main/java/ch/cyberduck/core/cryptomator/CryptoChecksumCompute.java
@@ -23,6 +23,7 @@ import ch.cyberduck.core.io.Checksum;
 import ch.cyberduck.core.io.ChecksumCompute;
 import ch.cyberduck.core.io.StreamCopier;
 import ch.cyberduck.core.io.VoidStatusOutputStream;
+import ch.cyberduck.core.preferences.PreferencesFactory;
 import ch.cyberduck.core.random.NonceGenerator;
 import ch.cyberduck.core.threading.ThreadPool;
 import ch.cyberduck.core.threading.ThreadPoolFactory;
@@ -68,7 +69,7 @@ public class CryptoChecksumCompute extends AbstractChecksumCompute implements Ch
             final PipedOutputStream source = new PipedOutputStream();
             final CryptoOutputStream<Void> out = new CryptoOutputStream<Void>(new VoidStatusOutputStream(source), vault.getCryptor(),
                     vault.getCryptor().fileHeaderCryptor().decryptHeader(header), nonces);
-            final PipedInputStream sink = new PipedInputStream(source);
+            final PipedInputStream sink = new PipedInputStream(source, PreferencesFactory.get().getInteger("connection.chunksize"));
             final ThreadPool pool = ThreadPoolFactory.get();
             try {
                 final Future execute = pool.execute(new Callable<TransferStatus>() {
