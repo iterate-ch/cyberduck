177e8a9f8d83402649cd8116b2010a5533ae0aa6
David Kocher <dkocher@iterate.ch>
2017-04-12 15:45:15 +0200

    Return computer name for description
    Former-commit-id: 3413894588a2fa2abb121b34bb659eeeb9a1a274

--- nio/src/main/java/ch/cyberduck/core/nio/LocalProtocol.java
+++ nio/src/main/java/ch/cyberduck/core/nio/LocalProtocol.java
@@ -19,6 +19,9 @@ import ch.cyberduck.core.AbstractProtocol;
 import ch.cyberduck.core.LocaleFactory;
 import ch.cyberduck.core.Scheme;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class LocalProtocol extends AbstractProtocol {
 
     @Override
@@ -33,7 +36,12 @@ public class LocalProtocol extends AbstractProtocol {
 
     @Override
     public String getDescription() {
-        return LocaleFactory.localizedString("Local Filesystem");
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        }
+        catch(UnknownHostException e) {
+            return LocaleFactory.localizedString("Local Filesystem");
+        }
     }
 
     @Override
