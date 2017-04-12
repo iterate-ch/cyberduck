f5f7fcc59c9f667a9d93f3dfb0b7957eb5aa744a
Jöran Malek <jmalek@iterate.ch>
2017-04-12 10:58:25 +0200

    Add rename Test
    Former-commit-id: 24274e832934d5f4386ca66b43fc5c19591bdf52

--- onedrive/src/test/java/ch/cyberduck/core/onedrive/OneDriveMoveFeatureTest.java
+++ onedrive/src/test/java/ch/cyberduck/core/onedrive/OneDriveMoveFeatureTest.java
@@ -15,5 +15,51 @@ package ch.cyberduck.core.onedrive;
  * GNU General Public License for more details.
  */
 
-public class OneDriveMoveFeatureTest {
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.DisabledListProgressListener;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.AttributesFinder;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.features.Move;
+import ch.cyberduck.core.features.Touch;
+import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.test.IntegrationTest;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.UUID;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@Category(IntegrationTest.class)
+public class OneDriveMoveFeatureTest extends AbstractOneDriveTest {
+    @Test
+    public void testRename() throws BackgroundException {
+        final Touch touch = new OneDriveTouchFeature(session);
+        final Move move = new OneDriveMoveFeature(session);
+        final Delete delete = new OneDriveDeleteFeature(session);
+        final AttributesFinder attributesFinder = new OneDriveAttributesFinderFeature(session);
+
+        final AttributedList<Path> list = new OneDriveListService(session).list(new Path("/", EnumSet.of(Path.Type.directory)), new DisabledListProgressListener());
+        assertFalse(list.isEmpty());
+        for(Path file : list) {
+            if(file.isDirectory()) {
+                Path touchedFile = new Path(file, UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
+                touch.touch(touchedFile, new TransferStatus().mime("x-application/cyberduck"));
+                assertNotNull(attributesFinder.find(touchedFile));
+                Path rename = new Path(file, UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
+                assertTrue(move.isSupported(touchedFile, rename));
+                move.move(touchedFile, rename, false, new Delete.DisabledCallback());
+                assertNotNull(attributesFinder.find(rename));
+                delete.delete(Collections.singletonList(rename), new DisabledLoginCallback(), new Delete.DisabledCallback());
+            }
+        }
+    }
 }
