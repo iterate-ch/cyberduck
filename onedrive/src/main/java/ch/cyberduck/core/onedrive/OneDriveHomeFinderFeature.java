76f1e98e97ea06f66e5338d60b2d558f970e1020
Jöran Malek <joeran3@gmail.com>
2017-04-18 11:16:04 +0200

    Use default drive for getting home drive
    Former-commit-id: 1e8efeeb4f15fa8c4a9b285500516a551c8ef775

--- onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveHomeFinderFeature.java
+++ onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveHomeFinderFeature.java
@@ -15,12 +15,15 @@ package ch.cyberduck.core.onedrive;
  * GNU General Public License for more details.
  */
 
-import ch.cyberduck.core.AttributedList;
-import ch.cyberduck.core.DisabledListProgressListener;
+import ch.cyberduck.core.DefaultIOExceptionMappingService;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.shared.DefaultHomeFinderService;
 
+import org.nuxeo.onedrive.client.OneDriveAPIException;
+import org.nuxeo.onedrive.client.OneDriveDrive;
+
+import java.io.IOException;
 import java.util.EnumSet;
 
 public class OneDriveHomeFinderFeature extends DefaultHomeFinderService {
@@ -36,13 +39,17 @@ public class OneDriveHomeFinderFeature extends DefaultHomeFinderService {
     public Path find() throws BackgroundException {
         final Path home = super.find();
         if(home == DEFAULT_HOME) {
-            final AttributedList<Path> list = new OneDriveContainerListService(session).list(
-                    new Path(String.valueOf(Path.DELIMITER), EnumSet.of(Path.Type.volume, Path.Type.directory)), new DisabledListProgressListener()
-            );
-            if(list.isEmpty()) {
-                return home;
+            try {
+                return new Path(
+                        Path.DELIMITER + OneDriveDrive.getDefaultDrive(session.getClient()).getMetadata().getId(),
+                        EnumSet.of(Path.Type.volume, Path.Type.directory));
             }
-            return list.iterator().next();
+            catch(OneDriveAPIException e) {
+                throw new OneDriveExceptionMappingService().map("Cannot get home {0}", e);
+            }
+            catch(IOException e) {
+                throw new DefaultIOExceptionMappingService().map("Cannot get home {0}", e);
+            }
         }
         return home;
     }
