a3a5f8b167c5987faf26fcab7f1a1fd06de16d64
David Kocher <dkocher@iterate.ch>
2017-04-24 11:28:20 +0200

    Review
    Former-commit-id: 96ae79abfb0194296496b303fe8bb918bc96093f

--- openstack/src/test/java/ch/cyberduck/core/openstack/SwiftLargeUploadWriteFeatureTest.java
+++ openstack/src/test/java/ch/cyberduck/core/openstack/SwiftLargeUploadWriteFeatureTest.java
@@ -24,11 +24,12 @@ import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathCache;
 import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.io.StreamCopier;
 import ch.cyberduck.core.transfer.TransferStatus;
 import ch.cyberduck.test.IntegrationTest;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -61,45 +62,11 @@ public class SwiftLargeUploadWriteFeatureTest {
         status.setLength(-1L);
         final Path file = new Path(container, UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
         final OutputStream out = feature.write(file, status, new DisabledConnectionCallback());
-        final byte[] content = RandomStringUtils.random(6 * 1024 * 1024).getBytes("UTF-8");
+        final byte[] content = RandomUtils.nextBytes(6 * 1024 * 1024);
         final ByteArrayInputStream in = new ByteArrayInputStream(content);
-        // Adjust buffer to be 5MB. This will write parts with 5MB size which is the minimum allowed
-        final byte[] buffer = new byte[5 * 1024 * 1024];
-        assertEquals(content.length, IOUtils.copyLarge(in, out, buffer));
-        in.close();
-        out.close();
-        assertTrue(new SwiftFindFeature(session).find(file));
-        final byte[] compare = new byte[content.length];
-        final InputStream stream = new SwiftReadFeature(session, regionService).read(file, new TransferStatus().length(content.length), new DisabledConnectionCallback());
-        IOUtils.readFully(stream, compare);
-        stream.close();
-        assertArrayEquals(content, compare);
-        new SwiftDeleteFeature(session).delete(Collections.singletonList(file), new DisabledLoginCallback(), new Delete.DisabledCallback());
-        session.close();
-    }
-
-    @Test
-    public void testWriteUploadSmallBuffer() throws Exception {
-        final Host host = new Host(new SwiftProtocol(), "identity.api.rackspacecloud.com", new Credentials(
-                System.getProperties().getProperty("rackspace.key"), System.getProperties().getProperty("rackspace.secret")
-        ));
-        final SwiftSession session = new SwiftSession(host).withAccountPreload(false).withCdnPreload(false).withContainerPreload(false);
-        session.open(new DisabledHostKeyCallback());
-        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
-        final SwiftRegionService regionService = new SwiftRegionService(session);
-        final SwiftLargeUploadWriteFeature feature = new SwiftLargeUploadWriteFeature(session, regionService,
-                new SwiftSegmentService(session, ".segments-test/"));
-        final Path container = new Path("test.cyberduck.ch", EnumSet.of(Path.Type.directory, Path.Type.volume));
-        container.attributes().setRegion("DFW");
-        final TransferStatus status = new TransferStatus();
-        status.setLength(-1L);
-        final Path file = new Path(container, UUID.randomUUID().toString(), EnumSet.of(Path.Type.file));
-        final OutputStream out = feature.write(file, status, new DisabledConnectionCallback());
-        final byte[] content = RandomStringUtils.random(5 * 1024 * 1024).getBytes("UTF-8");
-        final ByteArrayInputStream in = new ByteArrayInputStream(content);
-        // Adjust buffer to be 5MB. This will write parts with 5MB size which is the minimum allowed
-        final byte[] buffer = new byte[1 * 1024 * 1024];
-        assertEquals(content.length, IOUtils.copyLarge(in, out, buffer));
+        final TransferStatus progress = new TransferStatus();
+        new StreamCopier(new TransferStatus(), progress).transfer(in, out);
+        assertEquals(content.length, progress.getOffset());
         in.close();
         out.close();
         assertTrue(new SwiftFindFeature(session).find(file));
