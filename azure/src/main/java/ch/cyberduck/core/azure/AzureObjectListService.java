7f5fb8c656977fd23ba5a717e9053b972fe7c6af
David Kocher <dkocher@iterate.ch>
2017-04-25 11:41:02 +0200

    Fix parsing MD5 property.
    Former-commit-id: dd82d87de8106801ac702d8e840629dfd42c5501

--- azure/src/main/java/ch/cyberduck/core/azure/AzureObjectListService.java
+++ azure/src/main/java/ch/cyberduck/core/azure/AzureObjectListService.java
@@ -31,8 +31,6 @@ import ch.cyberduck.core.exception.NotfoundException;
 import ch.cyberduck.core.io.Checksum;
 import ch.cyberduck.core.preferences.PreferencesFactory;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.StringUtils;
 
 import java.net.URISyntaxException;
@@ -93,7 +91,7 @@ public class AzureObjectListService implements ListService {
                         attributes.setModificationDate(blob.getProperties().getLastModified().getTime());
                         attributes.setETag(blob.getProperties().getEtag());
                         if(StringUtils.isNotBlank(blob.getProperties().getContentMD5())) {
-                            attributes.setChecksum(Checksum.parse(Hex.encodeHexString(Base64.decodeBase64(blob.getProperties().getContentMD5()))));
+                            attributes.setChecksum(Checksum.parse(blob.getProperties().getContentMD5()));
                         }
                     }
                     // A directory is designated by a delimiter character.
