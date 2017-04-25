7e502cf575f0d87266f08b28dbfa96642bbe428a
Jöran Malek <joeran3@gmail.com>
2017-04-25 11:39:31 +0200

    Add Context Login
    Former-commit-id: cb56cd4e746ce193278a352b77e3daaf16d52542

---
+++ onedrive/src/test/java/ch/cyberduck/core/onedrive/OneDriveBusinessContextLoginTest.java
@@ -0,0 +1,79 @@
+package ch.cyberduck.core.onedrive;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.*;
+import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.ssl.DefaultX509KeyManager;
+import ch.cyberduck.core.ssl.DefaultX509TrustManager;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class OneDriveBusinessContextLoginTest {
+
+    protected OneDriveSession session;
+
+    @BeforeClass
+    public static void protocol() {
+        ProtocolFactory.register(new OneDriveProtocol());
+    }
+
+    @After
+    public void disconnect() throws Exception {
+        session.close();
+    }
+
+    @Before
+    public void setup() throws Exception {
+        final Profile profile = ProfileReaderFactory.get().read(
+                new Local("../profiles/Microsoft OneDrive Business.cyberduckprofile"));
+        final Host host = new Host(profile, profile.getDefaultHostname());
+        session = new OneDriveSession(host, new DefaultX509TrustManager(), new DefaultX509KeyManager());
+        new LoginConnectionService(new DisabledLoginCallback() {
+            @Override
+            public void prompt(final Host bookmark, final Credentials credentials, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+                fail(reason);
+            }
+        }, new DisabledHostKeyCallback(),
+                new DisabledPasswordStore() {
+                    @Override
+                    public String getPassword(Scheme scheme, int port, String hostname, String user) {
+                        if(user.endsWith("OAuth2 Access Token")) {
+                            return System.getProperties().getProperty("onedrive.business.accesstoken");
+                        }
+                        if(user.endsWith("OAuth2 Refresh Token")) {
+                            return System.getProperties().getProperty("onedrive.business.refreshtoken");
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    public String getPassword(String hostname, String user) {
+                        return super.getPassword(hostname, user);
+                    }
+                }, new DisabledProgressListener()).connect(session, PathCache.empty(), new DisabledCancelCallback());
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        new OneDriveHomeFinderFeature(session).find();
+    }
+}
