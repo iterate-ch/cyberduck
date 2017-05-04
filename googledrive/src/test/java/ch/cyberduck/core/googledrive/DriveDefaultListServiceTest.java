dedfdd24a9de553cf9acec2358b35944a9fadb58
David Kocher <dkocher@iterate.ch>
2017-05-04 22:23:43 +0200

    Fix test.
    Former-commit-id: 93b2bdb8e0296e7dfaa7d8781b3485164f03fde6

--- googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveDefaultListServiceTest.java
+++ googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveDefaultListServiceTest.java
@@ -15,22 +15,10 @@ package ch.cyberduck.core.googledrive;
  * GNU General Public License for more details.
  */
 
-import ch.cyberduck.core.AttributedList;
-import ch.cyberduck.core.Credentials;
-import ch.cyberduck.core.DisabledCancelCallback;
-import ch.cyberduck.core.DisabledHostKeyCallback;
-import ch.cyberduck.core.DisabledListProgressListener;
-import ch.cyberduck.core.DisabledLoginCallback;
-import ch.cyberduck.core.DisabledPasswordStore;
-import ch.cyberduck.core.DisabledProgressListener;
-import ch.cyberduck.core.Host;
-import ch.cyberduck.core.LoginConnectionService;
-import ch.cyberduck.core.LoginOptions;
-import ch.cyberduck.core.Path;
-import ch.cyberduck.core.PathCache;
-import ch.cyberduck.core.Scheme;
+import ch.cyberduck.core.*;
 import ch.cyberduck.core.exception.LoginCanceledException;
 import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.shared.DefaultFindFeature;
 import ch.cyberduck.core.shared.DefaultHomeFinderService;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DefaultX509TrustManager;
@@ -42,7 +30,6 @@ import org.junit.experimental.categories.Category;
 
 import java.util.Arrays;
 import java.util.EnumSet;
-import java.util.UUID;
 
 import static org.junit.Assert.*;
 
@@ -111,15 +98,12 @@ public class DriveDefaultListServiceTest {
                     }
                 }, new DisabledProgressListener()
         ).connect(session, PathCache.empty(), new DisabledCancelCallback());
-        final Path file = new Path(new DefaultHomeFinderService(session).find(), String.format("%s:name", UUID.randomUUID().toString()), EnumSet.of(Path.Type.file));
-        final Path folder = new Path(new DefaultHomeFinderService(session).find(), String.format("%s:name", UUID.randomUUID().toString()), EnumSet.of(Path.Type.directory));
+        final Path file = new Path(new DefaultHomeFinderService(session).find(), String.format("%s:name", new AlphanumericRandomStringService().random()), EnumSet.of(Path.Type.file));
+        final Path folder = new Path(new DefaultHomeFinderService(session).find(), String.format("%s:name", new AlphanumericRandomStringService().random()), EnumSet.of(Path.Type.directory));
         new DriveTouchFeature(session).touch(file, new TransferStatus());
         new DriveDirectoryFeature(session).mkdir(folder, null, new TransferStatus());
-        final AttributedList<Path> list = new DriveDefaultListService(session).list(new Path("/", EnumSet.of(Path.Type.directory, Path.Type.volume)), new DisabledListProgressListener());
-        assertNotNull(list);
-        assertFalse(list.isEmpty());
-        assertTrue(list.contains(file));
-        assertTrue(list.contains(folder));
+        assertTrue(new DefaultFindFeature(session).find(file));
+        assertTrue(new DefaultFindFeature(session).find(folder));
         new DriveDeleteFeature(session).delete(Arrays.asList(file, folder), new DisabledLoginCallback(), new Delete.DisabledCallback());
     }
 }
