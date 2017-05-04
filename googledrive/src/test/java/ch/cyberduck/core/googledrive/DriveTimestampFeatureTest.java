532f198612ef9353ed612b7f7d692cbaadaa6d4b
David Kocher <dkocher@iterate.ch>
2017-05-04 22:22:34 +0200

    Fix tests.
    Former-commit-id: 3b7ba61845facd56a38243ee9287e361247dc8fb

--- googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveTimestampFeatureTest.java
+++ googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveTimestampFeatureTest.java
@@ -18,7 +18,6 @@ package ch.cyberduck.core.googledrive;
 import ch.cyberduck.core.Credentials;
 import ch.cyberduck.core.DisabledCancelCallback;
 import ch.cyberduck.core.DisabledHostKeyCallback;
-import ch.cyberduck.core.DisabledListProgressListener;
 import ch.cyberduck.core.DisabledLoginCallback;
 import ch.cyberduck.core.DisabledPasswordStore;
 import ch.cyberduck.core.DisabledProgressListener;
@@ -30,6 +29,7 @@ import ch.cyberduck.core.PathCache;
 import ch.cyberduck.core.Scheme;
 import ch.cyberduck.core.exception.LoginCanceledException;
 import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.shared.DefaultAttributesFinderFeature;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DefaultX509TrustManager;
 import ch.cyberduck.core.transfer.TransferStatus;
@@ -77,7 +77,7 @@ public class DriveTimestampFeatureTest {
         new DriveMetadataFeature(session).setMetadata(test, Collections.singletonMap("test", "t"));
         final long modified = System.currentTimeMillis();
         new DriveTimestampFeature(session).setTimestamp(test, modified);
-        assertEquals(modified / 1000 * 1000, session.list(home, new DisabledListProgressListener()).get(test).attributes().getModificationDate(), 0L);
+        assertEquals(modified / 1000 * 1000, new DefaultAttributesFinderFeature(session).find(test).getModificationDate(), 0L);
         assertEquals(Collections.singletonMap("test", "t"), new DriveMetadataFeature(session).getMetadata(test));
         new DriveDeleteFeature(session).delete(Collections.<Path>singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
         session.close();
@@ -111,7 +111,7 @@ public class DriveTimestampFeatureTest {
         new DriveDirectoryFeature(session).mkdir(test, null, new TransferStatus());
         final long modified = System.currentTimeMillis();
         new DriveTimestampFeature(session).setTimestamp(test, modified);
-        assertEquals(modified / 1000 * 1000, session.list(home, new DisabledListProgressListener()).get(test).attributes().getModificationDate(), 0L);
+        assertEquals(modified / 1000 * 1000, new DefaultAttributesFinderFeature(session).find(test).getModificationDate(), 0L);
         new DriveDeleteFeature(session).delete(Collections.<Path>singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
         session.close();
     }
