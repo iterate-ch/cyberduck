8c453977f3e2a03657d6a893becc1bf98794a73c
David Kocher <dkocher@iterate.ch>
2017-05-15 11:53:09 +0200

    Add check for empty registry.

--- windows/src/main/csharp/ch/cyberduck/ui/controller/MainController.cs
+++ windows/src/main/csharp/ch/cyberduck/ui/controller/MainController.cs
@@ -811,7 +811,7 @@ namespace Ch.Cyberduck.Ui.Controller
             {
                 foreach (BrowserController c in Browsers)
                 {
-                    if (!c.IsMounted())
+                    if (c.getRegistry().isEmpty() && !c.IsMounted())
                     {
                         c.Invoke(delegate { c.View.BringToFront(); });
 
