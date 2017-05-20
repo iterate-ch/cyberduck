96df40ade691916a86fea7990d00000cf0c045cd
David Kocher <dkocher@iterate.ch>
2017-05-20 13:48:13 +0200

    Add failing test.

--- nio/src/test/java/ch/cyberduck/core/nio/LocalHomeFinderFeatureTest.java
+++ nio/src/test/java/ch/cyberduck/core/nio/LocalHomeFinderFeatureTest.java
@@ -20,20 +20,28 @@ import ch.cyberduck.core.DisabledHostKeyCallback;
 import ch.cyberduck.core.DisabledLoginCallback;
 import ch.cyberduck.core.DisabledPasswordStore;
 import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathCache;
 
 import org.junit.Test;
+
+import java.util.EnumSet;
 
 import static org.junit.Assert.assertEquals;
 
 public class LocalHomeFinderFeatureTest {
 
     @Test
-    public void find() throws Exception {
+    public void testFind() throws Exception {
         final LocalSession session = new LocalSession(new Host(new LocalProtocol(), new LocalProtocol().getDefaultHostname()));
         session.open(new DisabledHostKeyCallback());
         session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
         assertEquals(System.getProperty("user.home"), new LocalHomeFinderFeature(session).find().getAbsolute());
         session.close();
+    }
+
+    @Test
+    public void testFindUserHomeWindows() throws Exception {
+        assertEquals("C:\\Users\\username", new Path("C:\\Users\\username", EnumSet.of(Path.Type.directory)).getAbsolute());
     }
 }
