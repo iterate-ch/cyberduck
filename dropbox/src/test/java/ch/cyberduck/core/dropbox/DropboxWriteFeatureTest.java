31e2a65b3c09e80bef5f83dc1e580416a4ba96c4
David Kocher <dkocher@iterate.ch>
2017-05-24 22:57:46 +0200

    Fix test.

--- dropbox/src/test/java/ch/cyberduck/core/dropbox/DropboxWriteFeatureTest.java
+++ dropbox/src/test/java/ch/cyberduck/core/dropbox/DropboxWriteFeatureTest.java
@@ -89,7 +89,6 @@ public class DropboxWriteFeatureTest {
         final OutputStream out = write.write(test, status, new DisabledConnectionCallback());
         assertNotNull(out);
         new StreamCopier(new TransferStatus(), new TransferStatus()).transfer(new ByteArrayInputStream(content), out);
-        test.attributes().setVersionId(new DropboxFileIdProvider(session).getFileid(test));
         assertTrue(new DropboxFindFeature(session).find(test));
         assertEquals(content.length, session.list(test.getParent(), new DisabledListProgressListener()).get(test).attributes().getSize());
         assertEquals(content.length, write.append(test, status.getLength(), PathCache.empty()).size, 0L);
@@ -150,7 +149,6 @@ public class DropboxWriteFeatureTest {
         final OutputStream out = write.write(test, status, new DisabledConnectionCallback());
         assertNotNull(out);
         new StreamCopier(new TransferStatus(), new TransferStatus()).transfer(new ByteArrayInputStream(content), out);
-        test.attributes().setVersionId(new DropboxFileIdProvider(session).getFileid(test));
         assertTrue(new DropboxFindFeature(session).find(test));
         assertEquals(content.length, session.list(test.getParent(), new DisabledListProgressListener()).get(test).attributes().getSize());
         assertEquals(content.length, write.append(test, status.getLength(), PathCache.empty()).size, 0L);
