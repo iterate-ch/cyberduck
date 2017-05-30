f19f097bbe85719af866ef98628c28208f69da86
David Kocher <dkocher@iterate.ch>
2017-05-30 11:41:58 +0200

    Fix return type to include placeholder type.

--- backblaze/src/main/java/ch/cyberduck/core/b2/B2DirectoryFeature.java
+++ backblaze/src/main/java/ch/cyberduck/core/b2/B2DirectoryFeature.java
@@ -15,11 +15,13 @@ package ch.cyberduck.core.b2;
  * GNU General Public License for more details.
  */
 
+import ch.cyberduck.core.AbstractPath;
 import ch.cyberduck.core.Acl;
 import ch.cyberduck.core.DefaultIOExceptionMappingService;
 import ch.cyberduck.core.DisabledConnectionCallback;
 import ch.cyberduck.core.MimeTypeService;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
 import ch.cyberduck.core.PathContainerService;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.Directory;
@@ -33,6 +35,7 @@ import ch.cyberduck.core.transfer.TransferStatus;
 import org.apache.commons.io.input.NullInputStream;
 
 import java.io.IOException;
+import java.util.EnumSet;
 
 import synapticloop.b2.BucketType;
 import synapticloop.b2.exception.B2ApiException;
@@ -75,9 +78,12 @@ public class B2DirectoryFeature implements Directory<BaseB2Response> {
                     status.setChecksum(writer.checksum().compute(new NullInputStream(0L), status));
                 }
                 status.setMime(MimeTypeService.DEFAULT_CONTENT_TYPE);
-                final StatusOutputStream<BaseB2Response> out = writer.write(folder, status, new DisabledConnectionCallback());
+                final EnumSet<AbstractPath.Type> type = EnumSet.copyOf(folder.getType());
+                type.add(Path.Type.placeholder);
+                final Path placeholder = new Path(folder.getParent(), folder.getName(), type, new PathAttributes(folder.attributes()));
+                final StatusOutputStream<BaseB2Response> out = writer.write(placeholder, status, new DisabledConnectionCallback());
                 new DefaultStreamCloser().close(out);
-                return folder;
+                return placeholder;
             }
         }
         catch(B2ApiException e) {
