83591390a179b92c5a74a885eac0a1c80e26dfd1
David Kocher <dkocher@iterate.ch>
2017-06-02 15:55:56 +0200

    Add test.

--- cryptomator/src/test/java/ch/cyberduck/core/cryptomator/impl/CryptoVaultTest.java
+++ cryptomator/src/test/java/ch/cyberduck/core/cryptomator/impl/CryptoVaultTest.java
@@ -95,12 +95,14 @@ public class CryptoVaultTest {
         assertNotSame(home, vault.encrypt(session, home));
         assertEquals(vault.encrypt(session, home), vault.encrypt(session, home));
         final Path directory = new Path(home, "dir", EnumSet.of((Path.Type.directory)));
+        assertNull(directory.attributes().getVault());
+        assertEquals(home, vault.encrypt(session, directory).attributes().getVault());
+        assertEquals(home, directory.attributes().getVault());
         assertEquals(vault.encrypt(session, directory), vault.encrypt(session, directory));
         assertEquals(new Path(home, "dir", EnumSet.of(Path.Type.directory, Path.Type.decrypted)), vault.decrypt(session, vault.encrypt(session, directory, true)));
         assertNotEquals(vault.encrypt(session, directory), vault.encrypt(session, directory, true));
         assertEquals(vault.encrypt(session, directory).attributes().getDirectoryId(), vault.encrypt(session, directory).attributes().getDirectoryId());
         assertEquals(vault.encrypt(session, vault.encrypt(session, directory)).attributes().getDirectoryId(), vault.encrypt(session, vault.encrypt(session, directory)).attributes().getDirectoryId());
-        assertNull(vault.encrypt(session, directory, true).attributes().getDirectoryId());
         assertNull(vault.encrypt(session, directory, true).attributes().getDirectoryId());
         assertNull(vault.encrypt(session, vault.encrypt(session, directory), true).attributes().getDirectoryId());
         assertNotEquals(vault.encrypt(session, directory).attributes().getDirectoryId(), vault.encrypt(session, directory, true).attributes().getDirectoryId());
