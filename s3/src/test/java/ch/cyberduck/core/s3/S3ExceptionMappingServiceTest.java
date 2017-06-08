a0e22cbfca932709fe0570c124a3464d33dbbe22
David Kocher <dkocher@iterate.ch>
2017-06-08 21:51:26 +0200

    Fix tests.

--- s3/src/test/java/ch/cyberduck/core/s3/S3ExceptionMappingServiceTest.java
+++ s3/src/test/java/ch/cyberduck/core/s3/S3ExceptionMappingServiceTest.java
@@ -69,10 +69,10 @@ public class S3ExceptionMappingServiceTest {
 
     @Test
     public void testMapping() {
-        assertEquals("Message.", new S3ExceptionMappingService().map(new ServiceException("message")).getDetail());
-        assertEquals("Exceeded 403 retry limit (1).", new S3ExceptionMappingService().map(
+        assertEquals("Message. Please contact your web hosting service provider for assistance.", new S3ExceptionMappingService().map(new ServiceException("message")).getDetail());
+        assertEquals("Exceeded 403 retry limit (1). Please contact your web hosting service provider for assistance.", new S3ExceptionMappingService().map(
                 new ServiceException("Exceeded 403 retry limit (1).")).getDetail());
-        assertEquals("Connection failed", new S3ExceptionMappingService().map(
+        assertEquals("Interoperability failure", new S3ExceptionMappingService().map(
                 new ServiceException("Exceeded 403 retry limit (1).")).getMessage());
     }
 
@@ -88,7 +88,7 @@ public class S3ExceptionMappingServiceTest {
     public void testCustomMessage() {
         assertEquals("Custom.",
                 new S3ExceptionMappingService().map("custom", new ServiceException("message")).getMessage());
-        assertEquals("Message.",
+        assertEquals("Message. Please contact your web hosting service provider for assistance.",
                 new S3ExceptionMappingService().map("custom", new ServiceException("message")).getDetail());
     }
 
@@ -96,7 +96,7 @@ public class S3ExceptionMappingServiceTest {
     public void testIAMFailure() {
         assertEquals("The IAM policy must allow the action s3:GetBucketLocation on the resource arn:aws:s3:::endpoint-9a527d70-d432-4601-b24b-735e721b82c9.",
                 new S3ExceptionMappingService().map("The IAM policy must allow the action s3:GetBucketLocation on the resource arn:aws:s3:::endpoint-9a527d70-d432-4601-b24b-735e721b82c9", new ServiceException("message")).getMessage());
-        assertEquals("Message.",
+        assertEquals("Message. Please contact your web hosting service provider for assistance.",
                 new S3ExceptionMappingService().map("The IAM policy must allow the action s3:GetBucketLocation on the resource arn:aws:s3:::endpoint-9a527d70-d432-4601-b24b-735e721b82c9", new ServiceException("message")).getDetail());
     }
 
@@ -117,7 +117,7 @@ public class S3ExceptionMappingServiceTest {
 
     @Test
     public void testWrapped() {
-        assertEquals("Access Denied.", new S3ExceptionMappingService().map(new ServiceException(new S3ServiceException("m",
+        assertEquals("Access Denied. Please contact your web hosting service provider for assistance.", new S3ExceptionMappingService().map(new ServiceException(new S3ServiceException("m",
                         "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>D84EDAE486BD2D71</RequestId><HostId>tVNWw2hK+FVpFnWUVf2LdDM6rgtjo/cibINRUVc/HpqMZbgNTg311LSltHYvRQdX</HostId></Error>"))).getDetail()
         );
     }
