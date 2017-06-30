4edb15ce036f302f20af09bb73f2d77a4465a000
Yves Langisch <yla@iterate.ch>
2017-06-30 15:31:44 +0200

    Change room.

--- sds/src/test/java/ch/cyberduck/core/sds/SDSWriteFeatureTest.java
+++ sds/src/test/java/ch/cyberduck/core/sds/SDSWriteFeatureTest.java
@@ -59,7 +59,7 @@ public class SDSWriteFeatureTest {
         final SDSSession session = new SDSSession(host, new DisabledX509TrustManager(), new DefaultX509KeyManager());
         session.open(new DisabledHostKeyCallback());
         session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
-        final Path room = new Path("MD", EnumSet.of(Path.Type.directory, Path.Type.volume));
+        final Path room = new Path("CD-TEST", EnumSet.of(Path.Type.directory, Path.Type.volume));
         final Local local = new Local(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
         final byte[] content = RandomUtils.nextBytes(32768);
         final TransferStatus status = new TransferStatus();
