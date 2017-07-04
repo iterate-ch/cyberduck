2e01b86e16e8f1ee0311b15f404353b033afe221
David Kocher <dkocher@iterate.ch>
2017-07-04 16:48:40 +0200

    Run scheduler on login.

--- core/src/main/java/ch/cyberduck/core/preferences/Preferences.java
+++ core/src/main/java/ch/cyberduck/core/preferences/Preferences.java
@@ -1067,6 +1067,8 @@ public abstract class Preferences {
 
         defaults.put("cryptomator.enable", String.valueOf(true));
         defaults.put("cryptomator.vault.autodetect", String.valueOf(true));
+
+        defaults.put("sds.encryption.missingkeys.scheduler.period", String.valueOf(300000)); // 5 minutes
     }
 
     protected void setLogging() {
