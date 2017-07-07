9330e3233396f0e6bcce2da2baf6b24608332b46
David Kocher <dkocher@iterate.ch>
2017-07-07 10:55:37 +0200

    Return default identifier for bundled profiles.

--- core/src/main/java/ch/cyberduck/core/Profile.java
+++ core/src/main/java/ch/cyberduck/core/Profile.java
@@ -123,6 +123,9 @@ public class Profile implements Protocol, Serializable {
 
     @Override
     public String getProvider() {
+        if(this.isBundled()) {
+            return parent.getProvider();
+        }
         final String v = this.value("Vendor");
         if(StringUtils.isBlank(v)) {
             return parent.getProvider();
