17a0ed15bbf1ad5a3ccd62905e1d88ce516ec637
David Kocher <dkocher@iterate.ch>
2017-07-14 22:12:07 +0200

    Extract read feature for decryption.

--- sds/src/main/java/ch/cyberduck/core/sds/SDSReadFeature.java
+++ sds/src/main/java/ch/cyberduck/core/sds/SDSReadFeature.java
@@ -16,28 +16,17 @@ package ch.cyberduck.core.sds;
  */
 
 import ch.cyberduck.core.ConnectionCallback;
-import ch.cyberduck.core.DefaultIOExceptionMappingService;
 import ch.cyberduck.core.DisabledListProgressListener;
+import ch.cyberduck.core.PasswordCallback;
 import ch.cyberduck.core.Path;
-import ch.cyberduck.core.exception.AccessDeniedException;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.features.Read;
 import ch.cyberduck.core.http.HttpRange;
 import ch.cyberduck.core.sds.io.swagger.client.ApiException;
-import ch.cyberduck.core.sds.io.swagger.client.model.FileKey;
 import ch.cyberduck.core.sds.swagger.ExtendedNodesApi;
-import ch.cyberduck.core.sds.triplecrypt.CryptoInputStream;
 import ch.cyberduck.core.transfer.TransferStatus;
 
-import java.io.IOException;
 import java.io.InputStream;
-
-import eu.ssp_europe.sds.crypto.Crypto;
-import eu.ssp_europe.sds.crypto.CryptoException;
-import eu.ssp_europe.sds.crypto.FileDecryptionCipher;
-import eu.ssp_europe.sds.crypto.model.EncryptedFileKey;
-import eu.ssp_europe.sds.crypto.model.PlainFileKey;
-import eu.ssp_europe.sds.crypto.model.UserPrivateKey;
 
 public class SDSReadFeature implements Read {
 
@@ -48,7 +37,7 @@ public class SDSReadFeature implements Read {
     }
 
     @Override
-    public InputStream read(final Path file, final TransferStatus status, final ConnectionCallback callback) throws BackgroundException {
+    public InputStream read(final Path file, final TransferStatus status, final ConnectionCallback connectionCallback, final PasswordCallback passwordCallback) throws BackgroundException {
         try {
             final String header;
             if(status.isAppend()) {
@@ -63,35 +52,12 @@ public class SDSReadFeature implements Read {
             else {
                 header = null;
             }
-            final InputStream in = new ExtendedNodesApi(session.getClient()).getFileData(session.getToken(),
+            return new ExtendedNodesApi(session.getClient()).getFileData(session.getToken(),
                     Long.parseLong(new SDSNodeIdProvider(session).getFileid(file, new DisabledListProgressListener())),
                     header, true);
-            if(file.getType().contains(Path.Type.encrypted)) {
-                final FileKey key = new ExtendedNodesApi(session.getClient()).getUserFileKey(session.getToken(),
-                        Long.parseLong(new SDSNodeIdProvider(session).getFileid(file, new DisabledListProgressListener())));
-                final EncryptedFileKey encryptedFileKey = new EncryptedFileKey();
-                encryptedFileKey.setIv(key.getIv());
-                encryptedFileKey.setKey(key.getKey());
-                encryptedFileKey.setTag(key.getTag());
-                encryptedFileKey.setVersion(key.getVersion());
-                final UserPrivateKey privateKey = new UserPrivateKey();
-                privateKey.setPrivateKey(session.getKeys().getPrivateKeyContainer().getPrivateKey());
-                privateKey.setVersion(session.getKeys().getPrivateKeyContainer().getVersion());
-                // TODO PasswordCallback
-                final PlainFileKey plainFileKey = Crypto.decryptFileKey(encryptedFileKey, privateKey,
-                        "USER_PASSWORD");
-                return new CryptoInputStream(in, new FileDecryptionCipher(plainFileKey), status.getLength() + status.getOffset());
-            }
-            return in;
         }
         catch(ApiException e) {
             throw new SDSExceptionMappingService().map("Download {0} failed", e, file);
-        }
-        catch(CryptoException e) {
-            throw new AccessDeniedException(e.getMessage(), e);
-        }
-        catch(IOException e) {
-            throw new DefaultIOExceptionMappingService().map(e);
         }
     }
 
