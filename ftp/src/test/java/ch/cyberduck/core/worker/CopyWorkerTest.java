fc02b51779a2f28d6dd240c5d9cd332f3402cb2d
David Kocher <dkocher@iterate.ch>
2017-07-19 16:14:33 +0200

    Add test.

---
+++ ftp/src/test/java/ch/cyberduck/core/worker/CopyWorkerTest.java
@@ -0,0 +1,188 @@
+package ch.cyberduck.core.worker;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AlphanumericRandomStringService;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledCancelCallback;
+import ch.cyberduck.core.DisabledHostKeyCallback;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.DisabledProgressListener;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathCache;
+import ch.cyberduck.core.Session;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.ftp.FTPDirectoryFeature;
+import ch.cyberduck.core.ftp.FTPSession;
+import ch.cyberduck.core.ftp.FTPTLSProtocol;
+import ch.cyberduck.core.ftp.FTPWriteFeature;
+import ch.cyberduck.core.pool.SessionPool;
+import ch.cyberduck.core.shared.DefaultFindFeature;
+import ch.cyberduck.core.shared.DefaultHomeFinderService;
+import ch.cyberduck.core.shared.DefaultTouchFeature;
+import ch.cyberduck.core.shared.DefaultUploadFeature;
+import ch.cyberduck.core.threading.BackgroundActionState;
+import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.core.vault.VaultRegistry;
+import ch.cyberduck.test.IntegrationTest;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+
+import static org.junit.Assert.assertTrue;
+
+@Category(IntegrationTest.class)
+public class CopyWorkerTest {
+
+    @Test
+    public void testCopyFile() throws Exception {
+        final Host host = new Host(new FTPTLSProtocol(), "test.cyberduck.ch", new Credentials(
+                System.getProperties().getProperty("ftp.user"), System.getProperties().getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
+        final Path home = new DefaultHomeFinderService(session).find();
+        final Path source = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file));
+        final Path target = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file));
+        new DefaultTouchFeature<Integer>(new DefaultUploadFeature<Integer>(new FTPWriteFeature(session))).touch(source, new TransferStatus());
+        assertTrue(new DefaultFindFeature(session).find(source));
+        final FTPSession copySession = new FTPSession(host);
+        copySession.open(new DisabledHostKeyCallback());
+        copySession.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
+        final CopyWorker worker = new CopyWorker(Collections.singletonMap(source, target), new TestSessionPool(copySession), new DisabledProgressListener());
+        worker.run(session);
+        assertTrue(new DefaultFindFeature(session).find(source));
+        assertTrue(new DefaultFindFeature(session).find(target));
+        new DeleteWorker(new DisabledLoginCallback(), Arrays.asList(source, target), PathCache.empty(), new DisabledProgressListener()).run(session);
+        session.close();
+    }
+
+    @Test
+    public void testCopyFileToDirectory() throws Exception {
+        final Host host = new Host(new FTPTLSProtocol(), "test.cyberduck.ch", new Credentials(
+                System.getProperties().getProperty("ftp.user"), System.getProperties().getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
+        final Path home = new DefaultHomeFinderService(session).find();
+        final Path sourceFile = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file));
+        new DefaultTouchFeature<Integer>(new DefaultUploadFeature<Integer>(new FTPWriteFeature(session))).touch(sourceFile, new TransferStatus());
+        assertTrue(new DefaultFindFeature(session).find(sourceFile));
+        final Path targetFolder = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory));
+        final Path targetFile = new Path(targetFolder, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file));
+        new FTPDirectoryFeature(session).mkdir(targetFolder, null, new TransferStatus());
+        assertTrue(new DefaultFindFeature(session).find(targetFolder));
+        // copy file into vault
+        final FTPSession copySession = new FTPSession(host);
+        copySession.open(new DisabledHostKeyCallback());
+        copySession.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
+        final CopyWorker worker = new CopyWorker(Collections.singletonMap(sourceFile, targetFile), new TestSessionPool(copySession), new DisabledProgressListener());
+        worker.run(session);
+        assertTrue(new DefaultFindFeature(session).find(sourceFile));
+        assertTrue(new DefaultFindFeature(session).find(targetFile));
+        new DeleteWorker(new DisabledLoginCallback(), Arrays.asList(sourceFile, targetFolder), PathCache.empty(), new DisabledProgressListener()).run(session);
+        session.close();
+    }
+
+    @Test
+    public void testCopyDirectory() throws Exception {
+        final Host host = new Host(new FTPTLSProtocol(), "test.cyberduck.ch", new Credentials(
+                System.getProperties().getProperty("ftp.user"), System.getProperties().getProperty("ftp.password")
+        ));
+        final FTPSession session = new FTPSession(host);
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
+        final Path home = new DefaultHomeFinderService(session).find();
+        final Path folder = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory));
+        final Path sourceFile = new Path(folder, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file));
+        new FTPDirectoryFeature(session).mkdir(folder, null, new TransferStatus());
+        new DefaultTouchFeature<Integer>(new DefaultUploadFeature<Integer>(new FTPWriteFeature(session))).touch(sourceFile, new TransferStatus());
+        assertTrue(new DefaultFindFeature(session).find(folder));
+        assertTrue(new DefaultFindFeature(session).find(sourceFile));
+        // move directory into vault
+        final Path targetFolder = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory));
+        final Path targetFile = new Path(targetFolder, sourceFile.getName(), EnumSet.of(Path.Type.file));
+        final FTPSession copySession = new FTPSession(host);
+        copySession.open(new DisabledHostKeyCallback());
+        copySession.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback(), PathCache.empty());
+        final CopyWorker worker = new CopyWorker(Collections.singletonMap(folder, targetFolder), new TestSessionPool(copySession), new DisabledProgressListener());
+        worker.run(session);
+        assertTrue(new DefaultFindFeature(session).find(targetFolder));
+        assertTrue(new DefaultFindFeature(session).find(targetFile));
+        assertTrue(new DefaultFindFeature(session).find(folder));
+        assertTrue(new DefaultFindFeature(session).find(sourceFile));
+        new DeleteWorker(new DisabledLoginCallback(), Arrays.asList(folder, targetFolder), PathCache.empty(), new DisabledProgressListener()).run(session);
+        session.close();
+    }
+
+    private static class TestSessionPool implements SessionPool {
+        private final Session<?> session;
+
+        public TestSessionPool(final Session<?> session) {
+            this.session = session;
+        }
+
+        @Override
+        public Session<?> borrow(final BackgroundActionState callback) throws BackgroundException {
+            return session;
+        }
+
+        @Override
+        public void release(final Session<?> session, final BackgroundException failure) {
+        }
+
+        @Override
+        public void evict() {
+        }
+
+        @Override
+        public Host getHost() {
+            return session.getHost();
+        }
+
+        @Override
+        public PathCache getCache() {
+            return PathCache.empty();
+        }
+
+        @Override
+        public VaultRegistry getVault() {
+            return VaultRegistry.DISABLED;
+        }
+
+        @Override
+        public Session.State getState() {
+            return Session.State.open;
+        }
+
+        @Override
+        public <T> T getFeature(final Class<T> type) {
+            return session.getFeature(type);
+        }
+
+        @Override
+        public void shutdown() {
+        }
+    }
+}
