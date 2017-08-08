4a03a9984a1816443dd30d002c0c66629df30af6
David Kocher <dkocher@iterate.ch>
2017-08-08 12:09:23 +0200

    Set checkbox state from property.

--- osx/src/main/java/ch/cyberduck/ui/cocoa/controller/PasswordController.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/controller/PasswordController.java
@@ -93,7 +93,7 @@ public class PasswordController extends AlertController {
             keychainCheckbox.setAction(Foundation.selector("keychainCheckboxClicked:"));
             keychainCheckbox.setTarget(this.id());
             keychainCheckbox.setButtonType(NSButton.NSSwitchButton);
-            keychainCheckbox.setState(NSCell.NSOffState);
+            keychainCheckbox.setState(credentials.isSaved() ? NSCell.NSOnState : NSCell.NSOffState);
             keychainCheckbox.sizeToFit();
             // Override accessory view with location menu added
             keychainCheckbox.setFrameOrigin(new NSPoint(0, this.getFrame(alert, view).size.height.doubleValue()));
--- windows/src/main/csharp/ch/cyberduck/ui/controller/PromptPasswordController.cs
+++ windows/src/main/csharp/ch/cyberduck/ui/controller/PromptPasswordController.cs
@@ -43,7 +43,7 @@ namespace Ch.Cyberduck.Ui.Controller
                 View.Reason = reason;
                 View.OkButtonText = LocaleFactory.localizedString("Continue", "Credentials");
                 View.IconView = IconCache.Instance.IconForName(options.icon(), 64);
-                View.SavePassword = false;
+                View.SavePassword = credentials.isSaved();
                 View.ValidateInput += ValidateInputEventHandler;
                 if (DialogResult.Cancel == View.ShowDialog(_browser.View))
                 {
