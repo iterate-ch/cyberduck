b336286f18cb4a59b399336bbb20132739ee7adb
David Kocher <dkocher@iterate.ch>
2017-08-11 11:57:02 +0200

    Revert to return null for unknown identifier.

--- core/src/main/java/ch/cyberduck/core/ProtocolFactory.java
+++ core/src/main/java/ch/cyberduck/core/ProtocolFactory.java
@@ -163,12 +163,9 @@ public final class ProtocolFactory {
         );
         if(null == match) {
             if(enabled.isEmpty()) {
-                log.error("List of registered protocols is empty");
-                return null;
+                log.error(String.format("List of registered protocols in %s is empty", this));
             }
-            final Protocol next = enabled.iterator().next();
-            log.warn(String.format("Missing registered protocol for identifier %s. Return first in list %s", identifier, next));
-            return next;
+            log.error(String.format("Missing registered protocol for identifier %s", identifier));
         }
         return match;
     }
@@ -229,5 +226,14 @@ public final class ProtocolFactory {
         public Pattern toPattern() {
             return Pattern.compile(".*\\.cyberduckprofile");
         }
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ProtocolFactory{");
+        sb.append("registered=").append(registered);
+        sb.append(", bundle=").append(bundle);
+        sb.append('}');
+        return sb.toString();
     }
 }
