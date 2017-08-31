808967b627d57438b8e8d31b0f4604e2b30ebbee
David Kocher <dkocher@iterate.ch>
2017-08-31 09:56:07 +0200

    Add test.

---
+++ googledrive/src/test/java/ch/cyberduck/core/googledrive/DriveAttributesFinderFeatureTest.java
@@ -0,0 +1,70 @@
+package ch.cyberduck.core.googledrive;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AlphanumericRandomStringService;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.test.IntegrationTest;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+@Category(IntegrationTest.class)
+public class DriveAttributesFinderFeatureTest extends AbstractDriveTest {
+
+    @Test(expected = NotfoundException.class)
+    public void testNotFound() throws Exception {
+        final DriveAttributesFinderFeature f = new DriveAttributesFinderFeature(session);
+        f.find(new Path(new DriveHomeFinderService(session).find(), UUID.randomUUID().toString(), EnumSet.of(Path.Type.file)));
+    }
+
+    @Test
+    public void testFind() throws Exception {
+        final Path test = new Path(new DriveHomeFinderService(session).find(), UUID.randomUUID().toString() + ".txt", EnumSet.of(Path.Type.file));
+        new DriveTouchFeature(session).touch(test, new TransferStatus());
+        final DriveAttributesFinderFeature f = new DriveAttributesFinderFeature(session);
+        final PathAttributes attributes = f.find(test);
+        assertEquals(0L, attributes.getSize());
+        assertNotNull(attributes.getVersionId());
+        new DriveDeleteFeature(session).delete(Collections.singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
+        session.close();
+    }
+
+
+    @Test
+    public void testFindDirectory() throws Exception {
+        final Path file = new Path(new DriveHomeFinderService(session).find(), new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory));
+        new DriveDirectoryFeature(session).mkdir(file, null, new TransferStatus());
+        final PathAttributes attributes = new DriveAttributesFinderFeature(session).find(file);
+        assertNotNull(attributes);
+        assertEquals(-1L, attributes.getSize());
+        assertNotEquals(-1L, attributes.getCreationDate());
+        assertNotEquals(-1L, attributes.getModificationDate());
+        assertNotNull(attributes.getVersionId());
+        new DriveDeleteFeature(session).delete(Collections.singletonList(file), new DisabledLoginCallback(), new Delete.DisabledCallback());
+    }
+}
