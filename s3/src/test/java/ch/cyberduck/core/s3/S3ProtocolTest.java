c96fd4a52b054ae2090a91742c956e33b7cac1ff
David Kocher <dkocher@iterate.ch>
2017-10-03 10:29:38 +0200

    Add tests.

--- s3/src/test/java/ch/cyberduck/core/s3/S3ProtocolTest.java
+++ s3/src/test/java/ch/cyberduck/core/s3/S3ProtocolTest.java
@@ -1,6 +1,10 @@
 package ch.cyberduck.core.s3;
 
+import ch.cyberduck.core.Scheme;
+
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -18,4 +22,9 @@ public class S3ProtocolTest {
         assertTrue(new S3Protocol().isPortConfigurable());
     }
 
+    @Test
+    public void testSchemes() {
+        assertTrue(Arrays.asList(new S3Protocol().getSchemes()).contains(Scheme.s3));
+        assertTrue(Arrays.asList(new S3Protocol().getSchemes()).contains(Scheme.https));
+    }
 }
