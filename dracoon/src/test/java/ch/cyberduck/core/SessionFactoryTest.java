047c2eefd799e00e1db4ad839f5c8ab1a848a51d
David Kocher <dkocher@iterate.ch>
2017-10-11 17:05:04 +0200

    Add annotation.

--- dracoon/src/test/java/ch/cyberduck/core/SessionFactoryTest.java
+++ dracoon/src/test/java/ch/cyberduck/core/SessionFactoryTest.java
@@ -18,11 +18,14 @@ package ch.cyberduck.core;
 import ch.cyberduck.core.sds.SDSProtocol;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DefaultX509TrustManager;
+import ch.cyberduck.test.IntegrationTest;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.junit.Assert.assertNotNull;
 
+@Category(IntegrationTest.class)
 public class SessionFactoryTest {
 
     @Test
