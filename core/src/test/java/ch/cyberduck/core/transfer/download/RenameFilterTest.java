4ac74515151552284bd9fe8ba0770680b45c5f2e
David Kocher <dkocher@iterate.ch>
2017-10-16 11:18:53 +0200

    Add test.

--- core/src/test/java/ch/cyberduck/core/transfer/download/RenameFilterTest.java
+++ core/src/test/java/ch/cyberduck/core/transfer/download/RenameFilterTest.java
@@ -1,5 +1,6 @@
 package ch.cyberduck.core.transfer.download;
 
+import ch.cyberduck.core.AsciiRandomStringService;
 import ch.cyberduck.core.DisabledProgressListener;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.NullLocal;
@@ -21,15 +22,44 @@ public class RenameFilterTest {
     @Test
     public void testPrepare() throws Exception {
         RenameFilter f = new RenameFilter(new DisabledDownloadSymlinkResolver(), new NullSession(new Host(new TestProtocol())));
-        final NullLocal local = new NullLocal("/tmp/t") {
+        final String name = new AsciiRandomStringService().random();
+        final NullLocal local = new NullLocal("/tmp", name) {
             @Override
             public boolean exists() {
-                return "t".equals(this.getName());
+                return name.equals(this.getName());
             }
         };
-        final Path t = new Path("t", EnumSet.of(Path.Type.file));
+        final Path t = new Path(name, EnumSet.of(Path.Type.file));
         final TransferStatus status = f.prepare(t, local, new TransferStatus().exists(true), new DisabledProgressListener());
         assertNotNull(status.getRename().local);
-        assertEquals(status.getRename().local.getName(), "t-1");
+        assertEquals(String.format("%s-1", name), status.getRename().local.getName());
+    }
+
+    @Test
+    public void testDirectoryDownload() throws Exception {
+        RenameFilter f = new RenameFilter(new DisabledDownloadSymlinkResolver(), new NullSession(new Host(new TestProtocol())));
+        final String name = new AsciiRandomStringService().random();
+        final NullLocal local = new NullLocal("/tmp", name) {
+            @Override
+            public boolean exists() {
+                return name.equals(this.getName());
+            }
+
+            @Override
+            public boolean isFile() {
+                return false;
+            }
+
+            @Override
+            public boolean isDirectory() {
+                return true;
+            }
+        };
+        final Path directory = new Path("t", EnumSet.of(Path.Type.directory));
+        final Path file = new Path(directory, name, EnumSet.of(Path.Type.file));
+        final TransferStatus directoryStatus = f.prepare(directory, local, new TransferStatus().exists(true), new DisabledProgressListener());
+        final TransferStatus fileStatus = f.prepare(file, new NullLocal(local, "f"), directoryStatus, new DisabledProgressListener());
+        assertNotNull(fileStatus.getRename().local);
+        assertEquals(String.format("/tmp/t-1/%s", name), fileStatus.getRename().local.getAbsolute());
     }
 }
