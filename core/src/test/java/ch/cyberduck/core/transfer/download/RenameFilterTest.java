99e528edf3a34700743b63686c7ebedd1b8070e1
David Kocher <dkocher@iterate.ch>
2017-10-16 11:07:49 +0200

    Fix test.

--- core/src/test/java/ch/cyberduck/core/transfer/download/RenameFilterTest.java
+++ core/src/test/java/ch/cyberduck/core/transfer/download/RenameFilterTest.java
@@ -13,21 +13,23 @@ import org.junit.Test;
 
 import java.util.EnumSet;
 
-import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class RenameFilterTest {
 
     @Test
     public void testPrepare() throws Exception {
         RenameFilter f = new RenameFilter(new DisabledDownloadSymlinkResolver(), new NullSession(new Host(new TestProtocol())));
-        final NullLocal local = new NullLocal("t") {
+        final NullLocal local = new NullLocal("/tmp/t") {
             @Override
             public boolean exists() {
-                return this.getName().equals("/t");
+                return "t".equals(this.getName());
             }
         };
         final Path t = new Path("t", EnumSet.of(Path.Type.file));
-        f.prepare(t, local, new TransferStatus(), new DisabledProgressListener());
-        assertNotSame("/t", t.getName());
+        final TransferStatus status = f.prepare(t, local, new TransferStatus().exists(true), new DisabledProgressListener());
+        assertNotNull(status.getRename().local);
+        assertEquals(status.getRename().local.getName(), "t-1");
     }
 }
