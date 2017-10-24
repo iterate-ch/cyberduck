575c0a3ebb992ad2861c91f839613fd22ae0605a
David Kocher <dkocher@iterate.ch>
2017-10-24 22:38:34 +0200

    Typo.

--- core/src/main/java/ch/cyberduck/core/AuthenticationProvider.java
+++ core/src/main/java/ch/cyberduck/core/AuthenticationProvider.java
@@ -3,7 +3,7 @@ package ch.cyberduck.core;
 /*
  * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
  * https://cyberduck.io/
- *q
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
