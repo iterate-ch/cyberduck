2c2f7ae70bb303212f7b60c5e29e073f84de0bb3
David Kocher <dkocher@iterate.ch>
2017-11-06 22:09:48 +0100

    Add test.

---
+++ dracoon/src/test/java/ch/cyberduck/core/HostParserTest.java
@@ -0,0 +1,40 @@
+package ch.cyberduck.core;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.sds.SDSProtocol;
+import ch.cyberduck.core.serializer.impl.dd.ProfilePlistReader;
+
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertTrue;
+
+public class HostParserTest {
+
+    @Test
+    public void testParseUsernameFromUrlEvent() throws Exception {
+        final ProtocolFactory factory = new ProtocolFactory(Collections.singleton(new SDSProtocol()));
+        final Profile profile = new ProfilePlistReader(factory).read(
+            new Local("../profiles/default/DRACOON (Email Address).cyberduckprofile"));
+        assertTrue(new Host(new SDSProtocol(), "duck.ssp-europe.eu", 443, "/cyberduck-test/key", new Credentials(
+            System.getProperties().getProperty("sds.user")
+        ))
+            .compareTo(new HostParser(factory, profile).get(
+                "dracoon://post%40iterate.ch@duck.ssp-europe.eu/key")) == 0);
+    }
+}
