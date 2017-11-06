d330dae3f6f81dd86c82ef8b914d73dcaa3158a0
David Kocher <dkocher@iterate.ch>
2017-11-06 15:42:20 +0100

    Add test.

---
+++ s3/src/test/java/ch/cyberduck/core/worker/TouchWorkerTest.java
@@ -0,0 +1,65 @@
+package ch.cyberduck.core.worker;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AlphanumericRandomStringService;
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.DisabledCancelCallback;
+import ch.cyberduck.core.DisabledHostKeyCallback;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.features.Encryption;
+import ch.cyberduck.core.s3.S3EncryptionFeature;
+import ch.cyberduck.core.s3.S3Protocol;
+import ch.cyberduck.core.s3.S3Session;
+
+import org.junit.Test;
+
+import java.util.EnumSet;
+
+import static org.junit.Assert.assertEquals;
+
+public class TouchWorkerTest {
+
+    @Test
+    public void testSuccessWithServerSideEncryptionBucketPolicy() throws Exception {
+        final Host host = new Host(new S3Protocol(), new S3Protocol().getDefaultHostname(), new Credentials(
+            System.getProperties().getProperty("s3.key"), System.getProperties().getProperty("s3.secret")
+        ));
+        final S3Session session = new S3Session(host) {
+            @Override
+            @SuppressWarnings("unchecked")
+            public <T> T _getFeature(final Class<T> type) {
+                if(type == Encryption.class) {
+                    return (T) new S3EncryptionFeature(this) {
+                        @Override
+                        public Algorithm getDefault(final Path file) {
+                            return S3EncryptionFeature.SSE_AES256;
+                        }
+                    };
+                }
+                return super._getFeature(type);
+            }
+        };
+        session.open(new DisabledHostKeyCallback());
+        session.login(new DisabledPasswordStore(), new DisabledLoginCallback(), new DisabledCancelCallback());
+        final Path home = new Path("sse-test-us-east-1-cyberduck", EnumSet.of(Path.Type.volume));
+        final Path test = new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file));
+        assertEquals(test, new TouchWorker(test).run(session));
+    }
+}
