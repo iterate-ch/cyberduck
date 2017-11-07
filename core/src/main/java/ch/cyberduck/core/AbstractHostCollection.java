9633369980cf83848e2ee6c93a7331adc9cc6693
David Kocher <dkocher@iterate.ch>
2017-11-07 22:51:50 +0100

    Review.

--- core/src/main/java/ch/cyberduck/core/AbstractHostCollection.java
+++ core/src/main/java/ch/cyberduck/core/AbstractHostCollection.java
@@ -28,6 +28,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public abstract class AbstractHostCollection extends Collection<Host> implements EditableCollection {
     private static final long serialVersionUID = -255801158019850767L;
@@ -225,11 +226,14 @@ public abstract class AbstractHostCollection extends Collection<Host> implements
      * @param bookmark Bookmark to find that matches comparison
      */
     public boolean find(final Host bookmark) {
-        for(Host h : this) {
-            if(h.compareTo(bookmark) == 0) {
-                return true;
+        return this.stream().anyMatch(new Predicate<Host>() {
+            @Override
+            public boolean test(final Host h) {
+                if(h.compareTo(bookmark) == 0) {
+                    return true;
+                }
+                return false;
             }
-        }
-        return false;
+        });
     }
 }
