8b7f24a370ddb00dbf35a566a8a398b2b342cef9
Jöran Malek <joeran3@gmail.com>
2017-12-05 09:53:11 +0100

    Implement space-available (sftpv6), statvfs@openssh for determining sftp quota

--- ssh/src/main/java/ch/cyberduck/core/sftp/SFTPQuotaFeature.java
+++ ssh/src/main/java/ch/cyberduck/core/sftp/SFTPQuotaFeature.java
@@ -24,6 +24,13 @@ import ch.cyberduck.core.features.Quota;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
+
+import net.schmizz.sshj.sftp.PacketType;
+import net.schmizz.sshj.sftp.Request;
+import net.schmizz.sshj.sftp.Response;
+import net.schmizz.sshj.sftp.SFTPEngine;
+
 public class SFTPQuotaFeature implements Quota {
     private static final Logger log = Logger.getLogger(SFTPQuotaFeature.class);
 
@@ -35,34 +42,97 @@ public class SFTPQuotaFeature implements Quota {
 
     @Override
     public Space get() throws BackgroundException {
-        final ThreadLocal<Space> quota = new ThreadLocal<Space>() {
-            @Override
-            protected Space initialValue() {
-                return new Space(0L, Long.MAX_VALUE);
-            }
-        };
         final Path home = new SFTPHomeDirectoryService(session).find();
-        new SFTPCommandFeature(session).send(String.format("df -Pk %s | awk '{print $3, $4}'", home.getAbsolute()), new DisabledProgressListener(),
-                new TranscriptListener() {
-                    @Override
-                    public void log(final Type request, final String output) {
-                        switch(request) {
-                            case response:
-                                final String[] numbers = StringUtils.split(output, ' ');
-                                if(numbers.length == 2) {
-                                    try {
-                                        quota.set(new Space(Long.valueOf(numbers[0]) * 1000L, Long.valueOf(numbers[1]) * 1000L));
-                                    }
-                                    catch(NumberFormatException e) {
-                                        log.warn(String.format("Ignore line %s", output));
-                                    }
+        Space space = GetSpaceAvailable(session.sftp(), home);
+
+        if(null == space) {
+            space = GetSpaceStatVFSOpenSSH(session.sftp(), home);
+        }
+        if(null == space) {
+            space = GetSpaceSSHDF(home);
+        }
+
+        return space;
+    }
+
+    private Space GetSpaceAvailable(SFTPEngine sftp, Path path) {
+        try {
+            Request request = sftp.newExtendedRequest("space-available").putString(path.getAbsolute());
+            Response response = sftp.request(request).retrieve();
+            if(response.getType() == PacketType.STATUS) {
+                log.warn(response.readString());
+                return null;
+            }
+            Long bytesOnDevice = response.readUInt64();
+            Long unusedBytesOnDevice = response.readUInt64();
+            Long bytesAvailableToUser = response.readUInt64();
+            Long unusedBytesAvailableToUser = response.readUInt64();
+            Integer bytesPerAllocationUnit = response.readUInt32AsInt();
+
+            return new Space(bytesAvailableToUser - unusedBytesAvailableToUser, bytesAvailableToUser);
+        }
+        catch(IOException ex) {
+            return null;
+        }
+    }
+
+    private Space GetSpaceStatVFSOpenSSH(SFTPEngine sftp, Path path) {
+        try {
+            Request request = sftp.newExtendedRequest("statvfs@openssh.com").putString(path.getAbsolute());
+            Response response = sftp.request(request).retrieve();
+            if(response.getType() == PacketType.STATUS) {
+                log.warn(response.readString());
+                return null;
+            }
+            Long blockSize = response.readUInt64(); /* file system block size */
+            Long filesystemBlockSize = response.readUInt64(); /* fundamental fs block size */
+            Long totalBlocks = response.readUInt64(); /* number of blocks (unit f_frsize) */
+            Long filesystemFreeBlocks = response.readUInt64(); /* free blocks in file system */
+            Long blocksAvailable = response.readUInt64(); /* free blocks for non-root */
+            Long fileInodes = response.readUInt64(); /* total file inodes */
+            Long fileInodesFree = response.readUInt64(); /* free file inodes */
+            Long fileInodesAvailable = response.readUInt64(); /* free file inodes for to non-root */
+            Long filesystemID = response.readUInt64(); /* file system id */
+            Long flags = response.readUInt64(); /* bit mask of f_flag values */
+            Long maximumFilenameLength = response.readUInt64(); /* maximum filename length */
+
+            Long total = totalBlocks * filesystemBlockSize;
+            Long available = blocksAvailable * blockSize;
+            Long used = total - available;
+
+            return new Space(used, total);
+        }
+        catch(IOException ex) {
+            return null;
+        }
+    }
+
+    private Space GetSpaceSSHDF(Path path) throws BackgroundException {
+        final ThreadLocal<Space> quota = new ThreadLocal<Space>();
+
+        new SFTPCommandFeature(session).send(String.format("df -Pk %s | awk '{print $3, $4}'", path.getAbsolute()), new DisabledProgressListener(),
+            new TranscriptListener() {
+                @Override
+                public void log(final Type request, final String output) {
+                    switch(request) {
+                        case response:
+                            final String[] numbers = StringUtils.split(output, ' ');
+                            if(numbers.length == 2) {
+                                try {
+                                    Long used = Long.valueOf(numbers[0]) * 1000L;
+                                    Long available = Long.valueOf(numbers[1]) * 1000L;
+                                    quota.set(new Space(used, available));
                                 }
-                                else {
+                                catch(NumberFormatException e) {
                                     log.warn(String.format("Ignore line %s", output));
                                 }
-                        }
+                            }
+                            else {
+                                log.warn(String.format("Ignore line %s", output));
+                            }
                     }
-                });
+                }
+            });
         return quota.get();
     }
 }
