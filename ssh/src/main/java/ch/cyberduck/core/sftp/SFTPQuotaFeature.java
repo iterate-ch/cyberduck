5360693316b6eeba36099a39f0d6107d658b8eec
David Kocher <dkocher@iterate.ch>
2017-12-05 15:21:29 +0100

    Review. Check for EXTENDED_REPLY response type.

--- ssh/src/main/java/ch/cyberduck/core/sftp/SFTPQuotaFeature.java
+++ ssh/src/main/java/ch/cyberduck/core/sftp/SFTPQuotaFeature.java
@@ -26,7 +26,6 @@ import org.apache.log4j.Logger;
 
 import java.io.IOException;
 
-import net.schmizz.sshj.sftp.PacketType;
 import net.schmizz.sshj.sftp.Request;
 import net.schmizz.sshj.sftp.Response;
 import net.schmizz.sshj.sftp.SFTPEngine;
@@ -43,74 +42,81 @@ public class SFTPQuotaFeature implements Quota {
     @Override
     public Space get() throws BackgroundException {
         final Path home = new SFTPHomeDirectoryService(session).find();
-        Space space = GetSpaceAvailable(session.sftp(), home);
-
-        if(null == space) {
-            space = GetSpaceStatVFSOpenSSH(session.sftp(), home);
-        }
-        if(null == space) {
-            space = GetSpaceSSHDF(home);
-        }
-
-        return space;
-    }
-
-    private Space GetSpaceAvailable(SFTPEngine sftp, Path path) {
         try {
-            Request request = sftp.newExtendedRequest("space-available").putString(path.getAbsolute());
-            Response response = sftp.request(request).retrieve();
-            if(response.getType() == PacketType.STATUS) {
-                log.warn(response.readString());
-                return null;
-            }
-            Long bytesOnDevice = response.readUInt64();
-            Long unusedBytesOnDevice = response.readUInt64();
-            Long bytesAvailableToUser = response.readUInt64();
-            Long unusedBytesAvailableToUser = response.readUInt64();
-            Integer bytesPerAllocationUnit = response.readUInt32AsInt();
-
-            return new Space(bytesAvailableToUser - unusedBytesAvailableToUser, bytesAvailableToUser);
+            return this.getSpaceAvailable(session.sftp(), home);
         }
-        catch(IOException ex) {
-            return null;
+        catch(BackgroundException e) {
+            log.info(String.format("Failure obtaining disk quota. %s.", e.getDetail()));
         }
-    }
-
-    private Space GetSpaceStatVFSOpenSSH(SFTPEngine sftp, Path path) {
         try {
-            Request request = sftp.newExtendedRequest("statvfs@openssh.com").putString(path.getAbsolute());
-            Response response = sftp.request(request).retrieve();
-            if(response.getType() == PacketType.STATUS) {
-                log.warn(response.readString());
-                return null;
-            }
-            Long blockSize = response.readUInt64(); /* file system block size */
-            Long filesystemBlockSize = response.readUInt64(); /* fundamental fs block size */
-            Long totalBlocks = response.readUInt64(); /* number of blocks (unit f_frsize) */
-            Long filesystemFreeBlocks = response.readUInt64(); /* free blocks in file system */
-            Long blocksAvailable = response.readUInt64(); /* free blocks for non-root */
-            Long fileInodes = response.readUInt64(); /* total file inodes */
-            Long fileInodesFree = response.readUInt64(); /* free file inodes */
-            Long fileInodesAvailable = response.readUInt64(); /* free file inodes for to non-root */
-            Long filesystemID = response.readUInt64(); /* file system id */
-            Long flags = response.readUInt64(); /* bit mask of f_flag values */
-            Long maximumFilenameLength = response.readUInt64(); /* maximum filename length */
-
-            Long total = totalBlocks * filesystemBlockSize;
-            Long available = blocksAvailable * blockSize;
-            Long used = total - available;
-
-            return new Space(used, total);
+            return this.getSpaceStatVFSOpenSSH(session.sftp(), home);
         }
-        catch(IOException ex) {
-            return null;
+        catch(BackgroundException e) {
+            log.info(String.format("Failure obtaining disk quota. %s.", e.getDetail()));
+        }
+        try {
+            return this.getSpaceShellPrompt(home);
+        }
+        catch(BackgroundException e) {
+            log.info(String.format("Failure obtaining disk quota. %s.", e.getDetail()));
+        }
+        return new Space(0L, Long.MAX_VALUE);
+    }
+
+    private Space getSpaceAvailable(SFTPEngine sftp, final Path directory) throws BackgroundException {
+        try {
+            final Request request = sftp.newExtendedRequest("space-available").putString(directory.getAbsolute());
+            final Response response = sftp.request(request).retrieve();
+            switch(response.getType()) {
+                case EXTENDED_REPLY:
+                    Long bytesOnDevice = response.readUInt64();
+                    Long unusedBytesOnDevice = response.readUInt64();
+                    Long bytesAvailableToUser = response.readUInt64();
+                    Long unusedBytesAvailableToUser = response.readUInt64();
+                    Integer bytesPerAllocationUnit = response.readUInt32AsInt();
+                    return new Space(bytesAvailableToUser - unusedBytesAvailableToUser, bytesAvailableToUser);
+                default:
+                    throw new IOException(String.format("Unexpected response type %s", response.getType()));
+            }
+        }
+        catch(IOException e) {
+            throw new SFTPExceptionMappingService().map("Failure to read attributes of {0}", e, directory);
         }
     }
 
-    private Space GetSpaceSSHDF(Path path) throws BackgroundException {
+    private Space getSpaceStatVFSOpenSSH(SFTPEngine sftp, final Path directory) throws BackgroundException {
+        try {
+            final Request request = sftp.newExtendedRequest("statvfs@openssh.com").putString(directory.getAbsolute());
+            final Response response = sftp.request(request).retrieve();
+            switch(response.getType()) {
+                case EXTENDED_REPLY:
+                    Long blockSize = response.readUInt64(); /* file system block size */
+                    Long filesystemBlockSize = response.readUInt64(); /* fundamental fs block size */
+                    Long totalBlocks = response.readUInt64(); /* number of blocks (unit f_frsize) */
+                    Long filesystemFreeBlocks = response.readUInt64(); /* free blocks in file system */
+                    Long blocksAvailable = response.readUInt64(); /* free blocks for non-root */
+                    Long fileInodes = response.readUInt64(); /* total file inodes */
+                    Long fileInodesFree = response.readUInt64(); /* free file inodes */
+                    Long fileInodesAvailable = response.readUInt64(); /* free file inodes for to non-root */
+                    Long filesystemID = response.readUInt64(); /* file system id */
+                    Long flags = response.readUInt64(); /* bit mask of f_flag values */
+                    Long maximumFilenameLength = response.readUInt64(); /* maximum filename length */
+                    Long total = totalBlocks * filesystemBlockSize;
+                    Long available = blocksAvailable * blockSize;
+                    Long used = total - available;
+                    return new Space(used, total);
+                default:
+                    throw new IOException(String.format("Unexpected response type %s", response.getType()));
+            }
+        }
+        catch(IOException e) {
+            throw new SFTPExceptionMappingService().map("Failure to read attributes of {0}", e, directory);
+        }
+    }
+
+    private Space getSpaceShellPrompt(final Path directory) throws BackgroundException {
         final ThreadLocal<Space> quota = new ThreadLocal<Space>();
-
-        new SFTPCommandFeature(session).send(String.format("df -Pk %s | awk '{print $3, $4}'", path.getAbsolute()), new DisabledProgressListener(),
+        new SFTPCommandFeature(session).send(String.format("df -Pk %s | awk '{print $3, $4}'", directory.getAbsolute()), new DisabledProgressListener(),
             new TranscriptListener() {
                 @Override
                 public void log(final Type request, final String output) {
@@ -133,6 +139,9 @@ public class SFTPQuotaFeature implements Quota {
                     }
                 }
             });
+        if(null == quota.get()) {
+            throw new SFTPExceptionMappingService().map("Failure to read attributes of {0}", new IOException(), directory);
+        }
         return quota.get();
     }
 }
