8fd9ab1bb4d168d31026be6eb2b2dbc5de591554
David Kocher <dkocher@iterate.ch>
2017-12-12 18:03:13 +0100

    Allow to switch to auth with public key in prompt.

--- ssh/src/main/java/ch/cyberduck/core/sftp/auth/SFTPPasswordAuthentication.java
+++ ssh/src/main/java/ch/cyberduck/core/sftp/auth/SFTPPasswordAuthentication.java
@@ -1,19 +1,19 @@
 package ch.cyberduck.core.sftp.auth;
 
-/*
- * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
- * https://cyberduck.io/
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+    /*
+     * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+     * https://cyberduck.io/
+     *
+     * This program is free software; you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU General Public License for more details.
+     */
 
 import ch.cyberduck.core.AuthenticationProvider;
 import ch.cyberduck.core.BookmarkNameProvider;
@@ -60,7 +60,11 @@ public class SFTPPasswordAuthentication implements AuthenticationProvider<Boolea
                 String.format("%s %s", LocaleFactory.localizedString("Login", "Login"), bookmark.getHostname()),
                 MessageFormat.format(LocaleFactory.localizedString(
                     "Login {0} with username and password", "Credentials"), BookmarkNameProvider.toString(bookmark)),
-                new LoginOptions(bookmark.getProtocol()).publickey(false));
+                new LoginOptions(bookmark.getProtocol()));
+            if(input.isPublicKeyAuthentication()) {
+                credentials.setIdentity(input.getIdentity());
+                return new SFTPPublicKeyAuthentication(session).authenticate(bookmark, keychain, callback, cancel);
+            }
             credentials.setSaved(input.isSaved());
             credentials.setPassword(input.getPassword());
         }
