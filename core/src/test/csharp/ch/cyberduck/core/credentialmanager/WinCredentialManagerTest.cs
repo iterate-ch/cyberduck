f5e98315f67338b426ab8c4b52961e8f1271f21c
Jöran Malek <jmalek@iterate.ch>
2018-03-04 14:49:52 +0100

    Add credential manager tests

---
+++ core/src/test/csharp/ch/cyberduck/core/credentialmanager/WinCredentialManagerTest.cs
@@ -0,0 +1,96 @@
+﻿using Ch.Cyberduck.Core.CredentialManager;
+using java.util;
+using NUnit.Framework;
+using System.Net;
+
+namespace Ch.Cyberduck.Core.credentialmanager
+{
+    [TestFixture]
+    public class WinCredentialManagerTest
+    {
+        public static string[] UserNames
+        {
+            get
+            {
+                return new string[]
+                {
+                    "user",
+                    "user@service.domain.tld",
+                    "cdid1.credential.cd1..dywpmxvbnncbzmlappwofuzndjoqqonyenswravjbvuyckeihqcoirpumtxj",
+                    "svimjbjpojbxgvhgqivytxodldswkvzkoqeqgwcjciwueewpsrvsnygloehmveuvakwvqtsddmioyvawgl",
+                    "separated.user",
+                    "dash-user"
+                };
+            }
+        }
+
+        [Test]
+        public void TestGetInvalid()
+        {
+            var credentials = WinCredentialManager.GetCredentials(UUID.randomUUID().toString());
+            Assert.IsNullOrEmpty(credentials.UserName);
+            Assert.IsNullOrEmpty(credentials.Password);
+            Assert.IsNullOrEmpty(credentials.Domain);
+        }
+
+        [Test]
+        public void TestSaveServiceEmbeddedUser([ValueSource(nameof(UserNames))] string user)
+        {
+            var target = $"protocol://{user}@service.{UUID.randomUUID().toString()}.tld";
+            var credentials = new NetworkCredential(user, UUID.randomUUID().toString());
+            var saveResult = WinCredentialManager.SaveCredentials(target, credentials);
+            Assert.True(saveResult);
+            var query = WinCredentialManager.GetCredentials(target);
+            Assert.AreEqual(credentials.UserName, query.UserName);
+            Assert.AreEqual(credentials.Password, query.Password);
+            Assert.AreEqual(credentials.Domain, query.Domain);
+            var removeResult = WinCredentialManager.RemoveCredentials(target);
+            Assert.True(removeResult);
+        }
+
+        [Test]
+        public void TestSaveServiceUser([ValueSource(nameof(UserNames))] string user)
+        {
+            var target = $"protocol://service.{UUID.randomUUID().toString()}.tld";
+            var credentials = new NetworkCredential(user, UUID.randomUUID().toString());
+            var saveResult = WinCredentialManager.SaveCredentials(target, credentials);
+            Assert.True(saveResult);
+            var query = WinCredentialManager.GetCredentials(target);
+            Assert.AreEqual(credentials.UserName, query.UserName);
+            Assert.AreEqual(credentials.Password, query.Password);
+            Assert.AreEqual(credentials.Domain, query.Domain);
+            var removeResult = WinCredentialManager.RemoveCredentials(target);
+            Assert.True(removeResult);
+        }
+
+        [Test]
+        public void TestSaveSimpleEmbeddedUser([ValueSource(nameof(UserNames))]string user)
+        {
+            var target = $"{user}@{UUID.randomUUID().toString()}";
+            var credentials = new NetworkCredential(user, UUID.randomUUID().toString());
+            var saveResult = WinCredentialManager.SaveCredentials(target, credentials);
+            Assert.True(saveResult);
+            var query = WinCredentialManager.GetCredentials(target);
+            Assert.AreEqual(credentials.UserName, query.UserName);
+            Assert.AreEqual(credentials.Password, query.Password);
+            Assert.AreEqual(credentials.Domain, query.Domain);
+            var removeResult = WinCredentialManager.RemoveCredentials(target);
+            Assert.True(removeResult);
+        }
+
+        [Test]
+        public void TestSaveSimpleUser([ValueSource(nameof(UserNames))]string user)
+        {
+            var target = UUID.randomUUID().toString();
+            var credentials = new NetworkCredential(user, UUID.randomUUID().toString());
+            var saveResult = WinCredentialManager.SaveCredentials(target, credentials);
+            Assert.True(saveResult);
+            var query = WinCredentialManager.GetCredentials(target);
+            Assert.AreEqual(credentials.UserName, query.UserName);
+            Assert.AreEqual(credentials.Password, query.Password);
+            Assert.AreEqual(credentials.Domain, query.Domain);
+            var removeResult = WinCredentialManager.RemoveCredentials(target);
+            Assert.True(removeResult);
+        }
+    }
+}
