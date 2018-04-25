3273360f44c8e00a5f97127ac764b1791d2bc8e2
David Kocher <dkocher@iterate.ch>
2018-04-25 16:40:29 +0200

    Add abstract generic implementation for Unicode normalization forms.

--- core/src/main/java/ch/cyberduck/core/unicode/NFDNormalizer.java
+++ core/src/main/java/ch/cyberduck/core/unicode/NFDNormalizer.java
@@ -1,27 +1,24 @@
 package ch.cyberduck.core.unicode;
 
 /*
- * Copyright (c) 2002-2014 David Kocher. All rights reserved.
- * http://cyberduck.io/
+ * Copyright (c) 2002-2018 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * Bug fixes, suggestions and comments should be sent to:
- * feedback@cyberduck.io
  */
 
 import java.text.Normalizer;
 
-public class NFCNormalizer extends AbstractUnicodeNormalizer implements UnicodeNormalizer {
-    public NFCNormalizer(final Normalizer.Form form) {
-        super(Normalizer.Form.NFC);
+public class NFDNormalizer extends AbstractUnicodeNormalizer {
+    public NFDNormalizer() {
+        super(Normalizer.Form.NFD);
     }
 }
