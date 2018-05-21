6643f059e128432e3ab867b5bf7a64c6f078ab96
David Kocher <dkocher@iterate.ch>
2018-05-21 14:10:19 +0200

    Add validation for token.

--- core/src/main/java/ch/cyberduck/core/Credentials.java
+++ core/src/main/java/ch/cyberduck/core/Credentials.java
@@ -180,6 +180,10 @@ public class Credentials implements Comparable<Credentials> {
         return StringUtils.isNotBlank(password);
     }
 
+    public boolean isTokenAuthentication() {
+        return StringUtils.isNotBlank(token);
+    }
+
     /**
      * SSH specific
      *
