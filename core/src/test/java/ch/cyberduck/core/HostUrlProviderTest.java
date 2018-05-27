559f4ecb201e395451d4424e437346fbce335310
David Kocher <dkocher@iterate.ch>
2018-05-27 20:41:36 +0200

    Review.
    Signed-off-by: David Kocher <dkocher@iterate.ch>

--- core/src/test/java/ch/cyberduck/core/HostUrlProviderTest.java
+++ core/src/test/java/ch/cyberduck/core/HostUrlProviderTest.java
@@ -9,21 +9,21 @@ public class HostUrlProviderTest {
     @Test
     public void testToUrl() {
         assertEquals("sftp://user@localhost", new HostUrlProvider().get(new Host(new TestProtocol(Scheme.sftp), "localhost", new Credentials("user", "p"))));
-        assertEquals("sftp://localhost", new HostUrlProvider(false, false).get(new Host(new TestProtocol(Scheme.sftp), "localhost", new Credentials("user", "p"))));
+        assertEquals("sftp://localhost", new HostUrlProvider().withUsername(false).withPath(false).get(new Host(new TestProtocol(Scheme.sftp), "localhost", new Credentials("user", "p"))));
         assertEquals("sftp://localhost:222",
-            new HostUrlProvider(false, false).get(new Host(new TestProtocol(Scheme.sftp), "localhost", 222)));
+            new HostUrlProvider().withUsername(false).withPath(false).get(new Host(new TestProtocol(Scheme.sftp), "localhost", 222)));
     }
 
     @Test
     public void testPath() {
         final Host h = new Host(new TestProtocol(Scheme.sftp), "localhost", new Credentials("user", "p"));
         h.setDefaultPath("p");
-        assertEquals("sftp://user@localhost/p", new HostUrlProvider(true, true).get(h));
+        assertEquals("sftp://user@localhost/p", new HostUrlProvider().withUsername(true).withPath(true).get(h));
     }
 
     @Test
     public void testOAuth() {
         final Host h = new Host(new TestProtocol(Scheme.https), "login.microsoftonline.com", new Credentials("Microsoft OneDrive (t@domain.com) OAuth2 Access Token"));
-        assertEquals("https://Microsoft%20OneDrive%20%28t@domain.com%29%20OAuth2%20Access%20Token@login.microsoftonline.com", new HostUrlProvider(true, true).get(h));
+        assertEquals("https://Microsoft%20OneDrive%20%28t@domain.com%29%20OAuth2%20Access%20Token@login.microsoftonline.com", new HostUrlProvider().withUsername(true).withPath(true).get(h));
     }
 }
