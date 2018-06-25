3bdee59dbe553103bd8a72ee9fa5c1e6bfcdfe9c
David Kocher <dkocher@iterate.ch>
2018-06-25 14:37:17 +0200

    Add removal listener.

--- core/src/main/java/ch/cyberduck/core/cache/LRUCache.java
+++ core/src/main/java/ch/cyberduck/core/cache/LRUCache.java
@@ -15,6 +15,8 @@ package ch.cyberduck.core.cache;
  * GNU General Public License for more details.
  */
 
+import org.apache.log4j.Logger;
+
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
@@ -22,9 +24,16 @@ import java.util.function.Function;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 import com.google.common.util.concurrent.UncheckedExecutionException;
 
 public class LRUCache<Key, Value> {
+    private static final Logger log = Logger.getLogger(LRUCache.class);
+
+    public static <Key, Value> LRUCache<Key, Value> usingLoader(final Function<Key, Value> loader) {
+        return usingLoader(loader, -1L, -1L);
+    }
 
     public static <Key, Value> LRUCache<Key, Value> usingLoader(final Function<Key, Value> loader, final long maximumSize) {
         return usingLoader(loader, maximumSize, -1L);
@@ -37,9 +46,18 @@ public class LRUCache<Key, Value> {
     private final LoadingCache<Key, Value> delegate;
 
     private LRUCache(final Function<Key, Value> loader, final long maximumSize, final long expireDuration) {
-        final CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder();
-        builder
-            .maximumSize(maximumSize);
+        final CacheBuilder<Key, Value> builder = CacheBuilder.newBuilder()
+            .removalListener(new RemovalListener<Key, Value>() {
+                @Override
+                public void onRemoval(final RemovalNotification<Key, Value> notification) {
+                    if(log.isDebugEnabled()) {
+                        log.debug(String.format("Removed %s from cache with cause %s", notification.getKey(), notification.getCause()));
+                    }
+                }
+            });
+        if(maximumSize > 0) {
+            builder.maximumSize(maximumSize);
+        }
         if(expireDuration > 0) {
             builder.expireAfterAccess(expireDuration, TimeUnit.MILLISECONDS);
         }
