ace887661237de511a2dd1478f2a5303296c7a17
Yves Langisch <yla@iterate.ch>
2018-07-27 23:32:14 +0200

    Retain delete marker in custom attributes.

--- s3/src/main/java/ch/cyberduck/core/s3/S3VersionedObjectListService.java
+++ s3/src/main/java/ch/cyberduck/core/s3/S3VersionedObjectListService.java
@@ -41,8 +41,12 @@ import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 
+import com.google.common.collect.ImmutableMap;
+
 public class S3VersionedObjectListService implements ListService {
     private static final Logger log = Logger.getLogger(S3VersionedObjectListService.class);
+
+    public static final String KEY_DELETE_MARKER = "delete_marker";
 
     private final Preferences preferences
         = PreferencesFactory.get();
@@ -89,6 +93,9 @@ public class S3VersionedObjectListService implements ListService {
                     }
                     attributes.setRevision(++i);
                     attributes.setDuplicate((marker.isDeleteMarker() && marker.isLatest()) || !marker.isLatest());
+                    if(marker.isDeleteMarker()) {
+                        attributes.setCustom(ImmutableMap.of(KEY_DELETE_MARKER, Boolean.TRUE.toString()));
+                    }
                     attributes.setModificationDate(marker.getLastModified().getTime());
                     attributes.setRegion(bucket.attributes().getRegion());
                     if(marker instanceof S3Version) {
@@ -124,7 +131,9 @@ public class S3VersionedObjectListService implements ListService {
                             final BaseVersionOrDeleteMarker version = versions.getItems()[0];
                             if(version.getKey().equals(common)) {
                                 attributes.setVersionId(version.getVersionId());
-                                attributes.setDuplicate(version.isDeleteMarker());
+                                if(version.isDeleteMarker()) {
+                                    attributes.setCustom(ImmutableMap.of(KEY_DELETE_MARKER, Boolean.TRUE.toString()));
+                                }
                             }
                             else {
                                 // no placeholder but objects inside - need to check if all of them are deleted
