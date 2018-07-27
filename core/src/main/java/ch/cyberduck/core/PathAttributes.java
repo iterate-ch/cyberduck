32253efc6616864b247ef6d2b99e6cfc220f5283
David Kocher <dkocher@iterate.ch>
2018-07-27 17:45:46 +0200

    Fix null pointer.

--- core/src/main/java/ch/cyberduck/core/PathAttributes.java
+++ core/src/main/java/ch/cyberduck/core/PathAttributes.java
@@ -170,7 +170,7 @@ public class PathAttributes extends Attributes implements Serializable {
         if(modified != -1) {
             dict.setStringForKey(String.valueOf(modified), "Modified");
         }
-        if(revision != -1) {
+        if(revision != null) {
             dict.setStringForKey(String.valueOf(revision), "Revision");
         }
         if(permission != Permission.EMPTY) {
