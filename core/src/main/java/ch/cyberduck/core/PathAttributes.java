ff422b7bccfb5e9a2bd57c5636e3747b52153a4f
David Kocher <dkocher@iterate.ch>
2018-07-27 17:32:17 +0200

    Serialize revision in attributes.

--- core/src/main/java/ch/cyberduck/core/PathAttributes.java
+++ core/src/main/java/ch/cyberduck/core/PathAttributes.java
@@ -170,6 +170,9 @@ public class PathAttributes extends Attributes implements Serializable {
         if(modified != -1) {
             dict.setStringForKey(String.valueOf(modified), "Modified");
         }
+        if(revision != -1) {
+            dict.setStringForKey(String.valueOf(revision), "Revision");
+        }
         if(permission != Permission.EMPTY) {
             dict.setObjectForKey(permission, "Permission");
         }
