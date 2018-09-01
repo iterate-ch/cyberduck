5dd8ccbb410df0d5058f79a07e4cfbf19cacc7b5
David Kocher <dkocher@iterate.ch>
2018-09-01 20:34:34 +0200

    Add custom canceled type for transfers.

---
+++ core/src/main/java/ch/cyberduck/core/exception/TransferCanceledException.java
@@ -0,0 +1,33 @@
+package ch.cyberduck.core.exception;
+
+/*
+ * Copyright (c) 2002-2018 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+public class TransferCanceledException extends ConnectionCanceledException {
+    public TransferCanceledException() {
+    }
+
+    public TransferCanceledException(final String detail) {
+        super(detail);
+    }
+
+    public TransferCanceledException(final Throwable cause) {
+        super(cause);
+    }
+
+    public TransferCanceledException(final String detail, final Throwable cause) {
+        super(detail, cause);
+    }
+}
