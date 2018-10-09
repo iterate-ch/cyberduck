ac95cf8be4375afb30d4c285b00a81f2c22da694
David Kocher <dkocher@iterate.ch>
2018-10-09 12:48:34 +0200

    Add stateful property.

--- core/src/main/java/ch/cyberduck/core/Protocol.java
+++ core/src/main/java/ch/cyberduck/core/Protocol.java
@@ -56,6 +56,10 @@ public interface Protocol extends Comparable<Protocol> {
         manta
     }
 
+    default boolean isStateful() {
+        return false;
+    }
+
     /**
      * @return True if anonymous login is possible.
      */
