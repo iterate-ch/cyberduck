6c5b1f0302e81ffcfc897c88668bc6656612c4ca
David Kocher <dkocher@iterate.ch>
2018-12-11 14:40:50 +0100

    Review.

--- core/src/main/java/ch/cyberduck/core/AbstractProtocol.java
+++ core/src/main/java/ch/cyberduck/core/AbstractProtocol.java
@@ -23,7 +23,9 @@ import ch.cyberduck.core.preferences.PreferencesFactory;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
@@ -64,10 +66,8 @@ public abstract class AbstractProtocol implements Protocol {
 
     @Override
     public String[] getSchemes() {
-        if(StringUtils.equals(this.getScheme().name(), this.getIdentifier())) {
-            return new String[]{this.getScheme().name()};
-        }
-        return new String[]{this.getIdentifier(), this.getScheme().name()};
+        final HashSet<String> schemes = new HashSet<>(Arrays.asList(this.getIdentifier(), this.getScheme().name()));
+        return schemes.toArray(new String[schemes.size()]);
     }
 
     @Override
