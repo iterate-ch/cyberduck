608aa7505e1afef9f05567933e842ef02e68d1ba
Yves Langisch <yla@iterate.ch>
2019-01-02 13:16:52 +0100

    Add timezone support.

--- core/src/main/csharp/ch/cyberduck/core/date/UserDefaultsDateFormatter.cs
+++ core/src/main/csharp/ch/cyberduck/core/date/UserDefaultsDateFormatter.cs
@@ -1,18 +1,45 @@
-﻿using ch.cyberduck.core;
-using ch.cyberduck.core.date;
+﻿// 
+// Copyright (c) 2010-2019 Yves Langisch. All rights reserved.
+// http://cyberduck.io/
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// Bug fixes, suggestions and comments should be sent to:
+// feedback@cyberduck.io
+// 
+
 using System;
+using ch.cyberduck.core;
+using ch.cyberduck.core.date;
+using TimeZone = java.util.TimeZone;
 
 namespace Ch.Cyberduck.Core.Date
 {
     public class UserDefaultsDateFormatter : AbstractUserDateFormatter
     {
+        private readonly string tz;
+
+        public UserDefaultsDateFormatter(string tz)
+        {
+            this.tz = tz;
+        }
+
         public override string getLongFormat(long millis, bool natural)
         {
             if (-1 == millis)
             {
                 return LocaleFactory.localizedString("Unknown");
             }
-            return GetLongFormat(ConvertJavaMillisecondsToDateTime(millis));
+
+            return GetLongFormat(ConvertJavaMillisecondsToDateTime(millis, tz));
         }
 
         /// <summary>
@@ -27,7 +54,7 @@ namespace Ch.Cyberduck.Core.Date
                 return LocaleFactory.localizedString("Unknown");
             }
 
-            return GetShortFormat(ConvertJavaMillisecondsToDateTime(millis));
+            return GetShortFormat(ConvertJavaMillisecondsToDateTime(millis, tz));
         }
 
         public override string getMediumFormat(long millis, bool natural)
@@ -45,6 +72,12 @@ namespace Ch.Cyberduck.Core.Date
             return d.ToString("F");
         }
 
+        public static DateTime ConvertJavaMillisecondsToDateTime(long javaMS, string tz)
+        {
+            int offset = TimeZone.getTimeZone(tz).getOffset(javaMS);
+            return ConvertJavaMillisecondsToDateTime(javaMS + offset);
+        }
+
         public static DateTime ConvertJavaMillisecondsToDateTime(long javaMS)
         {
             DateTime utcBaseTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
@@ -54,7 +87,8 @@ namespace Ch.Cyberduck.Core.Date
 
         public static long ConvertDateTimeToJavaMilliseconds(DateTime dateTime)
         {
-            TimeSpan timeSpan = dateTime.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+            TimeSpan timeSpan = dateTime.ToUniversalTime()
+                .Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
             return timeSpan.Ticks / TimeSpan.TicksPerMillisecond;
         }
 
