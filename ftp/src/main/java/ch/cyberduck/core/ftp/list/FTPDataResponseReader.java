d1c1448857cb3dbaefda1190b7a293fe1d5ace60
David Kocher <dkocher@iterate.ch>
2019-01-10 13:15:06 +0100

    Review.

--- ftp/src/main/java/ch/cyberduck/core/ftp/list/FTPDataResponseReader.java
+++ ftp/src/main/java/ch/cyberduck/core/ftp/list/FTPDataResponseReader.java
@@ -18,13 +18,10 @@ package ch.cyberduck.core.ftp.list;
 import ch.cyberduck.core.AttributedList;
 import ch.cyberduck.core.ListProgressListener;
 import ch.cyberduck.core.Path;
-import ch.cyberduck.core.exception.ConnectionCanceledException;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface FTPDataResponseReader {
 
-    AttributedList<Path> read(Path parent, List<String> replies, ListProgressListener listener)
-        throws IOException, FTPInvalidListException, ConnectionCanceledException;
+    AttributedList<Path> read(Path parent, List<String> replies, ListProgressListener listener) throws FTPInvalidListException;
 }
