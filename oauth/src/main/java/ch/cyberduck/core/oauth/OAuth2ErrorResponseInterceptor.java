a3ded4be65e9fc57dc525b50b4cd859b53daa6dd
David Kocher <dkocher@iterate.ch>
2019-02-27 15:09:57 +0100

    Fix error handling.

--- oauth/src/main/java/ch/cyberduck/core/oauth/OAuth2ErrorResponseInterceptor.java
+++ oauth/src/main/java/ch/cyberduck/core/oauth/OAuth2ErrorResponseInterceptor.java
@@ -19,7 +19,7 @@ import ch.cyberduck.core.DisabledCancelCallback;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.LoginCallback;
 import ch.cyberduck.core.exception.BackgroundException;
-import ch.cyberduck.core.exception.ExpiredTokenException;
+import ch.cyberduck.core.exception.LoginFailureException;
 import ch.cyberduck.core.http.DisabledServiceUnavailableRetryStrategy;
 
 import org.apache.http.HttpResponse;
@@ -54,7 +54,7 @@ public class OAuth2ErrorResponseInterceptor extends DisabledServiceUnavailableRe
                             service.setTokens(service.refresh());
                             return true;
                         }
-                        catch(ExpiredTokenException e) {
+                        catch(LoginFailureException e) {
                             log.warn(String.format("Failure refreshing OAuth tokens. %s", e));
                             service.authorize(bookmark, prompt, new DisabledCancelCallback());
                         }
