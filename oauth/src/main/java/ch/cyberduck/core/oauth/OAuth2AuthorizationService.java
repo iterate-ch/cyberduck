5db499e8cbe5bf606bdcbb9149d5bd2c31c07c8f
David Kocher <dkocher@iterate.ch>
2019-03-22 12:12:37 +0100

    Logging

--- oauth/src/main/java/ch/cyberduck/core/oauth/OAuth2AuthorizationService.java
+++ oauth/src/main/java/ch/cyberduck/core/oauth/OAuth2AuthorizationService.java
@@ -120,8 +120,14 @@ public class OAuth2AuthorizationService {
                 }
             }
             else {
+                if(log.isDebugEnabled()) {
+                    log.debug(String.format("Returned saved OAuth tokens %s for %s", saved, bookmark));
+                }
                 return saved;
             }
+        }
+        if(log.isDebugEnabled()) {
+            log.debug(String.format("Start new OAuth flow for %s", bookmark));
         }
         // Start OAuth2 flow within browser
         final AuthorizationCodeFlow flow = new AuthorizationCodeFlow.Builder(
@@ -142,6 +148,9 @@ public class OAuth2AuthorizationService {
 
         // Direct the user to an authorization page to grant access to their protected data.
         final String url = authorizationCodeRequestUrl.build();
+        if(log.isDebugEnabled()) {
+            log.debug(String.format("Open browser with URL %s", url));
+        }
         if(!browser.open(url)) {
             log.warn(String.format("Failed to launch web browser for %s", url));
         }
@@ -165,6 +174,9 @@ public class OAuth2AuthorizationService {
         try {
             if(StringUtils.isBlank(input.getPassword())) {
                 throw new LoginCanceledException();
+            }
+            if(log.isDebugEnabled()) {
+                log.debug(String.format("Request tokens for authentication code %s", input.getPassword()));
             }
             // Swap the given authorization token for access/refresh tokens
             final TokenResponse response = flow.newTokenRequest(input.getPassword())
