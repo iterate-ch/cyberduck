abba65cf9d07eb51d57349b19200758908672768
David Kocher <dkocher@iterate.ch>
2019-03-28 09:53:13 +0100

    Use auto-close try block.

--- importer/src/main/java/ch/cyberduck/core/importer/FlashFxpBookmarkCollection.java
+++ importer/src/main/java/ch/cyberduck/core/importer/FlashFxpBookmarkCollection.java
@@ -25,7 +25,6 @@ import ch.cyberduck.core.Scheme;
 import ch.cyberduck.core.exception.AccessDeniedException;
 import ch.cyberduck.core.preferences.PreferencesFactory;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
@@ -45,71 +44,64 @@ public abstract class FlashFxpBookmarkCollection extends ThirdpartyBookmarkColle
 
     @Override
     protected void parse(final ProtocolFactory protocols, final Local file) throws AccessDeniedException {
-        try {
-            final BufferedReader in
-                    = new BufferedReader(new InputStreamReader(file.getInputStream(), Charset.forName("UTF-8")));
-            try {
-                Host current = null;
-                String line;
-                while((line = in.readLine()) != null) {
-                    if(line.startsWith("[")) {
-                        current = new Host(protocols.forScheme(Scheme.ftp));
-                        current.getCredentials().setUsername(
-                                PreferencesFactory.get().getProperty("connection.login.anon.name"));
-                        Pattern pattern = Pattern.compile("\\[(.*)\\]");
-                        Matcher matcher = pattern.matcher(line);
-                        if(matcher.matches()) {
-                            current.setNickname(matcher.group(1));
-                        }
-                    }
-                    else if(StringUtils.isBlank(line)) {
-                        this.add(current);
-                        current = null;
-                    }
-                    else {
-                        if(null == current) {
-                            log.warn("Failed to detect start of bookmark");
-                            continue;
-                        }
-                        Scanner scanner = new Scanner(line);
-                        scanner.useDelimiter("=");
-                        if(!scanner.hasNext()) {
-                            log.warn("Missing key in line:" + line);
-                            continue;
-                        }
-                        String name = scanner.next().toLowerCase(Locale.ROOT);
-                        if(!scanner.hasNext()) {
-                            log.warn("Missing value in line:" + line);
-                            continue;
-                        }
-                        String value = scanner.next();
-                        switch(name) {
-                            case "ip":
-                                current.setHostname(StringUtils.substringBefore(value, "\u0001"));
-                                break;
-                            case "port":
-                                try {
-                                    current.setPort(Integer.parseInt(value));
-                                }
-                                catch(NumberFormatException e) {
-                                    log.warn("Invalid Port:" + e.getMessage());
-                                }
-                                break;
-                            case "path":
-                                current.setDefaultPath(value);
-                                break;
-                            case "notes":
-                                current.setComment(value);
-                                break;
-                            case "user":
-                                current.getCredentials().setUsername(value);
-                                break;
-                        }
+        try (final BufferedReader in = new BufferedReader(new InputStreamReader(file.getInputStream(), Charset.forName("UTF-8")))) {
+            Host current = null;
+            String line;
+            while((line = in.readLine()) != null) {
+                if(line.startsWith("[")) {
+                    current = new Host(protocols.forScheme(Scheme.ftp));
+                    current.getCredentials().setUsername(
+                        PreferencesFactory.get().getProperty("connection.login.anon.name"));
+                    Pattern pattern = Pattern.compile("\\[(.*)\\]");
+                    Matcher matcher = pattern.matcher(line);
+                    if(matcher.matches()) {
+                        current.setNickname(matcher.group(1));
                     }
                 }
-            }
-            finally {
-                IOUtils.closeQuietly(in);
+                else if(StringUtils.isBlank(line)) {
+                    this.add(current);
+                    current = null;
+                }
+                else {
+                    if(null == current) {
+                        log.warn("Failed to detect start of bookmark");
+                        continue;
+                    }
+                    Scanner scanner = new Scanner(line);
+                    scanner.useDelimiter("=");
+                    if(!scanner.hasNext()) {
+                        log.warn("Missing key in line:" + line);
+                        continue;
+                    }
+                    String name = scanner.next().toLowerCase(Locale.ROOT);
+                    if(!scanner.hasNext()) {
+                        log.warn("Missing value in line:" + line);
+                        continue;
+                    }
+                    String value = scanner.next();
+                    switch(name) {
+                        case "ip":
+                            current.setHostname(StringUtils.substringBefore(value, "\u0001"));
+                            break;
+                        case "port":
+                            try {
+                                current.setPort(Integer.parseInt(value));
+                            }
+                            catch(NumberFormatException e) {
+                                log.warn("Invalid Port:" + e.getMessage());
+                            }
+                            break;
+                        case "path":
+                            current.setDefaultPath(value);
+                            break;
+                        case "notes":
+                            current.setComment(value);
+                            break;
+                        case "user":
+                            current.getCredentials().setUsername(value);
+                            break;
+                    }
+                }
             }
         }
         catch(IOException e) {
