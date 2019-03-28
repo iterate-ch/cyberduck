451d69049ace938329ccdff57f0c2194efd0ec36
Jöran Malek <jmalek@iterate.ch>
2019-03-28 18:49:01 +0100

    Implement builtin OpenSSH & bash SSH

--- core/src/main/csharp/ch/cyberduck/core/SshTerminalService.cs
+++ core/src/main/csharp/ch/cyberduck/core/SshTerminalService.cs
@@ -1,22 +1,23 @@
-﻿// 
+﻿//
 // Copyright (c) 2010-2016 Yves Langisch. All rights reserved.
 // http://cyberduck.io/
-// 
+//
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // Bug fixes, suggestions and comments should be sent to:
 // feedback@cyberduck.io
-// 
+//
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Windows.Forms;
@@ -24,13 +25,107 @@ using ch.cyberduck.core;
 using ch.cyberduck.core.local;
 using ch.cyberduck.core.preferences;
 using Application = ch.cyberduck.core.local.Application;
-using Path = ch.cyberduck.core.Path;
+using Path = System.IO.Path;
 
 namespace Ch.Cyberduck.Core
 {
     public class SshTerminalService : TerminalService
     {
-        public void open(Host host, Path workdir)
+        public void open(Host host, ch.cyberduck.core.Path workdir)
+        {
+            if (Utils.IsWin101809)
+            {
+                if (TryStartBuiltinOpenSSH(host, workdir.getAbsolute()))
+                {
+                    return;
+                }
+            }
+            if (Utils.IsWin10FallCreatorsUpdate)
+            {
+                if (TryStartBashSSH(host, workdir.getAbsolute()))
+                {
+                    return;
+                }
+            }
+
+            TryStartPuTTy(host, workdir.getAbsolute());
+        }
+
+        private static string GetSystemPath()
+        {
+            var system = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            if (Environment.Is64BitOperatingSystem)
+            {
+                system = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "SysNative");
+            }
+            return system;
+        }
+
+        private bool TryStartBashSSH(Host host, string workdir)
+        {
+            var system = GetSystemPath();
+            var bash = Path.Combine(system, "bash.exe");
+            if (!File.Exists(bash))
+            {
+                return false;
+            }
+
+            var credentials = host.getCredentials();
+            if (credentials.isPublicKeyAuthentication())
+            {
+                // there is no way of determining the mount path for each
+                // and every distribution. Assume no public key authentication.
+                return false;
+            }
+
+            var hostname = host.getHostname();
+            var username = credentials.getUsername();
+            var port = host.getPort();
+
+            Process process = new Process();
+            process.StartInfo.FileName = bash;
+            process.StartInfo.Arguments = $"-c \"{$"ssh {username}@{hostname} -T -P {port} \"cd \\\"{workdir}\\\""}\"";
+
+            var success = process.Start();
+            if (success)
+            {
+                if (!process.WaitForExit(500))
+                {
+                    return true;
+                }
+                return process.ExitCode == 0;
+            }
+            return false;
+        }
+
+        private bool TryStartBuiltinOpenSSH(Host host, string workdir)
+        {
+            var system = GetSystemPath();
+            var openSSH = new DirectoryInfo(Path.Combine(system, "OpenSSH"));
+            if (!openSSH.Exists)
+            {
+                return false;
+            }
+            var ssh = new FileInfo(Path.Combine(openSSH.FullName, "ssh.exe"));
+            if (!ssh.Exists)
+            {
+                return false;
+            }
+
+            var credentials = host.getCredentials();
+            var identity = credentials.isPublicKeyAuthentication();
+            var args = identity ? string.Format("-i \"{0}\"", credentials.getIdentity().getAbsolute()) : "";
+            var hostname = host.getHostname();
+            var username = credentials.getUsername();
+            var port = host.getPort();
+
+            Process process = new Process();
+            process.StartInfo.FileName = ssh.FullName;
+            process.StartInfo.Arguments = $"{args} {username}@{hostname} -T -P {port} \"cd \\\"{workdir}\\\";\\$SHELL\"";
+            return process.Start();
+        }
+
+        private bool TryStartPuTTy(Host host, string workdir)
         {
             if (!File.Exists(PreferencesFactory.get().getProperty("terminal.command.ssh")))
             {
@@ -48,20 +143,20 @@ namespace Ch.Cyberduck.Core
                 }
                 else
                 {
-                    return;
+                    return false;
                 }
             }
-            string tempFile = System.IO.Path.GetTempFileName();
+            string tempFile = Path.GetTempFileName();
             bool identity = host.getCredentials().isPublicKeyAuthentication();
             TextWriter tw = new StreamWriter(tempFile);
-            tw.WriteLine("cd {0} && exec $SHELL", workdir.getAbsolute());
+            tw.WriteLine("cd {0} && exec $SHELL", workdir);
             tw.Close();
             String ssh = String.Format(PreferencesFactory.get().getProperty("terminal.command.ssh.args"),
                 identity
                     ? string.Format("-i \"{0}\"", host.getCredentials().getIdentity().getAbsolute())
                     : String.Empty, host.getCredentials().getUsername(), host.getHostname(),
                 Convert.ToString(host.getPort()), tempFile);
-            ApplicationLauncherFactory.get()
+            return ApplicationLauncherFactory.get()
                 .open(
                     new Application(PreferencesFactory.get().getProperty("terminal.command.ssh"), null), ssh);
         }
