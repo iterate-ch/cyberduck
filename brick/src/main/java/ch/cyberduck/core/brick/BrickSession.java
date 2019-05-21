e39b87d64e51be5967b7b81d1d1567abf7767c21
David Kocher <dkocher@iterate.ch>
2019-05-21 23:08:26 +0200

    Use `POST` to query for pairing key.

--- brick/src/main/java/ch/cyberduck/core/brick/BrickSession.java
+++ brick/src/main/java/ch/cyberduck/core/brick/BrickSession.java
@@ -15,50 +15,43 @@ package ch.cyberduck.core.brick;
  * GNU General Public License for more details.
  */
 
-import ch.cyberduck.core.AbstractPath;
+import ch.cyberduck.core.Credentials;
 import ch.cyberduck.core.DefaultIOExceptionMappingService;
-import ch.cyberduck.core.DisabledConnectionCallback;
-import ch.cyberduck.core.DisabledHostKeyCallback;
-import ch.cyberduck.core.DisabledLoginCallback;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.HostKeyCallback;
-import ch.cyberduck.core.HostParser;
 import ch.cyberduck.core.LoginCallback;
-import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PreferencesUseragentProvider;
-import ch.cyberduck.core.ProtocolFactory;
 import ch.cyberduck.core.UUIDRandomStringService;
 import ch.cyberduck.core.dav.DAVClient;
-import ch.cyberduck.core.dav.DAVSSLProtocol;
 import ch.cyberduck.core.dav.DAVSession;
 import ch.cyberduck.core.dav.DAVUploadFeature;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.exception.LoginCanceledException;
 import ch.cyberduck.core.exception.LoginFailureException;
-import ch.cyberduck.core.exception.NotfoundException;
 import ch.cyberduck.core.features.Copy;
-import ch.cyberduck.core.features.Read;
 import ch.cyberduck.core.features.Upload;
 import ch.cyberduck.core.features.Write;
-import ch.cyberduck.core.io.StreamCopier;
+import ch.cyberduck.core.http.DefaultHttpResponseExceptionMappingService;
 import ch.cyberduck.core.local.BrowserLauncherFactory;
 import ch.cyberduck.core.proxy.Proxy;
 import ch.cyberduck.core.ssl.X509KeyManager;
 import ch.cyberduck.core.ssl.X509TrustManager;
 import ch.cyberduck.core.threading.CancelCallback;
-import ch.cyberduck.core.transfer.TransferStatus;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.AbstractResponseHandler;
 import org.apache.log4j.Logger;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.util.Collections;
-import java.util.EnumSet;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
@@ -87,56 +80,58 @@ public class BrickSession extends DAVSession {
     @Override
     public void login(final Proxy proxy, final LoginCallback prompt, final CancelCallback cancel) throws BackgroundException {
         try {
-            do {
+            final Credentials credentials = host.getCredentials();
+            if(!credentials.isPasswordAuthentication()) {
                 final String token = new BrickCredentialsConfigurator().configure(host).getToken();
-                // Query status
-                final Host resource = new HostParser(new ProtocolFactory(Collections.singleton(new DAVSSLProtocol() {
-                    @Override
-                    public boolean isEnabled() {
-                        return true;
-                    }
-                }))).get(String.format("https://app.files.com/api/rest/v1/sessions/pairing_key/%s", token));
-                if(log.isInfoEnabled()) {
-                    log.info(String.format("Fetch credentials for paring key %s from %s", token, resource));
+                if(!BrowserLauncherFactory.get().open(
+                    String.format("https://app.files.com/Login_For_Desktop?Pairing_Key=%s&Platform=%s&Computer=%s", token,
+                        new PreferencesUseragentProvider().get(), InetAddress.getLocalHost().getHostName())
+                )) {
+                    throw new LoginCanceledException();
                 }
-                final DAVSession connection = new DAVSession(resource, trust, key);
-                connection.open(proxy, new DisabledHostKeyCallback(), new DisabledLoginCallback());
-                final Read read = connection.getFeature(Read.class);
-                final TransferStatus options = new TransferStatus();
-                try {
-                    final InputStream in = read.read(new Path(resource.getDefaultPath(), EnumSet.of(AbstractPath.Type.file)), options, new DisabledConnectionCallback());
-                    final ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    final TransferStatus status = new TransferStatus().length(-1L);
-                    new StreamCopier(status, status).transfer(in, out);
-                    connection.close();
-                    final JsonParser parser = new JsonParser();
-                    final JsonObject json = parser.parse(new InputStreamReader(new ByteArrayInputStream(out.toByteArray()))).getAsJsonObject();
-                    if(json.has("username")) {
-                        host.getCredentials().setUsername(json.getAsJsonPrimitive("username").getAsString());
+                do {
+                    // Query status
+                    try {
+                        final HttpPost resource = new HttpPost(String.format("https://app.files.com/api/rest/v1/sessions/pairing_key/%s", token));
+                        if(log.isInfoEnabled()) {
+                            log.info(String.format("Fetch credentials for paring key %s from %s", token, resource));
+                        }
+                        final JsonObject json = client.execute(resource, new AbstractResponseHandler<JsonObject>() {
+                            @Override
+                            public JsonObject handleEntity(final HttpEntity entity) throws IOException {
+                                final ByteArrayOutputStream out = new ByteArrayOutputStream();
+                                IOUtils.copy(entity.getContent(), out);
+                                final JsonParser parser = new JsonParser();
+                                return parser.parse(new InputStreamReader(new ByteArrayInputStream(out.toByteArray()))).getAsJsonObject();
+                            }
+                        });
+                        if(json.has("username")) {
+                            credentials.setUsername(json.getAsJsonPrimitive("username").getAsString());
+                        }
+                        else {
+                            throw new LoginFailureException(String.format("Invalid response for pairing key %s", token));
+                        }
+                        if(json.has("password")) {
+                            credentials.setPassword(json.getAsJsonPrimitive("password").getAsString());
+                        }
+                        else {
+                            throw new LoginFailureException(String.format("Invalid response for pairing key %s", token));
+                        }
+                        break;
                     }
-                    else {
-                        throw new LoginFailureException(String.format("Invalid response for pairing key %s", token));
+                    catch(HttpResponseException e) {
+                        switch(e.getStatusCode()) {
+                            case HttpStatus.SC_NOT_FOUND:
+                                log.warn(String.format("Missing login for pairing key %s", token));
+                                cancel.verify();
+                                break;
+                            default:
+                                throw new DefaultHttpResponseExceptionMappingService().map(e);
+                        }
                     }
-                    if(json.has("password")) {
-                        host.getCredentials().setPassword(json.getAsJsonPrimitive("password").getAsString());
-                    }
-                    else {
-                        throw new LoginFailureException(String.format("Invalid response for pairing key %s", token));
-                    }
-                    break;
                 }
-                catch(NotfoundException e) {
-                    log.warn(String.format("Missing login for pairing key %s. %s", token, e.getDetail(false)));
-                    if(!BrowserLauncherFactory.get().open(
-                        String.format("https://app.files.com/Login_For_Desktop?Pairing_Key=%s&Platform=%s&Computer=%s", token,
-                            new PreferencesUseragentProvider().get(), InetAddress.getLocalHost().getHostName())
-                    )) {
-                        throw new LoginCanceledException();
-                    }
-                    cancel.verify();
-                }
+                while(true);
             }
-            while(true);
         }
         catch(JsonParseException e) {
             throw new DefaultIOExceptionMappingService().map(new IOException(e.getMessage(), e));
