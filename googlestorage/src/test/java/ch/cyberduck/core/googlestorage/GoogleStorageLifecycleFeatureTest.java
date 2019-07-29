c3ca689c550eaad00f7bd910f13676c59c419f28
David Kocher <dkocher@iterate.ch>
2019-07-29 16:00:10 +0200

    Add annotation.

--- googlestorage/src/test/java/ch/cyberduck/core/googlestorage/GoogleStorageLifecycleFeatureTest.java
+++ googlestorage/src/test/java/ch/cyberduck/core/googlestorage/GoogleStorageLifecycleFeatureTest.java
@@ -21,14 +21,17 @@ import ch.cyberduck.core.Path;
 import ch.cyberduck.core.features.Delete;
 import ch.cyberduck.core.lifecycle.LifecycleConfiguration;
 import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.test.IntegrationTest;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import java.util.Collections;
 import java.util.EnumSet;
 
 import static org.junit.Assert.assertEquals;
 
+@Category(IntegrationTest.class)
 public class GoogleStorageLifecycleFeatureTest extends AbstractGoogleStorageTest {
 
     @Test
