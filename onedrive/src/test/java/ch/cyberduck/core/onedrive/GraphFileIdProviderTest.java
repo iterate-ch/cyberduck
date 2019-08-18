8f7507315468ce8477802c07808ffcded142c90f
David Kocher <dkocher@iterate.ch>
2019-08-18 21:19:22 +0200

    Review.

--- onedrive/src/test/java/ch/cyberduck/core/onedrive/GraphFileIdProviderTest.java
+++ onedrive/src/test/java/ch/cyberduck/core/onedrive/GraphFileIdProviderTest.java
@@ -4,6 +4,8 @@ import ch.cyberduck.core.DisabledListProgressListener;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.features.Directory;
 import ch.cyberduck.core.features.IdProvider;
+import ch.cyberduck.core.onedrive.features.GraphDirectoryFeature;
+import ch.cyberduck.core.onedrive.features.GraphFileIdProvider;
 import ch.cyberduck.core.transfer.TransferStatus;
 import ch.cyberduck.test.IntegrationTest;
 
@@ -22,15 +24,17 @@ public class GraphFileIdProviderTest extends AbstractOneDriveTest {
         final Path path2R = new Path("/2R", EnumSet.of(Path.Type.directory));
         final Path path33 = new Path("/33", EnumSet.of(Path.Type.directory));
 
-        final Directory directoryFeature = session._getFeature(Directory.class);
+        final Directory directoryFeature = new GraphDirectoryFeature(session);
         final Path path2RWithId = directoryFeature.mkdir(path2R, null, new TransferStatus());
+        assertNotNull(path2RWithId.attributes().getVersionId());
         final Path path33WithId = directoryFeature.mkdir(path33, null, new TransferStatus());
+        assertNotNull(path33WithId.attributes().getVersionId());
+        assertNotEquals(path2RWithId.attributes().getVersionId(), path33WithId.attributes().getVersionId());
 
-        final IdProvider idProvider = session._getFeature(IdProvider.class);
+        final IdProvider idProvider = new GraphFileIdProvider(session);
         final String fileId = idProvider.getFileid(path33, new DisabledListProgressListener());
 
         assertEquals(fileId, path33WithId.attributes().getVersionId());
         assertNotEquals(fileId, path2RWithId.attributes().getVersionId());
     }
-
 }
