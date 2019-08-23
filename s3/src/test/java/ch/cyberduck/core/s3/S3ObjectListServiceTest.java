e5609bd7752731d02d7b621fb691a1cbc3c7400b
David Kocher <dkocher@iterate.ch>
2019-08-23 21:51:42 +0200

    Add test.

--- s3/src/test/java/ch/cyberduck/core/s3/S3ObjectListServiceTest.java
+++ s3/src/test/java/ch/cyberduck/core/s3/S3ObjectListServiceTest.java
@@ -136,7 +136,7 @@ public class S3ObjectListServiceTest extends AbstractS3Test {
         final Path container = new Path("test-us-east-1-cyberduck", EnumSet.of(Path.Type.directory, Path.Type.volume));
         container.attributes().setRegion("us-east-1");
         final Path placeholder = new S3TouchFeature(session).touch(
-            new Path(container, String.format("<%%%s", new AlphanumericRandomStringService().random()), EnumSet.of(Path.Type.file)), new TransferStatus());
+            new Path(container, String.format("^<%%%s", new AlphanumericRandomStringService().random()), EnumSet.of(Path.Type.file)), new TransferStatus());
         assertTrue(new S3ObjectListService(session).list(container, new DisabledListProgressListener()).contains(placeholder));
         new S3DefaultDeleteFeature(session).delete(Collections.singletonList(placeholder), new DisabledLoginCallback(), new Delete.DisabledCallback());
     }
