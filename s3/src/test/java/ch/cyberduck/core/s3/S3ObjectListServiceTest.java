db1ab7f13b52a1bb385af61a19380278e048a606
David Kocher <dkocher@iterate.ch>
2019-08-29 09:24:44 +0200

    Add test.

--- s3/src/test/java/ch/cyberduck/core/s3/S3ObjectListServiceTest.java
+++ s3/src/test/java/ch/cyberduck/core/s3/S3ObjectListServiceTest.java
@@ -142,6 +142,16 @@ public class S3ObjectListServiceTest extends AbstractS3Test {
     }
 
     @Test
+    public void testListInvisibleCharacter() throws Exception {
+        final Path container = new Path("test-us-east-1-cyberduck", EnumSet.of(Path.Type.directory, Path.Type.volume));
+        container.attributes().setRegion("us-east-1");
+        final Path placeholder = new S3TouchFeature(session).touch(
+            new Path(container, String.format("test-\u001F-%s", new AlphanumericRandomStringService().random()), EnumSet.of(Path.Type.file)), new TransferStatus());
+        assertTrue(new S3ObjectListService(session).list(container, new DisabledListProgressListener()).contains(placeholder));
+        new S3DefaultDeleteFeature(session).delete(Collections.singletonList(placeholder), new DisabledLoginCallback(), new Delete.DisabledCallback());
+    }
+
+    @Test
     public void testListPlaceholder() throws Exception {
         final Path container = new Path("test-us-east-1-cyberduck", EnumSet.of(Path.Type.directory, Path.Type.volume));
         container.attributes().setRegion("us-east-1");
