a8e967dc75254fb542cb0704884fd85305618aee
David Kocher <dkocher@iterate.ch>
2019-09-25 14:20:32 +0200

    Add test.

--- webdav/src/test/java/ch/cyberduck/core/dav/DAVSessionTest.java
+++ webdav/src/test/java/ch/cyberduck/core/dav/DAVSessionTest.java
@@ -569,4 +569,40 @@ public class DAVSessionTest extends AbstractDAVTest {
         assertTrue(verified.get());
         session.close();
     }
+
+    @Test(expected = ConnectionRefusedException.class)
+    public void testConnectProxyHttps() throws Exception {
+        final Host host = new Host(new DAVSSLProtocol(), "svn.cyberduck.io");
+        final AtomicBoolean verified = new AtomicBoolean();
+        final DAVSession session = new DAVSession(host, new DefaultX509TrustManager() {
+            @Override
+            public void verify(final String hostname, final X509Certificate[] certs, final String cipher) throws CertificateException {
+                assertNotNull(hostname);
+                verified.set(true);
+                super.verify(hostname, certs, cipher);
+            }
+        },
+            new KeychainX509KeyManager(host, new DisabledCertificateStore())) {
+        };
+        final LoginConnectionService c = new LoginConnectionService(
+            new DisabledLoginCallback() {
+                @Override
+                public Credentials prompt(final Host bookmark, final String username, final String title, final String reason, final LoginOptions options) {
+                    return new Credentials("test", "test");
+                }
+            },
+            new DisabledHostKeyCallback(),
+            new DisabledPasswordStore(),
+            new DisabledProgressListener(),
+            new ProxyFinder() {
+                @Override
+                public Proxy find(final Host target) {
+                    return new Proxy(Proxy.Type.HTTPS, "localhost", 8080);
+                }
+            }
+        );
+        c.connect(session, PathCache.empty(), new DisabledCancelCallback());
+        assertTrue(verified.get());
+        session.close();
+    }
 }
