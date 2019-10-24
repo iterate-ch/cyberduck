fc42bdc8d0255f8b18a078918980d972dff506b3
David Kocher <dkocher@iterate.ch>
2019-10-24 10:48:56 +0200

    Mark pending uploads with duplicate flag in addition to upload type in file mask. When searching for attributes upload type is not included.

--- backblaze/src/main/java/ch/cyberduck/core/b2/B2ObjectListService.java
+++ backblaze/src/main/java/ch/cyberduck/core/b2/B2ObjectListService.java
@@ -170,10 +170,11 @@ public class B2ObjectListService implements ListService {
                 // File version marking the file as hidden, so that it will not show up in b2_list_file_names
             case start:
                 // Large file has been started, but not finished or canceled
-                attributes.setSize(-1L);
+                attributes.setDuplicate(true);
                 break;
             default:
                 attributes.setSize(response.getContentLength());
+                break;
         }
         return attributes;
     }
