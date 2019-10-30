b90ddfd4148cf8c3d114bc3088a86ba783ca506f
David Kocher <dkocher@iterate.ch>
2019-10-30 13:16:37 +0100

    Fix setting.

--- core/src/main/csharp/ch/cyberduck/core/preferences/SettingsDictionaryPreferences.cs
+++ core/src/main/csharp/ch/cyberduck/core/preferences/SettingsDictionaryPreferences.cs
@@ -294,7 +294,7 @@ namespace Ch.Cyberduck.Core.Preferences
             this.setDefault("local.delimiter", "\\");
             this.setDefault("local.normalize.tilde", false.ToString());
 
-            this.setDefault("connection.ssl.provider.conscrypt", String.valueOf(false));
+            this.setDefault("connection.ssl.provider.conscrypt", false.ToString());
             // SSL Keystore
             // Add mscapi security provider
             Security.addProvider(new SunMSCAPI());
