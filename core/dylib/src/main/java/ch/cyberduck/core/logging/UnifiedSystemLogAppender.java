c6466ca3e28fdc334502fb4149ed7544642beb6f
David Kocher <dkocher@iterate.ch>
2019-11-21 22:02:42 +0100

    Javadoc.

--- core/dylib/src/main/java/ch/cyberduck/core/logging/UnifiedSystemLogAppender.java
+++ core/dylib/src/main/java/ch/cyberduck/core/logging/UnifiedSystemLogAppender.java
@@ -31,10 +31,43 @@ public class UnifiedSystemLogAppender extends AppenderSkeleton {
         Native.load("core");
     }
 
+    /**
+     * Default-level messages are initially stored in memory buffers. Without a configuration change, they are
+     * compressed and moved to the data store as memory buffers fill. They remain there until a storage quota is
+     * exceeded, at which point, the oldest messages are purged. Use this level to capture information about things that
+     * might result a failure. Logging a message of this type is equivalent to calling the os_log function.
+     */
     public static final int OS_LOG_TYPE_DEFAULT = 0x00;
+    /**
+     * Info-level messages are initially stored in memory buffers. Without a configuration change, they are not moved to
+     * the data store and are purged as memory buffers fill. They are, however, captured in the data store when faults
+     * and, optionally, errors occur. When info-level messages are added to the data store, they remain there until a
+     * storage quota is exceeded, at which point, the oldest messages are purged. Use this level to capture information
+     * that may be helpful, but isn’t essential, for troubleshooting errors. Logging a message of this type is
+     * equivalent to calling the os_log_info function.
+     */
     public static final int OS_LOG_TYPE_INFO = 0x01;
+    /**
+     * Debug-level messages are only captured in memory when debug logging is enabled through a configuration change.
+     * They’re purged in accordance with the configuration’s persistence setting. Messages logged at this level contain
+     * information that may be useful during development or while troubleshooting a specific problem. Debug logging is
+     * intended for use in a development environment and not in shipping software. Logging a message of this type is
+     * equivalent to calling the os_log_debug function.
+     */
     public static final int OS_LOG_TYPE_DEBUG = 0x02;
+    /**
+     * Error-level messages are always saved in the data store. They remain there until a storage quota is exceeded, at
+     * which point, the oldest messages are purged. Error-level messages are intended for reporting process-level
+     * errors. If an activity object exists, logging at this level captures information for the entire process chain.
+     * Logging a message of this type is equivalent to calling the os_log_error function.
+     */
     public static final int OS_LOG_TYPE_ERROR = 0x10;
+    /**
+     * Fault-level messages are always saved in the data store. They remain there until a storage quota is exceeded, at
+     * which point, the oldest messages are purged. Fault-level messages are intended for capturing system-level or
+     * multi-process errors only. If an activity object exists, logging at this level captures information for the
+     * entire process chain. Logging a message at this level is equivalent to calling the os_log_fault function.
+     */
     public static final int OS_LOG_TYPE_FAULT = 0x11;
 
     @Override
