908089964b92d9ed4bc95fd590d34d5b8ada4292
David Kocher <dkocher@iterate.ch>
2019-12-06 14:40:26 +0100

    Delete tests requiring view.

--- core/dylib/src/test/java/ch/cyberduck/core/KeychainCertificateStoreTest.java
+++ core/dylib/src/test/java/ch/cyberduck/core/KeychainCertificateStoreTest.java
@@ -4,14 +4,9 @@ import ch.cyberduck.binding.WindowController;
 
 import org.junit.Test;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
 import java.util.Collections;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class KeychainCertificateStoreTest {
 
@@ -24,33 +19,5 @@ public class KeychainCertificateStoreTest {
             }
         });
         assertFalse(k.isTrusted("cyberduck.ch", Collections.emptyList()));
-    }
-
-    @Test
-    public void testTrusted() throws Exception {
-        final KeychainCertificateStore k = new KeychainCertificateStore(new WindowController() {
-            @Override
-            protected String getBundleName() {
-                return null;
-            }
-        });
-        InputStream inStream = new FileInputStream("src/test/resources/OXxlRDVcWqdPEvFm.cer");
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        final X509Certificate cert = (X509Certificate) cf.generateCertificate(inStream);
-        assertTrue(k.isTrusted("test.cyberduck.ch", Collections.singletonList(cert)));
-    }
-
-    @Test
-    public void testTrustedHostnameMismatch() throws Exception {
-        final KeychainCertificateStore k = new KeychainCertificateStore(new WindowController() {
-            @Override
-            protected String getBundleName() {
-                return null;
-            }
-        });
-        InputStream inStream = new FileInputStream("src/test/resources/OXxlRDVcWqdPEvFm.cer");
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        final X509Certificate cert = (X509Certificate) cf.generateCertificate(inStream);
-        assertTrue(k.isTrusted("s.test.cyberduck.ch", Collections.singletonList(cert)));
     }
 }
