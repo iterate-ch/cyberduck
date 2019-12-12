e6ffec43f876ee58366e9c36ac7d123fb09ecf02
jmalek <jmalek@iterate.ch>
2019-12-12 10:52:21 +0100

    Provide TaskDialogExtensions

---
+++ core/src/main/csharp/ch/cyberduck/core/taskdialog/TaskDialogExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch.Cyberduck.Core.TaskDialog
+{
+    public static class TaskDialogExtensions
+    {
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+        public static void MakeTopMost(this IActiveTaskDialog taskDialog)
+        {
+            if (!(taskDialog is ActiveTaskDialog activeTaskDialog)) return;
+            // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowpos
+            // HWND_TOPMOST (HWND) - 1
+            // Places the window above all non-topmost windows.The window maintains its topmost position even when it is deactivated.
+
+           NativeMethods.SetWindowPos(
+                activeTaskDialog.Handle, HWND_TOPMOST,
+                0, 0, 0, 0,
+                Win32Constants.SWP_NOSIZE | Win32Constants.SWP_NOMOVE);
+        }
+    }
+}
