968c6a7407d0c84981e7da4605ee65b61cc8acc9
jmalek <jmalek@iterate.ch>
2020-03-19 13:39:11 +0100

    Add failing test

--- core/src/test/java/ch/cyberduck/core/ProtocolFactoryTest.java
+++ core/src/test/java/ch/cyberduck/core/ProtocolFactoryTest.java
@@ -25,6 +25,10 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.google.common.collect.Sets;
 
 import static org.junit.Assert.*;
 
@@ -142,5 +146,51 @@ public class ProtocolFactoryTest {
         }))).read(
             new Local("src/test/resources/Unknown.cyberduckprofile")
         );
+    }
+
+    @Test
+    public void testOverrideBundledProviderProtocols() {
+        final TestProtocol baseProtocol = new TestProtocol(Scheme.http);
+        final TestProtocol overrideProtocol = new TestProtocol(Scheme.http) {
+            @Override
+            public String getProvider() {
+                return "test-provider";
+            }
+
+            @Override
+            public boolean isBundled() {
+                return true;
+            }
+        };
+        final ProtocolFactory f = new ProtocolFactory(Stream.of(baseProtocol, overrideProtocol).collect(Collectors.toSet()));
+        assertEquals(overrideProtocol, f.forName("test", "test-provider"));
+    }
+
+    @Test
+    public void testOverrideBundledProtocols() {
+        final TestProtocol baseProtocol = new TestProtocol(Scheme.http) {
+            @Override
+            public String getProvider() {
+                return "test-provider1";
+            }
+
+            @Override
+            public boolean isBundled() {
+                return true;
+            }
+        };
+        final TestProtocol overrideProtocol = new TestProtocol(Scheme.http) {
+            @Override
+            public String getProvider() {
+                return "test-provider2";
+            }
+
+            @Override
+            public boolean isBundled() {
+                return true;
+            }
+        };
+        final ProtocolFactory f = new ProtocolFactory(Stream.of(baseProtocol, overrideProtocol).collect(Collectors.toSet()));
+        assertEquals(overrideProtocol, f.forName("test", "test-provider2"));
     }
 }
