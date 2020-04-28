d8a13904de78d78fbc0bb18eb0641426521012e1
David Kocher <dkocher@iterate.ch>
2020-04-28 13:44:33 +0200

    Fix test.

--- s3/src/test/java/ch/cyberduck/core/s3/S3StorageClassFeatureTest.java
+++ s3/src/test/java/ch/cyberduck/core/s3/S3StorageClassFeatureTest.java
@@ -41,7 +41,8 @@ public class S3StorageClassFeatureTest extends AbstractS3Test {
 
     @Test
     public void testGetClasses() {
-        assertEquals(Arrays.asList(S3Object.STORAGE_CLASS_STANDARD, S3Object.STORAGE_CLASS_INFREQUENT_ACCESS, "ONEZONE_IA", S3Object.STORAGE_CLASS_REDUCED_REDUNDANCY, S3Object.STORAGE_CLASS_GLACIER),
+        assertEquals(Arrays.asList(S3Object.STORAGE_CLASS_STANDARD, "INTELLIGENT_TIERING",
+            S3Object.STORAGE_CLASS_INFREQUENT_ACCESS, "ONEZONE_IA", S3Object.STORAGE_CLASS_REDUCED_REDUNDANCY, S3Object.STORAGE_CLASS_GLACIER, "DEEP_ARCHIVE"),
             new S3StorageClassFeature(new S3Session(new Host(new S3Protocol()))).getClasses());
     }
 
