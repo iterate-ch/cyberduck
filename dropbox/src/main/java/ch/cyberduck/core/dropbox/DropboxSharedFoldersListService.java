b196eb400757a8b8007a8df403dd36fb07815f7a
David Kocher <dkocher@iterate.ch>
2020-04-30 15:44:26 +0200

    Add list service for shared folders.

---
+++ dropbox/src/main/java/ch/cyberduck/core/dropbox/DropboxSharedFoldersListService.java
@@ -0,0 +1,79 @@
+package ch.cyberduck.core.dropbox;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANYdrivelexico WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.ListProgressListener;
+import ch.cyberduck.core.ListService;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.PathNormalizer;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.ConnectionCanceledException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+
+import java.util.EnumSet;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.sharing.DbxUserSharingRequests;
+import com.dropbox.core.v2.sharing.ListFoldersResult;
+import com.dropbox.core.v2.sharing.SharedFolderMetadata;
+
+public class DropboxSharedFoldersListService implements ListService {
+    private static final Logger log = Logger.getLogger(DropboxSharedFoldersListService.class);
+
+    private final DropboxSession session;
+
+    public DropboxSharedFoldersListService(final DropboxSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public AttributedList<Path> list(final Path directory, final ListProgressListener listener) throws BackgroundException {
+        try {
+            final AttributedList<Path> children = new AttributedList<>();
+            final String path = directory.isRoot() ? StringUtils.EMPTY : directory.getAbsolute();
+            ListFoldersResult listFoldersResult;
+            this.parse(directory, listener, children, listFoldersResult = new DbxUserSharingRequests(session.getClient()).listFolders());
+            while(listFoldersResult.getCursor() != null) {
+                this.parse(directory, listener, children, listFoldersResult = new DbxUserSharingRequests(session.getClient())
+                    .listMountableFoldersContinue(listFoldersResult.getCursor()));
+            }
+            return children;
+        }
+        catch(DbxException e) {
+            throw new DropboxExceptionMappingService().map("Listing directory {0} failed", e, directory);
+        }
+    }
+
+    private void parse(final Path directory, final ListProgressListener listener, final AttributedList<Path> children, final ListFoldersResult result) throws ConnectionCanceledException {
+        for(SharedFolderMetadata metadata : result.getEntries()) {
+            final Path f = this.parse(directory, metadata);
+            // Reference team folder
+            f.attributes().withVersionId(metadata.getSharedFolderId());
+            children.add(f);
+            listener.chunk(directory, children);
+        }
+    }
+
+    protected Path parse(final Path directory, final SharedFolderMetadata metadata) {
+        final PathAttributes attr = new PathAttributes();
+        return new Path(directory, PathNormalizer.name(metadata.getName()),
+            EnumSet.of(Path.Type.directory, Path.Type.volume, Path.Type.shared), attr);
+    }
+}
---
+++ dropbox/src/test/java/ch/cyberduck/core/dropbox/DropboxSharedFoldersListServiceTest.java
@@ -0,0 +1,42 @@
+package ch.cyberduck.core.dropbox;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AbstractDropboxTest;
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.DisabledListProgressListener;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.test.IntegrationTest;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.EnumSet;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@Category(IntegrationTest.class)
+public class DropboxSharedFoldersListServiceTest extends AbstractDropboxTest {
+
+    @Test
+    public void testList() throws Exception {
+        final AttributedList<Path> list = new DropboxSharedFoldersListService(session).list(
+            new Path("/", EnumSet.of(Path.Type.directory, Path.Type.volume)), new DisabledListProgressListener());
+        assertNotNull(list);
+        assertTrue(list.isEmpty());
+    }
+}
