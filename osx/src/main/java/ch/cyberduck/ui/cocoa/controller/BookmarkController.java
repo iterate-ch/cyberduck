3444e7f7f68cfd70103a51baf552d3a458bf4cd4
David Kocher <dkocher@iterate.ch>
2020-07-31 16:50:20 +0200

    Make sure disabled items are not enabled automatically.
    Signed-off-by: David Kocher <dkocher@iterate.ch>

--- osx/src/main/java/ch/cyberduck/ui/cocoa/controller/BookmarkController.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/controller/BookmarkController.java
@@ -136,6 +136,7 @@ public class BookmarkController extends SheetController implements CollectionLis
     public void setProtocolPopup(final NSPopUpButton button) {
         this.protocolPopup = button;
         this.protocolPopup.setEnabled(true);
+        this.protocolPopup.setAutoenablesItems(false);
         this.protocolPopup.setTarget(this.id());
         this.protocolPopup.setAction(Foundation.selector("protocolSelectionChanged:"));
         this.protocolPopup.removeAllItems();
