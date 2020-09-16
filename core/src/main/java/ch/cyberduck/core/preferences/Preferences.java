924ff9b17a1b600904312498867c1fec53ddafe8
David Kocher <dkocher@iterate.ch>
2020-09-16 08:09:26 +0200

    Add profile to authenticate using OAuth Password Flow.

--- core/src/main/java/ch/cyberduck/core/preferences/Preferences.java
+++ core/src/main/java/ch/cyberduck/core/preferences/Preferences.java
@@ -823,7 +823,7 @@ public abstract class Preferences implements Locales {
         this.setDefault("sds.upload.s3.enable", String.valueOf(true));
         this.setDefault("sds.versioning.references.enable", String.valueOf(false));
         this.setDefault("sds.upload.s3.status.period", String.valueOf(1000)); // 1 second
-        this.setDefault("sds.oauth.migrate.enable", String.valueOf(false));
+        this.setDefault("sds.oauth.migrate.enable", String.valueOf(true));
 
         this.setDefault("spectra.retry.delay", String.valueOf(60)); // 1 minute
 
