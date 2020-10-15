7a85e20296782acfa7f2bf438f5828c82b2d18e0
David Kocher <dkocher@iterate.ch>
2020-10-15 10:04:59 +0200

    Add other result types.

--- core/dylib/src/main/java/ch/cyberduck/core/keychain/SecTrustResultType.java
+++ core/dylib/src/main/java/ch/cyberduck/core/keychain/SecTrustResultType.java
@@ -20,6 +20,12 @@ import com.sun.jna.ptr.IntByReference;
 public class SecTrustResultType extends IntByReference {
 
     /**
+     * An indication of an invalid setting or result. This result typically indicates that the evaluation failed to
+     * complete successfully.
+     */
+    public static final int kSecTrustResultInvalid = 0;
+
+    /**
      * This value indicates that the user explicitly chose to trust a certificate in the chain, usually by clicking a
      * button in a certificate trust panel. Your app should trust the chain. The Keychain Access utility refers to this
      * value as “Always Trust.”
@@ -47,5 +53,20 @@ public class SecTrustResultType extends IntByReference {
      * of trusted anchors.
      */
     public static final int kSecTrustResultRecoverableTrustFailure = 5;
+
+    /**
+     * This value indicates that evaluation failed because a certificate in the chain is defective. This usually
+     * represents a fundamental defect in the certificate data, such as an invalid encoding for a critical
+     * subjectAltName extension, an unsupported critical extension, or some other critical portion of the certificate
+     * that couldn’t be interpreted. Changing parameter values and reevaluating is unlikely to succeed unless you
+     * provide different certificates.
+     */
+    public static final int kSecTrustResultFatalTrustFailure = 6;
+
+    /**
+     * This value indicates that evaluation failed for some other reason. This can be caused by either a revoked
+     * certificate or by OS-level errors that are unrelated to the certificates themselves.
+     */
+    public static final int kSecTrustResultOtherError = 7;
 
 }
