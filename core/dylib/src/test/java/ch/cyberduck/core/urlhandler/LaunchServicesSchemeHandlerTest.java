916266e0a2f68b6a838f982c07a65013e4e353cb
David Kocher <dkocher@iterate.ch>
2021-01-03 15:35:13 +0100

    Rewrite implementation.

--- core/dylib/src/test/java/ch/cyberduck/core/urlhandler/LaunchServicesSchemeHandlerTest.java
+++ core/dylib/src/test/java/ch/cyberduck/core/urlhandler/LaunchServicesSchemeHandlerTest.java
@@ -20,12 +20,13 @@ public class LaunchServicesSchemeHandlerTest {
         l.setDefaultHandler(
             new Application("none.app", null), Collections.singletonList(Scheme.ftp.name())
         );
-        assertEquals(Application.notfound, l.getDefaultHandler(Scheme.ftp.name()));
+        assertEquals(new Application("com.apple.finder"), l.getDefaultHandler(Scheme.ftp.name()));
         assertFalse(l.isDefaultHandler(Collections.singletonList(Scheme.ftp.name()), new Application("other.app", null)));
         l.setDefaultHandler(
             new Application("ch.sudo.cyberduck", null), Collections.singletonList(Scheme.ftp.name())
         );
         assertEquals("ch.sudo.cyberduck", l.getDefaultHandler(Scheme.ftp.name()).getIdentifier());
+        assertTrue(l.getAllHandlers(Scheme.ftp.name()).contains(new Application("ch.sudo.cyberduck")));
         assertNotSame("ch.sudo.cyberduck", l.getDefaultHandler(Scheme.http.name()).getIdentifier());
         assertTrue(l.isDefaultHandler(Collections.singletonList(Scheme.ftp.name()), new Application("ch.sudo.cyberduck", null)));
     }
