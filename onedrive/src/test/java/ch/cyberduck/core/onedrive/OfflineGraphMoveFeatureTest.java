35bd6f0755dc8c8914ebba7bfbfab1ed50251077
jmalek <jmalek@iterate.ch>
2021-02-01 10:43:12 +0100

    Review Tests

--- onedrive/src/test/java/ch/cyberduck/core/onedrive/OfflineGraphMoveFeatureTest.java
+++ onedrive/src/test/java/ch/cyberduck/core/onedrive/OfflineGraphMoveFeatureTest.java
@@ -20,7 +20,6 @@ import ch.cyberduck.core.features.Move;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DisabledX509TrustManager;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -30,6 +29,82 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class OfflineGraphMoveFeatureTest {
+    @Test
+    public void testSharepoint() {
+        final SharepointSession session = new SharepointSession(new Host(new TestProtocol()), new DisabledX509TrustManager(), new DefaultX509KeyManager());
+        final Move move = session.getFeature(Move.class);
+        final List<TestCase> cases = new ArrayList<>();
+        // cannot rename root
+        cases.add(new TestCase("/", "/", false));
+        // cannot rename Documents to root
+        cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/", "/", false));
+        // Cannot rename Drive
+        cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/", false));
+        // can rename drive subitems
+        cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/Subfolder/", true));
+        // cannot move to root
+        cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/Subfolder/", "/", false));
+        // cannot move Drives to Root
+        cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/", "/", false));
+        // cannot rename Drives
+        cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/", false));
+        // Cannot rename Site
+        cases.add(new TestCase("/Sites/tenant.sharepoint.com/", false));
+        // cannot rename Sites
+        cases.add(new TestCase("/Sites/", false));
+        // cannot move group Documents to root
+        cases.add(new TestCase("/Groups/Group Name/Documents/", "/", false));
+        // cannot rename group Drive
+        cases.add(new TestCase("/Groups/Group Name/Documents/", false));
+        // can rename in group drive
+        cases.add(new TestCase("/Groups/Group Name/Documents/Subfolder/", true));
+        // cannot move to root
+        cases.add(new TestCase("/Groups/Group Name/Documents/Subfolder/", "/", false));
+        // cannot rename group
+        cases.add(new TestCase("/Groups/Group Name/", false));
+        // cannot rename groups
+        cases.add(new TestCase("/Groups/", false));
+        // cannot move over drive boundary
+        cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/Folder", "/Groups/Group Name/Documents/Folder", false));
+
+        for(TestCase testCase : cases) {
+            final Path from = new Path(testCase.getSource(), EnumSet.of(Path.Type.directory));
+            final Path to = new Path(testCase.getTarget(), EnumSet.of(Path.Type.directory));
+
+            assertEquals(String.format("Move \"%s\" to \"%s\".", from.getAbsolute(), to.getAbsolute()), testCase.isValid(), move.isSupported(from, to));
+        }
+    }
+
+    @Test
+    public void testOneDrive() {
+        final OneDriveSession session = new OneDriveSession(new Host(new TestProtocol()), new DisabledX509TrustManager(), new DefaultX509KeyManager());
+        final Move move = session.getFeature(Move.class);
+        final List<TestCase> cases = new ArrayList<>();
+        // cannot rename root
+        cases.add(new TestCase("/", "/", false));
+        // cannot move My Files to root
+        cases.add(new TestCase("/My Files/", "/", false));
+        // cannot rename My Files
+        cases.add(new TestCase("/My Files/", false));
+        // cannot move folder to root
+        cases.add(new TestCase("/My Files/Folder", "/", false));
+        // can move rename folder
+        cases.add(new TestCase("/My Files/Folder", "/My Files/Sub Folder/Folder 2", true));
+        // cannot rename Shared
+        cases.add(new TestCase("/Shared/", false));
+        // cannot rename item in Shared
+        cases.add(new TestCase("/Shared/Folder", false));
+        // can rename item in folder in Shared
+        cases.add(new TestCase("/Shared/Folder/Item", true));
+
+        for(TestCase testCase : cases) {
+            final Path from = new Path(testCase.getSource(), EnumSet.of(Path.Type.directory));
+            final Path to = new Path(testCase.getTarget(), EnumSet.of(Path.Type.directory));
+
+            assertEquals(String.format("Move \"%s\" to \"%s\".", from, to), testCase.isValid(), move.isSupported(from, to));
+        }
+    }
+
     private static class TestCase {
         private final String source;
         private final String target;
@@ -57,98 +132,6 @@ public class OfflineGraphMoveFeatureTest {
 
         public boolean isValid() {
             return isValid;
-        }
-    }
-
-    public static class Sharepoint {
-        private SharepointSession session;
-
-        @Before
-        public void setup() {
-            session = new SharepointSession(new Host(new TestProtocol()), new DisabledX509TrustManager(), new DefaultX509KeyManager());
-        }
-
-        @Test
-        public void test() {
-            final Move move = session.getFeature(Move.class);
-            final List<TestCase> cases = new ArrayList<>();
-            // cannot rename root
-            cases.add(new TestCase("/", "/", false));
-            // cannot rename Documents to root
-            cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/", "/", false));
-            // Cannot rename Drive
-            cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/", false));
-            // can rename drive subitems
-            cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/Subfolder/", true));
-            // cannot move to root
-            cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/Subfolder/", "/", false));
-            // cannot move Drives to Root
-            cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/", "/", false));
-            // cannot rename Drives
-            cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/", false));
-            // Cannot rename Site
-            cases.add(new TestCase("/Sites/tenant.sharepoint.com/", false));
-            // cannot rename Sites
-            cases.add(new TestCase("/Sites/", false));
-            // cannot move group Documents to root
-            cases.add(new TestCase("/Groups/Group Name/Documents/", "/", false));
-            // cannot rename group Drive
-            cases.add(new TestCase("/Groups/Group Name/Documents/", false));
-            // can rename in group drive
-            cases.add(new TestCase("/Groups/Group Name/Documents/Subfolder/", true));
-            // cannot move to root
-            cases.add(new TestCase("/Groups/Group Name/Documents/Subfolder/", "/", false));
-            // cannot rename group
-            cases.add(new TestCase("/Groups/Group Name/", false));
-            // cannot rename groups
-            cases.add(new TestCase("/Groups/", false));
-            // cannot move over drive boundary
-            cases.add(new TestCase("/Sites/tenant.sharepoint.com/Drives/Documents/Folder", "/Groups/Group Name/Documents/Folder", false));
-
-            for(TestCase testCase : cases) {
-                final Path from = new Path(testCase.getSource(), EnumSet.of(Path.Type.directory));
-                final Path to = new Path(testCase.getTarget(), EnumSet.of(Path.Type.directory));
-
-                assertEquals(String.format("Move \"%s\" to \"%s\".", from.getAbsolute(), to.getAbsolute()), testCase.isValid(), move.isSupported(from, to));
-            }
-        }
-    }
-
-    public static class OneDrive {
-        private OneDriveSession session;
-
-        @Before
-        public void setup() {
-            session = new OneDriveSession(new Host(new TestProtocol()), new DisabledX509TrustManager(), new DefaultX509KeyManager());
-        }
-
-        @Test
-        public void test() {
-            final Move move = session.getFeature(Move.class);
-            final List<TestCase> cases = new ArrayList<>();
-            // cannot rename root
-            cases.add(new TestCase("/", "/", false));
-            // cannot move My Files to root
-            cases.add(new TestCase("/My Files/", "/", false));
-            // cannot rename My Files
-            cases.add(new TestCase("/My Files/", false));
-            // cannot move folder to root
-            cases.add(new TestCase("/My Files/Folder", "/", false));
-            // can move rename folder
-            cases.add(new TestCase("/My Files/Folder", "/My Files/Sub Folder/Folder 2", true));
-            // cannot rename Shared
-            cases.add(new TestCase("/Shared/", false));
-            // cannot rename item in Shared
-            cases.add(new TestCase("/Shared/Folder", false));
-            // can rename item in folder in Shared
-            cases.add(new TestCase("/Shared/Folder/Item", true));
-
-            for(TestCase testCase : cases) {
-                final Path from = new Path(testCase.getSource(), EnumSet.of(Path.Type.directory));
-                final Path to = new Path(testCase.getTarget(), EnumSet.of(Path.Type.directory));
-
-                assertEquals(String.format("Move \"%s\" to \"%s\".", from, to), testCase.isValid(), move.isSupported(from, to));
-            }
         }
     }
 }
