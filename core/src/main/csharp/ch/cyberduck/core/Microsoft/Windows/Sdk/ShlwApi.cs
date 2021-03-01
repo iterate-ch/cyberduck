90e961d9c4ca8c423abe288ded10e5661e6349e6
jmalek <jmalek@iterate.ch>
2021-03-01 14:06:39 +0100

    Add generic AssocCreate

---
+++ core/src/main/csharp/ch/cyberduck/core/Microsoft/Windows/Sdk/ShlwApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ch.Cyberduck.Core.Microsoft.Windows.Sdk
+{
+    partial class PInvoke
+    {
+        /// <inheritdoc cref = "AssocCreate(Guid, Guid*, void **)"/>
+        public static unsafe HRESULT AssocCreate<T>(Guid clsid, out T* ppv)
+            where T : unmanaged
+        {
+            var riid = typeof(T).GUID;
+            HRESULT __result = AssocCreate(clsid, riid, out var ppvLocal);
+            ppv = (T*)ppvLocal;
+            return __result;
+        }
+    }
+}
