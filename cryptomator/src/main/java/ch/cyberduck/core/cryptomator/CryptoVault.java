cf865288a058e4716915b17a0e0f54a52455f670
David Kocher <dkocher@iterate.ch>
2021-03-03 16:18:01 +0100

    Do not check save password checkbox by default.

--- cryptomator/src/main/java/ch/cyberduck/core/cryptomator/CryptoVault.java
+++ cryptomator/src/main/java/ch/cyberduck/core/cryptomator/CryptoVault.java
@@ -214,6 +214,7 @@ public class CryptoVault implements Vault {
                 bookmark, LocaleFactory.localizedString("Unlock Vault", "Cryptomator"),
                 message,
                 new LoginOptions()
+                    .keychain(preferences.getBoolean("vault.keychain"))
                     .user(false)
                     .anonymous(false)
                     .icon("cryptomator.tiff")
