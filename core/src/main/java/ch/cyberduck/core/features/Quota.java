7bf9c50382898a39f1a1a08b51cdbc281c76dc06
David Kocher <dkocher@iterate.ch>
2021-03-18 14:34:34 +0100

    Implement equals.

--- core/src/main/java/ch/cyberduck/core/features/Quota.java
+++ core/src/main/java/ch/cyberduck/core/features/Quota.java
@@ -17,6 +17,8 @@ package ch.cyberduck.core.features;
 
 import ch.cyberduck.core.exception.BackgroundException;
 
+import java.util.Objects;
+
 public interface Quota {
     Space get() throws BackgroundException;
 
@@ -36,6 +38,23 @@ public interface Quota {
             sb.append(", available=").append(available);
             sb.append('}');
             return sb.toString();
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if(this == o) {
+                return true;
+            }
+            if(!(o instanceof Space)) {
+                return false;
+            }
+            final Space space = (Space) o;
+            return Objects.equals(used, space.used) && Objects.equals(available, space.available);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(used, available);
         }
     }
 
