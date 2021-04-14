eb152904f81a94c0cf0485d244a1c8ae05bd4f7b
David Kocher <dkocher@iterate.ch>
2021-04-14 10:18:36 +0200

    Add implementations to match profiles on disk and found remote by metadata or content.

---
+++ core/src/main/java/ch/cyberduck/core/profiles/ChecksumProfileMatcher.java
@@ -0,0 +1,60 @@
+package ch.cyberduck.core.profiles;
+
+/*
+ * Copyright (c) 2002-2021 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.Profile;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class ChecksumProfileMatcher implements ProfileMatcher {
+    private static final Logger log = Logger.getLogger(ChecksumProfileMatcher.class.getName());
+
+    private final Stream<ProfilesFinder.ProfileDescription> installed;
+
+    public ChecksumProfileMatcher(final Stream<ProfilesFinder.ProfileDescription> installed) {
+        this.installed = installed;
+    }
+
+    /**
+     * @param next Description of profile from server
+     * @return Non null if profile from server has been updated
+     */
+    @Override
+    public Optional<Profile> compare(final ProfilesFinder.ProfileDescription next) {
+        // Read profile from server
+        final Profile profile = next.getProfile().get();
+        final Optional<ProfilesFinder.ProfileDescription> found = installed
+            .filter(description -> !description.getChecksum().equals(next.getChecksum()))
+            .filter(description -> {
+                // Matches identifier and provider
+                final Profile installed = description.getProfile().get();
+                if(null == installed) {
+                    // Failure parsing
+                    log.warn(String.format("Ignore unknown profile %s", installed));
+                    return false;
+                }
+                return StringUtils.equals(installed.getIdentifier(), profile.getIdentifier()) && StringUtils.equals(installed.getProvider(), profile.getProvider());
+            }).findFirst();
+        if(found.isPresent()) {
+            return Optional.of(profile);
+        }
+        return Optional.empty();
+    }
+}
---
+++ core/src/test/java/ch/cyberduck/core/profiles/ChecksumProfileMatcherTest.java
@@ -0,0 +1,57 @@
+package ch.cyberduck.core.profiles;
+
+/*
+ * Copyright (c) 2002-2021 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.Local;
+import ch.cyberduck.core.Profile;
+import ch.cyberduck.core.ProtocolFactory;
+import ch.cyberduck.core.TestProtocol;
+import ch.cyberduck.core.io.Checksum;
+import ch.cyberduck.core.io.MD5ChecksumCompute;
+import ch.cyberduck.core.serializer.impl.dd.ProfilePlistReader;
+import ch.cyberduck.core.transfer.TransferStatus;
+
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ChecksumProfileMatcherTest {
+
+    @Test
+    public void compare() throws Exception {
+        final ProfilePlistReader reader = new ProfilePlistReader(new ProtocolFactory(Collections.singleton(new TestProtocol() {
+            @Override
+            public Type getType() {
+                return Type.s3;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+        })));
+        final Local file = new Local("src/test/resources/Custom Regions S3.cyberduckprofile");
+        final Profile profile = reader.read(file);
+        final LocalProfilesFinder finder = new LocalProfilesFinder(reader, new Local("src/test/resources/"));
+        assertTrue(new ChecksumProfileMatcher(finder.find()).compare(new ProfilesFinder.ProfileDescription("Custom Regions S3.cyberduckprofile",
+            Checksum.NONE, () -> profile)).isPresent());
+        assertFalse(new ChecksumProfileMatcher(finder.find()).compare(new ProfilesFinder.ProfileDescription("Custom Regions S3.cyberduckprofile",
+            new MD5ChecksumCompute().compute(file.getInputStream(), new TransferStatus()), () -> profile)).isPresent());
+    }
+}
