5e86e4757518085ba02a8e4d7681d0092088cbd8
David Kocher <dkocher@iterate.ch>
2021-04-19 19:09:09 +0200

    Register custom implementation.

---
+++ core/dylib/src/main/java/ch/cyberduck/core/local/FileManagerTrashFeature.java
@@ -0,0 +1,46 @@
+package ch.cyberduck.core.local;
+
+/*
+ * Copyright (c) 2002-2021 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.binding.foundation.NSFileManager;
+import ch.cyberduck.binding.foundation.NSURL;
+import ch.cyberduck.core.Local;
+import ch.cyberduck.core.exception.LocalAccessDeniedException;
+import ch.cyberduck.core.local.features.Trash;
+
+import org.apache.log4j.Logger;
+import org.rococoa.ObjCObjectByReference;
+import org.rococoa.cocoa.foundation.NSError;
+
+public class FileManagerTrashFeature implements Trash {
+    private static final Logger log = Logger.getLogger(FileManagerTrashFeature.class);
+
+    @Override
+    public void trash(final Local file) throws LocalAccessDeniedException {
+        if(log.isDebugEnabled()) {
+            log.debug(String.format("Move %s to Trash", file));
+        }
+        final ObjCObjectByReference error = new ObjCObjectByReference();
+        if(!NSFileManager.defaultManager().trashItemAtURL_resultingItemURL_error(
+            NSURL.fileURLWithPath(file.getAbsolute()), null, error)) {
+            final NSError f = error.getValueAs(NSError.class);
+            if(null == f) {
+                throw new LocalAccessDeniedException(file.getAbsolute());
+            }
+            throw new LocalAccessDeniedException(String.format("%s", f.localizedDescription()));
+        }
+    }
+}
--- core/dylib/src/test/java/ch/cyberduck/core/local/FileManagerTrashFeatureTest.java
+++ core/dylib/src/test/java/ch/cyberduck/core/local/FileManagerTrashFeatureTest.java
@@ -1,5 +1,20 @@
 package ch.cyberduck.core.local;
 
+/*
+ * Copyright (c) 2002-2021 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 import ch.cyberduck.core.Local;
 import ch.cyberduck.core.LocalFactory;
 import ch.cyberduck.core.exception.LocalAccessDeniedException;
@@ -20,14 +35,14 @@ import java.util.UUID;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class WorkspaceTrashFeatureTest {
+public class FileManagerTrashFeatureTest {
 
     @Test
     public void testTrash() throws Exception {
         Local l = new Local(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
         new DefaultLocalTouchFeature().touch(l);
         assertTrue(l.exists());
-        new WorkspaceTrashFeature().trash(l);
+        new FileManagerTrashFeature().trash(l);
         assertFalse(l.exists());
     }
 
@@ -35,12 +50,12 @@ public class WorkspaceTrashFeatureTest {
     public void testTrashNotfound() throws Exception {
         Local l = new Local(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
         assertFalse(l.exists());
-        new WorkspaceTrashFeature().trash(l);
+        new FileManagerTrashFeature().trash(l);
     }
 
     @Test
     public void testTrashRepeated() throws Exception {
-        final WorkspaceTrashFeature f = new WorkspaceTrashFeature();
+        final FileManagerTrashFeature f = new FileManagerTrashFeature();
         Local l = new Local(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
         new DefaultLocalTouchFeature().touch(l);
         assertTrue(l.exists());
@@ -50,7 +65,7 @@ public class WorkspaceTrashFeatureTest {
 
     @Test
     public void testTrashNonEmpty() throws Exception {
-        final Trash trash = new WorkspaceTrashFeature();
+        final Trash trash = new FileManagerTrashFeature();
         final SupportDirectoryFinder finder = new TemporarySupportDirectoryFinder();
 
         final Local temp = finder.find();
@@ -67,7 +82,7 @@ public class WorkspaceTrashFeatureTest {
 
     @Test
     public void testTrashOpenFile() throws Exception {
-        final Trash trash = new WorkspaceTrashFeature();
+        final Trash trash = new FileManagerTrashFeature();
         final SupportDirectoryFinder finder = new TemporarySupportDirectoryFinder();
 
         final Local temp = finder.find();
@@ -86,7 +101,7 @@ public class WorkspaceTrashFeatureTest {
 
     @Test
     public void testTrashOpenDirectoryEnumeration() throws Exception {
-        final Trash trash = new WorkspaceTrashFeature();
+        final Trash trash = new FileManagerTrashFeature();
         final SupportDirectoryFinder finder = new TemporarySupportDirectoryFinder();
 
         final Local temp = finder.find();
