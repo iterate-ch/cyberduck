8b87a7b473c8cdefcb3832c1505230bf1310b84f
jmalek <jmalek@iterate.ch>
2021-05-19 10:21:55 +0200

    Update protocol in BookmarkController if found Works only for protocols defined in Scheme, only for Absolute URLs (scheme://)

--- osx/src/main/java/ch/cyberduck/ui/cocoa/controller/BookmarkController.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/controller/BookmarkController.java
@@ -241,6 +241,10 @@ public class BookmarkController extends SheetController implements CollectionLis
                 bookmark.setDefaultPath(parsed.getDefaultPath());
             }
             catch(HostParserException e) {
+                e.getProtocol().ifPresent(p -> {
+                    bookmark.setProtocol(p);
+                    hostField.setStringValue("");
+                });
                 log.warn(e);
             }
         }
