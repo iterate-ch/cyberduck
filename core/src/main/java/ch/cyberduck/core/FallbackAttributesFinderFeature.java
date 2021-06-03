1c8d95c523d595df10a402b6a1e207699e20d959
David Kocher <dkocher@iterate.ch>
2021-06-03 10:56:12 +0200

    Fix #11693. Add fallback to native attributes finder for downloads.

---
+++ core/src/main/java/ch/cyberduck/core/FallbackAttributesFinderFeature.java
@@ -0,0 +1,59 @@
+package ch.cyberduck.core;
+
+/*
+ * Copyright (c) 2002-2021 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.exception.AccessDeniedException;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.InteroperabilityException;
+import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.features.AttributesFinder;
+import ch.cyberduck.core.shared.DefaultAttributesFinderFeature;
+
+import org.apache.log4j.Logger;
+
+public class FallbackAttributesFinderFeature implements AttributesFinder {
+    private static final Logger log = Logger.getLogger(FallbackAttributesFinderFeature.class);
+
+    private final AttributesFinder standard;
+    private final AttributesFinder fallback;
+
+    /**
+     * Most commonly used to read attributes for files on server with no WebDAV support and thus 405 Method Not Allowed
+     * reply for parent directory PROPFIND
+     *
+     * @param standard Default attributes finder using list implementation
+     * @param fallback Fallback to native implementation
+     */
+    public FallbackAttributesFinderFeature(final AttributesFinder standard, final AttributesFinder fallback) {
+        this.standard = standard;
+        this.fallback = fallback;
+    }
+
+    @Override
+    public PathAttributes find(final Path file, final ListProgressListener listener) throws BackgroundException {
+        try {
+            return standard.find(file, listener);
+        }
+        catch(InteroperabilityException | AccessDeniedException | NotfoundException f) {
+            log.warn(String.format("Failure listing directory %s. %s", file.getParent(), f.getMessage()));
+            if(fallback instanceof DefaultAttributesFinderFeature) {
+                throw f;
+            }
+            // Try native implementation
+            return fallback.find(file, listener);
+        }
+    }
+}
---
+++ webdav/src/test/java/ch/cyberduck/core/shared/FallbackAttributesFinderFeatureTest.java
@@ -0,0 +1,56 @@
+package ch.cyberduck.core.shared;
+
+/*
+ * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.DisabledCancelCallback;
+import ch.cyberduck.core.DisabledHostKeyCallback;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.FallbackAttributesFinderFeature;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.dav.DAVAttributesFinderFeature;
+import ch.cyberduck.core.dav.DAVSSLProtocol;
+import ch.cyberduck.core.dav.DAVSession;
+import ch.cyberduck.core.features.AttributesFinder;
+import ch.cyberduck.core.proxy.Proxy;
+import ch.cyberduck.core.ssl.DefaultX509KeyManager;
+import ch.cyberduck.core.ssl.DisabledX509TrustManager;
+import ch.cyberduck.test.IntegrationTest;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.EnumSet;
+
+import static org.junit.Assert.*;
+
+@Category(IntegrationTest.class)
+public class FallbackAttributesFinderFeatureTest {
+
+    @Test
+    public void testFindNoWebDAV() throws Exception {
+        final DAVSession session = new DAVSession(new Host(new DAVSSLProtocol(), "ftp.gnu.org"), new DisabledX509TrustManager(), new DefaultX509KeyManager());
+        session.open(Proxy.DIRECT, new DisabledHostKeyCallback(), new DisabledLoginCallback(), new DisabledCancelCallback());
+        session.login(Proxy.DIRECT, new DisabledLoginCallback(), new DisabledCancelCallback());
+        // Handle 405 Method Not Allowed
+        final AttributesFinder f = new FallbackAttributesFinderFeature(new DefaultAttributesFinderFeature(session), new DAVAttributesFinderFeature(session));
+        final PathAttributes attr = f.find(new Path("/gnu/wget/wget-1.19.4.tar.gz", EnumSet.of(Path.Type.file)));
+        assertNotNull(attr);
+        assertNotEquals(PathAttributes.EMPTY, attr);
+        assertEquals(4310657L, attr.getSize());
+    }
+}
