2cee8cc84a3d468dcd54764f6dd6c034e6bc07e9
David Kocher <dkocher@iterate.ch>
2021-06-09 12:12:34 +0200

    No localization of storage class.

--- osx/src/main/java/ch/cyberduck/ui/cocoa/controller/PreferencesController.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/controller/PreferencesController.java
@@ -2238,7 +2238,7 @@ public class PreferencesController extends ToolbarWindowController {
         this.defaultStorageClassPopupGoogleStorage.setAutoenablesItems(false);
         this.defaultStorageClassPopupGoogleStorage.removeAllItems();
         for(String s : GoogleStorageStorageClassFeature.STORAGE_CLASS_LIST) {
-            this.defaultStorageClassPopupGoogleStorage.addItemWithTitle(LocaleFactory.localizedString(s, "S3"));
+            this.defaultStorageClassPopupGoogleStorage.addItemWithTitle(s);
             this.defaultStorageClassPopupGoogleStorage.lastItem().setRepresentedObject(s);
         }
         this.defaultStorageClassPopupGoogleStorage.setTarget(this.id());
