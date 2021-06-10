e2fa6453691ffdb9c7569f388499dae9a0513a0f
jmalek <jmalek@iterate.ch>
2021-06-10 17:31:11 +0200

    Adapt ResourcesGenerator for I18NResource generation Simplifies target BeforeTargets.

--- core/i18n-gen/ResourceGenTask.cs
+++ core/i18n-gen/ResourceGenTask.cs
@@ -26,34 +26,26 @@ namespace i18n_gen
             foreach (var lang in CommonResource.sdklyf(SourcePath).GroupBy(x => x.Lang))
             {
                 langs.Add(lang.Key);
-                var filename = "i18n." + lang.Key + ".resx";
+                var filename = "i18n." + lang.Key + ".resources";
                 var targetfile = Path.Combine(OutputPath, filename);
                 if (File.Exists(targetfile))
                 {
                     File.Delete(targetfile);
                 }
-                using var resxwriter = new ResXResourceWriter(targetfile);
+                using var resxwriter = new ResourceWriter(targetfile);
                 foreach (var property in lang)
                 {
                     resxwriter.AddResource(property.Table + ":" + property.Key, property.Value);
                 }
-                resources.Add(new TaskItem(targetfile, new Hashtable()
-                {
-                    ["LogicalName"] = Path.ChangeExtension(filename, ".resources"),
-                    ["WithCulture"] = "false"
-                }));
+                resources.Add(new TaskItem(targetfile));
             }
 
-            var i18nFile = "i18n.resx";
+            var i18nFile = "i18n.resources";
             var i18nPath = Path.Combine(OutputPath, i18nFile);
-            using (var resxwriter = new ResXResourceWriter(i18nPath))
+            using (var resxwriter = new ResourceWriter(i18nPath))
             {
                 resxwriter.AddResource("Locales", string.Join(" ", langs));
-                resources.Add(new TaskItem(i18nPath, new Hashtable()
-                {
-                    ["LogicalName"] = Path.ChangeExtension(i18nFile, ".resources"),
-                    ["WithCulture"] = "false"
-                }));
+                resources.Add(new TaskItem(i18nPath));
             }
 
             Resources = resources.ToArray();
