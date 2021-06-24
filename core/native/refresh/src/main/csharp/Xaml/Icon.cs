47d68634268dc050154684bfccb1bbe36235f782
jmalek <jmalek@iterate.ch>
2021-06-24 11:16:23 +0200

    Add search-inactive icon to ProfilesPage

---
+++ core/native/refresh/src/main/csharp/Xaml/Icon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Markup;
+using Ch.Cyberduck.Core.Refresh.Services;
+using StructureMap;
+
+namespace Ch.Cyberduck.Core.Refresh.Xaml
+{
+    public class Icon : MarkupExtension
+    {
+        private readonly WpfIconProvider icons;
+        private readonly string resourceName;
+
+        public int? Size { get; set; }
+
+        public Icon(string resourceName)
+        {
+            icons = ObjectFactory.TryGetInstance<WpfIconProvider>();
+            this.resourceName = resourceName;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => icons?.GetResource(resourceName, Size);
+    }
+}
