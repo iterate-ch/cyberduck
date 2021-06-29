f8f4c2af8f764d9bba55e9e18217a804ca5cdfb6
David Kocher <dkocher@iterate.ch>
2021-06-29 15:14:18 +0200

    Break early when looking up single key.

---
+++ core/src/main/java/ch/cyberduck/core/CancellingListProgressListener.java
@@ -0,0 +1,26 @@
+package ch.cyberduck.core;
+
+/*
+ * Copyright (c) 2002-2021 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.exception.ListCanceledException;
+
+public class CancellingListProgressListener extends DisabledListProgressListener {
+
+    @Override
+    public void chunk(final Path parent, final AttributedList<Path> list) throws ListCanceledException {
+        throw new ListCanceledException(list);
+    }
+}
