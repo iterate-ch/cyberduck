40d7b8c99a073be20bb32d6650965a51ede16763
David Kocher <dkocher@iterate.ch>
2021-07-24 21:25:47 +0200

    Add file migrations API to query copy status.

---
+++ brick/src/main/java/ch/cyberduck/core/brick/io/swagger/client/api/FileMigrationsApi.java
@@ -0,0 +1,75 @@
+package ch.cyberduck.core.brick.io.swagger.client.api;
+
+import ch.cyberduck.core.brick.io.swagger.client.ApiException;
+import ch.cyberduck.core.brick.io.swagger.client.ApiClient;
+import ch.cyberduck.core.brick.io.swagger.client.Configuration;
+import ch.cyberduck.core.brick.io.swagger.client.Pair;
+
+import javax.ws.rs.core.GenericType;
+
+import ch.cyberduck.core.brick.io.swagger.client.model.FileMigrationEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-07-23T20:47:00.742522+02:00[Europe/Paris]")public class FileMigrationsApi {
+  private ApiClient apiClient;
+
+  public FileMigrationsApi() {
+    this(Configuration.getDefaultApiClient());
+  }
+
+  public FileMigrationsApi(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  public ApiClient getApiClient() {
+    return apiClient;
+  }
+
+  public void setApiClient(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  /**
+   * Show File Migration
+   * Show File Migration
+   * @param id File Migration ID. (required)
+   * @return FileMigrationEntity
+   * @throws ApiException if fails to make API call
+   */
+  public FileMigrationEntity getFileMigrationsId(Integer id) throws ApiException {
+    Object localVarPostBody = null;
+    // verify the required parameter 'id' is set
+    if (id == null) {
+      throw new ApiException(400, "Missing the required parameter 'id' when calling getFileMigrationsId");
+    }
+    // create path and map variables
+    String localVarPath = "/file_migrations/{id}"
+      .replaceAll("\\{" + "id" + "\\}", apiClient.escapeString(id.toString()));
+
+    // query params
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+
+
+    final String[] localVarAccepts = {
+      "application/json"
+    };
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {
+      
+    };
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] {  };
+
+    GenericType<FileMigrationEntity> localVarReturnType = new GenericType<FileMigrationEntity>() {};
+    return apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+  }
+}
