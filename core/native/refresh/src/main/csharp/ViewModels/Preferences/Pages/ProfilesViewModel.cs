a75565c8bc8192de796bf089869dc676b7897d96
jmalek <jmalek@iterate.ch>
2021-08-06 10:08:00 +0200

    Fix import namespace

--- core/native/refresh/src/main/csharp/ViewModels/Preferences/Pages/ProfilesViewModel.cs
+++ core/native/refresh/src/main/csharp/ViewModels/Preferences/Pages/ProfilesViewModel.cs
@@ -15,13 +15,13 @@ using System.ComponentModel;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ch.Cyberduck.Core.Refresh.ViewModels.Preferences.Pages
 {
     using ch.cyberduck.core;
-    using System.Reactive.Subjects;
 
     public class ProfilesViewModel : ReactiveObject
     {
