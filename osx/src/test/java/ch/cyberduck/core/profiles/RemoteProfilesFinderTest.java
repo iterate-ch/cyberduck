bf07b48c9fe8cfa4caac4b488d4a7853bd7ea4a8
David Kocher <dkocher@iterate.ch>
2021-09-09 12:04:15 +0200

    Review test to use s3:/profiles.cyberduck.io.

--- osx/src/test/java/ch/cyberduck/core/profiles/RemoteProfilesFinderTest.java
+++ osx/src/test/java/ch/cyberduck/core/profiles/RemoteProfilesFinderTest.java
@@ -20,34 +20,39 @@ import ch.cyberduck.core.DisabledHostKeyCallback;
 import ch.cyberduck.core.DisabledLoginCallback;
 import ch.cyberduck.core.HostParser;
 import ch.cyberduck.core.ProtocolFactory;
-import ch.cyberduck.core.dav.DAVSSLProtocol;
-import ch.cyberduck.core.dav.DAVSession;
+import ch.cyberduck.core.Session;
 import ch.cyberduck.core.proxy.Proxy;
+import ch.cyberduck.core.s3.S3Protocol;
+import ch.cyberduck.core.s3.S3Session;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DisabledX509TrustManager;
 
 import org.junit.Test;
 
 import java.util.Collections;
-import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertFalse;
 
 public class RemoteProfilesFinderTest {
 
     @Test
-    public void find() throws Exception {
-        final ProtocolFactory protocols = new ProtocolFactory(Collections.singleton(new DAVSSLProtocol() {
+    public void testFind() throws Exception {
+        final ProtocolFactory protocols = new ProtocolFactory(Collections.singleton(new S3Protocol() {
             @Override
             public boolean isEnabled() {
                 return true;
             }
         }));
-        final DAVSession session = new DAVSession(new HostParser(protocols).get("https://svn.cyberduck.io/trunk/profiles"), new DisabledX509TrustManager(), new DefaultX509KeyManager());
+        final Session session = new S3Session(new HostParser(protocols).get("s3:/profiles.cyberduck.io"), new DisabledX509TrustManager(), new DefaultX509KeyManager());
         session.open(Proxy.DIRECT, new DisabledHostKeyCallback(), new DisabledLoginCallback(), new DisabledCancelCallback());
         final RemoteProfilesFinder finder = new RemoteProfilesFinder(session);
         final Set<ProfileDescription> stream = finder.find();
         assertFalse(stream.isEmpty());
+        // Check for versions
+        assertFalse(stream.stream().filter(ProfileDescription::isLatest).collect(Collectors.toSet()).isEmpty());
+        assertFalse(stream.stream().filter(description -> !description.isLatest()).collect(Collectors.toSet()).isEmpty());
         session.close();
     }
 }
