01bc61ddbd60262c5847df315f4984fac24263ba
David Kocher <dkocher@iterate.ch>
2021-09-13 10:11:48 +0200

    Add scripts and workflows to synchronize connection profiles.

---
+++ profiles/scripts/initialize-revisions.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2002-2021 iterate GmbH. All rights reserved.
+# https://cyberduck.io/
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+TARGET='s3:/profiles.cyberduck.io'
+DIRECTORY=$1
+if [ ! $DIRECTORY ]; then
+  DIRECTORY=$(dirname "$(cd -P -- "$(dirname -- "$0")" && pwd -P)")
+fi
+if [ ! $TMPDIR ]; then
+    TMPDIR=$RUNNER_TEMP
+fi
+echo "Finding profiles in $DIRECTORY"
+
+for path in $DIRECTORY/*.cyberduckprofile; do
+  filename=$(basename "$path")
+  # Read every version
+  git log --reverse --format=%H "$filename" |
+    while read hash; do
+      git show "$hash:profiles/$filename" > "$TMPDIR/$filename"
+      duck -qy --username $AWS_ACCESS_KEY_ID --password $AWS_SECRET_ACCESS_KEY --existing compare --upload "$TARGET/$filename" "$TMPDIR/$filename"
+      rm "$TMPDIR/$filename"
+    done
+done
---
+++ profiles/scripts/sync-latest.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2002-2021 iterate GmbH. All rights reserved.
+# https://cyberduck.io/
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+TARGET='s3:/profiles.cyberduck.io'
+DIRECTORY=$1
+if [ ! $DIRECTORY ]; then
+  DIRECTORY=$(dirname "$(cd -P -- "$(dirname -- "$0")" && pwd -P)")
+fi
+echo "Finding profiles in $DIRECTORY"
+
+# Upload changed profiles by comparing checksum
+duck -qy --username $AWS_ACCESS_KEY_ID --password $AWS_SECRET_ACCESS_KEY --existing compare --upload "$TARGET/" $DIRECTORY/*.cyberduckprofile
+
+# Delete profiles no longer maintained
+duck -qy --username $AWS_ACCESS_KEY_ID --password $AWS_SECRET_ACCESS_KEY --list $TARGET/ |
+  while read name; do
+    [ -z "$name" ] && continue
+    
+    if [ ! -f "$DIRECTORY/$name" ]; then
+      echo "Deleting $name"
+      duck -qy --username $AWS_ACCESS_KEY_ID --password $AWS_SECRET_ACCESS_KEY --delete "$TARGET/$name"
+    fi
+  done
