7ecff089820c79354e65d51fabecc2b3fc9deee6
David Kocher <dkocher@iterate.ch>
2021-10-21 08:19:26 +0200

    Fix test.

--- s3/src/test/java/ch/cyberduck/core/s3/S3AttributesFinderFeatureTest.java
+++ s3/src/test/java/ch/cyberduck/core/s3/S3AttributesFinderFeatureTest.java
@@ -225,11 +225,9 @@ public class S3AttributesFinderFeatureTest extends AbstractS3Test {
     @Test
     public void testRedirectWithNoLocationHeader() throws Exception {
         final Path container = new Path("profiles.cyberduck.io", EnumSet.of(Path.Type.directory, Path.Type.volume));
-        final Path test = new Path(container, "S3 (HTTP).cyberduckprofile", EnumSet.of(Path.Type.file),
-                new PathAttributes().withVersionId("RqwqOQxfEynDJK6LkgN84bFEjcqDSajg"));
+        final Path test = new Path(container, "S3 (HTTP).cyberduckprofile", EnumSet.of(Path.Type.file));
         final S3AttributesFinderFeature f = new S3AttributesFinderFeature(session);
         final PathAttributes attributes = f.find(test);
-        assertEquals("b9afd8d6da91e7b520559fa9eaac54c1", attributes.getChecksum().hash);
         assertTrue(session.getClient().getRegionEndpointCache().containsRegionForBucketName("profiles.cyberduck.io"));
         assertEquals("eu-west-1", session.getClient().getRegionEndpointCache().getRegionForBucketName("profiles.cyberduck.io"));
     }
