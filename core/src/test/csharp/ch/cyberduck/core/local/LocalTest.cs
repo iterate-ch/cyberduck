09476a4232f7965b607075de55715f7cab7aa22f
jmalek <jmalek@iterate.ch>
2021-10-25 09:59:48 +0200

    Add failing test

---
+++ core/src/test/csharp/ch/cyberduck/core/local/LocalTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Ch.Cyberduck.Core.Local
+{
+    using Local = ch.cyberduck.core.Local;
+
+    [TestFixture]
+    public class LocalTest
+    {
+        [Test]
+        public void TestWSL()
+        {
+            var temp = new SystemLocal(@"\\wsl$\home\user\.ssh\id_rsa");
+        }
+    }
+}
