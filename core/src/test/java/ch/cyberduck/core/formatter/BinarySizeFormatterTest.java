99f7818dd90b3046fabbb3ddc0767d086a2011e2
David Kocher <dkocher@iterate.ch>
2021-11-10 22:47:31 +0100

    Add test.

--- core/src/test/java/ch/cyberduck/core/formatter/BinarySizeFormatterTest.java
+++ core/src/test/java/ch/cyberduck/core/formatter/BinarySizeFormatterTest.java
@@ -9,12 +9,13 @@ public class BinarySizeFormatterTest {
     @Test
     public void testFormat() {
         BinarySizeFormatter f = new BinarySizeFormatter();
-        assertEquals("1.0 KiB", f.format(1024));
-        assertEquals("1.5 KiB", f.format(1500));
-        assertEquals("1.4 KiB", f.format(1480));
-        assertEquals("2.0 KiB", f.format(2000));
-        assertEquals("1.0 MiB", f.format(1048576));
-        assertEquals("1.0 GiB", f.format(1073741824));
-        assertEquals("375.3 MiB", f.format(393495974));
+        assertEquals("1.0 KiB", f.format(1024L));
+        assertEquals("1.5 KiB", f.format(1500L));
+        assertEquals("1.4 KiB", f.format(1480L));
+        assertEquals("2.0 KiB", f.format(2000L));
+        assertEquals("1.0 MiB", f.format(1048576L));
+        assertEquals("1.0 GiB", f.format(1073741824L));
+        assertEquals("375.3 MiB", f.format(393495974L));
+        assertEquals("4.0 GiB", f.format(4294967303L));
     }
 }
