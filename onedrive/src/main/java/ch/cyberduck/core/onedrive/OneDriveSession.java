bb861ee80e0e99a6f7d6ab616bfc267d40dd4202
AliveDevil <AliveDevil@users.noreply.github.com>
2022-01-19 13:28:14 +0100

    Workaround OneDrive business drives screwing around with Item IDs.

--- onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveSession.java
+++ onedrive/src/main/java/ch/cyberduck/core/onedrive/OneDriveSession.java
@@ -55,9 +55,20 @@ public class OneDriveSession extends GraphSession {
                     remoteParent.getDriveId(), remoteParent.getId());
             }
             else {
+                // this is fix for OneDrive Business shared folders.
+                // These have an ID in their remoteMetadata, but
+                // there is an ID in parentReference as well.
+                // remoteMetadata-ID doesn't resolve in parentReference
+                // driveId, so … use that parentReference Item Id first,
+                // and if it is non-existent use the remotemetadata
+                // as fallback for regular OneDrive Consumer shared items.
+                String remoteItemId = remoteParent.getId();
+                if (StringUtils.isBlank(remoteItemId)) {
+                    remoteItemId = remoteMetadata.getId();
+                }
                 return String.join(String.valueOf(Path.DELIMITER),
                     parent.getDriveId(), metadata.getId(),
-                    remoteParent.getDriveId(), remoteMetadata.getId());
+                    remoteParent.getDriveId(), remoteItemId);
             }
         }
         else {
