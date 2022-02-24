7adc9c80a6f499fe836c5c7ae3551e8347934773
AliveDevil <AliveDevil@users.noreply.github.com>
2022-02-24 12:26:39 +0100

    Add huge blob test

--- core/src/test/csharp/ch/cyberduck/core/credentialmanager/WinCredentialManagerTest.cs
+++ core/src/test/csharp/ch/cyberduck/core/credentialmanager/WinCredentialManagerTest.cs
@@ -2,6 +2,11 @@
 using java.util;
 using NUnit.Framework;
 using System.Net;
+using System;
+using System.Buffers;
+using System.Security.Cryptography;
+using Windows.Win32.Security.Credentials;
+using System.IO;
 
 namespace Ch.Cyberduck.Core.credentialmanager
 {
@@ -91,6 +96,57 @@ namespace Ch.Cyberduck.Core.credentialmanager
             Assert.AreEqual(credentials.Password, query.Password);
             var removeResult = WinCredentialManager.RemoveCredentials(target);
             Assert.True(removeResult);
+        }
+
+        [Test]
+        public void TestSaveHugeBlob()
+        {
+            var target = UUID.randomUUID().toString();
+            string blob;
+            using (MemoryStream memory = new MemoryStream())
+            {
+                byte[] buffer = ArrayPool<byte>.Shared.Rent(16);
+                try
+                {
+                    System.Random random = new();
+                    using (ToBase64Transform transform = new ToBase64Transform())
+                    using (CryptoStream cryptoStream = new CryptoStream(memory, transform, CryptoStreamMode.Write, true))
+                    {
+                        const int targetLength = 512;
+
+                        int remaining = targetLength;
+                        while (remaining > 0)
+                        {
+                            random.NextBytes(buffer);
+                            int write = Math.Min(remaining, buffer.Length);
+                            cryptoStream.Write(buffer, 0, write);
+                            remaining -= write;
+                        }
+                    }
+                    memory.Seek(0, SeekOrigin.Begin);
+                    using (StreamReader reader = new StreamReader(memory))
+                    {
+                        blob = reader.ReadToEnd();
+                    }
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(buffer);
+                }
+            }
+
+            WindowsCredentialManagerCredential cred = new WindowsCredentialManagerCredential(string.Empty, string.Empty, CRED_TYPE.CRED_TYPE_GENERIC, default, CRED_PERSIST.CRED_PERSIST_ENTERPRISE)
+            {
+                Attributes =
+                {
+                    ["Blob"] = blob
+                }
+            };
+            var credTarget = "test:large-blob" + target;
+            Assert.That(WinCredentialManager.SaveCredentials(credTarget, cred), Is.True);
+            cred = WinCredentialManager.GetCredentials(credTarget);
+            Assert.That(() => WinCredentialManager.GetCredentials(credTarget).Attributes["Blob"], Is.EqualTo(blob));
+            WinCredentialManager.RemoveCredentials(credTarget);
         }
     }
 }
