6860e8deb8257f7980527aab270bce7d187f8c26
David Kocher <dkocher@iterate.ch>
2022-03-23 12:19:45 +0100

    Revert "Skip prompt for wildcard downloads using CLI with glob pattern such as `--download s3:/profiles.cyberduck.io/Alibaba* ~/Downloads/`."
    This reverts commit f5202b1facdac4032fc7b8fee4d38b981d5c6541.

--- core/src/main/java/ch/cyberduck/core/transfer/DownloadTransfer.java
+++ core/src/main/java/ch/cyberduck/core/transfer/DownloadTransfer.java
@@ -199,10 +199,6 @@ public class DownloadTransfer extends Transfer {
                 final Local local = download.local;
                 if(local.exists()) {
                     if(local.isDirectory()) {
-                        if(download.remote.isDirectory()) {
-                            // Do not prompt when merging into existing directory
-                            continue;
-                        }
                         if(local.list().isEmpty()) {
                             // Do not prompt for existing empty directories
                             continue;
