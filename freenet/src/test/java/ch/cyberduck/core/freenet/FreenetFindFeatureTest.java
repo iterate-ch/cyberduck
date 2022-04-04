4ebb9535167b6aeac38418fc5d5da7cf914e2bf8
David Kocher <dkocher@iterate.ch>
2022-04-04 10:24:33 +0200

    Add tests.

---
+++ freenet/src/test/java/ch/cyberduck/core/freenet/FreenetFindFeatureTest.java
@@ -0,0 +1,58 @@
+package ch.cyberduck.core.freenet;
+
+import ch.cyberduck.core.AlphanumericRandomStringService;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.dav.DAVDeleteFeature;
+import ch.cyberduck.core.dav.DAVDirectoryFeature;
+import ch.cyberduck.core.dav.DAVFindFeature;
+import ch.cyberduck.core.dav.DAVTouchFeature;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.shared.DefaultHomeFinderService;
+import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.test.IntegrationTest;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.Collections;
+import java.util.EnumSet;
+
+import static org.junit.Assert.*;
+
+@Category(IntegrationTest.class)
+public class FreenetFindFeatureTest extends AbstractFreenetTest {
+
+    @Test
+    public void testFind() throws Exception {
+        assertTrue(new DAVFindFeature(session).find(new DefaultHomeFinderService(session).find()));
+        assertFalse(new DAVFindFeature(session).find(
+                new Path(new DefaultHomeFinderService(session).find(), new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory))
+        ));
+        assertFalse(new DAVFindFeature(session).find(
+                new Path(new DefaultHomeFinderService(session).find(), new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file))
+        ));
+    }
+
+    @Test
+    public void testFindFile() throws Exception {
+        final Path test = new DAVTouchFeature(session).touch(new Path(new DefaultHomeFinderService(session).find(),
+                new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file)), new TransferStatus());
+        assertNotEquals(PathAttributes.EMPTY, new DAVFindFeature(session).find(test));
+        new DAVDeleteFeature(session).delete(Collections.singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
+    }
+
+    @Test
+    public void testFindFolder() throws Exception {
+        final Path test = new DAVDirectoryFeature(session).mkdir(new Path(new DefaultHomeFinderService(session).find(),
+                new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory)), new TransferStatus());
+        assertNotEquals(PathAttributes.EMPTY, new DAVFindFeature(session).find(test));
+        new DAVDeleteFeature(session).delete(Collections.singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
+    }
+
+    @Test
+    public void testFindRoot() throws Exception {
+        assertTrue(new DAVFindFeature(session).find(new DefaultHomeFinderService(session).find()));
+    }
+}
