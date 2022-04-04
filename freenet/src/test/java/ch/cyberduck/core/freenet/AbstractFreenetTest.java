c2d72ba8028fe923c85ccf0d5186a0c0fd0266fe
David Kocher <dkocher@iterate.ch>
2022-04-04 10:24:33 +0200

    Add tests.

--- freenet/src/test/java/ch/cyberduck/core/freenet/AbstractFreenetTest.java
+++ freenet/src/test/java/ch/cyberduck/core/freenet/AbstractFreenetTest.java
@@ -17,10 +17,13 @@ import ch.cyberduck.core.Credentials;
 import ch.cyberduck.core.DisabledCancelCallback;
 import ch.cyberduck.core.DisabledHostKeyCallback;
 import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.DisabledProgressListener;
 import ch.cyberduck.core.Host;
+import ch.cyberduck.core.LoginConnectionService;
+import ch.cyberduck.core.LoginOptions;
 import ch.cyberduck.core.Profile;
 import ch.cyberduck.core.ProtocolFactory;
-import ch.cyberduck.core.proxy.Proxy;
 import ch.cyberduck.core.serializer.impl.dd.ProfilePlistReader;
 import ch.cyberduck.core.ssl.DefaultX509KeyManager;
 import ch.cyberduck.core.ssl.DefaultX509TrustManager;
@@ -30,6 +33,8 @@ import org.junit.Before;
 
 import java.util.Collections;
 import java.util.HashSet;
+
+import static org.junit.Assert.fail;
 
 public class AbstractFreenetTest {
 
@@ -44,12 +49,23 @@ public class AbstractFreenetTest {
     public void setup() throws Exception {
         final ProtocolFactory factory = new ProtocolFactory(new HashSet<>(Collections.singleton(new FreenetProtocol())));
         final Profile profile = new ProfilePlistReader(factory).read(
-            this.getClass().getResourceAsStream("/freenet.cyberduckprofile"));
+                this.getClass().getResourceAsStream("/freenet.cyberduckprofile"));
         final Host host = new Host(profile, profile.getDefaultHostname(), new Credentials(
-            System.getProperties().getProperty("freenet.user"), System.getProperties().getProperty("freenet.password")
+                System.getProperties().getProperty("freenet.user"), System.getProperties().getProperty("freenet.password")
         ));
         session = new FreenetSession(host, new DefaultX509TrustManager(), new DefaultX509KeyManager());
-        session.open(Proxy.DIRECT, new DisabledHostKeyCallback(), new DisabledLoginCallback(), new DisabledCancelCallback());
-        session.login(Proxy.DIRECT, new DisabledLoginCallback(), new DisabledCancelCallback());
+        final LoginConnectionService login = new LoginConnectionService(new DisabledLoginCallback() {
+            @Override
+            public Credentials prompt(final Host bookmark, final String title, final String reason, final LoginOptions options) {
+                fail(reason);
+                return null;
+            }
+
+            @Override
+            public void warn(final Host bookmark, final String title, final String message, final String continueButton, final String disconnectButton, final String preference) {
+                //
+            }
+        }, new DisabledHostKeyCallback(), new DisabledPasswordStore(), new DisabledProgressListener());
+        login.check(session, new DisabledCancelCallback());
     }
 }
