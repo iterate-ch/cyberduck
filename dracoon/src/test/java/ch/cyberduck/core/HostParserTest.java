a33e21629059de54afc3dda1c835967ac1f0fba7
Yves Langisch <yla@iterate.ch>
2022-04-19 10:00:34 +0200

    Fix tests.

--- dracoon/src/test/java/ch/cyberduck/core/HostParserTest.java
+++ dracoon/src/test/java/ch/cyberduck/core/HostParserTest.java
@@ -29,18 +29,9 @@ import static org.junit.Assert.assertEquals;
 public class HostParserTest {
 
     @Test
-    public void testParseUsernameFromUrlEvent() throws Exception {
-        final Profile profile = new ProfilePlistReader(new ProtocolFactory(Collections.singleton(new SDSProtocol()))).read(
-            this.getClass().getResourceAsStream("/DRACOON (Email Address).cyberduckprofile"));
-        assertEquals(0, new Host(profile, "duck.dracoon.com", 443, "/key", new Credentials("post@iterate.ch"))
-            .compareTo(new HostParser(new ProtocolFactory(new HashSet<>(Arrays.asList(new SDSProtocol(), profile)))).get(
-                "dracoon://post%40iterate.ch@duck.dracoon.com/key")));
-    }
-
-    @Test
     public void testParseDefaultPath() throws Exception {
         final Profile profile = new ProfilePlistReader(new ProtocolFactory(Collections.singleton(new SDSProtocol()))).read(
-            this.getClass().getResourceAsStream("/DRACOON (Email Address).cyberduckprofile"));
+            this.getClass().getResourceAsStream("/DRACOON (OAuth).cyberduckprofile"));
         assertEquals("/room/key", new HostParser(new ProtocolFactory(new HashSet<>(Arrays.asList(new SDSProtocol(), profile)))).get(
             "dracoon://duck.dracoon.com/room/key").getDefaultPath());
     }
@@ -48,18 +39,8 @@ public class HostParserTest {
     @Test
     public void testParseDefaultPathUmlautPercentEncoding() throws Exception {
         final Profile profile = new ProfilePlistReader(new ProtocolFactory(Collections.singleton(new SDSProtocol()))).read(
-            this.getClass().getResourceAsStream("/DRACOON (Email Address).cyberduckprofile"));
+            this.getClass().getResourceAsStream("/DRACOON (OAuth).cyberduckprofile"));
         assertEquals("/home/ä-test", new HostParser(new ProtocolFactory(new HashSet<>(Arrays.asList(new SDSProtocol(), profile)))).get(
             "dracoon://duck.dracoon.com/home%2F%C3%A4-test").getDefaultPath());
-    }
-
-    @Test
-    public void testParseDeprecatedPriorization() throws Exception {
-        final Profile profileEmail = new ProfilePlistReader(new ProtocolFactory(Collections.singleton(new SDSProtocol()))).read(
-                this.getClass().getResourceAsStream("/DRACOON (Email Address).cyberduckprofile"));
-        final Profile profileOAuth = new ProfilePlistReader(new ProtocolFactory(Collections.singleton(new SDSProtocol()))).read(
-                this.getClass().getResourceAsStream("/DRACOON (OAuth).cyberduckprofile"));
-        assertEquals(profileOAuth, new HostParser(new ProtocolFactory(new HashSet<>(Arrays.asList(new SDSProtocol(), profileEmail, profileOAuth)))).get(
-                "dracoon://duck.dracoon.com/home%2F%C3%A4-test").getProtocol());
     }
 }
