e9194bcb1f2b204f4472784ed0c44ade8a4e9e36
David Kocher <dkocher@iterate.ch>
2022-07-12 16:50:22 +0200

    Formatting.

--- i18n/src/main/resources/i18n.sh
+++ i18n/src/main/resources/i18n.sh
@@ -56,7 +56,7 @@ init() {
 }
 
 test() {
-	for lproj in `ls . | grep lproj`; do
+	for lproj in $(ls . | grep lproj); do
 		language=$lproj;
 		run;
 	done;
@@ -80,7 +80,7 @@ nib() {
 }
 
 import_strings() {
-	if($force == true); then
+	if [ true == "$force" ] ; then
 	{
 		# force update
 		echo "*** Updating $nib... (force) in $language..."
@@ -89,21 +89,22 @@ import_strings() {
                     --import-strings-file $language/$nib.strings \
                     $base_language/$nib$extension
 	}
-    else
+  else
 	{
 		# incremental update
 		echo "*** Updating $nib... (incremental) in $language..."
-	    # Checkout previous version from base language
-        git show `git log -2 --format="%H" $base_language/$nib$extension | tail -n 1`:./$base_language/$nib$extension > $base_language/$nib$extension.prev.xib
+	  # Checkout previous version from base language
+    # shellcheck disable=SC2046
+    git show $(git log -2 --format="%H" $base_language/$nib$extension | tail -n 1):./$base_language/$nib$extension > $base_language/$nib$extension.prev.xib
 		$nibtool    --write $language/$nib$extension \
-                    --incremental-file $language/$nib$extension \
-                    --previous-file $base_language/$nib$extension.prev.xib \
-                    --import-strings-file $language/$nib.strings \
-                    --localize-incremental \
-                    $base_language/$nib$extension
-        rm $base_language/$nib$extension.prev.xib
+                --incremental-file $language/$nib$extension \
+                --previous-file $base_language/$nib$extension.prev.xib \
+                --import-strings-file $language/$nib.strings \
+                --localize-incremental \
+                $base_language/$nib$extension
+    rm $base_language/$nib$extension.prev.xib
 	}
-    fi;
+  fi;
 }
 
 export_strings() {
@@ -115,22 +116,22 @@ export_strings_legacy() {
 	if [ "$language" = "all" ] ; then
 	{
 		echo "*** Updating all localizations...";
-    	for lproj in `ls . | grep lproj`; do
-    		language=$lproj;
-    		echo "*** Updating $language Localization...";
-            if [ "$nibfile" = "all" ] ; then
-        		for n in `ls $language | grep $extension`; do
-        			nib=`basename $n $extension`
-        			echo "Update $language/$nib.strings.1 from $base_language/$nib.strings"
-        			$convertstrings $base_language/$nib.strings $language/$nib.strings > $language/$nib.strings.1
-        		done;
-            fi;
-            if [ "$nibfile" != "all" ] ; then
-    			nib=`basename $nibfile $extension`
-    			echo "Update $language/$nib.strings.1 from $base_language/$nib.strings"
-    			$convertstrings $base_language/$nib.strings $language/$nib.strings > $language/$nib.strings.1
-            fi;
-    	done;
+    for lproj in $(ls . | grep lproj); do
+      language=$lproj;
+      echo "*** Updating $language Localization...";
+          if [ "$nibfile" = "all" ] ; then
+          for n in $(ls $language | grep $extension); do
+            nib=`basename $n $extension`
+            echo "Update $language/$nib.strings.1 from $base_language/$nib.strings"
+            $convertstrings $base_language/$nib.strings $language/$nib.strings > $language/$nib.strings.1
+          done;
+          fi;
+          if [ "$nibfile" != "all" ] ; then
+        nib=`basename $nibfile $extension`
+        echo "Update $language/$nib.strings.1 from $base_language/$nib.strings"
+        $convertstrings $base_language/$nib.strings $language/$nib.strings > $language/$nib.strings.1
+          fi;
+    done;
 	}
 	else
 	{
