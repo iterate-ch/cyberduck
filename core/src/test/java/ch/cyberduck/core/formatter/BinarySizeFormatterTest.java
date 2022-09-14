7413fee9c4460d38268d8e90d341407d7472360c
David Kocher <dkocher@iterate.ch>
2022-09-14 22:28:57 +0200

    Add tests.
    Signed-off-by: David Kocher <dkocher@iterate.ch>

--- core/src/test/java/ch/cyberduck/core/formatter/BinarySizeFormatterTest.java
+++ core/src/test/java/ch/cyberduck/core/formatter/BinarySizeFormatterTest.java
@@ -19,7 +19,9 @@ public class BinarySizeFormatterTest {
         assertEquals("4.0 GiB", f.format(4294967303L));
         assertEquals("5.0 GiB", f.format(5368709120L));
         assertEquals("10.0 GiB", f.format(10737418240L));
+        assertEquals("50.0 GiB", f.format(53687091200L));
         assertEquals("100.0 GiB", f.format(107374182400L));
+        assertEquals("200.0 GiB", f.format(214748364800L));
         assertEquals("500.0 GiB", f.format(536870912000L));
         assertEquals("1000.0 GiB", f.format(1073741824000L));
         assertEquals("1.0 TiB", f.format(1099511627776L));
