8df4f4b6e693e5604ae617df8dd66afca0e1031b
AliveDevil <AliveDevil@users.noreply.github.com>
2022-12-06 12:47:18 +0100

    Add failing test

---
+++ core/src/test/csharp/ch/cyberduck/core/local/SystemLocalTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using CoreLocal = ch.cyberduck.core.Local;
+
+namespace Ch.Cyberduck.Core.Local
+{
+    [TestFixture]
+    public class SystemLocalTest
+    {
+        const string PIPE_NAME = @"\\.\pipe\openssh-ssh-agent";
+
+        [Test]
+        public void TestPipeName()
+        {
+            CoreLocal local = new SystemLocal(PIPE_NAME);
+            Assert.AreEqual($"Local{{path='{PIPE_NAME}'}}", local.ToString());
+        }
+    }
+}
