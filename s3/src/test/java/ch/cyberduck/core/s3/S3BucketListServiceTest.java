a7a6a573540231ce889a6714bb3d79105661c60c
David Kocher <dkocher@iterate.ch>
2022-12-06 18:13:33 +0100

    Assert equal test for bucket attributes.

--- s3/src/test/java/ch/cyberduck/core/s3/S3BucketListServiceTest.java
+++ s3/src/test/java/ch/cyberduck/core/s3/S3BucketListServiceTest.java
@@ -3,6 +3,7 @@ package ch.cyberduck.core.s3;
 import ch.cyberduck.core.AttributedList;
 import ch.cyberduck.core.DisabledListProgressListener;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.SimplePathPredicate;
 import ch.cyberduck.test.IntegrationTest;
 
 import org.junit.Test;
@@ -18,9 +19,12 @@ public class S3BucketListServiceTest extends AbstractS3Test {
     @Test
     public void testList() throws Exception {
         final AttributedList<Path> list = new S3BucketListService(session).list(
-            new Path(String.valueOf(Path.DELIMITER), EnumSet.of(Path.Type.volume, Path.Type.directory)), new DisabledListProgressListener());
+                new Path(String.valueOf(Path.DELIMITER), EnumSet.of(Path.Type.volume, Path.Type.directory)), new DisabledListProgressListener());
         assertFalse(list.isEmpty());
-        assertTrue(list.contains(new Path("test-eu-central-1-cyberduck", EnumSet.of(Path.Type.directory, Path.Type.volume))));
+        assertNotNull(list.find(new SimplePathPredicate(new Path("test-eu-central-1-cyberduck", EnumSet.of(Path.Type.directory, Path.Type.volume)))));
+        for(Path bucket : list) {
+            assertEquals(bucket.attributes(), new S3AttributesFinderFeature(session, new S3AccessControlListFeature(session)).find(bucket, new DisabledListProgressListener()));
+        }
     }
 
     @Test
@@ -30,6 +34,7 @@ public class S3BucketListServiceTest extends AbstractS3Test {
         assertFalse(list.isEmpty());
         for(Path bucket : list) {
             assertEquals("eu-central-1", bucket.attributes().getRegion());
+            assertEquals(bucket.attributes(), new S3AttributesFinderFeature(session, new S3AccessControlListFeature(session)).find(bucket, new DisabledListProgressListener()));
         }
     }
 }
