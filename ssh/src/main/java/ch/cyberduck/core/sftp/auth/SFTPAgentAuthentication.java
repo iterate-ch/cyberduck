c5a5b0e87f8d8dc1acb916ed282367f05a8b70cc
David Kocher <dkocher@iterate.ch>
2022-12-06 12:42:36 +0100

    Resolve #13935.

--- ssh/src/main/java/ch/cyberduck/core/sftp/auth/SFTPAgentAuthentication.java
+++ ssh/src/main/java/ch/cyberduck/core/sftp/auth/SFTPAgentAuthentication.java
@@ -23,11 +23,16 @@ import ch.cyberduck.core.Local;
 import ch.cyberduck.core.LoginCallback;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.sftp.SFTPExceptionMappingService;
+import ch.cyberduck.core.sftp.openssh.OpenSSHCredentialsConfigurator;
+import ch.cyberduck.core.sftp.openssh.OpenSSHIdentitiesOnlyConfigurator;
 import ch.cyberduck.core.threading.CancelCallback;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Collections;
@@ -36,6 +41,7 @@ import com.jcraft.jsch.agentproxy.Identity;
 import com.jcraft.jsch.agentproxy.sshj.AuthAgent;
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.common.Buffer;
+import net.schmizz.sshj.common.IOUtils;
 import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.userauth.UserAuthException;
 
@@ -51,12 +57,37 @@ public class SFTPAgentAuthentication implements AuthenticationProvider<Boolean> 
     }
 
     @Override
-    public Boolean authenticate(final Host bookmark, final LoginCallback prompt, final CancelCallback cancel)
-            throws BackgroundException {
+    public Boolean authenticate(final Host bookmark, final LoginCallback prompt, final CancelCallback cancel) throws BackgroundException {
         if(log.isDebugEnabled()) {
             log.debug(String.format("Login using agent %s for %s", agent, bookmark));
         }
-        for(Identity identity : this.filter(bookmark.getCredentials(), agent.getIdentities())) {
+        final Collection<Identity> identities;
+        if(new OpenSSHIdentitiesOnlyConfigurator().isIdentitiesOnly(bookmark.getHostname())) {
+            final Credentials configuration = new OpenSSHCredentialsConfigurator().configure(bookmark);
+            if(configuration.isPublicKeyAuthentication()) {
+                try {
+                    final Local identity = configuration.getIdentity();
+                    if(log.isWarnEnabled()) {
+                        log.warn(String.format("Only read specific key %s from SSH agent with IdentitiesOnly configuration", identity));
+                    }
+                    final InputStream in = identity.getInputStream();
+                    final ByteArrayOutputStream out = IOUtils.readFully(in);
+                    final byte[] blob = out.toByteArray();
+                    identities = Collections.singletonList(new Identity(blob, blob));
+                }
+                catch(IOException e) {
+                    throw new DefaultIOExceptionMappingService().map(e);
+                }
+            }
+            else {
+                log.warn(String.format("Missing IdentityFile configuration for %s", bookmark));
+                identities = Collections.emptyList();
+            }
+        }
+        else {
+            identities = this.filter(bookmark.getCredentials(), agent.getIdentities());
+        }
+        for(Identity identity : identities) {
             try {
                 client.auth(bookmark.getCredentials().getUsername(), new AuthAgent(agent.getProxy(), identity));
                 // Successfully authenticated
