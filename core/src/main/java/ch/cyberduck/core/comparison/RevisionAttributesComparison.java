b796b5c5addc5994ed714bf6508b5e183f95b1ae
David Kocher <dkocher@iterate.ch>
2022-12-08 22:47:37 +0100

    Add comparison services for attributes.

---
+++ core/src/main/java/ch/cyberduck/core/comparison/RevisionAttributesComparison.java
@@ -0,0 +1,44 @@
+package ch.cyberduck.core.comparison;
+
+/*
+ * Copyright (c) 2002-2022 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.features.AttributesComparison;
+import ch.cyberduck.core.synchronization.ChecksumComparisonService;
+import ch.cyberduck.core.synchronization.Comparison;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class RevisionAttributesComparison implements AttributesComparison {
+    private static final Logger log = LogManager.getLogger(ChecksumComparisonService.class.getName());
+
+    public Comparison compare(final Path.Type type, final PathAttributes remote, final PathAttributes cached) {
+        if(null != remote.getRevision() && null != cached.getRevision()) {
+            if(remote.getRevision().equals(cached.getRevision())) {
+                // No conflict. Proceed with overwrite
+                if(log.isDebugEnabled()) {
+                    log.debug(String.format("Equal revision %s", cached.getRevision()));
+                }
+                return Comparison.equal;
+            }
+            log.warn(String.format("Revision %s in cache differs from %s on server", cached.getRevision(), remote.getRevision()));
+            return Comparison.notequal;
+        }
+        return Comparison.unknown;
+    }
+}
---
+++ core/src/main/java/ch/cyberduck/core/comparison/SizeAttributesComparison.java
@@ -0,0 +1,33 @@
+package ch.cyberduck.core.comparison;
+
+/*
+ * Copyright (c) 2012 David Kocher. All rights reserved.
+ * http://cyberduck.ch/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Bug fixes, suggestions and comments should be sent to:
+ * dkocher@cyberduck.ch
+ */
+
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.features.AttributesComparison;
+import ch.cyberduck.core.synchronization.Comparison;
+import ch.cyberduck.core.synchronization.SizeComparisonService;
+
+public class SizeAttributesComparison implements AttributesComparison {
+
+    @Override
+    public Comparison compare(final Path.Type type, final PathAttributes file, final PathAttributes other) {
+        return new SizeComparisonService().compare(file, other);
+    }
+}
