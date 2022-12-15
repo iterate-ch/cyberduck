2ff2571e40c96346b481d6e61d58d5d9f1331050
David Kocher <dkocher@iterate.ch>
2022-12-15 16:46:36 +0100

    Add test.

--- googlestorage/src/test/java/ch/cyberduck/core/googlestorage/GoogleStorageTimestampFeatureTest.java
+++ googlestorage/src/test/java/ch/cyberduck/core/googlestorage/GoogleStorageTimestampFeatureTest.java
@@ -19,15 +19,22 @@ import ch.cyberduck.core.AlphanumericRandomStringService;
 import ch.cyberduck.core.DisabledConnectionCallback;
 import ch.cyberduck.core.DisabledLoginCallback;
 import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
 import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.http.HttpResponseOutputStream;
+import ch.cyberduck.core.io.StreamCopier;
 import ch.cyberduck.core.transfer.TransferStatus;
 import ch.cyberduck.test.IntegrationTest;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.io.ByteArrayInputStream;
 import java.util.Collections;
 import java.util.EnumSet;
+
+import com.google.api.services.storage.model.StorageObject;
 
 import static org.junit.Assert.assertEquals;
 
@@ -38,13 +45,23 @@ public class GoogleStorageTimestampFeatureTest extends AbstractGoogleStorageTest
     public void testFindTimesteamp() throws Exception {
         final Path bucket = new Path("cyberduck-test-eu", EnumSet.of(Path.Type.directory, Path.Type.volume));
         final Path test = new GoogleStorageTouchFeature(session).touch(new Path(bucket,
-            new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file)), new TransferStatus().withTimestamp(1530305150672L));
+                new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file)), new TransferStatus().withTimestamp(1530305150672L));
         assertEquals(1530305150672L, new GoogleStorageAttributesFinderFeature(session).find(test).getModificationDate());
+        final TransferStatus status = new TransferStatus();
+        final byte[] content = RandomUtils.nextBytes(1033);
+        status.setLength(content.length);
+        status.setTimestamp(1530305150673L);
+        final HttpResponseOutputStream<StorageObject> out = new GoogleStorageWriteFeature(session).write(test, status, new DisabledConnectionCallback());
+        new StreamCopier(new TransferStatus(), new TransferStatus()).transfer(new ByteArrayInputStream(content), out);
+        out.close();
+        final PathAttributes response = status.getResponse();
+        test.withAttributes(response);
+        assertEquals(1530305150673L, response.getModificationDate());
         final GoogleStorageTimestampFeature feature = new GoogleStorageTimestampFeature(session);
         feature.setTimestamp(test, 1630305150672L);
         assertEquals(1630305150672L, new GoogleStorageAttributesFinderFeature(session).find(test).getModificationDate());
         final Path moved = new GoogleStorageMoveFeature(session).move(test, new Path(bucket,
-            new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file)), new TransferStatus(), new Delete.DisabledCallback(), new DisabledConnectionCallback());
+                new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file)), new TransferStatus(), new Delete.DisabledCallback(), new DisabledConnectionCallback());
         assertEquals(1630305150672L, moved.attributes().getModificationDate());
         assertEquals(1630305150672L, new GoogleStorageAttributesFinderFeature(session).find(moved).getModificationDate());
         new GoogleStorageDeleteFeature(session).delete(Collections.singletonList(moved), new DisabledLoginCallback(), new Delete.DisabledCallback());
