6f7e88e9871d257b93e0789994925ce46e2e9cfd
Yves Langisch <yla@iterate.ch>
2022-12-16 12:07:06 +0100

    Add support for public key identities.

--- ssh/src/main/java/ch/cyberduck/core/sftp/auth/SFTPAgentAuthentication.java
+++ ssh/src/main/java/ch/cyberduck/core/sftp/auth/SFTPAgentAuthentication.java
@@ -20,31 +20,40 @@ import ch.cyberduck.core.Credentials;
 import ch.cyberduck.core.DefaultIOExceptionMappingService;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.Local;
+import ch.cyberduck.core.LocalFactory;
 import ch.cyberduck.core.LoginCallback;
+import ch.cyberduck.core.exception.AccessDeniedException;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.sftp.SFTPExceptionMappingService;
 import ch.cyberduck.core.sftp.openssh.OpenSSHCredentialsConfigurator;
 import ch.cyberduck.core.sftp.openssh.OpenSSHIdentitiesOnlyConfigurator;
 import ch.cyberduck.core.threading.CancelCallback;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
+import com.hierynomus.sshj.userauth.keyprovider.OpenSSHKeyFileUtil;
 import com.jcraft.jsch.agentproxy.CustomIdentity;
 import com.jcraft.jsch.agentproxy.Identity;
 import com.jcraft.jsch.agentproxy.sshj.AuthAgent;
 import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.common.Base64;
 import net.schmizz.sshj.common.Buffer;
-import net.schmizz.sshj.common.IOUtils;
 import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.userauth.UserAuthException;
+import net.schmizz.sshj.userauth.keyprovider.KeyFormat;
+import net.schmizz.sshj.userauth.keyprovider.KeyProviderUtil;
 
 public class SFTPAgentAuthentication implements AuthenticationProvider<Boolean> {
     private static final Logger log = LogManager.getLogger(SFTPAgentAuthentication.class);
@@ -71,10 +80,9 @@ public class SFTPAgentAuthentication implements AuthenticationProvider<Boolean> 
                     if(log.isWarnEnabled()) {
                         log.warn(String.format("Only read specific key %s from SSH agent with IdentitiesOnly configuration", identity));
                     }
-                    final InputStream in = identity.getInputStream();
-                    final ByteArrayOutputStream out = IOUtils.readFully(in);
-                    final byte[] blob = out.toByteArray();
-                    identities = Collections.singletonList(new CustomIdentity(blob));
+                    identities = this.isPrivateKey(identity) ?
+                            this.identityFromPrivateKey(identity) :
+                            this.identityFromPublicKey(identity);
                 }
                 catch(IOException e) {
                     throw new DefaultIOExceptionMappingService().map(e);
@@ -129,5 +137,34 @@ public class SFTPAgentAuthentication implements AuthenticationProvider<Boolean> 
             }
         }
         return identities;
+    }
+
+    private boolean isPrivateKey(final Local identity) throws AccessDeniedException, IOException {
+        final KeyFormat format = KeyProviderUtil.detectKeyFileFormat(
+                new InputStreamReader(identity.getInputStream()), true);
+        return format != KeyFormat.Unknown;
+    }
+
+    private Collection<Identity> identityFromPrivateKey(final Local identity) throws IOException, AccessDeniedException {
+        final File pubKey = OpenSSHKeyFileUtil.getPublicKeyFile(new File(identity.getAbsolute()));
+        if(pubKey != null) {
+            return this.identityFromPublicKey(LocalFactory.get(pubKey.getAbsolutePath()));
+        }
+        log.warn(String.format("Unable to find public key file for identity %s", identity));
+        return Collections.emptyList();
+    }
+
+    private Collection<Identity> identityFromPublicKey(final Local identity) throws IOException, AccessDeniedException {
+        final List<String> lines = IOUtils.readLines(identity.getInputStream(), Charset.defaultCharset());
+        for(String line : lines) {
+            final String keydata = line.trim();
+            if(StringUtils.isNotBlank(keydata)) {
+                String[] parts = keydata.split("\\s+");
+                if(parts.length >= 2) {
+                    return Collections.singletonList(new CustomIdentity(Base64.decode(parts[1])));
+                }
+            }
+        }
+        throw new IOException(String.format("Failure reading public key %s", identity));
     }
 }
