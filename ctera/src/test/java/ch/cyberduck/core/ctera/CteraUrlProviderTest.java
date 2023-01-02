dbfd3aeb16b66e69f8b620f1069478de4a667c68
David Kocher <dkocher@iterate.ch>
2023-01-02 12:23:55 +0100

    Add custom provider.

---
+++ ctera/src/test/java/ch/cyberduck/core/ctera/CteraUrlProviderTest.java
@@ -0,0 +1,39 @@
+package ch.cyberduck.core.ctera;
+
+/*
+ * Copyright (c) 2002-2023 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AlphanumericRandomStringService;
+import ch.cyberduck.core.DescriptiveUrl;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.shared.DefaultHomeFinderService;
+
+import org.junit.Test;
+
+import java.util.EnumSet;
+
+import static org.junit.Assert.assertEquals;
+
+public class CteraUrlProviderTest extends AbstractCteraTest {
+
+    @Test
+    public void toUrl() throws Exception {
+        final String filename = new AlphanumericRandomStringService().random();
+        final Path test = new Path(new DefaultHomeFinderService(session).find(),
+                filename, EnumSet.of(Path.Type.file));
+        assertEquals("https://mountainduck.ctera.me/ServicesPortal/#/cloudDrive/" + filename,
+                new CteraUrlProvider(session.getHost()).toUrl(test).find(DescriptiveUrl.Type.provider).getUrl());
+    }
+}
