36b72328ae3b94431d87148045e59867b7b513df
David Kocher <dkocher@iterate.ch>
2023-02-13 18:01:28 +0100

    Add test.

---
+++ brick/src/test/java/ch/cyberduck/core/brick/BrickTouchFeatureTest.java
@@ -0,0 +1,52 @@
+package ch.cyberduck.core.brick;
+
+/*
+ * Copyright (c) 2002-2023 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AlphanumericRandomStringService;
+import ch.cyberduck.core.DisabledPasswordCallback;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.exception.ConflictException;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.transfer.TransferStatus;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.EnumSet;
+
+import static org.junit.Assert.fail;
+
+public class BrickTouchFeatureTest extends AbstractBrickTest {
+
+    @Test(expected = ConflictException.class)
+    public void testCaseSensitivity() throws Exception {
+        final Path container = new BrickDirectoryFeature(session).mkdir(new Path(
+                new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory, Path.Type.volume)), new TransferStatus());
+        final String filename = StringUtils.lowerCase(new AlphanumericRandomStringService().random());
+        final Path file = new BrickTouchFeature(session)
+                .touch(new Path(container, filename, EnumSet.of(Path.Type.file)), new TransferStatus().withLength(0L));
+        try {
+            // Create conflict
+            new BrickTouchFeature(session)
+                    .touch(new Path(container, StringUtils.capitalize(filename), EnumSet.of(Path.Type.file)), new TransferStatus().withLength(0L));
+            fail();
+        }
+        finally {
+            new BrickDeleteFeature(session).delete(Collections.singletonList(file), new DisabledPasswordCallback(), new Delete.DisabledCallback());
+        }
+    }
+}
