5a0920dbb4b1f5b7692a1becf593bc2d503cb722
AliveDevil <AliveDevil@users.noreply.github.com>
2023-03-13 15:19:21 +0100

    Review password form

--- windows/src/main/csharp/ch/cyberduck/ui/winforms/PasswordForm.cs
+++ windows/src/main/csharp/ch/cyberduck/ui/winforms/PasswordForm.cs
@@ -16,20 +16,30 @@
 // feedback@cyberduck.io
 // 
 
-using System.Windows.Forms;
 using Ch.Cyberduck.Ui.Controller;
+using System.Windows.Forms;
 
 namespace Ch.Cyberduck.Ui.Winforms
 {
     public partial class PasswordForm : PromptForm, IPasswordPromptView
     {
         private readonly CheckBox saveCheckBox;
+        private readonly Button okButton;
+        private readonly Button skipButton;
 
         public PasswordForm()
         {
             InitializeComponent();
 
             inputTextBox.UseSystemPasswordChar = true;
+            
+            tableLayoutPanel.SuspendLayout();
+
+            var lastRow = tableLayoutPanel.RowCount++;
+            var layoutRow = lastRow - 1;
+            tableLayoutPanel.RowStyles.Insert(layoutRow, new(SizeType.AutoSize));
+            tableLayoutPanel.SetRow(buttonPanel, lastRow);
+
             saveCheckBox = new CheckBox
             {
                 Name = "saveCheckBox",
@@ -37,12 +47,11 @@ namespace Ch.Cyberduck.Ui.Winforms
                 Anchor = (((AnchorStyles.Left | AnchorStyles.Right))),
                 Text = "Save Password"
             };
-            tableLayoutPanel.RowStyles.Insert(2, new RowStyle(SizeType.AutoSize));
-            tableLayoutPanel.SetRow(okButton, 3);
-            tableLayoutPanel.SetRow(cancelButton, 3);
-            tableLayoutPanel.SetRow(skipButton, 3);
-            tableLayoutPanel.Controls.Add(saveCheckBox, 1, 2);
-            tableLayoutPanel.SetColumnSpan(saveCheckBox, 3);
+            tableLayoutPanel.Controls.Add(saveCheckBox, 1, layoutRow);
+
+            SetupButtons(out okButton, out skipButton);
+
+            tableLayoutPanel.ResumeLayout();
         }
 
         public bool CanSkip
@@ -79,6 +88,37 @@ namespace Ch.Cyberduck.Ui.Winforms
         public string SkipButtonText
         {
             set => skipButton.Text = value;
+        }
+
+        private void SetupButtons(out Button createButton, out Button skipButton)
+        {
+            buttonPanel.SuspendLayout();
+
+            buttonPanel.ColumnCount = 4;
+            buttonPanel.Controls.Add(CreateButton(new Button()
+            {
+                DialogResult = DialogResult.OK,
+                TabIndex = 0,
+                Text = "Create",
+            }, out createButton), 1, 0);
+            AcceptButton = createButton;
+
+            buttonPanel.Controls.Add(CreateButton(new Button()
+            {
+                DialogResult = DialogResult.Yes,
+                TabIndex = 1,
+                Text = "Skip",
+            }, out skipButton), 2, 0);
+
+            buttonPanel.Controls.Add(CreateButton(new Button()
+            {
+                DialogResult = DialogResult.Cancel,
+                TabIndex = 5,
+                Text = "Cancel",
+            }, out var cancelButton), 3, 0);
+            CancelButton = cancelButton;
+
+            buttonPanel.ResumeLayout();
         }
     }
 }
