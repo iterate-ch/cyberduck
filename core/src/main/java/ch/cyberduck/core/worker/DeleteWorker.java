ded1d62d8860fb5180c99f8be831cfe69273c3d1
David Kocher <dkocher@iterate.ch>
2023-04-02 22:50:06 +0200

    Add enabled check.

--- core/src/main/java/ch/cyberduck/core/worker/DeleteWorker.java
+++ core/src/main/java/ch/cyberduck/core/worker/DeleteWorker.java
@@ -122,11 +122,13 @@ public class DeleteWorker extends Worker<List<Path>> {
         final Versioning versioning = session.getFeature(Versioning.class);
         for(Iterator<Path> iter = recursive.keySet().iterator(); iter.hasNext(); ) {
             final Path f = iter.next();
-            if(versioning.save(f)) {
-                if(log.isDebugEnabled()) {
-                    log.debug(String.format("Skip deleting %s", f));
+            if(versioning.getConfiguration(f).isEnabled()) {
+                if(versioning.save(f)) {
+                    if(log.isDebugEnabled()) {
+                        log.debug(String.format("Skip deleting %s", f));
+                    }
+                    iter.remove();
                 }
-                iter.remove();
             }
         }
         if(!recursive.isEmpty()) {
