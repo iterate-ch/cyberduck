57982cba9037f840ceb1c86b9b726f4d7a8df6fc
Marc Bruni <marc.bruni@students.fhnw.ch>
2023-04-25 17:21:49 +0200

    split connect and login, split logout and disconnect, fix immediate close

--- smb/src/main/java/ch/cyberduck/core/smb/SMBSession.java
+++ smb/src/main/java/ch/cyberduck/core/smb/SMBSession.java
@@ -31,6 +31,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
+import com.hierynomus.protocol.transport.TransportException;
 import com.hierynomus.smbj.SMBClient;
 import com.hierynomus.smbj.auth.AuthenticationContext;
 import com.hierynomus.smbj.connection.Connection;
@@ -39,9 +40,14 @@ import com.hierynomus.smbj.share.DiskShare;
 
 public class SMBSession extends ch.cyberduck.core.Session<SMBClient> {
 
-    private SMBClient client;
-    private Connection connection;
-    private DiskShare share;
+    protected Connection connection;
+    protected DiskShare share;
+    protected Session session;
+
+    protected SMBSession(final Host h) {
+        super(h);
+        client = new SMBClient();
+    }
 
     public SMBSession(final Host h, final X509TrustManager trust, final X509KeyManager key) {
         super(h);
@@ -53,46 +59,80 @@ public class SMBSession extends ch.cyberduck.core.Session<SMBClient> {
     @Override
     protected SMBClient connect(Proxy proxy, HostKeyCallback key, LoginCallback prompt, CancelCallback cancel)
             throws BackgroundException {
-        
-        String domain, username, shareString;
-        String[] parts = host.getCredentials().getUsername().split("/", 0);
-        String domainUsername = parts[0];
-        shareString = parts[1];
-        parts = domainUsername.split("@", 0);
-        if (parts.length == 1) {
-            domain = "WORKGROUP";
-            username = parts[0];
-        } else {
-            domain = parts[0];
-            username = parts[1];
+
+        try {
+            this.connection = client.connect(getHost().getHostname(), getHost().getPort());
         }
-
-        try (Connection connection = client.connect(getHost().getHostname(), getHost().getPort())) {
-            this.connection = connection;
-            AuthenticationContext ac = new AuthenticationContext(username, host.getCredentials().getPassword().toCharArray(), domain);
-            Session session = connection.authenticate(ac);
-
-            // Connect to Share
-            try (DiskShare share = (DiskShare) session.connectShare(shareString)) {
-                this.share = share;
+        catch(Exception e) {
+            try {
+                connection.close();
+                throw new RuntimeException(e);
             }
-        }
-        catch(IOException e) {
-            throw new RuntimeException(e);
+            catch(IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
         return client;
     }
 
     @Override
     public void login(Proxy proxy, LoginCallback prompt, CancelCallback cancel) throws BackgroundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+        if(connection == null) {
+            throw new BackgroundException(new IllegalStateException("Connection must be established before login"));
+        }
+        String domain, username, shareString;
+
+        String[] parts = host.getCredentials().getUsername().split("/", 0);
+        String domainUsername = parts[0];
+        shareString = parts[1];
+        parts = domainUsername.split("@", 0);
+        if(parts.length == 1) {
+            domain = "WORKGROUP";
+            username = parts[0];
+        }
+        else {
+            username = parts[0];
+            domain = parts[1];
+        }
+
+        AuthenticationContext ac = new AuthenticationContext(username, host.getCredentials().getPassword().toCharArray(), domain);
+        try {
+            session = connection.authenticate(ac);
+            share = (DiskShare) session.connectShare(shareString);
+        } catch(Exception e) {
+            throw new BackgroundException(e);
+        }
     }
 
     @Override
     protected void logout() throws BackgroundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'logout'");
+        if(session != null) {
+            try {
+                session.logoff();
+            }
+            catch(TransportException e) {
+                throw new BackgroundException(e);
+            }
+        }
+    }
+
+    @Override
+    protected void disconnect() {
+        super.disconnect();
+        try {
+            if(connection != null) {
+                connection.close();
+                connection = null;
+            }
+            if(client != null) {
+                client.close();
+                client = null;
+            }
+        }
+        catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
