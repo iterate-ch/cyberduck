094d5dc5d8ad3fc6b8402f364bf9691bdf62f8bd
Marc Bruni <marc.bruni@students.fhnw.ch>
2023-05-04 23:08:47 +0200

    map smbj exceptions to existing cyberduck exceptions

--- smb/src/main/java/ch/cyberduck/core/smb/SmbExceptionMappingService.java
+++ smb/src/main/java/ch/cyberduck/core/smb/SmbExceptionMappingService.java
@@ -16,6 +16,14 @@ package ch.cyberduck.core.smb;/*
 import ch.cyberduck.core.AbstractExceptionMappingService;
 import ch.cyberduck.core.exception.AccessDeniedException;
 import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.ConflictException;
+import ch.cyberduck.core.exception.ConnectionCanceledException;
+import ch.cyberduck.core.exception.ConnectionTimeoutException;
+import ch.cyberduck.core.exception.LockedException;
+import ch.cyberduck.core.exception.LoginFailureException;
+import ch.cyberduck.core.exception.NotfoundException;
+import ch.cyberduck.core.exception.QuotaException;
+import ch.cyberduck.core.exception.UnsupportedException;
 
 import com.hierynomus.mssmb2.SMBApiException;
 import com.hierynomus.smbj.common.SMBRuntimeException;
@@ -25,16 +33,38 @@ public class SmbExceptionMappingService extends AbstractExceptionMappingService<
     public BackgroundException map(final SMBRuntimeException exception) {
         if(exception instanceof SMBApiException) {
             switch(((SMBApiException) exception).getStatus()) {
-                // TODO: map all errors
+                case STATUS_NOT_FOUND:
+                case STATUS_OBJECT_NAME_NOT_FOUND:
+                case STATUS_OBJECT_PATH_NOT_FOUND:
+                    return new NotfoundException(exception.getMessage(), exception.getCause());
+                case STATUS_NOT_IMPLEMENTED:
+                case STATUS_NOT_SUPPORTED:
+                    return new UnsupportedException(exception.getMessage(), exception.getCause());
                 case STATUS_ACCESS_DENIED:
                     return new AccessDeniedException(exception.getMessage(), exception.getCause());
+                case STATUS_OBJECT_NAME_COLLISION:
+                    return new ConflictException(exception.getMessage(), exception.getCause());
+                case STATUS_FILE_LOCK_CONFLICT:
+                case STATUS_LOCK_NOT_GRANTED:
+                    return new LockedException(exception.getMessage(), exception.getCause());
+                case STATUS_LOGON_FAILURE:
+                case STATUS_PASSWORD_EXPIRED:
+                case STATUS_ACCOUNT_DISABLED:
+                case STATUS_LOGON_TYPE_NOT_GRANTED:
+                    return new LoginFailureException(exception.getMessage(), exception.getCause());
+                case STATUS_DISK_FULL:
+                    return new QuotaException(exception.getMessage(), exception.getCause());
+                case STATUS_IO_TIMEOUT:
+                    return new ConnectionTimeoutException(exception.getMessage(), exception.getCause());
+                case STATUS_CONNECTION_DISCONNECTED:
+                case STATUS_CONNECTION_RESET:
+                    return new ConnectionCanceledException(exception.getMessage(), exception.getCause());
                 default:
                     return new BackgroundException(exception.getMessage(), exception.getCause());
             }
-        } else {
+        }
+        else {
             return new BackgroundException(exception.getMessage(), exception.getCause());
         }
-
-
     }
 }
