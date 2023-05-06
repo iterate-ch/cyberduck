7cfedf8b2cc47a0e97bcf15250e5be6f8b6a671d
oaehmka <71215943+oaehmka@users.noreply.github.com>
2023-05-06 12:14:22 +0200

    add basic SMBReadFeature

--- smb/src/main/java/ch/cyberduck/core/smb/SMBReadFeature.java
+++ smb/src/main/java/ch/cyberduck/core/smb/SMBReadFeature.java
@@ -1,6 +1,14 @@
 package ch.cyberduck.core.smb;
 
 import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.hierynomus.msdtyp.AccessMask;
+import com.hierynomus.msfscc.FileAttributes;
+import com.hierynomus.mssmb2.SMB2CreateDisposition;
+import com.hierynomus.mssmb2.SMB2CreateOptions;
+import com.hierynomus.mssmb2.SMB2ShareAccess;
 
 import ch.cyberduck.core.ConnectionCallback;
 import ch.cyberduck.core.Path;
@@ -18,8 +26,21 @@ public class SMBReadFeature implements Read {
 
     @Override
     public InputStream read(Path file, TransferStatus status, ConnectionCallback callback) throws BackgroundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'read'");
+        
+        Set<SMB2ShareAccess> shareAccessSet = new HashSet<>();
+        shareAccessSet.add(SMB2ShareAccess.FILE_SHARE_READ);
+
+        Set<FileAttributes> fileAttributes = new HashSet<>();
+        fileAttributes.add(FileAttributes.FILE_ATTRIBUTE_NORMAL);
+        Set<SMB2CreateOptions> createOptions = new HashSet<>();
+        SMB2CreateDisposition smb2CreateDisposition = SMB2CreateDisposition.FILE_OPEN_IF;
+
+        Set<AccessMask> accessMask = new HashSet<>();
+        accessMask.add(AccessMask.MAXIMUM_ALLOWED);
+
+        createOptions.add(SMB2CreateOptions.FILE_NON_DIRECTORY_FILE);
+
+        return session.share.openFile(file.getAbsolute(), accessMask, fileAttributes, shareAccessSet, smb2CreateDisposition, createOptions).getInputStream();
     }
 
     
