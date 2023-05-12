86fd3816b0e5e8699d19a5efd628e606de361242
Marc Bruni <marc.bruni@students.fhnw.ch>
2023-05-12 23:31:42 +0200

    add missing error handling in smb features

--- smb/src/main/java/ch/cyberduck/core/smb/SMBWriteFeature.java
+++ smb/src/main/java/ch/cyberduck/core/smb/SMBWriteFeature.java
@@ -18,6 +18,7 @@ import com.hierynomus.msfscc.FileAttributes;
 import com.hierynomus.mssmb2.SMB2CreateDisposition;
 import com.hierynomus.mssmb2.SMB2CreateOptions;
 import com.hierynomus.mssmb2.SMB2ShareAccess;
+import com.hierynomus.mssmb2.SMBApiException;
 import com.hierynomus.smbj.share.File;
 
 public class SMBWriteFeature extends AppendWriteFeature<Void> {
@@ -31,26 +32,32 @@ public class SMBWriteFeature extends AppendWriteFeature<Void> {
     @Override
     public StatusOutputStream<Void> write(Path file, TransferStatus status, ConnectionCallback callback)
             throws BackgroundException {
+        try {
+            Set<SMB2ShareAccess> shareAccessSet = new HashSet<>();
+            shareAccessSet.add(SMB2ShareAccess.FILE_SHARE_READ);
+            shareAccessSet.add(SMB2ShareAccess.FILE_SHARE_WRITE);
+            shareAccessSet.add(SMB2ShareAccess.FILE_SHARE_DELETE);
 
-        Set<SMB2ShareAccess> shareAccessSet = new HashSet<>();
-        shareAccessSet.add(SMB2ShareAccess.FILE_SHARE_READ);
-        shareAccessSet.add(SMB2ShareAccess.FILE_SHARE_WRITE);
-        shareAccessSet.add(SMB2ShareAccess.FILE_SHARE_DELETE);
+            Set<FileAttributes> fileAttributes = new HashSet<>();
+            fileAttributes.add(FileAttributes.FILE_ATTRIBUTE_NORMAL);
+            Set<SMB2CreateOptions> createOptions = new HashSet<>();
+            SMB2CreateDisposition smb2CreateDisposition = SMB2CreateDisposition.FILE_OPEN_IF;
 
-        Set<FileAttributes> fileAttributes = new HashSet<>();
-        fileAttributes.add(FileAttributes.FILE_ATTRIBUTE_NORMAL);
-        Set<SMB2CreateOptions> createOptions = new HashSet<>();
-        SMB2CreateDisposition smb2CreateDisposition = SMB2CreateDisposition.FILE_OPEN_IF;
+            Set<AccessMask> accessMask = new HashSet<>();
+            accessMask.add(AccessMask.MAXIMUM_ALLOWED);
 
-        Set<AccessMask> accessMask = new HashSet<>();
-        accessMask.add(AccessMask.MAXIMUM_ALLOWED);
+            createOptions.add(SMB2CreateOptions.FILE_NON_DIRECTORY_FILE);
 
-        createOptions.add(SMB2CreateOptions.FILE_NON_DIRECTORY_FILE);
 
-        File fileEntry = session.share.openFile(file.getAbsolute(), accessMask, fileAttributes,
-                shareAccessSet, smb2CreateDisposition, createOptions);
+            File fileEntry = session.share.openFile(file.getAbsolute(), accessMask, fileAttributes,
+                    shareAccessSet, smb2CreateDisposition, createOptions);
 
-        return new VoidStatusOutputStream(new SMBOutputStream(fileEntry.getOutputStream(), fileEntry));
+            return new VoidStatusOutputStream(new SMBOutputStream(fileEntry.getOutputStream(), fileEntry));
+        }
+        catch(SMBApiException e) {
+            throw new SmbExceptionMappingService().map(e);
+        }
+
 
     }
 
