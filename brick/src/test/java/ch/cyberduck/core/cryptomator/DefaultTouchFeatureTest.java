6ee90f5c74aa6009e3bef1cc28e3cc371ccab128
David Kocher <dkocher@iterate.ch>
2023-05-14 21:49:04 +0200

    Add test.

---
+++ brick/src/test/java/ch/cyberduck/core/cryptomator/DefaultTouchFeatureTest.java
@@ -0,0 +1,85 @@
+package ch.cyberduck.core.cryptomator;
+
+/*
+ * Copyright (c) 2002-2023 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AlphanumericRandomStringService;
+import ch.cyberduck.core.DisabledLoginCallback;
+import ch.cyberduck.core.DisabledPasswordCallback;
+import ch.cyberduck.core.DisabledPasswordStore;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.brick.AbstractBrickTest;
+import ch.cyberduck.core.brick.BrickDeleteFeature;
+import ch.cyberduck.core.brick.BrickFindFeature;
+import ch.cyberduck.core.brick.BrickWriteFeature;
+import ch.cyberduck.core.cryptomator.features.CryptoFindV6Feature;
+import ch.cyberduck.core.cryptomator.features.CryptoTouchFeature;
+import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.features.Find;
+import ch.cyberduck.core.shared.DefaultFindFeature;
+import ch.cyberduck.core.shared.DefaultTouchFeature;
+import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.core.vault.DefaultVaultRegistry;
+import ch.cyberduck.core.vault.VaultCredentials;
+import ch.cyberduck.test.IntegrationTest;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+
+@Category(IntegrationTest.class)
+@RunWith(value = Parameterized.class)
+public class DefaultTouchFeatureTest extends AbstractBrickTest {
+
+    @Test
+    public void testTouchLongFilenameEncrypted() throws Exception {
+        assumeTrue(vaultVersion == CryptoVault.VAULT_VERSION_DEPRECATED);
+        final Path home = new Path("/", EnumSet.of(Path.Type.directory, Path.Type.volume));
+        final CryptoVault cryptomator = new CryptoVault(
+                new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory)));
+        final Path vault = cryptomator.create(session, new VaultCredentials("test"), new DisabledPasswordStore(), vaultVersion);
+        session.withRegistry(new DefaultVaultRegistry(new DisabledPasswordStore(), new DisabledPasswordCallback(), cryptomator));
+        final TransferStatus status = new TransferStatus();
+        final Path test = new CryptoTouchFeature<>(session, new DefaultTouchFeature(new BrickWriteFeature(session)), new BrickWriteFeature(session), cryptomator).touch(
+                new Path(vault, new AlphanumericRandomStringService(130).random(), EnumSet.of(Path.Type.file)), status);
+        assertEquals(TransferStatus.UNKNOWN_LENGTH, status.getResponse().getSize());
+        assertTrue(new CryptoFindV6Feature(session, new BrickFindFeature(session), cryptomator).find(test));
+        cryptomator.getFeature(session, Delete.class, new BrickDeleteFeature(session)).delete(Arrays.asList(test, vault), new DisabledLoginCallback(), new Delete.DisabledCallback());
+    }
+
+    @Test
+    public void testTouchLongFilenameEncryptedDefaultFeature() throws Exception {
+        assumeTrue(vaultVersion == CryptoVault.VAULT_VERSION_DEPRECATED);
+        final Path home = new Path("/", EnumSet.of(Path.Type.directory, Path.Type.volume));
+        final CryptoVault cryptomator = new CryptoVault(
+                new Path(home, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory)));
+        final Path vault = cryptomator.create(session, new VaultCredentials("test"), new DisabledPasswordStore(), vaultVersion);
+        session.withRegistry(new DefaultVaultRegistry(new DisabledPasswordStore(), new DisabledPasswordCallback(), cryptomator));
+        final TransferStatus status = new TransferStatus();
+        final Path test = new CryptoTouchFeature<>(session, new DefaultTouchFeature(new BrickWriteFeature(session)), new BrickWriteFeature(session), cryptomator).touch(
+                new Path(vault, new AlphanumericRandomStringService(130).random(), EnumSet.of(Path.Type.file)), status);
+        assertEquals(TransferStatus.UNKNOWN_LENGTH, status.getResponse().getSize());
+        assertTrue(cryptomator.getFeature(session, Find.class, new DefaultFindFeature(session)).find(test));
+        cryptomator.getFeature(session, Delete.class, new BrickDeleteFeature(session)).delete(Arrays.asList(test, vault), new DisabledLoginCallback(), new Delete.DisabledCallback());
+    }
+}
