75e2683cbe07b1f795137880b073ce10cd4d8e68
SheromS <sherom.sandmeier@students.fhnw.ch>
2023-05-29 17:46:16 +0200

    created NonAwsSTSCredentialsConfigurator.java to use non AWS compatible STS

---
+++ s3/src/main/java/ch/cyberduck/core/sts/NonAwsSTSCredentialsConfigurator.java
@@ -0,0 +1,58 @@
+package ch.cyberduck.core.sts;/*
+ * Copyright (c) 2002-2023 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.Credentials;
+import ch.cyberduck.core.Host;
+import ch.cyberduck.core.PasswordCallback;
+import ch.cyberduck.core.aws.CustomClientConfiguration;
+import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.exception.LoginFailureException;
+import ch.cyberduck.core.ssl.ThreadLocalHostnameDelegatingTrustManager;
+import ch.cyberduck.core.ssl.X509KeyManager;
+import ch.cyberduck.core.ssl.X509TrustManager;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
+import com.amazonaws.services.securitytoken.model.AssumeRoleWithWebIdentityRequest;
+
+public class NonAwsSTSCredentialsConfigurator extends STSCredentialsConfigurator {
+    private static final Logger log = LogManager.getLogger(NonAwsSTSCredentialsConfigurator.class);
+
+
+    public NonAwsSTSCredentialsConfigurator(final X509TrustManager trust, final X509KeyManager key, final PasswordCallback prompt) {
+       super(trust, key, prompt);
+    }
+
+    @Override
+    public Credentials configure(final Host host) throws LoginFailureException, LoginCanceledException {
+        final Credentials credentials = new Credentials(host.getCredentials());
+        final ClientConfiguration configuration = new CustomClientConfiguration(host,
+                new ThreadLocalHostnameDelegatingTrustManager(trust, host.getHostname()), key);
+
+
+        final AWSSecurityTokenService service = AWSSecurityTokenServiceClientBuilder.standard().build();
+
+        AssumeRoleWithWebIdentityRequest webIdReq = new AssumeRoleWithWebIdentityRequest()
+                .withWebIdentityToken("token")
+                .withPolicy("policy");
+        service.assumeRoleWithWebIdentity(webIdReq);
+
+        return null;
+    }
+}
