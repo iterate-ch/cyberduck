9247a118d88d97d374516124fcd66e60d8072e18
Ismail_Cadaroski <ismail.cadaroski@students.fhnw.ch>
2023-05-30 09:43:18 +0200

    moved oidc tests to right directory

--- s3/src/test/java/ch/cyberduck/core/oidc/testenv/ContainerEnvTest.java
+++ s3/src/test/java/ch/cyberduck/core/oidc/testenv/ContainerEnvTest.java
@@ -1,4 +1,19 @@
-package ch.oidc.testenv;/*
+package ch.cyberduck.core.oidc.testenv;/*
+ * Copyright (c) 2002-2023 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
  * Copyright (c) 2002-2023 iterate GmbH. All rights reserved.
  * https://cyberduck.io/
  *
