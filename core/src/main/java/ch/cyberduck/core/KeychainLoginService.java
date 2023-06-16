d3dc53c87ba84bcb1ebad6f2bc84dfea598d55cf
David Kocher <dkocher@iterate.ch>
2023-06-16 14:29:20 +0200

    Logging.

--- core/src/main/java/ch/cyberduck/core/KeychainLoginService.java
+++ core/src/main/java/ch/cyberduck/core/KeychainLoginService.java
@@ -187,17 +187,20 @@ public class KeychainLoginService implements LoginService {
         }
         try {
             if(log.isDebugEnabled()) {
-                log.debug(String.format("Attempt authentication for %s", bookmark));
+                log.debug(String.format("Attempt authentication for %s", session));
             }
             session.login(proxy, prompt, cancel);
             if(log.isDebugEnabled()) {
-                log.debug(String.format("Login successful for session %s", session));
+                log.debug(String.format("Login successful for %s", session));
             }
             listener.message(LocaleFactory.localizedString("Login successful", "Credentials"));
             this.save(bookmark);
             return true;
         }
         catch(LoginFailureException e) {
+            if(log.isDebugEnabled()) {
+                log.debug(String.format("Login failed for %s", session));
+            }
             listener.message(LocaleFactory.localizedString("Login failed", "Credentials"));
             credentials.setPassed(false);
             final LoginOptions options = new LoginOptions(bookmark.getProtocol());
@@ -207,6 +210,9 @@ public class KeychainLoginService implements LoginService {
             if(this.prompt(bookmark, details.toString(), prompt, options)) {
                 // Retry
                 return false;
+            }
+            if(log.isDebugEnabled()) {
+                log.debug(String.format("Reset credentials for %s", bookmark));
             }
             // No updated credentials. Nullify input
             credentials.reset();
@@ -233,6 +239,9 @@ public class KeychainLoginService implements LoginService {
         // Flag for successful authentication
         credentials.setPassed(true);
         // Nullify password and tokens
+        if(log.isDebugEnabled()) {
+            log.debug(String.format("Reset credentials for %s", bookmark));
+        }
         credentials.reset();
     }
 }
