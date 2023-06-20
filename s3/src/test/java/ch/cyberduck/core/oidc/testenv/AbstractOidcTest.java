455c64fa175c3f940982f6d4ed122857514362de
Ismail_Cadaroski <ismail.cadaroski@students.fhnw.ch>
2023-06-20 09:37:29 +0200

    add network

--- s3/src/test/java/ch/cyberduck/core/oidc/testenv/AbstractOidcTest.java
+++ s3/src/test/java/ch/cyberduck/core/oidc/testenv/AbstractOidcTest.java
@@ -57,7 +57,7 @@ public abstract class AbstractOidcTest {
             new File("src/test/resources/oidcTestcontainer/docker-compose.yml"))
             .withPull(false)
             .withLocalCompose(true)
-            //.withOptions("--compatibility")
+            .withOptions("--compatibility")
             .withExposedService("keycloak_1", 8080, Wait.forListeningPort())
             .withExposedService("minio_1", 9000, Wait.forListeningPort());
 
