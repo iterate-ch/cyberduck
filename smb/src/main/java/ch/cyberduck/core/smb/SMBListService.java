149585ba73e26cbcd6c56317447493afee753771
oaehmka <71215943+oaehmka@users.noreply.github.com>
2023-07-21 15:08:18 +0200

    add SMB module code

---
+++ smb/src/main/java/ch/cyberduck/core/smb/SMBListService.java
@@ -0,0 +1,95 @@
+package ch.cyberduck.core.smb;
+
+/*
+ * Copyright (c) 2002-2023 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AbstractPath.Type;
+import ch.cyberduck.core.AttributedList;
+import ch.cyberduck.core.ListProgressListener;
+import ch.cyberduck.core.ListService;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.UnsupportedException;
+
+import java.util.EnumSet;
+
+import com.hierynomus.msfscc.FileAttributes;
+import com.hierynomus.msfscc.fileinformation.FileIdBothDirectoryInformation;
+import com.hierynomus.mssmb2.SMBApiException;
+
+public class SMBListService implements ListService {
+
+    private final SMBSession session;
+
+    public SMBListService(SMBSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public AttributedList<Path> list(final Path directory, final ListProgressListener listener) throws BackgroundException {
+
+        String dst = directory.getAbsolute();
+        if(dst.charAt(0) == '/') {
+            dst = dst.substring(1).replace("/", "\\");
+        }
+        final AttributedList<Path> result = new AttributedList<>();
+
+        try {
+
+
+            for(FileIdBothDirectoryInformation f : session.share.list(dst)) {
+                String fileName = f.getFileName();
+                if(fileName.equals(".") || fileName.equals("..")) {
+                    continue; // skip the . and .. directories
+                }
+                EnumSet<Type> type = EnumSet.noneOf(Type.class);
+                long fileAttributes = f.getFileAttributes();
+
+                // check for all relevant file types and add them to the EnumSet
+                if((fileAttributes & FileAttributes.FILE_ATTRIBUTE_DIRECTORY.getValue()) != 0) {
+                    type.add(Type.directory);
+                }
+                if((fileAttributes & FileAttributes.FILE_ATTRIBUTE_NORMAL.getValue()) != 0) {
+                    type.add(Type.file);
+                }
+
+                final PathAttributes attributes = new PathAttributes();
+                attributes.setAccessedDate(f.getLastAccessTime().toEpochMillis());
+                attributes.setModificationDate(f.getLastWriteTime().toEpochMillis());
+                attributes.setCreationDate(f.getCreationTime().toEpochMillis());
+                attributes.setSize(f.getEndOfFile());
+                attributes.setDisplayname(f.getFileName());
+
+                // default to file
+                if(type.isEmpty()) {
+                    type.add(Type.file);
+                }
+
+                result.add(new Path(directory, fileName, type, attributes));
+
+            }
+            return result;
+        }
+        catch(SMBApiException e) {
+            if(e.getStatusCode() == 3221226071L) {
+                throw new UnsupportedException("This folder is a DFS Share, which is currently not supported");
+            } else {
+                throw new SMBExceptionMappingService().map("Listing directory {0} failed", e, directory);
+            }
+        }
+    }
+
+}
---
+++ smb/src/test/java/ch/cyberduck/core/smb/SMBTimestampFeatureTest.java
@@ -0,0 +1,55 @@
+package ch.cyberduck.core.smb;
+
+/*
+ * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.AlphanumericRandomStringService;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.PathAttributes;
+import ch.cyberduck.core.features.AttributesFinder;
+import ch.cyberduck.core.features.Timestamp;
+import ch.cyberduck.core.features.Touch;
+import ch.cyberduck.core.shared.DefaultHomeFinderService;
+import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.test.TestcontainerTest;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.EnumSet;
+
+import static org.junit.Assert.*;
+
+@Category(TestcontainerTest.class)
+public class SMBTimestampFeatureTest extends AbstractSMBTest {
+
+    @Test
+    public void testTimestamp() throws Exception {
+        final TransferStatus status = new TransferStatus();
+        final Path home = new DefaultHomeFinderService(session).find();
+        
+        final Path path = new Path(home, new AlphanumericRandomStringService(9).random(), EnumSet.of(Path.Type.file));
+        session.getFeature(Touch.class).touch(path, new TransferStatus())
+        .withAttributes(session.getFeature(AttributesFinder.class).find(path));
+        
+        // make sure timestamps are different
+        long oldTime = session.getFeature(AttributesFinder.class).find(path).getAccessedDate();
+        status.setTimestamp(oldTime + 2000);
+
+        session.getFeature(Timestamp.class).setTimestamp(path, status);
+        PathAttributes newAttributes = session.getFeature(AttributesFinder.class).find(path);
+        assertEquals(status.getTimestamp().longValue(), newAttributes.getAccessedDate());
+    }
+}
