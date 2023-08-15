4b82ff18342a8b73de50190bab2f2fa26d704c8f
David Kocher <dkocher@iterate.ch>
2023-08-15 15:20:50 +0200

    Merge bookmark and session loading.

--- osx/src/main/java/ch/cyberduck/ui/cocoa/controller/MainController.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/controller/MainController.java
@@ -910,45 +910,14 @@ public class MainController extends BundleController implements NSApplication.De
             TransferController c = TransferControllerFactory.get();
             c.window().makeKeyAndOrderFront(null);
         }
-        if(preferences.getBoolean("browser.serialize")) {
-            this.background(new AbstractBackgroundAction<Void>() {
-                @Override
-                public Void run() throws BackgroundException {
-                    sessions.load();
-                    return null;
-                }
-
-                @Override
-                public void cleanup() {
-                    for(Host host : sessions) {
-                        if(log.isInfoEnabled()) {
-                            log.info(String.format("New browser for saved session %s", host));
-                        }
-                        final BrowserController browser = newDocument(true, host.getUuid());
-                        browser.mount(host);
-                    }
-                    if(sessions.isEmpty()) {
-                        if(preferences.getBoolean("browser.open.untitled")) {
-                            final BrowserController c = newDocument();
-                            c.window().makeKeyAndOrderFront(null);
-                        }
-                    }
-                    sessions.clear();
-                }
-            });
-        }
-        else {
-            // Open default windows
-            if(preferences.getBoolean("browser.open.untitled")) {
-                final BrowserController c = newDocument();
-                c.window().makeKeyAndOrderFront(null);
-            }
-        }
         final AbstractHostCollection bookmarks = BookmarkCollection.defaultCollection();
-        // Load all bookmarks in background
         this.background(new AbstractBackgroundAction<Void>() {
             @Override
             public Void run() throws BackgroundException {
+                if(preferences.getBoolean("browser.serialize")) {
+                    sessions.load();
+                }
+                // Load all bookmarks in background
                 bookmarks.load();
                 bookmarksSemaphore.countDown();
                 return null;
@@ -956,11 +925,26 @@ public class MainController extends BundleController implements NSApplication.De
 
             @Override
             public void cleanup() {
-                if(preferences.getBoolean("browser.open.untitled")) {
-                    if(preferences.getProperty("browser.open.bookmark.default") != null) {
-                        openDefaultBookmark(newDocument());
+                for(Host host : sessions) {
+                    if(log.isInfoEnabled()) {
+                        log.info(String.format("New browser for saved session %s", host));
+                    }
+                    final BrowserController browser = newDocument(true, host.getUuid());
+                    browser.mount(host);
+                }
+                if(sessions.isEmpty()) {
+                    // Open default windows
+                    if(preferences.getBoolean("browser.open.untitled")) {
+                        if(preferences.getProperty("browser.open.bookmark.default") != null) {
+                            openDefaultBookmark(newDocument());
+                        }
+                        else {
+                            final BrowserController c = newDocument();
+                            c.window().makeKeyAndOrderFront(null);
+                        }
                     }
                 }
+                sessions.clear();
                 // Set delegate for NSService
                 NSApplication.sharedApplication().setServicesProvider(MainController.this.id());
             }
