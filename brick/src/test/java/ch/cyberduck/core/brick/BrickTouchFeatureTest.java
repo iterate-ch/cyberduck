dbad6462f1508da05e7ef7c4d49cd6dc8ae1eb37
David Kocher <dkocher@iterate.ch>
2023-08-28 09:37:23 +0200

    Review tests.

--- brick/src/test/java/ch/cyberduck/core/brick/BrickTouchFeatureTest.java
+++ brick/src/test/java/ch/cyberduck/core/brick/BrickTouchFeatureTest.java
@@ -30,8 +30,8 @@ import org.junit.experimental.categories.Category;
 import java.util.Arrays;
 import java.util.EnumSet;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @Category(IntegrationTest.class)
 public class BrickTouchFeatureTest extends AbstractBrickTest {
