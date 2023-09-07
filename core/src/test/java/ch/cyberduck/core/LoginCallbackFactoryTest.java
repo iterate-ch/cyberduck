4078ed8e08ac1661a4fccfc6a31f4d5266298201
AliveDevil <AliveDevil@users.noreply.github.com>
2023-09-07 14:07:10 +0200

    Add failing test

--- core/src/test/java/ch/cyberduck/core/LoginCallbackFactoryTest.java
+++ core/src/test/java/ch/cyberduck/core/LoginCallbackFactoryTest.java
@@ -15,10 +15,18 @@ package ch.cyberduck.core;
  * GNU General Public License for more details.
  */
 
+import ch.cyberduck.core.exception.ConnectionCanceledException;
+import ch.cyberduck.core.exception.LoginCanceledException;
+import ch.cyberduck.core.preferences.MemoryPreferences;
+import ch.cyberduck.core.preferences.PreferencesFactory;
 import ch.cyberduck.core.threading.MainAction;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class LoginCallbackFactoryTest {
@@ -32,4 +40,60 @@ public class LoginCallbackFactoryTest {
             }
         }));
     }
+
+    @Test
+    @Ignore
+    public void testCreateOrder() {
+        final MemoryPreferences preferences = new MemoryPreferences();
+        PreferencesFactory.set(preferences);
+        preferences.setDefault("factory.logincallback.class", TestLoginCallback.class.getName());
+        assertEquals(DisabledLoginCallback.class, LoginCallbackFactory.get(new BaseController()).getClass());
+        assertEquals(TestLoginCallback.class, LoginCallbackFactory.get(new TestBrowserController()).getClass());
+        assertEquals(DisabledLoginCallback.class, LoginCallbackFactory.get(new BaseController()).getClass());
+    }
+
+    final static class BaseController extends AbstractController {
+        @Override
+        public void invoke(MainAction runnable, boolean wait) {
+        }
+    }
+
+    final static class TestBrowserController extends AbstractController {
+        @Override
+        public void invoke(MainAction runnable, boolean wait) {
+        }
+    }
+
+    final static class TestLoginCallback implements LoginCallback {
+        public TestLoginCallback(TestBrowserController controller) {
+        }
+
+        @Override
+        public void warn(final Host bookmark, final String title, final String message, final String defaultButton, final String cancelButton, final String preference) throws ConnectionCanceledException {
+        }
+
+        @Override
+        public void await(final CountDownLatch signal, final Host bookmark, final String title, final String message) throws ConnectionCanceledException {
+        }
+
+        @Override
+        public Credentials prompt(final Host bookmark, final String username, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+            return null;
+        }
+
+        @Override
+        public Local select(final Local identity) throws LoginCanceledException {
+            return null;
+        }
+
+        @Override
+        public void close(final String input) {
+        }
+
+        @Override
+        public Credentials prompt(final Host bookmark, final String title, final String reason, final LoginOptions options) throws LoginCanceledException {
+            return null;
+        }
+    }
+
 }
