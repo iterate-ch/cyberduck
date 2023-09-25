6dc32df96afa145345ddbc82ba7acdd5b380d214
David Kocher <dkocher@iterate.ch>
2023-09-25 15:12:40 +0200

    Add tests.

--- core/src/test/java/ch/cyberduck/core/date/ISO8601DateFormatterTest.java
+++ core/src/test/java/ch/cyberduck/core/date/ISO8601DateFormatterTest.java
@@ -20,19 +20,22 @@ import org.junit.Test;
 import java.util.TimeZone;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class ISO8601DateFormatterTest {
 
     @Test
     public void testParseWithoutMilliseconds() throws Exception {
-        assertEquals(1667567722000L, new ISO8601DateFormatter().parse("2022-11-04T13:15:22Z").getTime(), 0L);
-        assertEquals(1667567722000L, new ISO8601DateFormatter().parse("20221104T131522Z").getTime(), 0L);
-        assertEquals(1679159330000L, new ISO8601DateFormatter().parse("20230318T180941.516+0100").getTime(), 0L);
+        final ISO8601DateFormatter formatter = new ISO8601DateFormatter();
+        assertThrows(InvalidDateException.class, () -> formatter.parse("").getTime());
+        assertEquals(1667567722000L, formatter.parse("2022-11-04T13:15:22Z").getTime(), 0L);
     }
 
     @Test
     public void testParseWithMilliseconds() throws Exception {
-        assertEquals(1667567722123L, new ISO8601DateFormatter().parse("2022-11-04T13:15:22.123Z").getTime(), 0L);
+        final ISO8601DateFormatter formatter = new ISO8601DateFormatter();
+        assertEquals(1667567722123L, formatter.parse("2022-11-04T13:15:22.123Z").getTime(), 0L);
+        assertEquals("2022-11-04T13:15:22.123Z", formatter.format(1667567722123L, TimeZone.getTimeZone("UTC")));
     }
 
     @Test
