4a4fc6d6d1901586d660ed20654f99957685cce7
David Kocher <dkocher@iterate.ch>
2023-11-02 11:16:10 +0100

    Add preflight checks.

--- box/src/main/java/ch/cyberduck/core/box/BoxCopyFeature.java
+++ box/src/main/java/ch/cyberduck/core/box/BoxCopyFeature.java
@@ -16,6 +16,7 @@ package ch.cyberduck.core.box;
  */
 
 import ch.cyberduck.core.ConnectionCallback;
+import ch.cyberduck.core.LocaleFactory;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.box.io.swagger.client.ApiException;
 import ch.cyberduck.core.box.io.swagger.client.api.FilesApi;
@@ -25,6 +26,7 @@ import ch.cyberduck.core.box.io.swagger.client.model.FilesfileIdcopyParent;
 import ch.cyberduck.core.box.io.swagger.client.model.FolderIdCopyBody;
 import ch.cyberduck.core.box.io.swagger.client.model.FoldersfolderIdcopyParent;
 import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.InvalidFilenameException;
 import ch.cyberduck.core.features.Copy;
 import ch.cyberduck.core.features.Delete;
 import ch.cyberduck.core.io.StreamListener;
@@ -33,6 +35,7 @@ import ch.cyberduck.core.transfer.TransferStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.EnumSet;
 
@@ -81,5 +84,12 @@ public class BoxCopyFeature implements Copy {
     @Override
     public EnumSet<Flags> features(final Path source, final Path target) {
         return EnumSet.of(Flags.recursive);
+    }
+
+    @Override
+    public void preflight(final Path source, final Path target) throws BackgroundException {
+        if(!BoxTouchFeature.validate(target.getName())) {
+            throw new InvalidFilenameException(MessageFormat.format(LocaleFactory.localizedString("Cannot create {0}", "Error"), target.getName()));
+        }
     }
 }
