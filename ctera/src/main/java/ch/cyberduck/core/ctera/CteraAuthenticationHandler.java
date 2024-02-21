024ee769c98798ef2e46f110b9d43df610e1699b
David Kocher <dkocher@iterate.ch>
2024-02-21 17:44:25 +0100

    Fix null pointer on missing response header.

--- ctera/src/main/java/ch/cyberduck/core/ctera/CteraAuthenticationHandler.java
+++ ctera/src/main/java/ch/cyberduck/core/ctera/CteraAuthenticationHandler.java
@@ -67,7 +67,7 @@ public class CteraAuthenticationHandler implements ServiceUnavailableRetryStrate
                 public Void handleResponse(final HttpResponse response) throws IOException {
                     final Header header = response.getFirstHeader("Set-Cookie");
                     if(log.isDebugEnabled()) {
-                        log.debug(String.format("Received cookie %s", header.getValue()));
+                        log.debug(String.format("Received cookie %s", header));
                     }
                     return super.handleResponse(response);
                 }
