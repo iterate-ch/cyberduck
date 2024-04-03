e5de5d19f6c3a2146e2a2bb203c37955869112ea
David Kocher <dkocher@iterate.ch>
2024-04-03 14:36:04 +0200

    Revert "Review progress message when setting ACLs."
    This reverts commit 18512e6b547e902a98c552c35ce6ff746263e4d0.

--- core/src/main/java/ch/cyberduck/core/Acl.java
+++ core/src/main/java/ch/cyberduck/core/Acl.java
@@ -583,9 +583,4 @@ public final class Acl extends HashMap<Acl.User, Set<Acl.Role>> implements Seria
     public int hashCode() {
         return Objects.hash(super.hashCode(), canned);
     }
-
-    @Override
-    public String toString() {
-        return canned;
-    }
 }
