4b6a1b9412e7e33491134e6cc95ba1c5cf1181d6
chenkins <chenkins44@gmail.com>
2024-06-04 20:59:29 +0200

    Update copyright.

--- deepbox/src/main/java/ch/cyberduck/core/deepbox/DeepboxApiClient.java
+++ deepbox/src/main/java/ch/cyberduck/core/deepbox/DeepboxApiClient.java
@@ -1,12 +1,12 @@
 package ch.cyberduck.core.deepbox;
 
 /*
- * Copyright (c) 2002-2017 iterate GmbH. All rights reserved.
+ * Copyright (c) 2002-2024 iterate GmbH. All rights reserved.
  * https://cyberduck.io/
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
