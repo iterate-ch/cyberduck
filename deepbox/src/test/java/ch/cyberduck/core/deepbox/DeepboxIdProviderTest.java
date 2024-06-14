53e9a0c9ed4a2248461263626728a1255ac214fe
chenkins <chenkins44@gmail.com>
2024-06-14 21:53:22 +0200

    Cleanup DeepboxIdProviderTest.java.

--- deepbox/src/test/java/ch/cyberduck/core/deepbox/DeepboxIdProviderTest.java
+++ deepbox/src/test/java/ch/cyberduck/core/deepbox/DeepboxIdProviderTest.java
@@ -29,7 +29,6 @@ import static org.junit.Assert.assertNull;
 
 @Category(IntegrationTest.class)
 public class DeepboxIdProviderTest extends AbstractDeepboxTest {
-    // "/Mountainduck Buddies/My Box/Documents/Auditing/nix4.txt"
     @Test
     public void testRoot() throws Exception {
         final DeepboxIdProvider nodeid = new DeepboxIdProvider(session);
@@ -43,43 +42,47 @@ public class DeepboxIdProviderTest extends AbstractDeepboxTest {
     @Test
     public void testDeepBox() throws Exception {
         final DeepboxIdProvider nodeid = new DeepboxIdProvider(session);
-        assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getFileId(deepBox));
+        final Path directory = new Path("/Mountainduck Buddies/", EnumSet.of(AbstractPath.Type.directory, Path.Type.volume));
+        assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getFileId(directory));
         assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getDeepBoxNodeId(deepBox));
-        assertNull(nodeid.getBoxNodeId(deepBox));
-        assertNull(nodeid.getThirdLevelId(deepBox));
+        assertNull(nodeid.getBoxNodeId(directory));
+        assertNull(nodeid.getThirdLevelId(directory));
     }
 
     @Test
     public void testBox() throws Exception {
         final DeepboxIdProvider nodeid = new DeepboxIdProvider(session);
-        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059", nodeid.getFileId(box));
-        assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getDeepBoxNodeId(box));
-        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059", nodeid.getBoxNodeId(box));
+        final Path directory = new Path("/Mountainduck Buddies/My Box/", EnumSet.of(AbstractPath.Type.directory, Path.Type.volume));
+        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059", nodeid.getFileId(directory));
+        assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getDeepBoxNodeId(directory));
+        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059", nodeid.getBoxNodeId(directory));
         assertNull(nodeid.getThirdLevelId(box));
     }
 
     @Test
     public void testDocuments() throws Exception {
         final DeepboxIdProvider nodeid = new DeepboxIdProvider(session);
-        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059_Documents", nodeid.getFileId(documents));
-        assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getDeepBoxNodeId(documents));
-        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059", nodeid.getBoxNodeId(documents));
-        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059_Documents", nodeid.getThirdLevelId(documents));
+        final Path directory = new Path("/Mountainduck Buddies/My Box/Documents/", EnumSet.of(AbstractPath.Type.directory, Path.Type.volume));
+        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059_Documents", nodeid.getFileId(directory));
+        assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getDeepBoxNodeId(directory));
+        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059", nodeid.getBoxNodeId(directory));
+        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059_Documents", nodeid.getThirdLevelId(directory));
     }
 
     @Test
     public void testAuditing() throws Exception {
         final DeepboxIdProvider nodeid = new DeepboxIdProvider(session);
-        assertEquals("423ab938-57b0-4b28-ad84-d27dd41aa7c4", nodeid.getFileId(auditing));
-        assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getDeepBoxNodeId(auditing));
-        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059", nodeid.getBoxNodeId(auditing));
-        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059_Documents", nodeid.getThirdLevelId(auditing));
+        final Path directory = new Path("/Mountainduck Buddies/My Box/Documents/Auditing/", EnumSet.of(AbstractPath.Type.directory, Path.Type.volume));
+        assertEquals("423ab938-57b0-4b28-ad84-d27dd41aa7c4", nodeid.getFileId(directory));
+        assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getDeepBoxNodeId(directory));
+        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059", nodeid.getBoxNodeId(directory));
+        assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059_Documents", nodeid.getThirdLevelId(directory));
     }
 
     @Test
     public void testFile() throws Exception {
         final DeepboxIdProvider nodeid = new DeepboxIdProvider(session);
-        final Path file = new Path(auditing, "nix4.txt", EnumSet.of(Path.Type.file));
+        final Path file = new Path("/Mountainduck Buddies/My Box/Documents/Auditing/nix4.txt", EnumSet.of(Path.Type.file));
         assertEquals("3faf6efe-1ecc-47f9-9a6c-5efb9216b3dd", nodeid.getFileId(file));
         assertEquals("8e0b546e-fe1b-47ba-b82d-c11682b9360b", nodeid.getDeepBoxNodeId(file));
         assertEquals("5402aff1-0f0b-416b-a225-4f55d3812059", nodeid.getBoxNodeId(file));
