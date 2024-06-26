1b70a274b5d723746ae84fa7a19088489a0da68f
chenkins <chenkins44@gmail.com>
2024-06-26 17:01:03 +0200

    Cleanup.

--- deepbox/src/test/java/ch/cyberduck/core/deepbox/nodepolicy/DeepboxNodePolicyTest.java
+++ deepbox/src/test/java/ch/cyberduck/core/deepbox/nodepolicy/DeepboxNodePolicyTest.java
@@ -1,4 +1,6 @@
-package ch.cyberduck.core.deepbox.nodepoliy;/*
+package ch.cyberduck.core.deepbox.nodepolicy;
+
+/*
  * Copyright (c) 2002-2024 iterate GmbH. All rights reserved.
  * https://cyberduck.io/
  *
