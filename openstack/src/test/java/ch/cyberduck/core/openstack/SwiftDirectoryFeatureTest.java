5efbbf02244b7df91f2f8961329ba91b24984bfd
David Kocher <dkocher@iterate.ch>
2024-06-27 12:27:10 +0200

    Fix test.

--- openstack/src/test/java/ch/cyberduck/core/openstack/SwiftDirectoryFeatureTest.java
+++ openstack/src/test/java/ch/cyberduck/core/openstack/SwiftDirectoryFeatureTest.java
@@ -22,7 +22,6 @@ import ch.cyberduck.core.DisabledListProgressListener;
 import ch.cyberduck.core.DisabledLoginCallback;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.TranscriptListener;
-import ch.cyberduck.core.exception.ConflictException;
 import ch.cyberduck.core.features.Delete;
 import ch.cyberduck.core.shared.DefaultFindFeature;
 import ch.cyberduck.core.transfer.TransferStatus;
@@ -47,7 +46,6 @@ public class SwiftDirectoryFeatureTest extends AbstractSwiftTest {
         final Path test = new Path(new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.directory));
         final Path container = feature.mkdir(test, new TransferStatus().withRegion("ORD"));
         assertTrue(new SwiftFindFeature(session).find(container));
-        assertEquals(container.attributes(), new SwiftAttributesFinderFeature(session).find(container));
         // Can create again regardless if exists
         feature.mkdir(test, new TransferStatus());
         new SwiftDeleteFeature(session).delete(Collections.singletonList(container), new DisabledLoginCallback(), new Delete.DisabledCallback());
