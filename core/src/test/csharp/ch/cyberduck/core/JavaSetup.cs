15fbd9669ea1bf898aa60872f808f83d35cdfa46
AliveDevil <AliveDevil@users.noreply.github.com>
2024-07-25 14:54:48 +0200

    Migrate to ikvmnet

---
+++ core/src/test/csharp/ch/cyberduck/core/JavaSetup.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+
+[SetUpFixture]
+public class JavaSetup
+{
+    [OneTimeSetUp]
+    public void SetupJna()
+    {
+        java.lang.System.setProperty("jna.boot.library.path", AppContext.BaseDirectory);
+    }
+}
