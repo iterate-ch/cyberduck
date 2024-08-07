9294a0d1c5654189fc0f364fb29c93375c20e027
AliveDevil <AliveDevil@users.noreply.github.com>
2024-08-07 18:05:15 +0200

    Dual-Target net472 and net8.0

--- core/src/main/csharp/System/Diagnostics/CodeAnalysis/UnscopedRefAttribute.cs
+++ core/src/main/csharp/System/Diagnostics/CodeAnalysis/UnscopedRefAttribute.cs
@@ -1,6 +1,10 @@
 ﻿namespace System.Diagnostics.CodeAnalysis;
 
+#if NETFRAMEWORK
+
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
 public sealed class UnscopedRefAttribute : Attribute
 {
 }
+
+#endif
---
+++ core/src/main/csharp/System/IO/StringWriterPolyfills.cs
@@ -0,0 +1,16 @@
+﻿#if NETFRAMEWORK
+
+namespace System.IO;
+
+public static class StringWriterPolyfills
+{
+    public static void Write(this StringWriter writer, in ReadOnlySpan<char> chars)
+    {
+        foreach (ref readonly var c in chars)
+        {
+            writer.Write(c);
+        }
+    }
+}
+
+#endif
