f1290db8d2e22ddc99931fd07760a0dfee0394f1
AliveDevil <AliveDevil@users.noreply.github.com>
2024-08-19 11:24:37 +0200

    Fix querying system proxy

---
+++ core/src/main/csharp/ch/cyberduck/core/proxy/SystemProxy.net8.0.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace Ch.Cyberduck.Core.Proxy
+{
+    partial class SystemProxy
+    {
+        private static IWebProxy DefaultSystemProxy => HttpClient.DefaultProxy;
+    }
+}
