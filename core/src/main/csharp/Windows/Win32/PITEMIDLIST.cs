05a576d9fc0b5b5de1922f45e869f610cc92867c
AliveDevil <AliveDevil@users.noreply.github.com>
2024-08-23 12:50:59 +0200

    Update CsWin32

---
+++ core/src/main/csharp/Windows/Win32/PITEMIDLIST.cs
@@ -0,0 +1,12 @@
+﻿using Windows.Win32.UI.Shell.Common;
+
+namespace Windows.Win32;
+
+public readonly unsafe struct PITEMIDLIST(ITEMIDLIST* value)
+{
+    public readonly ITEMIDLIST* Value = value;
+
+    public ref readonly ITEMIDLIST ValueRef => ref *Value;
+
+    public static implicit operator PITEMIDLIST(ITEMIDLIST* value) => new(value);
+}
