e50ac610928798e10903a2598ec6c09aa96eb64c
AliveDevil <AliveDevil@users.noreply.github.com>
2024-08-23 13:05:29 +0200

    Move Test

---
+++ windows/src/test/csharp/ch/cyberduck/core/ShlwApiTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using static Windows.Win32.CoreRefreshMethods;
+
+namespace Cyberduck.Core.Test
+{
+    [TestFixture]
+    public class ShlwApiTests
+    {
+        [Test]
+        public static void TestLoadIndirect()
+        {
+            string original = "C:\\Windows\\System32\\imageres.dll";
+            Assert.That(SHLoadIndirectString(original), Is.EqualTo(original));
+        }
+    }
+}
