2dbcf5d817c108ca732872c7f0d1229f6060725a
AliveDevil <AliveDevil@users.noreply.github.com>
2024-08-27 14:03:55 +0200

    Make string -> PCWSTR -> PWSTR dangerous

--- core/src/main/csharp/Windows/Win32/PWSTR.cs
+++ core/src/main/csharp/Windows/Win32/PWSTR.cs
@@ -6,12 +6,10 @@ namespace Windows.Win32.Foundation;
 
 public unsafe partial struct PWSTR
 {
-    public static PWSTR DangerousFromString(in ReadOnlySpan<char> value)
+    public static PWSTR DangerousFromString(in Span<char> value)
     {
         return (char*)Unsafe.AsPointer(ref MemoryMarshal.GetReference(value));
     }
-
-    public static PWSTR DangerousFromString(string value) => DangerousFromString(value.AsSpan());
 
     public string ToString(int length) => Value is null ? null : new(Value, 0, length);
 }
