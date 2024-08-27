b86fa81d261f4becdc47871daab83dd3357954c3
AliveDevil <AliveDevil@users.noreply.github.com>
2024-08-27 14:03:55 +0200

    Make string -> PCWSTR -> PWSTR dangerous

---
+++ core/src/main/csharp/Windows/Win32/Foundation_PWSTR_Extensions.cs
@@ -0,0 +1,10 @@
+﻿using Windows.Win32.Foundation;
+
+namespace Windows.Win32;
+
+public static partial class Foundation_PWSTR_Extensions
+{
+
+    public static unsafe PWSTR DangerousAsPWSTR(this in PCWSTR pwstr) => pwstr.Value;
+
+}
