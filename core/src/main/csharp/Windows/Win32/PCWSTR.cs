6627b48ee939ee026b9d2667f0c353bd1a28c147
AliveDevil <AliveDevil@users.noreply.github.com>
2024-08-28 11:31:26 +0200

    Review

--- core/src/main/csharp/Windows/Win32/PCWSTR.cs
+++ core/src/main/csharp/Windows/Win32/PCWSTR.cs
@@ -6,12 +6,14 @@ namespace Windows.Win32.Foundation;
 
 public unsafe partial struct PCWSTR
 {
-    public static PCWSTR DangerousFromString(in ReadOnlySpan<char> value)
+    public static PCWSTR DangerousFromSpan(in ReadOnlySpan<char> value)
     {
         return (char*)Unsafe.AsPointer(ref MemoryMarshal.GetReference(value));
     }
 
-    public static PCWSTR DangerousFromString(string value) => DangerousFromString(value.AsSpan());
+    public static PCWSTR DangerousFromString(string value) => DangerousFromSpan(value.AsSpan());
+
+    public PWSTR DangerousAsPWSTR() => Value;
 
     public string ToString(int length) => Value is null ? new string(Value, 0, length) : null;
 }
