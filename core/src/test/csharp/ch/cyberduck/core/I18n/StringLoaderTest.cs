03e2a2daa9eb9fbe95aa891099b50052e234d686
AliveDevil <AliveDevil@users.noreply.github.com>
2024-09-03 14:24:16 +0200

    Fix StringLoader GetString

---
+++ core/src/test/csharp/ch/cyberduck/core/I18n/StringLoaderTest.cs
@@ -0,0 +1,16 @@
+﻿using Ch.Cyberduck.Core.I18n;
+using NUnit.Framework;
+
+namespace Cyberduck.Core.Test.I18n
+{
+    [TestFixture]
+    public class StringLoaderTest
+    {
+        [Test]
+        public void LoadString()
+        {
+            using StringLoader stringLoader = new();
+            Assert.That(stringLoader.GetString(30396u), Is.Not.WhiteSpace);
+        }
+    }
+}
