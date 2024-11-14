209a4c2a197d1bbf821510a5cd371c5fb5626385
David Kocher <dkocher@iterate.ch>
2024-11-14 20:50:47 +0100

    Fix import.

--- osx/src/main/java/ch/cyberduck/ui/cocoa/controller/ProfilesPreferencesController.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/controller/ProfilesPreferencesController.java
@@ -47,7 +47,7 @@ import ch.cyberduck.core.profiles.ProfilesWorkerBackgroundAction;
 import ch.cyberduck.core.profiles.SearchProfilePredicate;
 import ch.cyberduck.core.resources.IconCacheFactory;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.rococoa.Foundation;
