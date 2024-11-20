55b40fa8dbec840208785cecf36871c3f0ff691e
David Kocher <dkocher@iterate.ch>
2024-11-20 12:41:26 +0100

    Synchronize access to tokens.

--- oauth/src/main/java/ch/cyberduck/core/oauth/OAuth2RequestInterceptor.java
+++ oauth/src/main/java/ch/cyberduck/core/oauth/OAuth2RequestInterceptor.java
@@ -38,11 +38,14 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 import com.google.api.client.auth.oauth2.Credential;
 
 public class OAuth2RequestInterceptor extends OAuth2AuthorizationService implements HttpRequestInterceptor {
     private static final Logger log = LogManager.getLogger(OAuth2RequestInterceptor.class);
+
+    private final ReentrantLock lock = new ReentrantLock();
 
     /**
      * Currently valid tokens
@@ -75,22 +78,34 @@ public class OAuth2RequestInterceptor extends OAuth2AuthorizationService impleme
 
     @Override
     public OAuthTokens authorize() throws BackgroundException {
-        return tokens = super.authorize();
+        lock.lock();
+        try {
+            return tokens = super.authorize();
+        }
+        finally {
+            lock.unlock();
+        }
     }
 
     /**
      * Refresh with cached refresh token
      */
     public OAuthTokens refresh() throws BackgroundException {
-        return tokens = this.refresh(tokens);
+        lock.lock();
+        try {
+            return tokens = this.refresh(tokens);
+        }
+        finally {
+            lock.unlock();
+        }
     }
 
     /**
-     *
      * @param previous Refresh token
      */
     @Override
     public OAuthTokens refresh(final OAuthTokens previous) throws BackgroundException {
+        lock.lock();
         try {
             return tokens = super.refresh(previous);
         }
@@ -98,28 +113,37 @@ public class OAuth2RequestInterceptor extends OAuth2AuthorizationService impleme
             log.warn("Failure {} refreshing OAuth tokens", e.getMessage());
             return tokens = this.authorize();
         }
+        finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public void process(final HttpRequest request, final HttpContext context) throws HttpException, IOException {
-        if(tokens.isExpired()) {
-            try {
-                final OAuthTokens previous = tokens;
-                final OAuthTokens refreshed = this.refresh(tokens);
-                // Skip saving tokens when not changed
-                if(!refreshed.equals(previous)) {
-                    this.save(refreshed);
+        lock.lock();
+        try {
+            if(tokens.isExpired()) {
+                try {
+                    final OAuthTokens previous = tokens;
+                    final OAuthTokens refreshed = this.refresh(tokens);
+                    // Skip saving tokens when not changed
+                    if(!refreshed.equals(previous)) {
+                        this.save(refreshed);
+                    }
+                }
+                catch(BackgroundException e) {
+                    log.warn("Failure {} refreshing OAuth tokens {}", e, tokens);
+                    // Follow-up error 401 handled in error interceptor
                 }
             }
-            catch(BackgroundException e) {
-                log.warn("Failure {} refreshing OAuth tokens {}", e, tokens);
-                // Follow-up error 401 handled in error interceptor
+            if(StringUtils.isNotBlank(tokens.getAccessToken())) {
+                log.info("Authorizing service request with OAuth2 tokens {}", tokens);
+                request.removeHeaders(HttpHeaders.AUTHORIZATION);
+                request.addHeader(new BasicHeader(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", tokens.getAccessToken())));
             }
         }
-        if(StringUtils.isNotBlank(tokens.getAccessToken())) {
-            log.info("Authorizing service request with OAuth2 tokens {}", tokens);
-            request.removeHeaders(HttpHeaders.AUTHORIZATION);
-            request.addHeader(new BasicHeader(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", tokens.getAccessToken())));
+        finally {
+            lock.unlock();
         }
     }
 
@@ -148,6 +172,12 @@ public class OAuth2RequestInterceptor extends OAuth2AuthorizationService impleme
     }
 
     public OAuthTokens getTokens() {
-        return tokens;
+        lock.lock();
+        try {
+            return tokens;
+        }
+        finally {
+            lock.unlock();
+        }
     }
 }
