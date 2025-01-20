3e90a8f3e755ee8c210ba279440c6492485708aa
Yves Langisch <yla@iterate.ch>
2025-01-20 09:21:57 +0100

    Intermediate interface for Cryptomator stuff.

--- cryptomator/src/main/java/ch/cyberduck/core/cryptomator/features/CryptoDirectoryV7Feature.java
+++ cryptomator/src/main/java/ch/cyberduck/core/cryptomator/features/CryptoDirectoryV7Feature.java
@@ -20,7 +20,7 @@ import ch.cyberduck.core.RandomStringService;
 import ch.cyberduck.core.Session;
 import ch.cyberduck.core.UUIDRandomStringService;
 import ch.cyberduck.core.cryptomator.ContentWriter;
-import ch.cyberduck.core.cryptomator.CryptoVault;
+import ch.cyberduck.core.cryptomator.CryptoVaultInterface;
 import ch.cyberduck.core.cryptomator.impl.CryptoDirectoryV7Provider;
 import ch.cyberduck.core.cryptomator.random.RandomNonceGenerator;
 import ch.cyberduck.core.exception.BackgroundException;
@@ -42,11 +42,11 @@ public class CryptoDirectoryV7Feature<Reply> implements Directory<Reply> {
     private final Session<?> session;
     private final Write<Reply> writer;
     private final Directory<Reply> delegate;
-    private final CryptoVault vault;
+    private final CryptoVaultInterface vault;
     private final RandomStringService random = new UUIDRandomStringService();
 
     public CryptoDirectoryV7Feature(final Session<?> session, final Directory<Reply> delegate,
-                                    final Write<Reply> writer, final CryptoVault cryptomator) {
+                                    final Write<Reply> writer, final CryptoVaultInterface cryptomator) {
         this.session = session;
         this.writer = writer;
         this.delegate = delegate;
