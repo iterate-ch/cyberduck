0c4c8104760a47db44d6def923ca0ced1e34eed8
Yves Langisch <yla@iterate.ch>
2025-02-25 12:18:39 +0100

    Directory id for root is derived from key material.

---
+++ cryptomator/src/main/java/ch/cyberduck/core/cryptomator/impl/CryptoDirectoryUVFProvider.java
@@ -0,0 +1,41 @@
+package ch.cyberduck.core.cryptomator.impl;
+
+/*
+ * Copyright (c) 2002-2025 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.Session;
+import ch.cyberduck.core.SimplePathPredicate;
+import ch.cyberduck.core.cryptomator.CryptoFilename;
+import ch.cyberduck.core.cryptomator.CryptorCache;
+import ch.cyberduck.core.exception.BackgroundException;
+
+public class CryptoDirectoryUVFProvider extends CryptoDirectoryV7Provider {
+
+    private final Path home;
+
+    public CryptoDirectoryUVFProvider(final Path vault, final CryptoFilename filenameProvider, final CryptorCache filenameCryptor) {
+        super(vault, filenameProvider, filenameCryptor);
+        this.home = vault;
+    }
+
+    @Override
+    protected byte[] toDirectoryId(final Session<?> session, final Path directory, final byte[] directoryId) throws BackgroundException {
+        if(new SimplePathPredicate(home).test(directory)) {
+            return directoryId;
+        }
+        return super.toDirectoryId(session, directory, directoryId);
+    }
+}
