b4a57d2f993a91b77d0f9a05d1ff083942433714
David Kocher <dkocher@iterate.ch>
2025-03-22 19:12:30 +0100

    Revert "No failure when accessing single empty bucket."
    This reverts commit e47baafb

--- s3/src/main/java/ch/cyberduck/core/s3/S3ObjectListService.java
+++ s3/src/main/java/ch/cyberduck/core/s3/S3ObjectListService.java
@@ -143,19 +143,15 @@ public class S3ObjectListService extends S3AbstractListService implements ListSe
             if(!hasDirectoryPlaceholder && objects.isEmpty()) {
                 // Only for AWS
                 if(S3Session.isAwsHostname(session.getHost().getHostname())) {
-                    if(StringUtils.isEmpty(RequestEntityRestStorageService.findBucketInHostname(session.getHost()))) {
-                        log.warn("No placeholder found for directory {}", directory);
-                        throw new NotfoundException(directory.getAbsolute());
-                    }
+                    log.warn("No placeholder found for directory {}", directory);
+                    throw new NotfoundException(directory.getAbsolute());
                 }
-                else {
-                    // Handle missing prefix for directory placeholders in Minio
-                    final StorageObjectsChunk chunk = session.getClient().listObjectsChunked(
-                            bucket.isRoot() ? StringUtils.EMPTY : bucket.getName(),
-                            String.format("%s%s", this.createPrefix(directory.getParent()), directory.getName()), delimiter, 1, null);
-                    if(Arrays.stream(chunk.getCommonPrefixes()).map(URIEncoder::decode).noneMatch(common -> common.equals(prefix))) {
-                        throw new NotfoundException(directory.getAbsolute());
-                    }
+                // Handle missing prefix for directory placeholders in Minio
+                final StorageObjectsChunk chunk = session.getClient().listObjectsChunked(
+                        bucket.isRoot() ? StringUtils.EMPTY : bucket.getName(),
+                        String.format("%s%s", this.createPrefix(directory.getParent()), directory.getName()), delimiter, 1, null);
+                if(Arrays.stream(chunk.getCommonPrefixes()).map(URIEncoder::decode).noneMatch(common -> common.equals(prefix))) {
+                    throw new NotfoundException(directory.getAbsolute());
                 }
             }
             return objects;
