54511a306f383926f1ccff6583fadc5ef53c9fc9
Yves Langisch <yla@iterate.ch>
2025-04-07 17:53:29 +0200

    Review DirectIO classes.

--- ctera/src/main/java/ch/cyberduck/core/ctera/directio/DecryptKey.java
+++ ctera/src/main/java/ch/cyberduck/core/ctera/directio/DecryptKey.java
@@ -1,42 +1,58 @@
 package ch.cyberduck.core.ctera.directio;
 
+/*
+ * Copyright (c) 2002-2025 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 import org.jose4j.keys.AesKey;
 
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DecryptKey {
+    private static final String ENCRYPTION_ALGORITHM = "AES/ECB/PKCS5Padding";
+    private static final String ENCRYPTION_KEY_ALGORITHM = "AES";
+
     private String encryptedKey;
     private String decryptedKey;
 
-    public DecryptKey(String encryptedKey) {
+    public DecryptKey(final String encryptedKey) {
         this.encryptedKey = encryptedKey;
     }
 
-    public void decrypt(String wrappingKey) {
-        try {
-            final byte[] decoded = java.util.Base64.getDecoder().decode(wrappingKey);
-            AesKey wrappingKeyObj = new AesKey(Arrays.copyOf(decoded, 32));
+    public void decrypt(final String wrappingKey) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, JsonProcessingException {
+        final byte[] decoded = java.util.Base64.getDecoder().decode(wrappingKey);
+        final AesKey wrappingKeyObj = new AesKey(Arrays.copyOf(decoded, 32));
 
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.UNWRAP_MODE, wrappingKeyObj);
-            byte[] wrappedKey = Base64.getDecoder().decode(this.encryptedKey);
+        final Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+        cipher.init(Cipher.UNWRAP_MODE, wrappingKeyObj);
+        final byte[] wrappedKey = Base64.getDecoder().decode(this.encryptedKey);
+        final Key unwrappedKey = cipher.unwrap(wrappedKey, ENCRYPTION_KEY_ALGORITHM, Cipher.SECRET_KEY);
 
-            // Unwrap the key (assuming it's an AES key)
-            Key unwrappedKey = cipher.unwrap(wrappedKey, "AES", Cipher.SECRET_KEY);
-
-            // Convert the unwrapped key bytes back to a string
-            byte[] keyBytes = unwrappedKey.getEncoded();
-            this.decryptedKey = new String(keyBytes, StandardCharsets.UTF_8);
-
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
+        final byte[] keyBytes = unwrappedKey.getEncoded();
+        final String jsonString = new String(keyBytes, StandardCharsets.UTF_8);
+        final ObjectMapper mapper = new ObjectMapper();
+        this.decryptedKey = mapper.readValue(jsonString, String.class);
     }
 
     public String getDecryptedKey() {
--- ctera/src/main/java/ch/cyberduck/core/ctera/directio/DirectIOInputStream.java
+++ ctera/src/main/java/ch/cyberduck/core/ctera/directio/DirectIOInputStream.java
@@ -1,5 +1,20 @@
 package ch.cyberduck.core.ctera.directio;
 
+/*
+ * Copyright (c) 2002-2025 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.ProxyInputStream;
 
@@ -9,20 +24,19 @@ import java.io.InputStream;
 public class DirectIOInputStream extends ProxyInputStream {
 
     private InputStream decryptedInputStream;
-    private final DecryptData decryptor;
+    private final Decryptor decryptor;
     private final EncryptInfo encryptInfo;
-
 
     public DirectIOInputStream(final InputStream proxy, final EncryptInfo encryptInfo) {
         super(proxy);
-        this.decryptor = new DecryptData();
+        this.decryptor = new Decryptor();
         this.encryptInfo = encryptInfo;
         this.decryptedInputStream = null;
     }
 
     @Override
     public int read() throws IOException {
-        byte[] b = new byte[1];
+        final byte[] b = new byte[1];
         return this.read(b) == IOUtils.EOF ? IOUtils.EOF : b[0];
     }
 
@@ -33,7 +47,7 @@ public class DirectIOInputStream extends ProxyInputStream {
 
     @Override
     public int read(final byte[] dst, final int off, final int len) throws IOException {
-        initStream();
+        this.initStream();
         return decryptedInputStream.read(dst, off, len);
     }
 
@@ -49,12 +63,7 @@ public class DirectIOInputStream extends ProxyInputStream {
     }
 
     private void readNextChunk() throws IOException {
-        try {
-            decryptedInputStream = decryptor.decryptData(this.in, encryptInfo);
-        }
-        catch(Exception e) {
-            throw new IOException(e.getMessage());
-        }
+        decryptedInputStream = decryptor.decryptData(this.in, encryptInfo);
     }
 }
 
