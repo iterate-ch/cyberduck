a7ffa2bababe544cffcbe284d6142c9bdbbd5c7e
Yves Langisch <yla@iterate.ch>
2025-04-23 09:25:34 +0200

    Add failing test for files with size below segment size.

--- ctera/src/test/java/ch/cyberduck/core/ctera/CteraConcurrentTransferWorkerTest.java
+++ ctera/src/test/java/ch/cyberduck/core/ctera/CteraConcurrentTransferWorkerTest.java
@@ -15,16 +15,7 @@ package ch.cyberduck.core.ctera;
  * GNU General Public License for more details.
  */
 
-import ch.cyberduck.core.AlphanumericRandomStringService;
-import ch.cyberduck.core.BytecountStreamListener;
-import ch.cyberduck.core.DisabledConnectionCallback;
-import ch.cyberduck.core.DisabledLoginCallback;
-import ch.cyberduck.core.DisabledProgressListener;
-import ch.cyberduck.core.Host;
-import ch.cyberduck.core.Local;
-import ch.cyberduck.core.NullFilter;
-import ch.cyberduck.core.Path;
-import ch.cyberduck.core.TestProtocol;
+import ch.cyberduck.core.*;
 import ch.cyberduck.core.dav.DAVAttributesFinderFeature;
 import ch.cyberduck.core.dav.DAVUploadFeature;
 import ch.cyberduck.core.features.Delete;
@@ -34,18 +25,9 @@ import ch.cyberduck.core.local.DefaultTemporaryFileService;
 import ch.cyberduck.core.notification.DisabledNotificationService;
 import ch.cyberduck.core.pool.SessionPool;
 import ch.cyberduck.core.shared.DefaultHomeFinderService;
-import ch.cyberduck.core.transfer.DisabledTransferErrorCallback;
-import ch.cyberduck.core.transfer.DisabledTransferPrompt;
-import ch.cyberduck.core.transfer.DownloadTransfer;
-import ch.cyberduck.core.transfer.Transfer;
-import ch.cyberduck.core.transfer.TransferAction;
-import ch.cyberduck.core.transfer.TransferItem;
-import ch.cyberduck.core.transfer.TransferOptions;
-import ch.cyberduck.core.transfer.TransferSpeedometer;
-import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.core.transfer.*;
 import ch.cyberduck.core.worker.ConcurrentTransferWorker;
 import ch.cyberduck.test.IntegrationTest;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
@@ -59,6 +41,34 @@ import static org.junit.Assert.*;
 
 @Category(IntegrationTest.class)
 public class CteraConcurrentTransferWorkerTest extends AbstractCteraDirectIOTest {
+
+    @Test
+    public void testBelowSegmentSizeUpAndDownload() throws Exception {
+        final Path test = new Path(new DefaultHomeFinderService(session).find(), new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file));
+        final Local local = new Local(System.getProperty("java.io.tmpdir"), new AlphanumericRandomStringService().random());
+        final byte[] content = RandomUtils.nextBytes(500); // Below segment size
+        final OutputStream out = local.getOutputStream(false);
+        assertNotNull(out);
+        IOUtils.write(content, out);
+        out.close();
+        new DAVUploadFeature(session).upload(test, local, new BandwidthThrottle(BandwidthThrottle.UNLIMITED), new DisabledProgressListener(), new DisabledStreamListener(), new TransferStatus().setLength(content.length), new DisabledConnectionCallback());
+        assertEquals(content.length, new DAVAttributesFinderFeature(session).find(test).getSize());
+        final Local localFile = new DefaultTemporaryFileService().create(test.getName());
+        final Transfer download = new DownloadTransfer(new Host(new TestProtocol()), Collections.singletonList(new TransferItem(test, localFile)), new NullFilter<>());
+        final SessionPool pool = new SessionPool.SingleSessionPool(session);
+        final BytecountStreamListener bytecount = new BytecountStreamListener();
+        assertTrue(new ConcurrentTransferWorker(pool, pool, download, new TransferOptions(), new TransferSpeedometer(download), new DisabledTransferPrompt() {
+            @Override
+            public TransferAction prompt(final TransferItem file) {
+                return TransferAction.overwrite;
+            }
+        }, new DisabledTransferErrorCallback(), new DisabledConnectionCallback(), new DisabledProgressListener(), bytecount, new DisabledNotificationService()).run(session));
+        assertArrayEquals(content, IOUtils.toByteArray(localFile.getInputStream()));
+        assertEquals(content.length, bytecount.getRecv());
+        new CteraDeleteFeature(session).delete(Collections.singletonList(test), new DisabledLoginCallback(), new Delete.DisabledCallback());
+        local.delete();
+        localFile.delete();
+    }
 
     @Test
     public void testLargeUpAndDownload() throws Exception {
