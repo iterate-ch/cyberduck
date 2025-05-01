27051ab0354ebc5c084be7d34d90619eee9afc4b
David Kocher <dkocher@iterate.ch>
2025-05-01 10:35:06 +0200

    Add feature annotation.

--- core/src/main/java/ch/cyberduck/core/features/Find.java
+++ core/src/main/java/ch/cyberduck/core/features/Find.java
@@ -24,6 +24,7 @@ import ch.cyberduck.core.exception.NotfoundException;
 /**
  * Check for existence of file or folder on server
  */
+@Optional
 public interface Find {
 
     default boolean find(Path file) throws BackgroundException {
