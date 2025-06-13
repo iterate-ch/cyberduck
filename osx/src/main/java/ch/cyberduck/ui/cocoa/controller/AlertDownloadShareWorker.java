3826be16285fe42ad535d07ae2a7d52b768309f4
David Kocher <dkocher@iterate.ch>
2025-06-13 08:39:53 +0200

    Initialize fields in constructor using builders with no explicit size.

--- osx/src/main/java/ch/cyberduck/ui/cocoa/controller/AlertDownloadShareWorker.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/controller/AlertDownloadShareWorker.java
@@ -21,8 +21,6 @@ import ch.cyberduck.core.worker.DownloadShareWorker;
 import ch.cyberduck.ui.pasteboard.PasteboardService;
 import ch.cyberduck.ui.pasteboard.PasteboardServiceFactory;
 
-import org.rococoa.cocoa.foundation.NSRect;
-
 import java.text.MessageFormat;
 
 public final class AlertDownloadShareWorker<Options> extends DownloadShareWorker<Options> {
@@ -49,7 +47,7 @@ public final class AlertDownloadShareWorker<Options> extends DownloadShareWorker
                     if(DescriptiveUrl.EMPTY == url) {
                         return null;
                     }
-                    final NSTextField field = NSTextField.textfieldWithFrame(new NSRect(0, 22));
+                    final NSTextField field = NSTextField.textFieldWithString(url.getUrl());
                     field.setEditable(false);
                     field.setSelectable(true);
                     field.cell().setWraps(false);
