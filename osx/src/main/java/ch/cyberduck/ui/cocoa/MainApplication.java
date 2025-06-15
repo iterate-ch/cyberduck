ca0895e9844315f673ac3e7f179a3a0c58b2dcf0
David Kocher <dkocher@iterate.ch>
2025-06-15 16:11:23 +0200

    Revert "Explicitly set application icon."
    This reverts commit 94cbf235fc0baaebefd5bda8c4ce0775fa4b5a51.

--- osx/src/main/java/ch/cyberduck/ui/cocoa/MainApplication.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/MainApplication.java
@@ -19,7 +19,6 @@ package ch.cyberduck.ui.cocoa;
  */
 
 import ch.cyberduck.binding.application.NSApplication;
-import ch.cyberduck.binding.application.NSImage;
 import ch.cyberduck.core.Protocol;
 import ch.cyberduck.core.ProtocolFactory;
 import ch.cyberduck.core.logging.LoggerPrintStream;
@@ -54,9 +53,6 @@ public final class MainApplication {
             // Register factory implementations.
             final Preferences preferences = new ApplicationUserDefaultsPreferences();
             PreferencesFactory.set(preferences);
-
-            app.setApplicationIconImage(NSImage.imageNamed(
-                    preferences.getProperty("application.icon.filename")));
 
             final ProtocolFactory protocols = ProtocolFactory.get();
             for(Protocol p : AutoServiceLoaderFactory.<Protocol>get().load(Protocol.class)) {
