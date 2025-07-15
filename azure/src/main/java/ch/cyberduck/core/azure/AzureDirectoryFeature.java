b2a570a9eb58c6d018e2678a6065102d630812d1
David Kocher <dkocher@iterate.ch>
2025-07-15 12:19:11 +0200

    Rewrite Azure implementation.
    This reverts commit c52d0be8

--- azure/src/main/java/ch/cyberduck/core/azure/AzureDirectoryFeature.java
+++ azure/src/main/java/ch/cyberduck/core/azure/AzureDirectoryFeature.java
@@ -1,21 +1,18 @@
 package ch.cyberduck.core.azure;
 
 /*
- * Copyright (c) 2002-2014 David Kocher. All rights reserved.
- * http://cyberduck.io/
+ * Copyright (c) 2002-2024 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * Bug fixes, suggestions and comments should be sent to:
- * feedback@cyberduck.io
  */
 
 import ch.cyberduck.core.DirectoryDelimiterPathContainerService;
@@ -23,8 +20,8 @@ import ch.cyberduck.core.LocaleFactory;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathContainerService;
 import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.exception.InteroperabilityException;
 import ch.cyberduck.core.exception.InvalidFilenameException;
-import ch.cyberduck.core.exception.NotfoundException;
 import ch.cyberduck.core.features.Directory;
 import ch.cyberduck.core.features.Write;
 import ch.cyberduck.core.transfer.TransferStatus;
@@ -33,52 +30,43 @@ import org.apache.commons.io.input.NullInputStream;
 import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.net.URISyntaxException;
 import java.text.MessageFormat;
 import java.util.EnumSet;
 
-import com.microsoft.azure.storage.OperationContext;
-import com.microsoft.azure.storage.StorageException;
-import com.microsoft.azure.storage.blob.BlobRequestOptions;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.azure.core.exception.HttpResponseException;
 
 public class AzureDirectoryFeature implements Directory<Void> {
 
+    private final AzureSession session;
     private final PathContainerService containerService
             = new DirectoryDelimiterPathContainerService();
 
-    private final AzureSession session;
-    private final OperationContext context;
-
     private Write<Void> writer;
 
-    public AzureDirectoryFeature(final AzureSession session, final OperationContext context) {
+    public AzureDirectoryFeature(final AzureSession session) {
         this.session = session;
-        this.context = context;
-        this.writer = new AzureWriteFeature(session, context);
+        this.writer = new AzureWriteFeature(session);
     }
 
     @Override
     public Path mkdir(final Path folder, final TransferStatus status) throws BackgroundException {
         try {
-            final BlobRequestOptions options = new BlobRequestOptions();
             if(containerService.isContainer(folder)) {
                 // Container name must be lower case.
-                final CloudBlobContainer container = session.getClient().getContainerReference(containerService.getContainer(folder).getName());
-                container.create(options, context);
-                return folder;
+                session.getClient().getBlobContainerClient(containerService.getContainer(folder).getName()).create();
+                return new Path(folder.getParent(), folder.getName(), folder.getType());
             }
             else {
                 final EnumSet<Path.Type> type = EnumSet.copyOf(folder.getType());
                 type.add(Path.Type.placeholder);
-                return new AzureTouchFeature(session, context).withWriter(writer).touch(folder.withType(type),
+                return new AzureTouchFeature(session).withWriter(writer).touch(folder.withType(type),
                         status.setChecksum(writer.checksum(folder, status).compute(new NullInputStream(0L), status)));
             }
         }
-        catch(URISyntaxException e) {
-            throw new NotfoundException(e.getMessage(), e);
+        catch(IllegalArgumentException e) {
+            throw new InteroperabilityException();
         }
-        catch(StorageException e) {
+        catch(HttpResponseException e) {
             throw new AzureExceptionMappingService().map("Cannot create folder {0}", e, folder);
         }
     }
--- azure/src/test/java/ch/cyberduck/core/azure/AzureMoveFeatureTest.java
+++ azure/src/test/java/ch/cyberduck/core/azure/AzureMoveFeatureTest.java
@@ -6,6 +6,8 @@ import ch.cyberduck.core.DisabledLoginCallback;
 import ch.cyberduck.core.Path;
 import ch.cyberduck.core.PathAttributes;
 import ch.cyberduck.core.features.Delete;
+import ch.cyberduck.core.synchronization.Comparison;
+import ch.cyberduck.core.synchronization.ComparisonService;
 import ch.cyberduck.core.transfer.TransferStatus;
 import ch.cyberduck.test.IntegrationTest;
 
@@ -16,8 +18,7 @@ import java.util.Collections;
 import java.util.EnumSet;
 import java.util.Optional;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @Category(IntegrationTest.class)
 public class AzureMoveFeatureTest extends AbstractAzureTest {
@@ -25,20 +26,22 @@ public class AzureMoveFeatureTest extends AbstractAzureTest {
     @Test
     public void testMove() throws Exception {
         final Path container = new Path("cyberduck", EnumSet.of(Path.Type.directory, Path.Type.volume));
-        final Path test = new AzureTouchFeature(session, null).touch(new Path(container, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file)), new TransferStatus());
-        assertTrue(new AzureFindFeature(session, null).find(test));
-        final Path target = new AzureMoveFeature(session, null).move(test, new Path(container, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file)), new TransferStatus(), new Delete.DisabledCallback(), new DisabledConnectionCallback());
-        assertFalse(new AzureFindFeature(session, null).find(test));
-        assertTrue(new AzureFindFeature(session, null).find(target));
-        final PathAttributes targetAttr = new AzureAttributesFinderFeature(session, null).find(target);
-        new AzureDeleteFeature(session, null).delete(Collections.<Path>singletonList(target), new DisabledLoginCallback(), new Delete.DisabledCallback());
+        final Path test = new AzureTouchFeature(session).touch(new Path(container, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file)), new TransferStatus());
+        final PathAttributes sourceAttr = new AzureAttributesFinderFeature(session).find(test);
+        assertTrue(new AzureFindFeature(session).find(test));
+        final Path target = new AzureMoveFeature(session).move(test, new Path(container, new AlphanumericRandomStringService().random(), EnumSet.of(Path.Type.file)), new TransferStatus(), new Delete.DisabledCallback(), new DisabledConnectionCallback());
+        assertFalse(new AzureFindFeature(session).find(test));
+        assertTrue(new AzureFindFeature(session).find(target));
+        final PathAttributes targetAttr = new AzureAttributesFinderFeature(session).find(target);
+        assertEquals(Comparison.equal, session.getHost().getProtocol().getFeature(ComparisonService.class).compare(Path.Type.file, sourceAttr, targetAttr));
+        new AzureDeleteFeature(session).delete(Collections.<Path>singletonList(target), new DisabledLoginCallback(), new Delete.DisabledCallback());
     }
 
     @Test
     public void testSupport() {
         final Path c = new Path("/c", EnumSet.of(Path.Type.directory));
-        assertFalse(new AzureMoveFeature(session, null).isSupported(c, Optional.of(new Path("/d", EnumSet.of(Path.Type.directory)))));
+        assertFalse(new AzureMoveFeature(session).isSupported(c, Optional.of(new Path("/d", EnumSet.of(Path.Type.directory)))));
         final Path cf = new Path("/c/f", EnumSet.of(Path.Type.directory));
-        assertTrue(new AzureMoveFeature(session, null).isSupported(cf, Optional.of(new Path("/c/f2", EnumSet.of(Path.Type.directory)))));
+        assertTrue(new AzureMoveFeature(session).isSupported(cf, Optional.of(new Path("/c/f2", EnumSet.of(Path.Type.directory)))));
     }
 }
