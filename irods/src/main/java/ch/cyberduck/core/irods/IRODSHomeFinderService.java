acaacc61aa93ec945a1e9da04166f7c23845b8eb
Kory Draughn <korydraughn@ymail.com>
2025-08-05 12:29:17 -0400

    iRODS: Fix bugs, expand options, and clean up implementation

--- irods/src/main/java/ch/cyberduck/core/irods/IRODSHomeFinderService.java
+++ irods/src/main/java/ch/cyberduck/core/irods/IRODSHomeFinderService.java
@@ -1,11 +1,7 @@
 package ch.cyberduck.core.irods;
 
-import java.util.EnumSet;
-
-import org.apache.commons.lang3.StringUtils;
-
 /*
- * Copyright (c) 2002-2016 iterate GmbH. All rights reserved.
+ * Copyright (c) 2002-2025 iterate GmbH. All rights reserved.
  * https://cyberduck.io/
  *
  * This program is free software; you can redistribute it and/or modify
@@ -24,6 +20,10 @@ import ch.cyberduck.core.Path;
 import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.shared.AbstractHomeFeature;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.EnumSet;
+
 public class IRODSHomeFinderService extends AbstractHomeFeature {
 
     private final IRODSSession session;
@@ -36,16 +36,18 @@ public class IRODSHomeFinderService extends AbstractHomeFeature {
     public Path find() throws BackgroundException {
         final String user;
         final Credentials credentials = session.getHost().getCredentials();
+
         if(StringUtils.contains(credentials.getUsername(), ':')) {
             user = StringUtils.splitPreserveAllTokens(credentials.getUsername(), ':')[1];
         }
         else {
             user = credentials.getUsername();
         }
+
         return new Path(new StringBuilder()
-            .append(Path.DELIMITER).append(session.getRegion())
-            .append(Path.DELIMITER).append("home")
-            .append(Path.DELIMITER).append(user)
-            .toString(), EnumSet.of(Path.Type.directory, Path.Type.volume));
+                .append(Path.DELIMITER).append(session.getRegion())
+                .append(Path.DELIMITER).append("home")
+                .append(Path.DELIMITER).append(user)
+                .toString(), EnumSet.of(Path.Type.directory, Path.Type.volume));
     }
 }
