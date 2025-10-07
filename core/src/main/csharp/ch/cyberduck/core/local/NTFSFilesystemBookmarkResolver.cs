58979958bbe864d8e64cc5f54d8f2916808316df
AliveDevil <AliveDevil@users.noreply.github.com>
2025-10-07 11:10:07 +0200

    Try find existing parent for volume hint of local

--- core/src/main/csharp/ch/cyberduck/core/local/NTFSFilesystemBookmarkResolver.cs
+++ core/src/main/csharp/ch/cyberduck/core/local/NTFSFilesystemBookmarkResolver.cs
@@ -12,56 +12,60 @@
 // GNU General Public License for more details.
 
 using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.InteropServices;
-using Windows.Win32;
-using Windows.Win32.Storage.FileSystem;
 using ch.cyberduck.core;
 using ch.cyberduck.core.exception;
 using ch.cyberduck.core.local;
 using Microsoft.Win32.SafeHandles;
 using org.apache.logging.log4j;
+using Windows.Win32;
+using Windows.Win32.Storage.FileSystem;
+using CoreLocal = ch.cyberduck.core.Local;
 
 namespace Ch.Cyberduck.Core.Local
 {
-    public class NTFSFilesystemBookmarkResolver(ch.cyberduck.core.Local root) : FilesystemBookmarkResolver
+    public class NTFSFilesystemBookmarkResolver(CoreLocal local) : FilesystemBookmarkResolver
     {
         private static readonly Logger Log = LogManager.getLogger(typeof(NTFSFilesystemBookmarkResolver).FullName);
 
-        public string create(ch.cyberduck.core.Local file) => FilesystemBookmarkResolver.__DefaultMethods.create(this, file);
+        public NTFSFilesystemBookmarkResolver() : this(null)
+        {
+        }
 
-        public string create(ch.cyberduck.core.Local file, bool prompt) => null;
+        public string create(CoreLocal file) => FilesystemBookmarkResolver.__DefaultMethods.create(this, file);
+
+        public string create(CoreLocal file, bool prompt)
+        {
+            Debug.Assert(local is null, "Unecessary usage of Local-constructor.");
+
+            // ToDo: Backport
+
+            return null;
+        }
 
         public object resolve(string bookmark)
         {
+            if (local is null)
+            {
+                throw new LocalAccessDeniedException("Unsupported Interface usage");
+            }
+
             if (!ToFileId(bookmark, out var fileId))
             {
                 throw new LocalAccessDeniedException(bookmark);
             }
 
-            var rootPath = root.getAbsolute();
             SafeFileHandle fileHandle = null;
             try
             {
-                using (var rootHandle = CorePInvoke.CreateFile(
-                           lpFileName: rootPath,
-                           dwDesiredAccess: 0,
-                           dwShareMode: (FILE_SHARE_MODE)7,
-                           lpSecurityAttributes: null,
-                           dwCreationDisposition: FILE_CREATION_DISPOSITION.OPEN_EXISTING,
-                           dwFlagsAndAttributes: FILE_FLAGS_AND_ATTRIBUTES.FILE_FLAG_BACKUP_SEMANTICS,
-                           hTemplateFile: null))
+                SafeFileHandle rootHandle = null;
+                try
                 {
-                    if (rootHandle.IsInvalid)
+                    if (!TryFindRoot(local, out rootHandle))
                     {
-                        if (Log.isDebugEnabled())
-                        {
-                            var errorCode = Marshal.GetHRForLastWin32Error();
-                            Log.debug(
-                                $"Opening root {rootPath} error ({errorCode:X8})");
-                        }
-
-                        throw new LocalAccessDeniedException(bookmark);
+                        throw new LocalAccessDeniedException($"Cannot find root for \"{local}\"");
                     }
 
                     FILE_ID_DESCRIPTOR fileDescriptor = new()
@@ -85,9 +89,13 @@ namespace Ch.Cyberduck.Core.Local
                     {
                         var errorCode = Marshal.GetHRForLastWin32Error();
                         Log.warn(
-                            $"Opening file id {bookmark} on {rootPath} ({errorCode:X8})");
+                            $"Opening file {local.getAbsolute()} with id {bookmark} ({errorCode:X8})");
                         throw new LocalAccessDeniedException(bookmark);
                     }
+                }
+                finally
+                {
+                    rootHandle?.Dispose();
                 }
 
                 // Allocate enough space to store 32768-wchars.
@@ -101,7 +109,7 @@ namespace Ch.Cyberduck.Core.Local
                 {
                     var errorCode = Marshal.GetHRForLastWin32Error();
                     Log.warn(
-                        $"Get final path name for {fileId} in {rootPath} ({errorCode:X8})");
+                        $"Get final path name for {fileId} originally {local.getAbsolute()} ({errorCode:X8})");
                     throw new LocalAccessDeniedException(bookmark);
                 }
 
@@ -126,14 +134,7 @@ namespace Ch.Cyberduck.Core.Local
                     throw new LocalAccessDeniedException(bookmark);
                 }
 
-                var finalName = LocalFactory.get(finalNameBuffer.ToString());
-                if (!finalName.equals(root) && !finalName.isChild(root))
-                {
-                    Log.warn($"Mismatched root: \"{finalName.getAbsolute()}\", expected \"{rootPath}\"");
-                    throw new LocalAccessDeniedException(bookmark);
-                }
-
-                return finalName.setBookmark(bookmark);
+                return LocalFactory.get(finalNameBuffer.ToString()).setBookmark(bookmark);
             }
             finally
             {
@@ -153,6 +154,40 @@ namespace Ch.Cyberduck.Core.Local
             {
                 fileId = fileIdResult;
             }
+        }
+
+        private static bool TryFindRoot(CoreLocal local, out SafeFileHandle handle)
+        {
+            while (!local.isRoot())
+            {
+                local = local.getParent();
+                SafeFileHandle result = null;
+                try
+                {
+                    result = CorePInvoke.CreateFile(
+                        lpFileName: local.getAbsolute(),
+                        dwDesiredAccess: 0,
+                        dwShareMode: (FILE_SHARE_MODE)7,
+                        lpSecurityAttributes: null, dwCreationDisposition: FILE_CREATION_DISPOSITION.OPEN_EXISTING,
+                        dwFlagsAndAttributes: FILE_FLAGS_AND_ATTRIBUTES.FILE_FLAG_BACKUP_SEMANTICS,
+                        hTemplateFile: null);
+                    if (result.IsInvalid)
+                    {
+                        continue;
+                    }
+
+                    handle = result;
+                    result = null;
+                    return true;
+                }
+                finally
+                {
+                    result?.Dispose();
+                }
+            }
+
+            handle = null;
+            return false;
         }
     }
 }
