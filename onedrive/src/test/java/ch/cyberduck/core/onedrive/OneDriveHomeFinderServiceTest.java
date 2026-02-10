ebbf371993fdd30d75c7130b529bdb8814ee59c4
David Kocher <dkocher@iterate.ch>
2026-02-10 12:55:07 +0100

    Enable test.

--- onedrive/src/test/java/ch/cyberduck/core/onedrive/OneDriveHomeFinderServiceTest.java
+++ onedrive/src/test/java/ch/cyberduck/core/onedrive/OneDriveHomeFinderServiceTest.java
@@ -20,13 +20,11 @@ import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.test.IntegrationTest;
 
 import org.apache.commons.lang3.StringUtils;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import static org.junit.Assert.*;
 
-@Ignore
 @Category(IntegrationTest.class)
 public class OneDriveHomeFinderServiceTest extends AbstractOneDriveTest {
 
