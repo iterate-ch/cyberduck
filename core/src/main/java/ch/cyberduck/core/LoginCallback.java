ec53ee9cae5386310819fc45db3fff86e041e49e
David Kocher <dkocher@iterate.ch>
2026-02-24 16:47:30 +0100

    Move to upper class.

--- core/src/main/java/ch/cyberduck/core/LoginCallback.java
+++ core/src/main/java/ch/cyberduck/core/LoginCallback.java
@@ -18,11 +18,7 @@ package ch.cyberduck.core;
  * feedback@cyberduck.ch
  */
 
-import ch.cyberduck.core.exception.ConnectionCanceledException;
 import ch.cyberduck.core.exception.LoginCanceledException;
-import ch.cyberduck.core.features.Location;
-
-import java.util.Set;
 
 public interface LoginCallback extends ConnectionCallback {
 
@@ -46,19 +42,4 @@ public interface LoginCallback extends ConnectionCallback {
      * @return Selected file or null
      */
     Local select(final Local identity) throws LoginCanceledException;
-
-    @SuppressWarnings("unchecked")
-    default <T> T getFeature(final Class<T> type) {
-        if(type == LocationCallback.class) {
-            return (T) new LocationCallback() {
-                @Override
-                public Location.Name select(final Host bookmark, final String title, final String message,
-                                            final Set<Location.Name> regions, final Location.Name defaultRegion) throws ConnectionCanceledException {
-                    return new Location.Name(prompt(bookmark, title, message, new LoginOptions().icon(bookmark.getProtocol().disk())
-                            .passwordPlaceholder(message).password(false)).getPassword());
-                }
-            };
-        }
-        return null;
-    }
 }
