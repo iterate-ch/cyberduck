7a6928c68cb0d78e41e1804752bd0b1cc2e0bc76
David Kocher <dkocher@iterate.ch>
2026-05-11 14:22:49 +0200

    Add implementation to fetch available profiles from index file.

---
+++ core/src/main/java/ch/cyberduck/core/profiles/RemoteIndexProfilesFinder.java
@@ -0,0 +1,201 @@
+package ch.cyberduck.core.profiles;
+
+/*
+ * Copyright (c) 2002-2026 iterate GmbH. All rights reserved.
+ * https://cyberduck.io/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+import ch.cyberduck.core.ConnectionCallback;
+import ch.cyberduck.core.DefaultIOExceptionMappingService;
+import ch.cyberduck.core.DefaultPathAttributes;
+import ch.cyberduck.core.Local;
+import ch.cyberduck.core.LocalFactory;
+import ch.cyberduck.core.Path;
+import ch.cyberduck.core.ProgressListener;
+import ch.cyberduck.core.ProtocolFactory;
+import ch.cyberduck.core.Session;
+import ch.cyberduck.core.exception.BackgroundException;
+import ch.cyberduck.core.features.Read;
+import ch.cyberduck.core.io.Checksum;
+import ch.cyberduck.core.preferences.TemporaryApplicationResourcesFinder;
+import ch.cyberduck.core.shared.DefaultPathHomeFeature;
+import ch.cyberduck.core.shared.DelegatingHomeFeature;
+import ch.cyberduck.core.transfer.TransferPathFilter;
+import ch.cyberduck.core.transfer.TransferStatus;
+import ch.cyberduck.core.transfer.download.CompareFilter;
+import ch.cyberduck.core.transfer.symlink.DisabledDownloadSymlinkResolver;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.concurrent.ConcurrentException;
+import org.apache.commons.lang3.concurrent.LazyInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class RemoteIndexProfilesFinder implements ProfilesFinder {
+    private static final Logger log = LogManager.getLogger(RemoteIndexProfilesFinder.class);
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final ProtocolFactory protocols;
+    private final Session<?> session;
+    private final TransferPathFilter comparison;
+    private final Local temporary = LocalFactory.get(new TemporaryApplicationResourcesFinder().find(), "profiles");
+
+    public RemoteIndexProfilesFinder(final Session<?> session) {
+        this(ProtocolFactory.get(), session);
+    }
+
+    public RemoteIndexProfilesFinder(final ProtocolFactory protocols, final Session<?> session) {
+        this(protocols, session, new CompareFilter(new DisabledDownloadSymlinkResolver(), session));
+    }
+
+    public RemoteIndexProfilesFinder(final ProtocolFactory protocols, final Session<?> session, final TransferPathFilter comparison) {
+        this.protocols = protocols;
+        this.session = session;
+        this.comparison = comparison;
+    }
+
+    /**
+     *     {
+     *       "filename": "AWS PrivateLink for Amazon S3 (VPC endpoint).cyberduckprofile",
+     *       "protocol": "s3",
+     *       "vendor": "s3-privatelink",
+     *       "versions": [
+     *         {
+     *           "checksum": "255b117667ac1f0fd1ecfe25ae99440d",
+     *           "modified": "2025-12-02T09:09:07Z",
+     *           "version_id": "sMABDfcz3m0pwQeU85uI2.pW.JTGGT4T",
+     *           "latest": true
+     *         },
+     *         {
+     *           "checksum": "9a6fcc93e68a669952da5e47719af3c9",
+     *           "modified": "2022-09-15T12:55:09Z",
+     *           "version_id": ".iXSL9g6EWEIthW6gENMGgSileSI0XG8",
+     *           "latest": false
+     *         },
+     *         {
+     *           "checksum": "3fbf79c16d3187f135a5fbf32e0cbaf7",
+     *           "modified": "2021-11-30T11:54:30Z",
+     *           "version_id": "S71y2mc.dq8BXtF11g85Z9ZLvulhZDJk",
+     *           "latest": false
+     *         },
+     *         {
+     *           "checksum": "3fbf79c16d3187f135a5fbf32e0cbaf7",
+     *           "modified": "2021-10-27T06:00:55Z",
+     *           "version_id": "E.cXq21hr0xLkqtNMUZjBuZaj2HfA.n9",
+     *           "latest": false
+     *         },
+     *         {
+     *           "checksum": "7188dafb0c649fe123b70cbe5b7bfa40",
+     *           "modified": "2021-10-27T06:00:49Z",
+     *           "version_id": "nosIgW.rj3jrGn9jwdf_S.8fnKMO2ZW1",
+     *           "latest": false
+     *         }
+     *       ]
+     *     }
+     */
+    @Override
+    public Set<ProfileDescription> find(final Visitor visitor) throws BackgroundException {
+        log.info("Fetch profiles from {}", session.getHost());
+        final Set<ProfileDescription> profiles = new HashSet<>();
+        final Path directory = new DelegatingHomeFeature(new DefaultPathHomeFeature(session.getHost())).find();
+        try(final InputStream in = session.getFeature(Read.class).read(new Path(directory, "index.json", EnumSet.of(Path.Type.file)),
+                new TransferStatus().setLength(TransferStatus.UNKNOWN_LENGTH), ConnectionCallback.noop)) {
+            final ProfileMetadataList list = mapper.readValue(in, ProfileMetadataList.class);
+            for(ProfileMetadata metadata : list.profiles) {
+                for(ProfileMetadataVersion version : metadata.versions) {
+                    profiles.add(new ProfileDescription(protocols, protocol -> true,
+                            new LazyInitializer<Checksum>() {
+                                @Override
+                                protected Checksum initialize() {
+                                    return Checksum.parse(version.checksum);
+                                }
+                            },
+                            new LazyInitializer<Local>() {
+                                @Override
+                                protected Local initialize() throws ConcurrentException {
+                                    try {
+                                        final Local local = LocalFactory.get(temporary, metadata.filename);
+                                        final Path file = new Path(directory, metadata.filename, EnumSet.of(Path.Type.file));
+                                        if(comparison.accept(file, local, new TransferStatus().setExists(true), ProgressListener.noop)) {
+                                            final Read read = session.getFeature(Read.class);
+                                            log.info("Download profile {}", file);
+                                            // Read latest version
+                                            try(InputStream in = read.read(file.withAttributes(new DefaultPathAttributes(file.attributes())
+                                                    .setVersionId(version.version_id)), new TransferStatus().setLength(TransferStatus.UNKNOWN_LENGTH), ConnectionCallback.noop); OutputStream out = local.getOutputStream(false)) {
+                                                IOUtils.copy(in, out);
+                                            }
+                                        }
+                                        // Skip download if previously cached
+                                        return local;
+                                    }
+                                    catch(BackgroundException | IOException e) {
+                                        throw new ConcurrentException(e);
+                                    }
+                                }
+                            }
+                    ) {
+                        @Override
+                        public boolean isLatest() {
+                            return version.latest;
+                        }
+                    });
+                }
+            }
+        }
+        catch(IOException e) {
+            throw new DefaultIOExceptionMappingService().map(e);
+        }
+        return profiles;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class ProfileMetadataList {
+        @JsonProperty("profiles")
+        private ProfileMetadata[] profiles;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class ProfileMetadata {
+        @JsonProperty("filename")
+        private String filename;
+        @JsonProperty("protocol")
+        private String protocol;
+        @JsonProperty("vendor")
+        private String vendor;
+        @JsonProperty("versions")
+        private ProfileMetadataVersion[] versions;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class ProfileMetadataVersion {
+        @JsonProperty("checksum")
+        private String checksum;
+        @JsonProperty("modified")
+        private String modified;
+        @JsonProperty("version_id")
+        private String version_id;
+        @JsonProperty("latest")
+        private Boolean latest;
+    }
+}
