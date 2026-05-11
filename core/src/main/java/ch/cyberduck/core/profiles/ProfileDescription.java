b295e5e916ae1f95cb73ede1a0394ea988e5223f
David Kocher <dkocher@iterate.ch>
2026-05-11 14:14:48 +0200

    Rename parameter.

--- core/src/main/java/ch/cyberduck/core/profiles/ProfileDescription.java
+++ core/src/main/java/ch/cyberduck/core/profiles/ProfileDescription.java
@@ -53,12 +53,12 @@ public class ProfileDescription {
 
     /**
      * @param protocols Registered protocols
-     * @param parent    Filter to apply for parent protocol reference in registered protocols
+     * @param filter    Filter to apply for parent protocol reference in registered protocols
      * @param checksum  Checksum of connection profile
      * @param local     File on disk
      */
-    public ProfileDescription(final ProtocolFactory protocols, final Predicate<Protocol> parent, final Checksum checksum, final Local local) {
-        this(protocols, parent, new LazyInitializer<Checksum>() {
+    public ProfileDescription(final ProtocolFactory protocols, final Predicate<Protocol> filter, final Checksum checksum, final Local local) {
+        this(protocols, filter, new LazyInitializer<Checksum>() {
             @Override
             protected Checksum initialize() {
                 return checksum;
@@ -71,7 +71,7 @@ public class ProfileDescription {
         });
     }
 
-    public ProfileDescription(final ProtocolFactory protocols, final Predicate<Protocol> parent,
+    public ProfileDescription(final ProtocolFactory protocols, final Predicate<Protocol> filter,
                               final LazyInitializer<Checksum> checksum, final LazyInitializer<Local> local) {
         this.checksum = checksum;
         this.local = local;
@@ -79,7 +79,7 @@ public class ProfileDescription {
             @Override
             protected Profile initialize() throws ConcurrentException {
                 try {
-                    return new ProfilePlistReader(protocols, parent).read(local.get());
+                    return new ProfilePlistReader(protocols, filter).read(local.get());
                 }
                 catch(AccessDeniedException e) {
                     log.warn("Failure {} reading profile {}", e, local.get());
