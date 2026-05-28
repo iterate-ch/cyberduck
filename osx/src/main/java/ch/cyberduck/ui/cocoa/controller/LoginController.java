iterate-ch/cyberduck

commit e6ca91ffc065b99298805f26fa27aa56a5f754e0

Refactor `focus` implementation and improve key window and first responder handling in alert and window controllers.

osx/src/main/java/ch/cyberduck/ui/cocoa/controller/LoginController.java
@@ -153,13 +153,15 @@ public class LoginController extends AlertController {
     }
 
     @Override
-    protected void focus(final NSAlert alert) {
-        super.focus(alert);
-        if(options.user) {
-            usernameField.selectText(null);
-        }
+    public void focus() {
+        super.focus();
         if(options.password && !StringUtils.isBlank(bookmark.getCredentials().getUsername())) {
+            window.makeFirstResponder(passwordField);
             passwordField.selectText(null);
+        }
+        else {
+            window.makeFirstResponder(usernameField);
+            usernameField.selectText(null);
         }
     }
 

binding/src/main/java/ch/cyberduck/binding/AlertController.java
@@ -66,10 +66,6 @@ public abstract class AlertController extends SheetController implements InputVa
 
     public abstract NSAlert loadAlert();
 
-    public NSAlert alert() {
-        return alert;
-    }
-
     @Override
     public void loadBundle() {
         alert = this.loadAlert();
@@ -83,18 +79,6 @@ public abstract class AlertController extends SheetController implements InputVa
         final NSWindow window = alert.window();
         log.debug("Use window {}", window);
         this.setWindow(window);
-        // Layout alert view on main thread
-        this.focus(alert);
-    }
-
-    @Override
-    public void setWindow(final NSWindow window) {
-        super.setWindow(window);
-        window.setReleasedWhenClosed(false);
-    }
-
-    protected void focus(final NSAlert alert) {
-        log.debug("Focus alert {}", alert);
         final NSEnumerator buttons = alert.buttons().objectEnumerator();
         NSObject button;
         while((button = buttons.nextObject()) != null) {
@@ -104,14 +88,27 @@ public abstract class AlertController extends SheetController implements InputVa
         }
         final NSView accessory = this.getAccessoryView(alert);
         if(accessory != null) {
+            log.debug("Add accessory view {}", accessory);
             final NSRect frame = this.getFrame(accessory);
             accessory.setFrameSize(frame.size);
             alert.setAccessoryView(accessory);
-            window.makeFirstResponder(accessory);
         }
-        // First call layout and then do any special positioning and sizing of the accessory view prior to running the alert
+        log.debug("Layout alert {}", alert);
+        // Layout alert view on main thread
         alert.layout();
+        this.awakeFromNib();
+    }
+
+    @Override
+    public void focus() {
+        log.debug("Realculate key view loop for alert {}", alert);
         window.recalculateKeyViewLoop();
+    }
+
+    @Override
+    public void setWindow(final NSWindow window) {
+        super.setWindow(window);
+        window.setReleasedWhenClosed(false);
     }
 
     protected NSRect getFrame(final NSView accessory) {

binding/src/main/java/ch/cyberduck/binding/SystemAlertController.java
@@ -30,9 +30,4 @@ public class SystemAlertController extends AlertController {
     public NSAlert loadAlert() {
         return alert;
     }
-
-    @Override
-    public NSAlert alert() {
-        return alert;
-    }
 }

binding/src/main/java/ch/cyberduck/binding/WindowController.java
@@ -63,6 +63,12 @@ public abstract class WindowController extends BundleController implements NSWin
     }
 
     @Override
+    public void awakeFromNib() {
+        super.awakeFromNib();
+        this.focus();
+    }
+
+    @Override
     public void invalidate() {
         listeners.clear();
         if(window != null) {
@@ -132,6 +138,13 @@ public abstract class WindowController extends BundleController implements NSWin
         else {
             window.orderFront(null);
         }
+    }
+
+    /**
+     * Make window the key window and select any first responder
+     */
+    public void focus() {
+
     }
 
     /**

osx/src/main/java/ch/cyberduck/ui/cocoa/controller/BookmarkController.java
@@ -479,6 +479,11 @@ public class BookmarkController extends SheetController implements CollectionLis
     @Override
     public void awakeFromNib() {
         super.awakeFromNib();
+        this.update();
+    }
+
+    @Override
+    public void focus() {
         if(bookmark.getProtocol().isHostnameConfigurable()) {
             window.makeFirstResponder(hostField);
         }
@@ -487,7 +492,7 @@ public class BookmarkController extends SheetController implements CollectionLis
                 window.makeFirstResponder(usernameField);
             }
         }
-        this.update();
+        super.focus();
     }
 
     @Override

osx/src/main/java/ch/cyberduck/ui/cocoa/controller/BrowserController.java
@@ -427,7 +427,6 @@ public class BrowserController extends WindowController implements NSToolbar.Del
      * Marks the current browser as the first responder
      */
     private void getFocus() {
-        NSView view;
         if(this.getSelectedTabView() == BrowserTab.bookmarks) {
             window.makeFirstResponder(bookmarkTable);
             bookmarkTableDelegate.selectionDidChange(NSNotification.notificationWithName(StringUtils.EMPTY, this.id()));

osx/src/main/java/ch/cyberduck/ui/cocoa/controller/ConnectionController.java
@@ -43,14 +43,14 @@ public class ConnectionController extends BookmarkController {
     }
 
     @Override
-    public void awakeFromNib() {
-        super.awakeFromNib();
+    public void focus() {
         if(options.user) {
             window.makeFirstResponder(usernameField);
         }
         if(options.password && !StringUtils.isBlank(bookmark.getCredentials().getUsername())) {
             window.makeFirstResponder(passwordField);
         }
+        super.focus();
     }
 
     @Override

osx/src/main/java/ch/cyberduck/ui/cocoa/controller/DefaultBookmarkController.java
@@ -87,9 +87,9 @@ public class DefaultBookmarkController extends BookmarkController {
     }
 
     @Override
-    public void awakeFromNib() {
-        super.awakeFromNib();
+    public void focus() {
         window.makeFirstResponder(hostField);
+        super.focus();
     }
 
     public void setNicknameField(final NSTextField f) {

commit 5d8c2c583efe7b36db89784e1b6f4c243cb1a330

Fix null pointer regression from e6ca91ff.

osx/src/main/java/ch/cyberduck/ui/cocoa/controller/LoginController.java
@@ -160,8 +160,10 @@ public class LoginController extends AlertController {
             passwordField.selectText(null);
         }
         else {
-            window.makeFirstResponder(usernameField);
-            usernameField.selectText(null);
+            if(options.user) {
+                window.makeFirstResponder(usernameField);
+                usernameField.selectText(null);
+            }
         }
     }
 