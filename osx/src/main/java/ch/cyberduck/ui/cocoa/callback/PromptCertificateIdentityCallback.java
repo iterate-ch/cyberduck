79e77b97c8579315b878ada920cfa095af7a9e78
David Kocher <dkocher@iterate.ch>
2026-07-27 12:04:50 +0200

    Set button title to "Cancel" as connection may continue with no selection.

--- osx/src/main/java/ch/cyberduck/ui/cocoa/callback/PromptCertificateIdentityCallback.java
+++ osx/src/main/java/ch/cyberduck/ui/cocoa/callback/PromptCertificateIdentityCallback.java
@@ -80,7 +80,7 @@ public class PromptCertificateIdentityCallback implements CertificateIdentityCal
                 panel.setPolicies(policyRef);
                 FoundationKitFunctions.library.CFRelease(policyRef);
                 panel.setShowsHelp(false);
-                panel.setAlternateButtonTitle(LocaleFactory.localizedString("Disconnect"));
+                panel.setAlternateButtonTitle(LocaleFactory.localizedString("Cancel"));
                 panel.setInformativeText(MessageFormat.format(LocaleFactory.localizedString(
                         "The server requires a certificate to validate your identity. Select the certificate to authenticate yourself to {0}."), hostname));
             }
