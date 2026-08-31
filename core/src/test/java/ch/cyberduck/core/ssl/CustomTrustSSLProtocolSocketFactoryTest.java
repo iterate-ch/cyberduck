1b90e7a6641a899246bdd7f7f42a619303b2568e
Yves Langisch <yla@iterate.ch>
2026-08-31 17:03:12 +0200

    Copy enabled cipher suites to mutable list.

--- core/src/test/java/ch/cyberduck/core/ssl/CustomTrustSSLProtocolSocketFactoryTest.java
+++ core/src/test/java/ch/cyberduck/core/ssl/CustomTrustSSLProtocolSocketFactoryTest.java
@@ -4,10 +4,15 @@ import ch.cyberduck.core.DisabledCertificateIdentityCallback;
 import ch.cyberduck.core.DisabledCertificateStore;
 import ch.cyberduck.core.Host;
 import ch.cyberduck.core.TestProtocol;
+import ch.cyberduck.core.preferences.Preferences;
+import ch.cyberduck.core.preferences.PreferencesFactory;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import javax.net.ssl.SSLSocket;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
 
 public class CustomTrustSSLProtocolSocketFactoryTest {
 
@@ -17,5 +22,25 @@ public class CustomTrustSSLProtocolSocketFactoryTest {
             new DisabledCertificateIdentityCallback(),
             new Host(new TestProtocol()), new DisabledCertificateStore()
         )).getSSLContext());
+    }
+
+    @Test
+    public void testConfigureCipherBlacklist() throws Exception {
+        final CustomTrustSSLProtocolSocketFactory factory = new CustomTrustSSLProtocolSocketFactory(new DefaultX509TrustManager(), new CertificateStoreX509KeyManager(
+                new DisabledCertificateIdentityCallback(),
+                new Host(new TestProtocol()), new DisabledCertificateStore()
+        ));
+        final SSLSocket socket = (SSLSocket) factory.getSSLContext().getSocketFactory().createSocket();
+        final String blacklisted = socket.getEnabledCipherSuites()[0];
+        final Preferences preferences = PreferencesFactory.get();
+        preferences.setProperty("connection.ssl.cipher.blacklist", blacklisted);
+        try {
+            factory.configure(socket, socket.getEnabledProtocols());
+            assertTrue(socket.getEnabledCipherSuites().length > 0);
+            assertFalse(Arrays.asList(socket.getEnabledCipherSuites()).contains(blacklisted));
+        }
+        finally {
+            preferences.deleteProperty("connection.ssl.cipher.blacklist");
+        }
     }
 }
