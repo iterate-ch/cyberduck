67cbd77328c56d9fd3533d62a28ea2979d27ca86
David Kocher <dkocher@iterate.ch>
2026-09-01 09:43:52 +0200

    Auto type.

--- owncloud/src/main/java/ch/cyberduck/core/owncloud/OwncloudSession.java
+++ owncloud/src/main/java/ch/cyberduck/core/owncloud/OwncloudSession.java
@@ -133,7 +133,7 @@ public class OwncloudSession extends DAVSession {
             if(ArrayUtils.contains(tus.versions, TUS_VERSION) && tus.extensions.contains(TusCapabilities.Extension.creation)) {
                 return (T) new OcisUploadFeature(this, tus);
             }
-            return (T) new HttpUploadFeature();
+            return (T) new HttpUploadFeature<>();
         }
         if(type == Write.class) {
             if(ArrayUtils.contains(tus.versions, TUS_VERSION) && tus.extensions.contains(TusCapabilities.Extension.creation)) {
